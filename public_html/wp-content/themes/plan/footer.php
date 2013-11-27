c29ca7ae6a40c5452918dab78b3fe5224092590b
iandunn <iandunn@git.wordpress.org>
2013-11-27 00:05:25 +0000

    Plan: Initial commit.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@133 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/themes/plan/footer.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * The template for displaying the footer
+ *
+ * Contains footer content and the closing of the #main and #page div elements.
+ *
+ * @package WordPress
+ * @subpackage Twenty_Twelve
+ * @since Twenty Twelve 1.0
+ */
+?>
+	</div><!-- #main .wrapper -->
+	<footer id="colophon" role="contentinfo">
+		<?php if ( is_active_sidebar( 'sidebar-5' ) ) : ?>
+			<div id="sidebar-footer" class="widget-area" role="complementary">
+				<?php dynamic_sidebar( 'sidebar-5' ); ?>
+			</div><!-- #secondary -->
+		<?php endif; ?>
+		
+		<div class="site-info">
+			<?php do_action( 'twentytwelve_credits' ); ?>
+			<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'twentytwelve' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'twentytwelve' ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentytwelve' ), 'WordPress' ); ?></a>
+		</div><!-- .site-info -->
+	</footer><!-- #colophon -->
+</div><!-- #page -->
+
+<?php wp_footer(); ?>
+</body>
+</html>

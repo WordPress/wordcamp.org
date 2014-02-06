69bdec2058c7be6511f47e4fa10da24baf3b7099
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2014-02-06 23:50:42 +0000

    Coming Soon: Correct spelling of "Incognito."
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@389 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/plugins/wordcamp-coming-soon-page/views/settings-section-headers.php
+++ public_html/wp-content/plugins/wordcamp-coming-soon-page/views/settings-section-headers.php
@@ -1,1 +1,1 @@
-<p>When enabled, the Coming Soon page will be displayed to logged-out users, giving you a chance to setup all of your content before your site is visible to the world. To preview the page, just open your site in a Ignognito/Private tab, or log out of your account.</p>
+<p>When enabled, the Coming Soon page will be displayed to logged-out users, giving you a chance to setup all of your content before your site is visible to the world. To preview the page, just open your site in an Incognito/Private tab, or log out of your account.</p>

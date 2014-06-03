5a50e07d3eae3a5d05182fffc7c43f0cb8bc242b
iandunn <iandunn@git.wordpress.org>
2014-06-03 19:21:24 +0000

    WordCamp Organizer Reminders: Abstract the retreval of posts assigned to a trigger.
    We'll be adding new triggers soon, and this will be needed for all of them, so it should be DRY.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@660 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/plugins/wordcamp-organizer-reminders/wcor-mailer.php
+++ public_html/wp-content/plugins/wordcamp-organizer-reminders/wcor-mailer.php
@@ -217,10 +217,35 @@ class WCOR_Mailer {
 	}
 
 	/**
+	 * Get all of the reminder posts assigned to the given trigger.
+	 *
+	 * @param string $trigger
+	 * @return array
+	 */
+	protected function get_triggered_posts( $trigger ) {
+		$posts = get_posts( array(
+			'posts_per_page' => -1,
+			'post_type'      => WCOR_Reminder::POST_TYPE_SLUG,
+			'meta_query'     => array(
+				array(
+					'key'    => 'wcor_send_when',
+					'value'  => 'wcor_send_trigger',
+				),
+				array(
+					'key'    => 'wcor_which_trigger',
+					'value'  => $trigger,
+				),
+			),
+		) );
+
+		return $posts;
+	}
+
+	/**
 	 * Sends e-mails hooked to the wcor_added_to_schedule trigger.
 	 *
 	 * This fires when a WordCamp is added to the schedule (i.e., when they set the start date in their `wordcamp` post).
-	 * 
+	 *
 	 * Since Core doesn't support revisions on post meta, we're not actually checking to see if the start date was added during
 	 * the current post update, but just that it has a start data. By itself, that would lead to the e-mail being sent every time
 	 * the post is updated, but to avoid that we're checking the `wcor_sent_email_id` post meta for the `wordcamp` post to see if
@@ -233,23 +258,10 @@ class WCOR_Mailer {
 		if ( 'wordcamp' == $post->post_type && 'publish' == $post->post_status ) {
 			$start_date     = get_post_meta( $post_id, 'Start Date (YYYY-mm-dd)', true );
 			$sent_email_ids = (array) get_post_meta( $post_id, 'wcor_sent_email_ids', true );
-		
+
 			if ( $start_date ) {
-				$emails = get_posts( array(
-					'posts_per_page' => -1,
-					'post_type'      => WCOR_Reminder::POST_TYPE_SLUG,
-					'meta_query'     => array(
-						array(
-							'key'    => 'wcor_send_when',
-							'value'  => 'wcor_send_trigger',
-						),
-						array(
-							'key'    => 'wcor_which_trigger',
-							'value'  => 'wcor_added_to_schedule',
-						),
-					),
-				) );
-				
+				$emails = $this->get_triggered_posts( 'wcor_added_to_schedule' );
+
 				foreach( $emails as $email ) {
 					$send_where = get_post_meta( $email->ID, 'wcor_send_where', true );
 
@@ -258,11 +270,11 @@ class WCOR_Mailer {
 					} else {
 						$recipient = get_post_meta( $post_id, 'E-mail Address', true );
 					}
-					
+
 					if ( is_email( $recipient ) && ! in_array( $email->ID, $sent_email_ids ) ) {
 						$subject = $this->replace_placeholders( $post, $email, $email->post_title );
 						$body    = $this->replace_placeholders( $post, $email, $email->post_content );
-						
+
 						if ( $this->mail( $recipient, $subject, $body ) ) {
 							$sent_email_ids[] = $email->ID;
 							update_post_meta( $post_id, 'wcor_sent_email_ids', $sent_email_ids );

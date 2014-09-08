6a39b5978d73e8a1ebac9e2eb9adfc92e5cb8257
iandunn <iandunn@git.wordpress.org>
2014-09-08 20:50:56 +0000

    Disallow Password Reset: Redirect reset requests to WordPress.org.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@841 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/mu-plugins/wcorg-disallow-password-reset.php
+++ public_html/wp-content/mu-plugins/wcorg-disallow-password-reset.php
@@ -6,3 +6,14 @@
 
 add_filter( 'allow_password_reset', '__return_false' );
 add_filter( 'show_password_fields', '__return_false' );
+
+/**
+ * Redirect users to WordPress.org to reset their passwords.
+ *
+ * Otherwise, there's nothing to indicate where they can reset it.
+ */
+function wcorg_reset_passwords_at_wporg() {
+	wp_redirect( 'https://wordpress.org/support/bb-login.php' );
+	die();
+}
+add_action( 'login_form_lostpassword', 'wcorg_reset_passwords_at_wporg' );

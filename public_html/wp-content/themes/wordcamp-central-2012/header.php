a0c1e37f1ecbbe5162116cf74a2d31917f26dffc
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2014-09-10 18:49:58 +0000

    Central Theme: Enqueue CSS and JS instead of printing directly.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@849 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/themes/wordcamp-central-2012/header.php
+++ public_html/wp-content/themes/wordcamp-central-2012/header.php
@@ -40,18 +40,8 @@
 
 	?></title>
 <link rel="profile" href="http://gmpg.org/xfn/11" />
-<link rel="stylesheet" type="text/css" media="all" href="<?php echo get_stylesheet_uri(); ?>?v=2" />
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>" />
 <?php
-	/* We add some JavaScript to pages with the comment form
-	 * to support sites with threaded comments (when in use).
-	 */
-	if ( is_singular() && get_option( 'thread_comments' ) )
-		wp_enqueue_script( 'comment-reply' );
-		
-	if ( is_front_page() || is_page('about') )
-		wp_enqueue_script( 'jquery-cycle', get_stylesheet_directory_uri() . '/js/jquery.cycle.min.js', array( 'jquery' ) )	;
-
 	/* Always have wp_head() just before the closing </head>
 	 * tag of your theme, or you will break many plugins, which
 	 * generally use this hook to add elements to <head> such

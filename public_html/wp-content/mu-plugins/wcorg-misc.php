ad350bc6fc57777e7a7506b000eb9fc7f9df15a0
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2014-10-01 19:09:21 +0000

    Misc: Initial commit.
    Almost everything in mu-plugins-private/wcorg-misc.php had no reason to be private, so they were moved here.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@882 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/mu-plugins/wcorg-misc.php
@@ -0,0 +1,78 @@
+<?php
+
+/*
+ * Miscellaneous snippets that don't warrant their own file
+ */
+
+
+/*
+ * Prevents 'index.php' from being prepended to permalink options.
+ *
+ * is_nginx() returns false on WordCamp.org because $_SERVER['SERVER_SOFTWARE'] is empty.
+ */
+add_filter( 'got_url_rewrite', '__return_true' );
+
+/*
+ * Include pages in the list of posts types that can have comments closed automatically
+ */
+function wcorg_close_comments_for_post_types( $post_types ) {
+	$post_types[] = 'page';
+	return $post_types;
+}
+add_filter( 'close_comments_for_post_types', 'wcorg_close_comments_for_post_types' );
+
+/*
+ * Determine which Jetpack modules should be automatically activated when new sites are created
+ */
+function wcorg_default_jetpack_modules( $modules ) {
+	$modules = array_diff( $modules, array( 'widget-visibility' ) );
+	array_push( $modules, 'contact-form', 'shortcodes', 'custom-css', 'subscriptions' );
+
+	return $modules;
+}
+add_filter( 'jetpack_get_default_modules', 'wcorg_default_jetpack_modules' );
+
+/*
+ * We want to let organizers use shortcodes inside Text widgets
+ */
+add_filter( 'widget_text', 'do_shortcode' );
+
+/**
+ * Disable certain network-activate plugins on specific sites.
+ *
+ * @param array $plugins
+ *
+ * @return array
+ */
+function wcorg_disable_network_activated_plugins_on_sites( $plugins ) {
+
+	/*
+	 * central.wordcamp.org, plan.wordcamp.org
+     *
+	 * These are plugins for individual WordCamp sites, so they aren't relevant for Central and Plan.
+	 * They clutter the admin menu and slow down page loads.
+	 */
+	if ( in_array( get_current_blog_id(), array( BLOG_ID_CURRENT_SITE, 63 ) ) ) {
+		unset( $plugins['camptix-extras/camptix-extras.php'] );
+		unset( $plugins['camptix-network-tools/camptix-network-tools.php'] );
+		unset( $plugins['tagregator/bootstrap.php'] );
+		unset( $plugins['wc-canonical-years/wc-canonical-years.php'] );
+		unset( $plugins['wc-post-types/wc-post-types.php'] );
+		unset( $plugins['wordcamp-coming-soon-page/bootstrap.php'] );
+		unset( $plugins['wordcamp-organizer-nags/wordcamp-organizer-nags.php'] );
+		unset( $plugins['wordcamp-payments/bootstrap.php'] );
+	}
+
+	return $plugins;
+}
+add_filter( 'site_option_active_sitewide_plugins', 'wcorg_disable_network_activated_plugins_on_sites' );
+
+/*
+ * Show Tagregator's log to network admins
+ */
+function wcorg_show_tagregator_log() {
+	if ( current_user_can( 'manage_network' ) ) {
+		add_filter( 'tggr_show_log', '__return_true' );
+	}
+}
+add_action( 'init', 'wcorg_show_tagregator_log' );

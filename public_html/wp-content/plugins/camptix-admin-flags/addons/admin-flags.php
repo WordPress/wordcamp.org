0c4bc9a5a2e2ac2ad36141d6b0be894b02f1a102
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2014-10-14 22:54:11 +0000

    CampTix Admin Flags: Support bulk-toggling from the Attendees screen.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@908 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/plugins/camptix-admin-flags/addons/admin-flags.php
+++ public_html/wp-content/plugins/camptix-admin-flags/addons/admin-flags.php
@@ -37,8 +37,17 @@ class CampTix_Admin_Flags_Addon extends CampTix_Addon {
 		$this->flags = (array) $camptix_options['camptix-admin-flags-data-parsed'];
 
 		if ( current_user_can( $camptix->caps['manage_attendees'] ) ) {
+			// Individual editing from Edit Attendee screen
 			add_action( 'save_post', array( $this, 'save_post' ), 11, 1 );
 			add_action( 'camptix_attendee_submitdiv_misc', array( $this, 'publish_metabox_actions' ) );
+
+			// Bulk editing from Attendees screen
+			add_filter( 'manage_tix_attendee_posts_columns', array( $this, 'add_custom_columns' ) );
+			add_action( 'manage_tix_attendee_posts_custom_column', array( $this, 'render_custom_columns' ), 10, 2 );
+			add_action( 'admin_footer-edit.php', array( $this, 'render_client_side_templates' ) );
+			add_action( 'wp_ajax_tix_admin_flag_toggle', array( $this, 'toggle_flag' ) );
+			add_action( 'admin_footer-edit.php', array( $this, 'print_javascript' ) );
+			add_action( 'admin_print_styles-edit.php', array( $this, 'print_css' ) );
 		}
 
 		// Export Handlers.
@@ -231,6 +240,220 @@ class CampTix_Admin_Flags_Addon extends CampTix_Addon {
 
 			<?php endforeach; ?>
 		</div>
+
+		<?php
+	}
+
+	/**
+	 * Add extra columns to the Attendees screen.
+	 *
+	 * @param array $columns
+	 * @return array
+	 */
+	public function add_custom_columns( $columns ) {
+		$columns = array_merge( array( 'admin-flags' => __( 'Admin Flags', 'camptix' ) ), $columns );
+
+		return $columns;
+	}
+
+	/**
+	 * Render custom columns on the Attendees screen.
+	 *
+	 * @param string $column
+	 * @param int $attendee_id
+	 */
+	public function render_custom_columns( $column, $attendee_id ) {
+		$attendee_flags = (array) get_post_meta( $attendee_id, 'camptix-admin-flag' );
+
+		switch ( $column ) {
+			case 'admin-flags':
+				echo '<ul>';
+					foreach ( $this->flags as $key => $label ) {
+						$enabled = in_array( $key, $attendee_flags );
+
+						?>
+
+						<li>
+							<span class="tix-admin-flag-label"><?php echo esc_html( $label ); ?></span>:
+
+							<a
+								href="#"
+								data-attendee-id="<?php echo esc_attr( $attendee_id ); ?>"
+								data-key="<?php echo esc_attr( $key ); ?>"
+								data-nonce="<?php echo esc_attr( wp_create_nonce( sprintf( 'tix_toggle_flag_%s_%s', $attendee_id, $key ) ) ); ?>"
+								data-command="<?php echo esc_attr( $enabled ? 'disable' : 'enable' ); ?>"
+								class="tix-toggle-flag">
+
+								<?php if ( $enabled ) : ?>
+									<?php _e( 'Disable', 'camptix' ); ?>
+								<?php else : ?>
+									<?php _e( 'Enable', 'camptix' ); ?>
+								<?php endif; ?>
+							</a>
+						</li>
+
+						<?php
+					}
+				echo '</ul>';
+
+				break;
+		}
+	}
+
+	/**
+	 * Render the templates used by JavaScript
+	 */
+	public function render_client_side_templates() {
+		if ( 'tix_attendee' != $GLOBALS['typenow'] ) {
+			return;
+		}
+
+		?>
+
+		<script type="text/html" id="tmpl-tix-admin-flags-spinner">
+			<div class="spinner"></div>
+		</script>
+
+		<script type="text/html" id="tmpl-tix-admin-flag-toggle">
+			<span class="tix-admin-flag-label">{{data.label}}</span>:
+
+			<a
+				href="#"
+				data-attendee-id="{{data.attendee_id}}"
+				data-key="{{data.key}}"
+				data-nonce="{{data.nonce}}"
+				data-command="{{data.command}}"
+				class="tix-toggle-flag">
+
+				{{data.command}}    <?php // todo use i18n var ?>
+			</a>
+		</script>
+
+		<?php
+	}
+
+	/**
+	 * AJAX handler to toggle a flag from the Attendees screen.
+	 */
+	public function toggle_flag() {
+		/** @var $camptix CampTix_Plugin */
+		global $camptix;
+
+		if ( empty( $_REQUEST['action'] ) || empty( $_REQUEST['attendee_id'] ) || empty( $_REQUEST['key'] ) || empty( $_REQUEST['command'] ) || empty( $_REQUEST['nonce'] ) ) {
+			wp_send_json_error( array( 'error' => 'Required parameters not set.' ) );
+		}
+
+		$attendee_id = absint( $_REQUEST['attendee_id'] );
+		$key         = sanitize_text_field( $_REQUEST['key'] );
+		$command     = sanitize_text_field( $_REQUEST['command'] );
+
+		if ( ! wp_verify_nonce( $_REQUEST['nonce'], sprintf( 'tix_toggle_flag_%s_%s', $attendee_id, $key ) ) || ! current_user_can( $camptix->caps['manage_attendees'], $attendee_id ) ) {
+			wp_send_json_error( array( 'error' => 'Permission denied.' ) );
+		}
+
+		$attendee = get_post( $attendee_id );
+
+		if ( ! is_a( $attendee, 'WP_Post' ) || 'tix_attendee' != $attendee->post_type ) {
+			wp_send_json_error( array( 'error' => 'Invalid attendee.' ) );
+		}
+
+		if ( 'enable' == $command ) {
+			add_post_meta( $attendee_id, 'camptix-admin-flag', $key );
+		} elseif ( 'disable' == $command ) {
+			delete_post_meta( $attendee_id, 'camptix-admin-flag', $key );
+		}
+
+		wp_send_json_success();
+	}
+
+	/**
+	 * Print our JavaScript
+	 */
+	public function print_javascript() {
+		if ( 'tix_attendee' != $GLOBALS['typenow'] ) {
+			return;
+		}
+
+		?>
+
+		<!-- camptix-admin-flags -->
+		<script type="text/javascript">
+			// Bulk toggling of flags
+			( function( $ ) {
+				$( '#posts-filter' ).on( 'click', 'a.tix-toggle-flag', function( event ) {
+					var itemTemplate,
+						item       = $( this ).parent(),
+						attendeeID = $( this ).data( 'attendee-id' ),
+						key        = $( this ).data( 'key' ),
+						command    = $( this ).data( 'command' ),
+						label      = $( item ).find( '.tix-admin-flag-label' ).text(),
+						nonce      = $( this ).data( 'nonce' );
+
+					event.preventDefault();
+
+					// Show a spinner until the AJAX call is done
+					itemTemplate = _.template( $( '#tmpl-tix-admin-flags-spinner' ).html(), null, camptix.template_options );
+					item.html( itemTemplate( {} ) );
+
+					// Send the request to toggle the flag
+					$.post(
+						ajaxurl,
+
+						{
+							action:      'tix_admin_flag_toggle',
+							attendee_id: attendeeID,
+							key:         key,
+							command:     command,
+							nonce:       nonce
+						},
+
+						function( response ) {
+							if ( response.hasOwnProperty( 'success' ) && true === response.success ) {
+								if ( 'enable' == command ) {
+									command = 'disable';
+								} else if ( 'disable' == command ) {
+									command = 'enable';
+								}
+							}
+
+							itemTemplate = _.template( $( '#tmpl-tix-admin-flag-toggle' ).html(), null, camptix.template_options );
+							item.html( itemTemplate( {
+								attendee_id: attendeeID,
+								key:         key,
+								command:     command,
+								label:       label,
+								nonce:       nonce
+							} ) );
+						}
+					);
+				} );
+			} ( jQuery ) );
+		</script>
+		<!-- end camptix-admin-flags -->
+
+		<?php
+	}
+
+	/**
+	 * Print our CSS
+	 */
+	public function print_css() {
+		if ( 'tix_attendee' != $GLOBALS['typenow'] ) {
+			return;
+		}
+
+		?>
+
+		<!-- camptix-admin-flags -->
+		<style type="text/css">
+			body.post-type-tix_attendee table.posts td.admin-flags div.spinner {
+				display: block;
+				float: none;
+				margin-left: auto;
+				margin-right: auto;
+			}
+		</style>
+		<!-- end camptix-admin-flags -->
 
 		<?php
 	}

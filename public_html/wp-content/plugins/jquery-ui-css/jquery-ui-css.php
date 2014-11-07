7d591205c321510f479c7d28c4be8cb758317c2f
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2014-11-07 01:38:56 +0000

    jQuery UI CSS: Initial commit.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@963 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/plugins/jquery-ui-css/jquery-ui-css.php
@@ -0,0 +1,32 @@
+<?php
+/*
+Plugin Name: jQuery UI CSS
+Plugin URI:  http://wordcamp.org/
+Description: Registers jQuery-UI's CSS so that all plugins can use it.
+Version:     0.1
+*/
+
+/*
+ * Register the stylesheets
+ *
+ * TODO: This can be removed when #18909-core is committed
+ */
+function juicss_register_styles( $hook ) {
+	wp_register_style(
+		'jquery-ui',
+		plugins_url( 'jquery-ui.min.css', __FILE__ ),
+		array(),
+		'1.11.2'
+	);
+
+	// https://github.com/x-team/wp-jquery-ui-datepicker-skins
+	wp_register_style(
+		'wp-datepicker-skins',
+		plugins_url( 'wp-datepicker-skins.css', __FILE__ ),
+		array( 'jquery-ui' ),
+		'1712f05a1c6a76ef0ac0b0a9bf79224e52e461ab'
+	);
+}
+add_action( 'admin_enqueue_scripts', 'juicss_register_styles' );
+
+// todo need to jquery-ui bundle images too? maybe put it in an includes folder in wp-content for all plugins to use, and update payments?

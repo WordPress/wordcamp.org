0520852fe33c93f0ed596d05327f7f418453157c
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2015-01-14 17:15:34 +0000

    Central Theme: Require a date to be included on the Past WordCamps template.
    Because they're now grouped by year (since r1113), camps that are missing a date were being grouped under 1970 (the start of the Unix epoch).
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@1142 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/themes/wordcamp-central-2012/functions.php
+++ public_html/wp-content/themes/wordcamp-central-2012/functions.php
@@ -618,7 +618,9 @@ class WordCamp_Central_Theme {
 		$grouped_wordcamps = array();
 
 		foreach ( $wordcamps as $wordcamp ) {
-			if ( $year = date( 'Y', (int) get_post_meta( $wordcamp->ID, 'Start Date (YYYY-mm-dd)', true ) ) ) {
+			$date = get_post_meta( $wordcamp->ID, 'Start Date (YYYY-mm-dd)', true );
+
+			if ( $date && $year = date( 'Y', (int) $date ) ) {
 				$grouped_wordcamps[ $year ][] = $wordcamp;
 			}
 		}

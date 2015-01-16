5d5336dafc657b32a99ae7de5a1d179e5f4ad62f
kovshenin <kovshenin@74240141-8908-4e6f-9713-ba540dce6ec7>
2015-01-16 10:44:04 +0000

    WordCamp.org: Match the /year/month/post-name/ permalink when attempting to redirect to /post-name/.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@1153 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/mu-plugins/wcorg-misc.php
+++ public_html/wp-content/mu-plugins/wcorg-misc.php
@@ -145,8 +145,11 @@ function wcorg_subdomactories_redirect() {
 	if ( get_option( 'permalink_structure' ) != '/%postname%/' )
 		return;
 
-	// russia.wordcamp.org/2014(/2014)?/11/25/post-name/
-	if ( ! preg_match( '#^(?:/[0-9]{4})?/[0-9]{4}/[0-9]{2}/[0-9]{2}/(.+)$#', $_SERVER['REQUEST_URI'], $matches ) )
+	// russia.wordcamp.org/2014/2014/11/25/post-name/
+	// russia.wordcamp.org/2014/11/25/post-name/
+	// russia.wordcamp.org/2014/2014/25/post-name/
+
+	if ( ! preg_match( '#^/[0-9]{4}/(?:[0-9]{4}/[0-9]{2}|[0-9]{2}|[0-9]{4})/[0-9]{2}/(.+)$#', $_SERVER['REQUEST_URI'], $matches ) )
 		return;
 
 	wp_safe_redirect( esc_url_raw( set_url_scheme( home_url( $matches[1] ) ) ) );

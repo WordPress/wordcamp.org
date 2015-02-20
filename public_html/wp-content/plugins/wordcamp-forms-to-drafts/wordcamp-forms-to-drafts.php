d9065e1822538178982b9aa83277b34dde5fe941
iandunn <iandunn@git.wordpress.org>
2015-02-20 19:55:57 +0000

    WordCamp Forms to Drafts: Add handler for Call for Speakers form.
    Props nvwd
    See #567
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@1287 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/plugins/wordcamp-forms-to-drafts/wordcamp-forms-to-drafts.php
+++ public_html/wp-content/plugins/wordcamp-forms-to-drafts/wordcamp-forms-to-drafts.php
@@ -21,9 +21,127 @@ class WordCamp_Forms_To_Drafts {
 	 * Constructor
 	 */
 	public function __construct() {
+		add_filter( 'the_content',              array( $this, 'force_login_to_view_form' ), 9 );
+		add_action( 'template_redirect',        array( $this, 'populate_form_based_on_user' ), 9 );
 		add_action( 'grunion_pre_message_sent', array( $this, 'returning_organizer_application' ), 10, 3 );
 		add_action( 'grunion_pre_message_sent', array( $this, 'new_organizer_application' ), 10, 3 );
 		add_action( 'grunion_pre_message_sent', array( $this, 'call_for_sponsors' ), 10, 3 );
+		add_action( 'grunion_pre_message_sent', array( $this, 'call_for_speakers' ), 10, 3 );
+	}
+
+	/**
+	 * Force user to login to view certain forms.
+	 *
+	 * @param string $content
+	 *
+	 * @return string
+	 */
+	public function force_login_to_view_form( $content ) {
+		global $post;
+		$forms_that_require_login = array( 'call-for-speakers' );
+		$shortcode_pattern        = '/\[contact-form[\D\S]+\[\/contact-form\]/';
+		$form_id                  = get_post_meta( $post->ID, 'wcfd-key', true );
+
+		if ( in_array( $form_id, $forms_that_require_login ) && ! is_user_logged_in() ) {
+			switch ( $form_id ) {
+				case 'call-for-speakers':
+					$please_login_message = sprintf(
+						__( 'Before submitting your speaker proposal, please <a href="%s">log into your WordPress.org account</a>*.', 'wordcamporg' ),
+						wp_login_url( get_permalink() )
+					);
+					break;
+			}
+
+			if ( ! empty( $please_login_message ) ) {
+				$please_login_message = str_replace(
+					__( 'Please use your <strong>WordPress.org</strong>* account to log in.', 'wordcamporg' ),
+					$please_login_message,
+					wcorg_login_message( '', get_permalink() )
+				);
+
+				$content = preg_replace( $shortcode_pattern, $please_login_message, $content );
+			}
+		}
+
+		return $content;
+	}
+
+	/**
+	 * Populate certain form fields based on the current user.
+	 *
+	 * @todo Maybe remove username field, or make it readonly. We don't need an explicit field since we can just
+	 *       grab the current user value, but it might be good to let them see/change which one they're using.
+	 */
+	public function populate_form_based_on_user() {
+		global $current_user, $post;
+
+		if ( ! is_user_logged_in() ) {
+			return;
+		}
+
+		get_currentuserinfo();
+		$form_id = get_post_meta( $post->ID, 'wcfd-key', true );
+
+		switch ( $form_id ) {
+			case 'call-for-speakers':
+				$default_values = array(
+					'Name'                   => $current_user->display_name,
+					'Email Address'          => $current_user->user_email,
+					'WordPress.org Username' => $current_user->user_login,
+				);
+
+				foreach ( $default_values as $field_label => $default_value ) {
+					$field_id = $this->get_grunion_field_id( $post->ID, $field_label );
+
+					if ( ! isset( $_POST[ $field_id ] ) ) {
+						$_POST[ $field_id ] = $default_value;
+					}
+				}
+
+				break;
+		}
+	}
+
+	/**
+	 * Get the Grunion field ID
+	 *
+	 * This is a simplified version of what happens in Grunion_Contact_Form_Field::__construct()
+	 *
+	 * @todo submit Jetpack PR to modularize that logic so we can just call it directly instead of duplicating it
+	 *
+	 * @param int $page_id
+	 * @param string $label
+	 *
+	 * @return string
+	 */
+	protected function get_grunion_field_id( $page_id, $label ) {
+		$id = sprintf(
+			'g%s-%s',
+			$page_id,
+			sanitize_title_with_dashes( preg_replace( '/[^a-zA-Z0-9.-_:]/', '', $label ) )
+		);
+
+		return $id;
+	}
+
+	/**
+	 * Remove prefixes from form labels
+	 *
+	 * Grunion prefixes the field keys with 'N_', so that 'Name becomes '1_Name'. That prevents directly accessing
+	 * the values, since the number is unknown.
+	 *
+	 * @param array $prefixed_values
+	 *
+	 * @return array
+	 */
+	protected function get_unprefixed_grunion_form_values( $prefixed_values ) {
+		$unprefixed_values = array();
+
+		foreach ( $prefixed_values as $key => $value ) {
+			$unprefixed_values[ preg_replace( '#^\d+_#i', '', $key ) ] = $value;
+		}
+
+		return $unprefixed_values;
 	}
 
 	/**
@@ -33,6 +151,7 @@ class WordCamp_Forms_To_Drafts {
 	 * with the value of the corresponding submission handler.
 	 *
 	 * @param int $submission_id
+	 *
 	 * @return string | false
 	 */
 	protected function get_form_key( $submission_id ) {
@@ -50,6 +169,7 @@ class WordCamp_Forms_To_Drafts {
 	 * Get a user's ID based on their username.
 	 *
 	 * @param string $username
+	 *
 	 * @return int
 	 */
 	protected function get_user_id_from_username( $username ) {
@@ -61,7 +181,7 @@ class WordCamp_Forms_To_Drafts {
 	 * Simulate the existence of a post type.
 	 *
 	 * This plugin may need to insert a form into a different site, and the targeted post type may not be active
-	 * on the current site. If we don't do this, PHP notices will be generated and will break the post/get/redirect
+	 * on the current site. If we don't do this, PHP notices will be generated and will break the post/redirect/get
 	 * flow because of the early headers.
 	 *
 	 * Yes, this is an ugly hack.
@@ -184,6 +304,8 @@ class WordCamp_Forms_To_Drafts {
 	 * Create a draft Sponsor post from a Call for Sponsors form submission.
 	 *
 	 * @todo
+	 * - Update WordCamp_New_Site to inject wcfd-key meta and anything else necessary to make this active for new
+	 *   sites
 	 * - Add jetpack form field for Sponsor Level, where options are automatically pulled from wcb_sponsor_level
 	 *   taxonomy and the selected term is applied to the drafted post. Maybe need to send PR to add filter to
 	 *   insert custom fields programmatically.
@@ -205,6 +327,7 @@ class WordCamp_Forms_To_Drafts {
 		$this->simulate_post_type( 'wordcamp' );
 
 		// Create the post
+		// todo need to unprefix $all_values, like call_for_speakers() does? if so, extract into DRY function
 		$draft_id = wp_insert_post( array(
 			'post_type'    => 'wcb_sponsor',
 			'post_title'   => $all_values['Company name'],
@@ -221,6 +344,125 @@ class WordCamp_Forms_To_Drafts {
 				}
 			}
 		}
+	}
+
+	/**
+	 * Create draft Speaker and Session posts from a Call for Speakers form submission.
+	 *
+	 * @todo Add jetpack form field for Track, where options are automatically pulled from wcb_track
+	 *       taxonomy and the selected term(s) is applied to the drafted post.
+	 * @todo If creating speaker or session fails, report to organizer so that submission doesn't get missed
+	 *
+	 * @param int   $submission_id
+	 * @param array $all_values
+	 * @param array $extra_values
+	 */
+	public function call_for_speakers( $submission_id, $all_values, $extra_values ) {
+		if ( 'call-for-speakers' != $this->get_form_key( $submission_id ) ) {
+			return;
+		}
+
+		global $current_user;
+
+		$all_values = $this->get_unprefixed_grunion_form_values( $all_values );
+
+		if ( ! $speaker_user_id = $this->get_user_id_from_username( $all_values['WordPress.org Username'] ) ) {
+			$speaker_user_id                      = $current_user->ID;
+			$all_values['WordPress.org Username'] = $current_user->user_login;
+		}
+
+		$speaker = $this->get_speaker_from_user_id( $speaker_user_id );
+
+		if ( ! is_a( $speaker, 'WP_Post' ) ) {
+			$speaker_id = $this->create_draft_speaker( $all_values );
+
+			if ( ! is_a( $speaker_id, 'WP_Error' ) ) {
+				$speaker = get_post( $speaker_id );
+			}
+		}
+
+		if ( is_a( $speaker, 'WP_Post' ) ) {
+			$this->create_draft_session( $all_values, $speaker );
+		}
+	}
+
+	/**
+	 * Get speaker post based on WordPress.org user name
+	 *
+	 * @param int $user_id
+	 *
+	 * @return WP_Post | false
+	 */
+	protected function get_speaker_from_user_id( $user_id ) {
+		$speaker_query = new WP_Query( array(
+			'post_type'      => 'wcb_speaker',
+			'posts_per_page' => 1,
+			'post_status'    => array( 'publish', 'pending', 'draft', 'future', 'private' ),    // Trashed speakers are ignored because they'll likely be deleted
+			'meta_query'     => array(
+				array(
+					'key'     => '_wcpt_user_id',
+					'value'   => $user_id,
+					'compare' => '=',
+				),
+			),
+		) );
+
+		return empty( $speaker_query->post ) ? false : $speaker_query->post;
+	}
+
+	/**
+	 * Create a drafted speaker post
+	 *
+	 * @param array $speaker
+	 *
+	 * @return int | WP_Error
+	 */
+	protected function create_draft_speaker( $speaker ) {
+		$speaker_id = wp_insert_post(
+			array(
+				'post_type'    => 'wcb_speaker',
+				'post_title'   => $speaker['Name'],
+				'post_content' => $speaker['Your Bio'],
+				'post_status'  => 'draft',
+				'post_author'  => $this->get_user_id_from_username( 'wordcamp' ),
+			),
+			true
+		);
+
+		if ( $speaker_id ) {
+			update_post_meta( $speaker_id, '_wcb_speaker_email', $speaker[ 'Email Address' ] );
+			update_post_meta( $speaker_id, '_wcpt_user_id',      $this->get_user_id_from_username( $speaker['WordPress.org Username'] ) );
+		}
+
+		return $speaker_id;
+	}
+
+	/**
+	 * Create a drafted session post
+	 *
+	 * @param array   $session
+	 * @param WP_Post $speaker
+	 *
+	 * @return int | WP_Error
+	 */
+	protected function create_draft_session( $session, $speaker ) {
+		$session_id = wp_insert_post(
+			array(
+				'post_type'    => 'wcb_session',
+				'post_title'   => $session['Topic Title'],
+				'post_content' => $session['Topic Description'],
+				'post_status'  => 'draft',
+				'post_author'  => $this->get_user_id_from_username( $session['WordPress.org Username'] ),
+			),
+			true
+		);
+
+		if ( $session_id ) {
+			update_post_meta( $session_id, '_wcpt_speaker_id',      $speaker->ID );
+			update_post_meta( $session_id, '_wcb_session_speakers', $speaker->post_title );
+		}
+
+		return $session_id;
 	}
 } // end WordCamp_Forms_To_Drafts
 

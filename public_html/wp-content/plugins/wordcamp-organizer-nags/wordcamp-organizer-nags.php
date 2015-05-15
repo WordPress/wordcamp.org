c31dc71df45ebfa0491633be38cdfed98e1727b2
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2015-05-15 19:02:58 +0000

    WordCamp Organizer Nags: Migrate admin notices to a dashboard widget.
    Admin notices should only be used for relatively urgent messages, not day-to-day tasks, and they should only appear on screens that are relevant. Global, non-dismissable notices with mundane content are counter-productive in the long run, because they train people to ignore all notices.
    
    A dashboard widget is a much more appropriate and sustainable medium for this type of communication.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@1583 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/plugins/wordcamp-organizer-nags/wordcamp-organizer-nags.php
+++ public_html/wp-content/plugins/wordcamp-organizer-nags/wordcamp-organizer-nags.php
@@ -8,25 +8,45 @@
  */
 
 class WordCampOrganizerNags {
-	protected $notices;
+	protected $need_central_about_info, $needed_pages;
 
 	/**
 	 * Constructor
 	 */
 	public function __construct() {
-		$this->notices = array( 'updated' => array(), 'error' => array() );
-
+		add_action( 'admin_print_styles-index.php', array( $this, 'print_styles' ) );
 		add_action( 'wp_dashboard_setup', array( $this, 'create_dashboard_widgets' ) );
-		add_action( 'admin_notices',      array( $this, 'print_admin_notices' ) );
-		add_action( 'admin_init',         array( $this, 'central_about_info' ) );
-		add_action( 'admin_init',         array( $this, 'published_attendees_schedule_pages' ) );
+
+		// todo align
+	}
+
+	/**
+	 * Print wp-admin styles
+	 */
+	public function print_styles() {
+		?>
+
+		<style type="text/css">
+			#wordcamp_reminders ul {
+				margin-left: 12px;
+				list-style-type: disc;
+			}
+		</style>
+
+		<?php
 	}
 
 	/**
 	 * Create dashboard widgets
 	 */
 	public function create_dashboard_widgets() {
-		global $wp_meta_boxes;
+		if ( $this->show_wordcamp_reminders() ) {
+			wp_add_dashboard_widget(
+				'wordcamp_reminders',
+				'WordCamp Reminders',
+				array( $this, 'render_wordcamp_reminders' )
+			);
+		}
 
 		// The survey expires 2015-05-23 at 11:45pm EST
 		if ( time() < 1432350000 ) {
@@ -35,86 +55,73 @@ class WordCampOrganizerNags {
 				'WordCamp Organizer Survey',
 				array( $this, 'improving_tools_survey' )
 			);
-
-			// Move it to the top of the side column, so it'll be seen better
-			$wp_meta_boxes['dashboard']['side']['core'] = array_merge(
-				array( 'improving_tools_survey' => $wp_meta_boxes['dashboard']['normal']['core']['improving_tools_survey'] ),
-				$wp_meta_boxes['dashboard']['side']['core']
-			);
-			unset( $wp_meta_boxes['dashboard']['normal']['core']['improving_tools_survey'] );
 		}
+
+		$this->prioritize_wordcamp_widgets();
 	}
 
 	/**
-	 * Render the content for the WordCamp Organizer Survey dashboard widget
+	 * Determine if we need to show the WordCamp Reminders widget
+	 *
+	 * @return bool
 	 */
-	public function improving_tools_survey() {
-		?>
+	protected function show_wordcamp_reminders() {
+		$this->need_central_about_info  = $this->check_central_about_info();
+		$this->needed_pages             = $this->check_needed_pages();
 
-		<p>We need feedback from WordCamp organizers to drive some decisions about improving WordCamp.org tools.</p>
-
-		<p>
-			Please <a href="https://make.wordpress.org/community/2015/05/05/wordcamp-organizer-survey/">take the survey</a>
-			so we can learn from your experiences.
-		</p>
-
-		<?php
-	}
-
-	/**
-	 * Prints all of the notices and errors
-	 */
-	public function print_admin_notices() {
-		$this->notices = apply_filters( 'wcon_notices', $this->notices );
-		
-		foreach( array( 'updated', 'error' ) as $type ) :
-			if ( $this->notices[ $type ] ) : ?>
-				
-				<div class="<?php echo $type; ?>">
-					<?php foreach( $this->notices[ $type ] as $nag ) : ?>
-						<p><?php echo $nag; ?></p>
-					<?php endforeach; ?>
-				</div>
-				
-			<?php endif;
-		endforeach;
+		return $this->need_central_about_info || $this->needed_pages;
 	}
 
 	/**
 	 * Check if the organizers have given us their "About" text and banner image for their central.wordcamp.org page
+	 *
+	 * @return bool
 	 */
-	public function central_about_info() {
-		$site_url = parse_url( site_url() );
-		switch_to_blog( BLOG_ID_CURRENT_SITE );	// central.wordcamp.org
-		
-		$wordcamp = get_posts( array(
-			'post_type'      => 'wordcamp',
-			'posts_per_page' => 1,
-			'meta_query'     => array(
-				array(
-					'key'     => 'URL',
-					'value'   => $site_url['host'],
-					'compare' => 'LIKE',
-				)
-			)
-		) );
-		
-		if ( isset( $wordcamp[0]->ID ) ) {
-			if ( ! has_post_thumbnail( $wordcamp[0]->ID ) || empty( $wordcamp[0]->post_content ) ) {
-				$this->notices['updated'][] = 'Please send us the <a href="http://plan.wordcamp.org/first-steps/web-presence/your-page-on-central-wordcamp-org/">"about" text and banner image</a> for your central.wordcamp.org page.</a>';
-			}
+	protected function check_central_about_info() {
+		$transient_key = 'wcorg_need_central_info';
+
+		if ( $need_info = get_transient( $transient_key ) ) {
+			return 'yes' == $need_info;
 		}
 
-		restore_current_blog();
+		$need_info  = 'yes';
+		$wordcamp   = get_wordcamp_post();
+
+		if ( isset( $wordcamp->ID ) ) {
+			switch_to_blog( BLOG_ID_CURRENT_SITE ); // central.wordcamp.org
+
+			if ( has_post_thumbnail( $wordcamp->ID ) && $wordcamp->post_content ) {
+				$need_info = 'no';
+			}
+
+			restore_current_blog();
+		}
+
+		set_transient( $transient_key, $need_info, HOUR_IN_SECONDS );
+
+		return 'yes' == $need_info;
 	}
 
 	/**
 	 * Checks if Attendees and Schedule pages have been published along with the Ticket Registration page
+	 *
+	 * @todo When publishing an attendee or schedule page, delete the transient to trigger a refresh
+	 *
+	 * @return array
 	 */
-	public function published_attendees_schedule_pages() {
+	protected function check_needed_pages() {
+		$transient_key  = 'wcorg_needed_pages';
+		$needed_pages   = get_transient( $transient_key );
+
+		if ( false !== $needed_pages ) {
+			return $needed_pages;
+		}
+
+		$needed_pages       = array();
 		$found_registration = $found_attendees = $found_schedule = false;
 		$published_pages    = get_posts( array(
 			'post_type'      => 'page',
+			'post_status'    => 'publish',
 			'posts_per_page' => -1
 		) );
 
@@ -137,12 +144,84 @@ class WordCampOrganizerNags {
 		}
 
 		if ( $found_registration && ! $found_attendees ) {
-			$this->notices['updated'][] = 'Tickets are on sale now! Don’t forget to <a href="http://plan.wordcamp.org/using-camptix/#attendees-list">publish an Attendees page</a>, so everyone can see what amazing people are coming to your WordCamp.';
+			$needed_pages[] = 'attendees';
 		}
 
 		if ( $found_registration && ! $found_schedule ) {
-			$this->notices['updated'][] = 'Tickets sell a lot faster when people can see who\'s speaking at your WordCamp. How about <a href="http://plan.wordcamp.org/first-steps/web-presence/working-with-speakers-sessions-and-sponsors/#schedule">publishing a schedule</a> today?';
+			$needed_pages[] = 'schedule';
 		}
+
+		set_transient( $transient_key, $needed_pages, HOUR_IN_SECONDS );
+
+		return $needed_pages;
+	}
+
+	/**
+	 * Make our custom dashboard widgets more visible
+	 */
+	protected function prioritize_wordcamp_widgets() {
+		global $wp_meta_boxes;
+
+		// Move WordCamp Reminders to the top of the primary column
+		if ( isset( $wp_meta_boxes['dashboard']['normal']['core']['wordcamp_reminders'] ) ) {
+			$reminders_temp = $wp_meta_boxes['dashboard']['normal']['core']['wordcamp_reminders'];
+			unset( $wp_meta_boxes['dashboard']['normal']['core']['wordcamp_reminders'] );
+
+			$wp_meta_boxes['dashboard']['normal']['core'] = array_merge(
+				array( 'wordcamp_reminders' => $reminders_temp ),
+				$wp_meta_boxes['dashboard']['normal']['core']
+			);
+		}
+
+		// Move WordCamp Organizer Survey to the top of the side column
+		if ( isset( $wp_meta_boxes['dashboard']['normal']['core']['improving_tools_survey'] ) ) {
+			$wp_meta_boxes['dashboard']['side']['core'] = array_merge(
+				array( 'improving_tools_survey' => $wp_meta_boxes['dashboard']['normal']['core']['improving_tools_survey'] ),
+				$wp_meta_boxes['dashboard']['side']['core']
+			);
+
+			unset( $wp_meta_boxes['dashboard']['normal']['core']['improving_tools_survey'] );
+		}
+	}
+
+	/**
+	 * Render the content for the WordCamp Reminders dashboard widget
+	 */
+	public function render_wordcamp_reminders() {
+		?>
+
+		<ul>
+			<?php if ( $this->need_central_about_info ) : ?>
+				<li>Please send us <a href="http://plan.wordcamp.org/first-steps/web-presence/your-page-on-central-wordcamp-org/">the "about" text and banner image</a> for your central.wordcamp.org page.</a></li>
+			<?php endif; ?>
+
+			<?php if ( in_array( 'attendees', $this->needed_pages ) ) : ?>
+				<li>Tickets are on sale now! Don’t forget to <a href="http://plan.wordcamp.org/using-camptix/#attendees-list">publish an Attendees page</a>, so everyone can see what amazing people are coming to your WordCamp.</li>
+			<?php endif; ?>
+
+			<?php if ( in_array( 'schedule', $this->needed_pages ) ) : ?>
+				<li>Tickets sell a lot faster when people can see who's speaking at your WordCamp. How about <a href="http://plan.wordcamp.org/first-steps/web-presence/working-with-speakers-sessions-and-sponsors/#schedule">publishing a schedule</a> today?</li>
+			<?php endif; ?>
+		</ul>
+
+		<?php
+	}
+
+	/**
+	 * Render the content for the WordCamp Organizer Survey dashboard widget
+	 */
+	public function improving_tools_survey() {
+		// todo rename to render_tools_survey() for consistency
+		?>
+
+		<p>We need feedback from WordCamp organizers to drive some decisions about improving WordCamp.org tools.</p>
+
+		<p>
+			Please <a href="https://make.wordpress.org/community/2015/05/05/wordcamp-organizer-survey/">take the survey</a>
+			so we can learn from your experiences.
+		</p>
+
+		<?php
 	}
 }
 

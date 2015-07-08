6f6e6ae09619ab430f2a07a1407d9f9fde90c407
iandunn <iandunn@git.wordpress.org>
2015-07-08 17:41:01 +0000

    WordCamp Organizer Nags: Link to newer, more specific post for Better Docs.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@1713 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/plugins/wordcamp-organizer-nags/wordcamp-organizer-nags.php
+++ public_html/wp-content/plugins/wordcamp-organizer-nags/wordcamp-organizer-nags.php
@@ -197,7 +197,7 @@ class WordCampOrganizerNags {
 			<li><a href="https://make.wordpress.org/community/2015/06/10/wordcamp-org-tools-survey-results/">Quickly clone another WordCamp site</a> instead of building yours from scratch.</li>
 			<li><a href="https://make.wordpress.org/community/2015/06/16/editing-wordcamp-css-locally-with-git/">Edit CSS in your local environment</a> and manage it in a Git(Hub) repository.</li>
 			<li><a href="https://make.wordpress.org/community/2015/07/02/results-from-the-wordcamp-org-tools-follow-up-survey/">Build a new theme</a> for WordCamp sites.</li>
-			<li><a href="https://make.wordpress.org/community/2015/06/10/wordcamp-org-tools-survey-results/">Improve documentation</a>.</li>
+			<li><a href="https://make.wordpress.org/community/2015/07/08/better-wordcamp-org-docs/">Improve documentation</a>.</li>
 		</ul>
 
 		<?php

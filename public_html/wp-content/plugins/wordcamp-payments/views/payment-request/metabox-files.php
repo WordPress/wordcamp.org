55637f9a50380f6f09d58d4b1dc419285b4e085a
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2015-10-06 00:11:17 +0000

    WordCamp Payments: Attach existing files to payment request.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@1939 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/plugins/wordcamp-payments/views/payment-request/metabox-files.php
+++ public_html/wp-content/plugins/wordcamp-payments/views/payment-request/metabox-files.php
@@ -1,4 +1,6 @@
 <table class="form-table">
+	<input id="wcp_existing_files_to_attach" name="wcp_existing_files_to_attach" type="hidden" value="" />
+
 	<?php $this->render_files_input( $post, 'Files', 'files', __( 'Attach supporting documentation including invoices, contracts, or other vendor correspondence. If no supporting documentation is available, please indicate the reason in the notes below.', 'wordcamporg' ) ); ?>
 	<?php $this->render_textarea_input( $post, 'Notes', 'file_notes' ); ?>
 </table>

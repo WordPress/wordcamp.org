0d6f342d41e13601e01bce8d5cd1e1c7a17194ce
iandunn <iandunn@git.wordpress.org>
2015-11-30 19:40:05 +0000

    WordCamp Remote CSS: Add unit test to cover `sanitize_and_save_unsafe_css()`.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@2145 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/plugins/wordcamp-remote-css/tests/bootstrap.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace WordCamp\RemoteCSS;
+
+$core_tests_directory = getenv( 'WP_TESTS_DIR' );
+
+if ( ! $core_tests_directory ) {
+	echo "\nPlease set the WP_TESTS_DIR environment variable to the folder where WordPress' PHPUnit tests live --";
+	echo "\ne.g., export WP_TESTS_DIR=/srv/www/wordpress-develop/tests/phpunit\n";
+
+	return;
+}
+
+require_once( $core_tests_directory . '/includes/functions.php' );
+
+/**
+ * Load the plugins that we'll need to be active for the tests
+ */
+function manually_load_plugin() {
+	define( 'WP_ADMIN',          true ); // This is so that wordcamp-remote-css/bootstrap.php will load the app/*.php files. It may need to be refactored if we add tests for output-cached-css.php.
+	define( 'JETPACK_DEV_DEBUG', true );
+
+	require_once( dirname(                   __DIR__ )     . '/bootstrap.php'                 );
+	require_once( dirname( dirname(          __DIR__ ) )   . '/jetpack/jetpack.php'           );
+	require_once( dirname( dirname( dirname( __DIR__ ) ) ) . '/mu-plugins/jetpack-tweaks.php' ); // Some of the sanitization lives here because it runs for both Custom CSS and Remote CSS
+}
+tests_add_filter( 'muplugins_loaded', __NAMESPACE__ . '\manually_load_plugin' );
+
+require $core_tests_directory . '/includes/bootstrap.php';
+
+\Jetpack::activate_module( 'custom-css', false, false );

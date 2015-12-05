c3854dde0f3d3cae5e841c3c248a5bd603396ad2
iandunn <iandunn@git.wordpress.org>
2015-12-05 17:33:32 +0000

    WordCamp WP-CLI: Centralize WP-CLI commands for consistency and organization.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@2165 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/mu-plugins/wp-cli-commands.php
@@ -0,0 +1,16 @@
+<?php
+
+if ( ! defined( 'WP_CLI' ) || ! WP_CLI ) {
+	return;
+}
+
+$wp_cli_commands = glob( __DIR__ . '/wp-cli-commands/*.php' );
+
+if ( is_array( $wp_cli_commands ) ) {
+	foreach ( $wp_cli_commands as $command ) {
+		require_once( $command );
+	}
+}
+
+WP_CLI::add_command( 'wc-rewrite', 'WordCamp_CLI_Rewrite_Rules' );
+WP_CLI::add_command( 'wc-rest',    'WordCamp_CLI_REST_API'      );

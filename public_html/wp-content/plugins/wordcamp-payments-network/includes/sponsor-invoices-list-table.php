7afb0135b0bceef660be6b6e29a718a707d4d6ff
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2016-01-16 00:22:43 +0000

    WordCamp Budgets Dashboard: Add screen to monitor/approve Sponsor Invoices.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@2302 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/plugins/wordcamp-payments-network/includes/sponsor-invoices-list-table.php
@@ -0,0 +1,169 @@
+<?php
+
+namespace WordCamp\Budgets_Dashboard\Sponsor_Invoices;
+defined( 'WPINC' ) or die();
+
+class Sponsor_Invoices_List_Table extends \WP_List_Table {
+
+	/**
+	 * Define the table columns that will be rendered
+	 */
+	public function get_columns() {
+		$columns = array(
+			'invoice_title' => 'Invoice',
+			'wordcamp_name' => 'WordCamp',
+			'sponsor_name'  => 'Sponsor',
+			'description'   => 'Description',
+			'due_date'      => 'Due Date',
+			'amount'        => 'Amount',
+		);
+
+		if ( 'submitted' === $_GET['section'] ) {
+			$columns['approve_invoice'] = 'Approve';
+		}
+
+		return $columns;
+	}
+
+	/**
+	 * Parses query arguments and queries the index table in the database.
+	 */
+	public function prepare_items() {
+		global $wpdb;
+
+		/*
+		 * Manually build the column headers
+		 *
+		 * See https://codex.wordpress.org/Class_Reference/WP_List_Table#Using_within_Meta_Boxes
+		 *
+		 * The alternative to this would be instantiating this object during `load-$hook-suffix`, and setting it
+		 * to a global variable so it could be accessed later by render_submenu_page(). This is hacky, but that's
+		 * worse.
+		 */
+		$this->_column_headers = array(
+			$this->get_columns(),
+			array(),
+			array(),
+			$this->get_primary_column_name()
+		);
+
+		$table_name = get_index_table_name();
+		$status     = 'wcbsi_' . $_GET['section'];
+		$paged      = isset( $_REQUEST['paged'] ) ? absint( $_REQUEST['paged'] ) : 1;
+		$limit      = 30;
+		$offset     = $limit * ( $paged - 1 );
+
+		$this->items = $wpdb->get_results( $wpdb->prepare( "
+			SELECT *
+			FROM $table_name
+			WHERE status = %s
+			ORDER BY due_date ASC
+			LIMIT %d
+			OFFSET %d",
+			$status,
+			$limit,
+			$offset
+		) );
+
+		// A second query is faster than using SQL_CALC_FOUND_ROWS during the first query
+		$total_items = $wpdb->get_var( $wpdb->prepare( "
+			SELECT count(blog_id)
+			FROM $table_name
+			WHERE status = %s",
+			$status
+		) );
+
+		$this->set_pagination_args( array(
+			'total_items' => $total_items,
+			'total_pages' => ceil( $total_items / $limit ),
+			'per_page'    => $limit,
+		) );
+	}
+
+	/**
+	 * Render the value for the Invoice column
+	 *
+	 * @param object $index_row
+	 */
+	protected function column_invoice_title( $index_row ) {
+		$edit_url = get_admin_url(
+			$index_row->blog_id,
+			sprintf( 'post.php?post=%s&action=edit', $index_row->invoice_id )
+		);
+
+		ob_start();
+		?>
+
+		<a href="<?php echo esc_url( $edit_url ); ?>">
+			<?php echo esc_html( $index_row->invoice_title ); ?>
+		</a>
+
+		<?php
+
+		return ob_get_clean();
+	}
+
+	/**
+	 * Render the value for the Description column
+	 *
+	 * @param object $index_row
+	 */
+	protected function column_description( $index_row ) {
+		return esc_html( substr( $index_row->description, 0, 75 ) );
+	}
+
+	/**
+	 * Render the value for the Due Date column
+	 *
+	 * @param object $index_row
+	 */
+	protected function column_due_date( $index_row ) {
+		return esc_html( date( 'Y-m-d', $index_row->due_date ) );
+	}
+
+	/**
+	 * Render the value for the Due Date column
+	 *
+	 * @param object $index_row
+	 */
+	protected function column_amount( $index_row ) {
+		return wp_kses(
+			\WordCamp\Budgets_Dashboard\format_amount( $index_row->amount, $index_row->currency ),
+			array( 'br' => array() )
+		);
+	}
+
+	/**
+	 * Render the value for the Approve column
+	 *
+	 * @param object $index_row
+	 */
+	protected function column_approve_invoice( $index_row ) {
+		$nonce = wp_create_nonce( "wcbdsi-approve-invoice-{$index_row->blog_id}-{$index_row->invoice_id}" );
+
+		?>
+
+		<button
+			class="wcbdsi-approve-invoice button-secondary"
+			data-site-id="<?php    echo esc_attr( $index_row->blog_id    ); ?>"
+			data-invoice-id="<?php echo esc_attr( $index_row->invoice_id ); ?>"
+			data-nonce="<?php      echo esc_attr( $nonce                 ); ?>"
+		>
+			Approve
+		</button>
+
+		<div class="wcbdsi-inline-notice hidden"><div> <?php // Populated dynamically ?>
+
+		<?php
+	}
+
+	/**
+	 * Render the value for columns that don't have a explicit handler
+	 *
+	 * @param object $index_row
+	 * @param string $column_name
+	 */
+	protected function column_default( $index_row, $column_name ) {
+		echo esc_html( $index_row->$column_name );
+	}
+}
---
+++ public_html/wp-content/plugins/wordcamp-payments-network/javascript/sponsor-invoices.js
@@ -0,0 +1,91 @@
+( function( $ ) {
+	'use strict';
+
+	window.WordCampBudgetsDashboard = window.WordCampBudgetsDashboard || {};
+
+	var app = window.WordCampBudgetsDashboard.SponsorInvoices = {
+		/**
+		 * Initialization that runs as soon as this file has loaded
+		 */
+		start : function() {
+			try {
+				$( '.wcbdsi-approve-invoice' ).click( app.approveInvoice );
+			} catch ( exception ) {
+				app.log( exception );
+			}
+		},
+
+		/**
+		 * Send an AJAX request to approve the invoice
+		 *
+		 * @param {event} event
+		 */
+		approveInvoice : function( event ) {
+			var approvalButton = $( this ),
+			    statusMessage  = $( this ).parent().find( '.wcbdsi-inline-notice' ),
+			    siteID         = approvalButton.data( 'site-id'    ),
+				invoiceID      = approvalButton.data( 'invoice-id' ),
+				nonce          = approvalButton.data( 'nonce'      );
+
+			event.preventDefault();
+
+			try {
+				approvalButton.addClass( 'hidden' );
+				statusMessage.html( 'Submitting to QuickBooks...' );   // todo show spinner instead
+				statusMessage.removeClass( 'hidden' );
+
+				$.post(
+					ajaxurl,
+					{
+						action     : 'wcbdsi_approve_invoice',
+						nonce      : nonce,
+						site_id    : siteID,
+						invoice_id : invoiceID
+					},
+
+					function( response ) {
+						// todo modularize this
+
+						try {
+							if ( response.hasOwnProperty( 'success' ) && true === response.success ) {
+								statusMessage.addClass( 'notice notice-success' );
+								statusMessage.html( _.escape( response.data.message ) );
+							} else {
+								statusMessage.addClass( 'notice notice-error' );
+								statusMessage.html( _.escape( 'ERROR: ' + response.data.error ) );
+
+								// todo bring button back so they can try again?
+							}
+						} catch ( exception ) {
+							app.log( exception );
+						}
+					}
+				);
+			} catch ( exception ) {
+				app.log( exception );
+			}
+		},
+
+		/**
+		 * Log a message to the console
+		 *
+		 * todo centralize for other modules
+		 *
+		 * @param {*} error
+		 */
+		log : function( error ) {
+			if ( ! window.console ) {
+				return;
+			}
+
+			if ( 'string' === typeof error ) {
+				console.log( 'WordCamp Budgets Dashboard: ' + error );
+			} else {
+				console.log( 'WordCamp Budgets Dashboard: ', error );
+			}
+		}
+	};
+
+} )( jQuery );
+
+window.WordCampBudgetsDashboard.SponsorInvoices.start();

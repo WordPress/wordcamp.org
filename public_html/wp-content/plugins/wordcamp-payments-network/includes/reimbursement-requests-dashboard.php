8393e7fad7c7a8ecaf9ecf2847c0e74de637c4fb
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2016-01-28 00:29:34 +0000

    WordCamp Budgets Dashboard: Add dashboard for Reimbursement Requests.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@2393 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/plugins/wordcamp-payments-network/includes/reimbursement-requests-dashboard.php
@@ -0,0 +1,274 @@
+<?php
+
+namespace WordCamp\Budgets_Dashboard\Reimbursement_Requests;
+
+defined( 'WPINC' ) or die();
+
+const LATEST_DATABASE_VERSION = 1;
+
+if ( is_network_admin() ) {
+	add_action( 'network_admin_menu',    __NAMESPACE__ . '\register_submenu_page' );
+	add_action( 'init',                  __NAMESPACE__ . '\upgrade_database'      );
+
+} elseif ( is_admin() ) {
+	add_action( 'save_post',    __NAMESPACE__ . '\update_index_row', 11, 2 );   // See note in callback about priority
+	add_action( 'trashed_post', __NAMESPACE__ . '\delete_index_row'        );
+	add_action( 'delete_post',  __NAMESPACE__ . '\delete_index_row'        );
+}
+
+/**
+ * Register the admin page
+ */
+function register_submenu_page() {
+	add_submenu_page(
+		'wordcamp-budgets-dashboard',
+		'WordCamp Reimbursement Requests',
+		'Reimbursements',
+		'manage_network',
+		'reimbursement-requests-dashboard',
+		__NAMESPACE__ . '\render_submenu_page'
+	);
+}
+
+/**
+ * Render the admin page
+ */
+function render_submenu_page() {
+	require_once( __DIR__ . '/reimbursement-requests-list-table.php' );
+
+	$list_table = new Reimbursement_Requests_List_Table();
+	$sections   = get_submenu_page_sections();
+
+	$list_table->prepare_items();
+
+	switch ( get_current_section() ) {
+		case 'submitted':
+			$section_explanation = 'These requests have been completed by the organizer, and need to be reviewed by a deputy.';
+			break;
+
+		case 'info_requested':
+			$section_explanation = 'These requests have been reviewed by a deputy, and sent back to the organizer because they lacked some required information.';
+			break;
+
+		case 'rejected':
+			$section_explanation = 'These requests have been reviewed by a deputy and rejected.';
+			break;
+
+		case 'in_process':
+			$section_explanation = "These requests have been reviewed by a deputy and payment is pending.";
+			break;
+
+		case 'paid':
+			$section_explanation = 'These requests have been paid and closed.';
+			break;
+	}
+
+	require_once( dirname( __DIR__ ) . '/views/reimbursement-requests/page-reimbursement-requests.php' );
+}
+
+/**
+ * Get the current section
+ *
+ * @return string
+ */
+function get_current_section() {
+	$sections        = get_section_slugs();
+	$current_section = 'submitted';
+
+	if ( isset( $_GET['section'] ) && in_array( $_GET['section'], $sections, true ) ) {
+		$current_section = $_GET['section'];
+	}
+
+	return $current_section;
+}
+
+/**
+ * Get the slugs for each page section
+ *
+ * @return array
+ */
+function get_section_slugs() {
+	$slugs = array_keys( \WordCamp\Budgets\Reimbursement_Requests\get_custom_statuses() );
+
+	foreach( $slugs as & $slug ) {
+		$slug = str_replace( 'wcbrr_', '', $slug );
+	}
+
+	return $slugs;
+}
+
+/**
+ * Get all the data needed to render the section tabs
+ *
+ * @return array
+ */
+function get_submenu_page_sections() {
+	$statuses        = \WordCamp\Budgets\Reimbursement_Requests\get_custom_statuses();
+	$sections        = array();
+	$current_section = get_current_section();
+
+	foreach ( $statuses as $status_slug => $status_name ) {
+		$status_slug = str_replace( 'wcbrr_', '', $status_slug );    // make the URL easier to read
+
+		$classes = 'nav-tab';
+		if ( $status_slug === $current_section ) {
+			$classes .= ' nav-tab-active';
+		}
+
+		$href = add_query_arg(
+			array(
+				'page'    => 'reimbursement-requests-dashboard',
+				'section' => $status_slug,
+			),
+			network_admin_url( 'admin.php' )
+		);
+
+		$sections[ $status_slug ] = array(
+			'classes' => $classes,
+			'href'    => $href,
+			'text'    => $status_name,
+		);
+	}
+
+	return $sections;
+}
+
+/**
+ * Create or update the database tables
+ */
+function upgrade_database() {
+	global $wpdb;
+
+	$current_database_version = get_site_option( 'wcbdrr_database_version', 0 );
+
+	if ( version_compare( $current_database_version, LATEST_DATABASE_VERSION, '>=' ) ) {
+		return;
+	}
+
+	$table_name = get_index_table_name();
+	require_once( ABSPATH . 'wp-admin/includes/upgrade.php' );
+
+	$schema = "
+		CREATE TABLE $table_name (
+			blog_id        int( 11 )        unsigned NOT NULL default '0',
+			request_id     int( 11 )        unsigned NOT NULL default '0',
+			date_requested int( 11 )        unsigned NOT NULL default '0',
+			request_title  varchar( 75 )             NOT NULL default '',
+			status         varchar( 30 )             NOT NULL default '',
+			wordcamp_name  varchar( 75 )             NOT NULL default '',
+			currency       varchar( 3  )             NOT NULL default '',
+			amount         numeric( 10, 2 ) unsigned NOT NULL default '0',
+
+			PRIMARY KEY (blog_id, request_id),
+			KEY status (status)
+		)
+		DEFAULT CHARACTER SET {$wpdb->charset}
+		COLLATE {$wpdb->collate};
+	";
+
+	dbDelta( $schema );
+
+	update_site_option( 'wcbdrr_database_version', LATEST_DATABASE_VERSION );
+}
+
+/**
+ * Returns the name of the custom table.
+ */
+function get_index_table_name() {
+	global $wpdb;
+
+	return $wpdb->get_blog_prefix( 0 ) . 'wcbd_reimbursement_requests_index';
+}
+
+/**
+ * Add or update a row in the index
+ *
+ * NOTE: This must run after \WordCamp\Budgets\Reimbursement_Requests\save_request(), because otherwise the
+ * get_post_meta() calls would be fetching the old data, rather than the latest from the current process.
+ *
+ * @param int      $request_id
+ * @param \WP_Post $request
+ */
+function update_index_row( $request_id, $request ) {
+	global $wpdb;
+
+	if ( \WordCamp\Budgets\Reimbursement_Requests\POST_TYPE !== $request->post_type ) {
+		return;
+	}
+
+	// Drafts, etc aren't displayed in the list table, so there's no reason to index them
+	$ignored_statuses = array( 'auto-draft', 'draft', 'trash' );
+
+	if ( in_array( $request->post_status, $ignored_statuses, true ) ) {
+		return;
+	}
+
+	$index_row = array(
+		'blog_id'        => get_current_blog_id(),
+		'request_id'     => $request_id,
+		'date_requested' => strtotime( $request->post_date_gmt ),
+		'request_title'  => $request->post_title,
+		'status'         => $request->post_status,
+		'wordcamp_name'  => get_wordcamp_name(),
+		'currency'       => get_post_meta( $request_id, '_wcbrr_currency', true ),
+		'amount'         => get_amount( $request_id ),
+	);
+
+	/*
+	 * Some fields have default values like 'null-select-one', 'null-separator1', etc, but $wpdb->process_fields()
+	 * will choke on those, so we need to set them to empty strings instead.
+	 */
+	foreach ( $index_row as & $column ) {
+		if ( 'null' === substr( $column, 0, 4 ) ) {
+			$column = '';
+		}
+	}
+
+	$formats = array( '%d', '%d', '%s', '%s', '%s', '%s', '%s', '%f' );
+
+	$wpdb->replace( get_index_table_name(), $index_row, $formats );
+}
+
+/**
+ * Calculate the total expense amount for a given request
+ *
+ * @param int $request_id
+ *
+ * @return float
+ */
+function get_amount( $request_id ) {
+	$amount   = 0.0;
+	$expenses = get_post_meta( $request_id, '_wcbrr_expenses', true );
+
+	if ( is_array( $expenses ) ) {
+		foreach ( $expenses as $expense ) {
+			$amount += (float) $expense['_wcbrr_amount'];
+		}
+	}
+
+	return $amount;
+}
+
+/**
+ * Delete a row from the index
+ *
+ * @param int $request_id
+ */
+function delete_index_row( $request_id ) {
+	global $wpdb;
+
+	/*
+	 * Normally we would check if $request_id is from the kind of post type we want, but that's not necessary in
+	 * this case, because only requests are added to the index to begin with. If $request_id is from some other
+	 * post type, then $wpdb->delete() will return false with no negative consequences. That's quicker than having
+	 * to query for the $request post in order to check the post type.
+	 */
+
+	$wpdb->delete(
+		get_index_table_name(),
+		array(
+			'blog_id'    => get_current_blog_id(),
+			'request_id' => $request_id,
+		)
+	);
+}
---
+++ public_html/wp-content/plugins/wordcamp-payments-network/includes/reimbursement-requests-list-table.php
@@ -0,0 +1,120 @@
+<?php
+
+namespace WordCamp\Budgets_Dashboard\Reimbursement_Requests;
+defined( 'WPINC' ) or die();
+
+class Reimbursement_Requests_List_Table extends \WP_List_Table {
+
+	/**
+	 * Define the table columns that will be rendered
+	 */
+	public function get_columns() {
+		$columns = array(
+			'request_title' => 'Request',
+			'wordcamp_name' => 'WordCamp',
+			'amount'        => 'Amount',
+		);
+
+		return $columns;
+	}
+
+	/**
+	 * Parses query arguments and queries the index table in the database.
+	 */
+	public function prepare_items() {
+		global $wpdb;
+
+		/*
+		 * Manually build the column headers
+		 *
+		 * See https://codex.wordpress.org/Class_Reference/WP_List_Table#Using_within_Meta_Boxes
+		 *
+		 * The alternative to this would be instantiating this object during `load-$hook-suffix`, and setting it
+		 * to a global variable so it could be accessed later by render_submenu_page(). This is hacky, but that's
+		 * worse.
+		 */
+		$this->_column_headers = array(
+			$this->get_columns(),
+			array(),
+			array(),
+			$this->get_primary_column_name()
+		);
+
+		$table_name = get_index_table_name();
+		$status     = 'wcbrr_' . get_current_section();
+		$paged      = isset( $_REQUEST['paged'] ) ? absint( $_REQUEST['paged'] ) : 1;
+		$limit      = 30;
+		$offset     = $limit * ( $paged - 1 );
+
+		$this->items = $wpdb->get_results( $wpdb->prepare( "
+			SELECT *
+			FROM $table_name
+			WHERE status = %s
+			ORDER BY date_requested ASC
+			LIMIT %d
+			OFFSET %d",
+			$status,
+			$limit,
+			$offset
+		) );
+
+		// A second query is faster than using SQL_CALC_FOUND_ROWS during the first query
+		$total_items = $wpdb->get_var( $wpdb->prepare( "
+			SELECT count(blog_id)
+			FROM $table_name
+			WHERE status = %s",
+			$status
+		) );
+
+		$this->set_pagination_args( array(
+			'total_items' => $total_items,
+			'total_pages' => ceil( $total_items / $limit ),
+			'per_page'    => $limit,
+		) );
+	}
+
+	/**
+	 * Render the value for the Invoice column
+	 *
+	 * @param object $index_row
+	 */
+	protected function column_request_title( $index_row ) {
+		$edit_url = get_admin_url(
+			$index_row->blog_id,
+			sprintf( 'post.php?post=%s&action=edit', $index_row->request_id )
+		);
+
+		ob_start();
+		?>
+
+		<a href="<?php echo esc_url( $edit_url ); ?>">
+			<?php echo esc_html( $index_row->request_title ); ?>
+		</a>
+
+		<?php
+
+		return ob_get_clean();
+	}
+
+	/**
+	 * Render the value for the Due Date column
+	 *
+	 * @param object $index_row
+	 */
+	protected function column_amount( $index_row ) {
+		return wp_kses(
+			\WordCamp\Budgets_Dashboard\format_amount( $index_row->amount, $index_row->currency ),
+			array( 'br' => array() )
+		);
+	}
+
+	/**
+	 * Render the value for columns that don't have a explicit handler
+	 *
+	 * @param object $index_row
+	 * @param string $column_name
+	 */
+	protected function column_default( $index_row, $column_name ) {
+		echo esc_html( $index_row->$column_name );
+	}
+}

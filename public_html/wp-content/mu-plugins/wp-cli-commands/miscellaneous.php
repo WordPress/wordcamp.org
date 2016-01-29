2cb7f2109677c3182c74bde3eeec1a8a68b7cbe6
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2016-01-29 04:42:03 +0000

    WordCamp WP-CLI: Convert set-skip-feature-flags.php to a WP-CLI command.
    Previously it was an old-school bin script in our private repository. This is much nicer, and open-source.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@2418 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/mu-plugins/wp-cli-commands/miscellaneous.php
@@ -0,0 +1,87 @@
+<?php
+
+defined( 'WP_CLI' ) or die();
+
+/**
+ * WordCamp.org: Miscellaneous commands.
+ */
+class WordCamp_CLI_Miscellaneous extends WP_CLI_Command {
+	/**
+	 * Sets skip-feature flags on existing sites when new functionality is introduced
+	 *
+	 * See wcorg_skip_feature() for context.
+	 *
+	 * ## OPTIONS
+	 *
+	 * <flag_name>
+	 * : The name of the flag that will be set
+	 *
+	 * [<max_site_id>]
+	 * : The ID of the newest site that the flag will be set on. If empty,
+	 * the flag will be applied to all sites.
+	 *
+	 * [--dry-run]
+	 * : Show a report, but don't perform the changes.
+	 *
+	 * ## EXAMPLES
+	 *
+	 * wp wc-misc set-skip-feature-flag wcb_viewport_initial_scale
+	 * wp wc-misc set-skip-feature-flag wcb_viewport_initial_scale 437
+	 * wp wc-misc set-skip-feature-flag wcb_viewport_initial_scale 437 --dry-run
+	 *
+	 * @subcommand set-skip-feature-flag
+	 *
+	 * @param array $args
+	 * @param array $assoc_args
+	 */
+	public function set_skip_feature_flag( $args, $assoc_args ) {
+		$flag_name   = $args[0];
+		$max_site_id = empty( $args[1] ) ? false : absint( $args[1] );
+		$dry_run     = isset( $assoc_args[ 'dry-run' ] );
+		$sites       = wp_get_sites( array( 'limit' => false ) );
+		$results     = array();
+
+		WP_CLI::line();
+
+		foreach ( $sites as $site ) {
+			switch_to_blog( $site['blog_id'] );
+
+			$site_domain = parse_url( get_option( 'siteurl' ) );
+			$site_domain = sprintf( '%s%s', $site_domain['host'], $site_domain['path'] );
+
+			// Skip sites that are above the requested maximum site ID
+			if ( $max_site_id && $site['blog_id'] > $max_site_id ) {
+				$results[] = array( $site_domain, 'skipped' );
+				continue;
+			}
+
+			// Apply the flag to the requested sites
+			$flags = get_option( 'wordcamp_skip_features', array() );
+			$flags[ $flag_name ] = true;
+
+			if ( ! $dry_run ) {
+				update_option( 'wordcamp_skip_features', $flags );
+			}
+
+			$results[] = array( $site_domain, 'applied' );
+
+			restore_current_blog();
+		}
+
+		$table = new \cli\Table();
+		$table->setHeaders( array( 'Site', 'Action' ) );
+		$table->setRows( $results );
+		$table->display();
+
+		WP_CLI::line();
+		WP_CLI::success( sprintf(
+			'%s has been applied to all sites%s.',
+			$flag_name,
+			$max_site_id ? ' up through ' . $max_site_id : ''
+		) );
+
+		if ( $dry_run ) {
+			WP_CLI::warning( 'This was only a dry-run.' );
+		}
+	}
+}

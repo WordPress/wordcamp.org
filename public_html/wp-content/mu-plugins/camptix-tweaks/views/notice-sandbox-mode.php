1f5b5538b8c00ee341af226b9f55d2f85e950bc7
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2016-02-01 23:20:27 +0000

    CampTix Tweaks: Open-source part of the CampTix Extras plugin.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@2442 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/mu-plugins/camptix-tweaks/views/notice-sandbox-mode.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace WordCamp\CampTix_Tweaks;
+defined( 'WPINC' ) or die();
+
+?>
+
+<div class="notice notice-warning">
+	<p>
+		<?php _e(
+			'CampTix is in <strong>sandbox (test) mode</strong>. Make sure you disable it before you open ticket sales!',
+			'wordcamporg'
+		); ?>
+	</p>
+</div>
--- public_html/wp-content/mu-plugins/load-other-mu-plugins.php
+++ public_html/wp-content/mu-plugins/load-other-mu-plugins.php
@@ -4,6 +4,7 @@ defined( 'WPINC' ) or die();
 
 // mu-plugins in sub-directories
 require_once( __DIR__ . '/wp-cli-commands/bootstrap.php' );
+require_once( __DIR__ . '/camptix-tweaks/camptix-tweaks.php' );
 
 // Private mu-plugins
 $private_mu_plugins = glob( WP_CONTENT_DIR . '/mu-plugins-private/*.php' );

db4bf5446100372f653be1c1208bab3ffdd0bd22
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2016-02-25 01:21:51 +0000

    WordCamp Budgets: Mark Sponsor Invoice form fields as required.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@2605 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/plugins/wordcamp-payments/views/wordcamp-budgets/form-field-required-indicator.php
@@ -0,0 +1,9 @@
+
+<span class="wcb-form-required" aria-hidden="true">
+	<?php // translators: The symbol to indicate the form field is required ?>
+	<?php _e( '*', 'wordcamporg' ); ?>
+</span>
+
+<span class="screen-reader-text">
+	<?php _e( 'required field', 'wordcamporg' ); ?>
+</span>

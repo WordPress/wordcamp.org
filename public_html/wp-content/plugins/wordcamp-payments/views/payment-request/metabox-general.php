9755a13433c3ba2198038c2787d790e9fcce281b
iandunn <iandunn@git.wordpress.org>
2016-03-07 23:24:32 +0000

    WordCamp Budgets: Make the Notes field for Vendor Payments optional.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@2700 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/plugins/wordcamp-payments/views/payment-request/metabox-general.php
+++ public_html/wp-content/plugins/wordcamp-payments/views/payment-request/metabox-general.php
@@ -21,7 +21,7 @@
 	?>
 
 	<?php $this->render_files_input( $post, 'Files', 'files', __( 'Attach supporting documentation including invoices, contracts, or other vendor correspondence. If no supporting documentation is available, please indicate the reason in the notes below.', 'wordcamporg' ) ); ?>
-	<?php $this->render_textarea_input( $post, 'Notes', 'general_notes', 'Any other details you want to share.' ); ?>
+	<?php $this->render_textarea_input( $post, 'Notes', 'general_notes', 'Any other details you want to share.', false ); ?>
 </table>
 
 <p class="wcb-form-required">

6e8ea0e584dc9f1b417d64ab920a1a8a07962d38
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2016-05-12 14:54:42 +0000

    WordCamp Post Type: Require a Sponsor Region when creating a new site.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@3122 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/plugins/wcpt/javascript/wcpt-wordcamp/admin.js
+++ public_html/wp-content/plugins/wcpt/javascript/wcpt-wordcamp/admin.js
@@ -9,11 +9,33 @@ wp.wcpt.WcptWordCamp = ( function( $ ) {
 	 * Initialize
 	 */
 	self.initialize = function() {
+		var createSiteCheckbox = $( '#wcpt_create-site-in-network' );
+
+		createSiteCheckbox.change( self.toggleSponsorRegionRequired );
+		createSiteCheckbox.trigger( 'change' );
+
 		$( '.date-field' ).datepicker( {
 			dateFormat: 'yy-mm-dd',
 			changeMonth: true,
 			changeYear:  true
 		} );
+	};
+
+	/**
+	 * Toggle whether the Sponsor Region field is required or not.
+	 *
+	 * \WordCamp_New_Site::maybe_create_new_site() requires it to be set to create a new site.
+	 *
+	 * @param {object} event
+	 */
+	self.toggleSponsorRegionRequired = function( event ) {
+		var sponsorRegion = $( '#wcpt_multi-event_sponsor_region' );
+
+		if ( $( this ).is( ':checked' ) ) {
+			sponsorRegion.prop( 'required', true );
+		} else {
+			sponsorRegion.prop( 'required', false );
+		}
 	};
 
 	$( document ).ready( function( $ ) {

a28e7ce06c4c9a797814a0cf62d2ae2ab919015f
iandunn <iandunn@git.wordpress.org>
2016-06-14 22:46:04 +0000

    WordCamp Post Type: Enforce required fields during application processing.
    The client-side validation is best for UX, but this is still necessary to prevent bots.
    
    See https://make.wordpress.org/community/2016/06/13/monday-daily-updates-47/#comment-22200
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@3366 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/plugins/wcpt/views/applications/common/submission-results.php
+++ public_html/wp-content/plugins/wcpt/views/applications/common/submission-results.php
@@ -5,6 +5,6 @@ defined( 'WPINC' ) or die();
 
 ?>
 
-<div class="notice notice-large notice-success">
+<div class="notice notice-large <?php echo esc_attr( $notice_classes ); ?>">
 	<?php echo esc_html( $message ); ?>
 </div>

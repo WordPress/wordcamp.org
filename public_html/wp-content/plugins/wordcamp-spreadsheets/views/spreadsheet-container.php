5ec38911889801766383129ef91d38a7978a1fad
iandunn <iandunn@git.wordpress.org>
2016-08-01 23:10:39 +0000

    WordCamp Spreadsheets: Delete the plugin.
    This plugin was originally intended to replace the need for Google Sheets, but was never finished or used. The new Budget modules in the WordCamp Payments plugin is filling that need now, so there's no reason to keep this around.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@3761 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/plugins/wordcamp-spreadsheets/views/spreadsheet-container.php
+++
@@ -1,9 +0,0 @@
-<div id="wcss_spreadsheet_container"></div>
-
-<?php if ( is_admin() ) : ?>
-	<input id="wcss_spreadsheet_data" name="wcss_spreadsheet_data" type="hidden" value="<?php echo esc_attr( json_encode( $spreadsheet_data ) ); ?>" />
-<?php endif; ?>
-
-<script type="text/javascript">
-	var wcssSpreadSheetData = <?php echo json_encode( $spreadsheet_data ); ?>;	<?php // todo make sure json_encode() is enough sanitization here and above ?>
-</script>

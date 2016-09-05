3fa026ed0c6a34472d25b22600adfb3960981569
kovshenin <kovshenin@74240141-8908-4e6f-9713-ba540dce6ec7>
2016-09-05 19:16:11 +0000

    WordCamp Central: Flush CSS cache.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@3967 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/themes/wordcamp-central-2012/functions.php
+++ public_html/wp-content/themes/wordcamp-central-2012/functions.php
@@ -183,7 +183,7 @@ class WordCamp_Central_Theme {
 	 * Enqueue scripts and styles.
 	 */
 	static function enqueue_scripts() {
-		wp_enqueue_style( 'central', get_stylesheet_uri(), array(), 7 );
+		wp_enqueue_style( 'central', get_stylesheet_uri(), array(), 8 );
 		wp_enqueue_script( 'wordcamp-central', get_stylesheet_directory_uri() . '/js/central.js', array( 'jquery', 'underscore' ), 2, true );
 
 		wp_localize_script( 'wordcamp-central', 'wordcampCentralOptions', self::get_javascript_options() );

6d3359c80add580c3fbc737de6763ebc41f01786
iandunn <iandunn@git.wordpress.org>
2016-12-23 17:27:38 +0000

    WordCamp Site Cloner: Refactor for compatibility with WP 4.7 and Jetpack 4.2.2
    WordPress 4.7 added a Customizer feature that allows editing CSS. Jetpack 4.2.2 replaced the Custom CSS module with an entirely new one that is compatible with the Customizer feature, but broke backwards-compatibility with the Site Cloner and Remote CSS plugins.
    
    This commit refactors the Site Cloner to restore compatibility with Jetpack, and add support for the new Core feature.
    
    https://make.wordpress.org/core/2016/10/11/feature-proposal-better-theme-customizations-via-custom-css-with-live-previews/
    https://make.wordpress.org/core/2016/11/26/extending-the-custom-css-editor/
    https://github.com/Automattic/jetpack/issues/5539
    https://github.com/Automattic/jetpack/pull/5804
    https://core.trac.wordpress.org/ticket/35395
    https://core.trac.wordpress.org/ticket/38672
    https://core.trac.wordpress.org/ticket/39270
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@4577 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/plugins/wordcamp-site-cloner/includes/source-site-id-setting.php
+++ public_html/wp-content/plugins/wordcamp-site-cloner/includes/source-site-id-setting.php
@@ -1,9 +1,10 @@
 <?php
 
 namespace WordCamp\Site_Cloner;
-defined( 'WPINC' ) or die();
-
 use \WordCamp\RemoteCSS;
+use Jetpack_Custom_CSS_Enhancements;
+
+defined( 'WPINC' ) or die();
 
 /**
  * Customizer Setting for source site ID
@@ -11,6 +12,8 @@ use \WordCamp\RemoteCSS;
  * This isn't an actual setting that gets stored in the database, it's just a temporary value to track the ID of
  * the site that that will be cloned. There are actually many data points that we'll import, but they don't fit
  * into the default model that the Customizer expects.
+ *
+ * @todo - We may no longer need a setting; see https://make.wordpress.org/core/2016/03/10/customizer-improvements-in-4-5/
  */
 class Source_Site_ID_Setting extends \WP_Customize_Setting {
 	public $type              = 'wcsc-source-site-id';
@@ -28,87 +31,47 @@ class Source_Site_ID_Setting extends \WP_Customize_Setting {
 		}
 
 		add_action( 'wp_head',                 array( $this, 'preview_source_site_css'  ), 99 );   // wp_print_styles is too early; the theme's stylesheet would get enqueued later and take precedence
-		add_filter( 'get_post_metadata',       array( $this, 'preview_jetpack_postmeta' ), 10, 4 );
-		add_filter( 'safecss_skip_stylesheet', array( $this, 'preview_skip_stylesheet'  ) );
+		add_filter( 'safecss_skip_stylesheet', array( $this, 'preview_skip_stylesheet'  ), 15 );   // After Jetpack_Custom_CSS_Enhancements::preview_skip_stylesheet()
 
 		// Disable the current site's Custom CSS from being output
-		remove_action( 'wp_head', array( 'Jetpack_Custom_CSS', 'link_tag' ), 101 );
+		remove_action( 'wp_head', 'wp_custom_css_cb', 11 ); // todo compat with WP 4.7. this line can be removed when r39616 is merged to the 4.7 branch
+		remove_action( 'wp_head', 'wp_custom_css_cb', 101 );
 	}
 
 	/**
 	 * Print the source site's custom CSS in an inline style block
-	 *
-	 * It can't be easily enqueued as an external stylesheet because Jetpack_Custom_CSS::link_tag() returns early
-	 * in the Customizer if the theme being previewed is different from the live theme.
 	 */
 	public function preview_source_site_css() {
-		if ( method_exists( '\Jetpack', 'get_module_path' ) ) {
-			require_once( \Jetpack::get_module_path( 'custom-css' ) );
-		} else {
-			return;
-		}
-
 		switch_to_blog( $this->preview_source_site_id );
 
 		printf(
 			'<style id="wcsc-source-site-custom-css">%s %s</style>',
 			$this->get_cached_remote_css(),
-			\Jetpack_Custom_CSS::get_css( true )
+			wp_get_custom_css()
 		);
 
 		restore_current_blog();
 	}
 
 	/**
-	 * Overwrite the previewing site's Custom CSS settings with values from the source site
-	 *
-	 * @param mixed  $pre_filter_value
-	 * @param int    $post_id
-	 * @param string $meta_key
-	 * @param bool   $return_single_value
-	 *
-	 * @return mixed
-	 */
-	public function preview_jetpack_postmeta( $pre_filter_value, $post_id, $meta_key, $return_single_value ) {
-		if ( ! in_array( $meta_key, array( 'content_width' ) ) ) {
-			return $pre_filter_value;
-		}
-
-		remove_filter( 'get_post_metadata', array( $this, 'preview_jetpack_postmeta' ), 10, 4 );    // avoid infinite recursion
-		switch_to_blog( $this->preview_source_site_id );
-
-		if ( $source_cite_css_post = \Jetpack_Custom_CSS::get_current_revision() ) {
-			$pre_filter_value = get_post_meta( $source_cite_css_post['ID'], $meta_key, $return_single_value );
-		}
-
-		restore_current_blog();
-		add_filter( 'get_post_metadata', array( $this, 'preview_jetpack_postmeta' ), 10, 4 );
-
-		return $pre_filter_value;
-	}
-
-	/**
 	 * Determine whether or not to skip the primary stylesheet
 	 *
-	 * When the Custom CSS value for `Mode` is `Replacement`, Jetpack will prevent the primary stylesheet from
+	 * When the `Start Fresh` theme mod is set, Jetpack will prevent the primary stylesheet from
 	 * being enqueued. We need to trigger that behavior based on the value on the source site, rather than the
 	 * current site.
-	 *
-	 * This could be actually handled along with other Jetpack options in preview_jetpack_postmeta(), if it
-	 * weren't for the fact that Jetpack_Custom_CSS::skip_stylesheet always returns false in the Previewer.
 	 *
 	 * @param bool $skip
 	 *
 	 * @return bool
 	 */
 	public function preview_skip_stylesheet( $skip ) {
+		remove_filter( 'safecss_skip_stylesheet', array( $this, 'preview_skip_stylesheet'  ), 15 ); // Avoid infinite recursion. Also, use same priority as originally set in preview()
 		switch_to_blog( $this->preview_source_site_id );
 
-		if ( $source_cite_css_post = \Jetpack_Custom_CSS::get_current_revision() ) {
-			$skip = 'no' === get_post_meta( $source_cite_css_post['ID'], 'custom_css_add', true );
-		}
+		$skip = Jetpack_Custom_CSS_Enhancements::skip_stylesheet();
 
 		restore_current_blog();
+		add_filter( 'safecss_skip_stylesheet', array( $this, 'preview_skip_stylesheet'  ), 15 );
 
 		return $skip;
 	}
@@ -133,7 +96,7 @@ class Source_Site_ID_Setting extends \WP_Customize_Setting {
 			$remote_css = str_replace(
 				array( '\\\00BB \\\0020', '\0BB \020', '0BB 020' ),
 				'\00BB \0020',
-				$post->post_content_filtered
+				$post->post_content
 			);
 		}
 
@@ -153,11 +116,7 @@ class Source_Site_ID_Setting extends \WP_Customize_Setting {
 	protected function update( $source_site_id ) {
 		switch_to_blog( $source_site_id );
 
-		if ( ! $source_cite_css_post = \Jetpack_Custom_CSS::get_current_revision() ) {
-			restore_current_blog();
-			return;
-		}
-
+		$source_site_theme_mods = get_theme_mod( 'jetpack_custom_css' );
 		$source_site_css = '';
 
 		// Add Remote CSS first
@@ -173,30 +132,32 @@ class Source_Site_ID_Setting extends \WP_Customize_Setting {
 			);
 		}
 
-		// Add Jetpack Custom CSS second
-		if ( $custom_css = \Jetpack_Custom_CSS::get_css( false ) ) {
+		/*
+		 * Add Core Custom CSS second to maintain correct cascading order.
+		 *
+		 * Processed CSS and vanilla CSS are stored in `post_content`, but pre-processed CSS is stored in
+		 * `post_content_filtered`.
+		 */
+		if ( $custom_css_post = wp_get_custom_css_post() ) {
+			if ( empty( $source_site_theme_mods['preprocessor'] ) ) {
+				$custom_css = $custom_css_post->post_content;
+			} else {
+				$custom_css = $custom_css_post->post_content_filtered;
+			}
+
 			$source_site_css .= sprintf(
 				"/* %s */\n%s\n\n",
 				sprintf(
 					esc_html__( 'Custom CSS from %s', 'wordcamporg' ),
-					get_bloginfo( 'name' )
+					parse_url( home_url(), PHP_URL_HOST )
 				),
 				$custom_css
 			);
 		}
 
-		$source_site_preprocessor  = get_post_meta( $source_cite_css_post['ID'], 'custom_css_preprocessor', true );
-		$source_site_mode          = get_post_meta( $source_cite_css_post['ID'], 'custom_css_add', true );
-		$source_site_content_width = get_post_meta( $source_cite_css_post['ID'], 'content_width', true );
-
 		restore_current_blog();
 
-		\Jetpack_Custom_CSS::save( array(
-			'css'             => $source_site_css,
-			'is_preview'      => false,
-			'preprocessor'    => $source_site_preprocessor,
-			'add_to_existing' => 'yes' === $source_site_mode ? true : false,
-			'content_width'   => $source_site_content_width,
-		) );
+		wp_update_custom_css_post( $source_site_css );
+		set_theme_mod( 'jetpack_custom_css', $source_site_theme_mods );
 	}
 }

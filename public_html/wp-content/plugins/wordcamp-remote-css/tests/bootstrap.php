f72eebd4c698443fdc9dcbe016a17f522a2992c1
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2016-12-30 19:18:35 +0000

    WordCamp Remote CSS: Remove unneeded dependencies from test bootstrap
    Jetpack 4.4.2 modularized the sanitization function, so we no longer need to include all of Jetpack, making this more of a pure unit test.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@4601 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/plugins/wordcamp-remote-css/tests/bootstrap.php
+++ public_html/wp-content/plugins/wordcamp-remote-css/tests/bootstrap.php
@@ -16,6 +16,7 @@ if ( ! $core_tests_directory ) {
 }
 
 require_once( $core_tests_directory . '/includes/functions.php' );
+require_once( dirname( dirname( $core_tests_directory ) ) . '/src/wp-admin/includes/plugin.php' );
 
 /**
  * Load the plugins that we'll need to be active for the tests
@@ -26,14 +27,17 @@ function manually_load_plugin() {
 	/*
 	 * Defining WP_ADMIN is so that wordcamp-remote-css/bootstrap.php will load the app/*.php files.
 	 * It may need to be refactored if we add tests for output-cached-css.php.
-	 * We shouldn't really be using PHPUnit for functional tests, though, so it'd be better to switch those tests
-	 * to Codeception or Behat.
 	 */
  	define( 'WP_ADMIN',          true );
 	define( 'JETPACK_DEV_DEBUG', true );
+	define( 'JETPACK__PLUGIN_DIR', dirname( dirname( __DIR__ ) ) . '/jetpack' );
+
+	$jetpack_plugin_data = get_plugin_data( JETPACK__PLUGIN_DIR . '/jetpack.php', false );
+
+	define( 'JETPACK__VERSION', $jetpack_plugin_data['Version'] );
 
 	require_once( dirname(          __DIR__ )   . '/bootstrap.php'       );
-	require_once( dirname( dirname( __DIR__ ) ) . '/jetpack/jetpack.php' );
+	require_once( JETPACK__PLUGIN_DIR . '/modules/custom-css/custom-css-4.7.php' );
 
 	// Some of the sanitization lives here because it runs for both Custom CSS and Remote CSS
 	require_once( dirname( dirname( dirname( __DIR__ ) ) ) . '/mu-plugins/jetpack-tweaks/css-sanitization.php' );
@@ -41,5 +45,3 @@ function manually_load_plugin() {
 tests_add_filter( 'muplugins_loaded', __NAMESPACE__ . '\manually_load_plugin' );
 
 require( $core_tests_directory . '/includes/bootstrap.php' );
-
-\Jetpack::activate_module( 'custom-css', false, false );

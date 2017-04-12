2c9aa319c57d8fda84cb7be1d84e92e33b32389b
iandunn <iandunn@git.wordpress.org>
2017-04-12 17:31:50 +0000

    Jetpack Tweaks: Disable irrelevant and intrusive just-in-time messages.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@5323 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/mu-plugins/jetpack-tweaks/miscellaneous.php
+++ public_html/wp-content/mu-plugins/jetpack-tweaks/miscellaneous.php
@@ -3,5 +3,22 @@
 namespace WordCamp\Jetpack_Tweaks;
 defined( 'WPINC' ) or die();
 
+add_action( 'plugins_loaded', __NAMESPACE__ . '\modify_hooks_after_jetpack_init' );
+
+// Allow Photon to fetch images that are served via HTTPS
 add_filter( 'jetpack_photon_reject_https',    '__return_false' );
+
+// Disable Snow to avoid performance and accessibility problems
 add_filter( 'jetpack_is_holiday_snow_season', '__return_false' );
+
+/**
+ * Modify hooks after Jetpack::init()
+ */
+function modify_hooks_after_jetpack_init() {
+	/*
+	 * Many of these messages are just marketing upsells for services we don't need, don't want, or have
+	 * alternatives for already. Those messages aren't relevant to organizers, so they just intrude on their work
+	 * unnecessarily.
+	 */
+	add_filter( 'jetpack_just_in_time_msgs', '__return_false' );
+}

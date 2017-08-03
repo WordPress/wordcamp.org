2c66474e09c70262b93e2b62f8c5fed89e741a74
coreymckrill <coreymckrill@git.wordpress.org>
2017-08-03 01:11:02 +0000

    WordCamp Post Types: Add Sponsor Agreement metabox
    This adds a metabox to the Edit Sponsor screen for uploading a signed,
    dated sponsor agreement as a PDF or image file.
    
    In this commit it is only enabled on development environments and
    testing sites.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@5735 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/plugins/wc-post-types/js/wcb-spon.js
@@ -0,0 +1,130 @@
+/* global jQuery, Backbone, wp, wcbSponsors */
+
+( function( $ ) {
+
+	'use strict';
+
+	wcbSponsors.view    = wcbSponsors.view    || {};
+	wcbSponsors.metabox = wcbSponsors.metabox || {};
+
+	wcbSponsors.view.Agreement = Backbone.View.extend( {
+		/**
+		 * Set up the metabox view.
+		 *
+		 * @returns {wcbSponsors.view.Agreement}
+		 */
+		initialize: function() {
+			var self = this;
+
+			this.$input = $( '#sponsor-agreement-id' );
+
+			this.frame = wp.media( {
+				id: 'sponsor-agreement-media-modal',
+				title: wcbSponsors.l10n.modalTitle,
+				multiple: false,
+				library: {
+					type: wcbSponsors.modal.allowedTypes
+				}
+			} );
+
+			this.frame.on( 'select', function() {
+				var attachment = self.frame.state().get( 'selection' ).first().toJSON();
+
+				self.$input.val( attachment.id );
+				self.elements.$view.find( 'a' ).first().attr( 'href', attachment.url );
+				self.toggleElements();
+			} );
+
+			this.toggleElements();
+
+			return this;
+		},
+
+		/**
+		 * Cache metabox elements.
+		 */
+		elements: {
+			$description: $( '#sponsor-agreement-description-container' ),
+			$upload     : $( '#sponsor-agreement-upload-container' ),
+			$view       : $( '#sponsor-agreement-view-container' ),
+			$remove     : $( '#sponsor-agreement-remove-container' )
+		},
+
+		/**
+		 * Bind events.
+		 */
+		events: {
+			'click #sponsor-agreement-upload': 'upload',
+			//'click #sponsor-agreement-view'  : 'view',
+			'click #sponsor-agreement-remove': 'remove'
+		},
+
+		/**
+		 * Toggle the visibility of metabox elements based on the value of the hidden field.
+		 *
+		 * @returns {wcbSponsors.view.Agreement}
+		 */
+		toggleElements: function() {
+			var agreementId         = this.$input.val(),
+				$uiWithoutAgreement = this.elements.$upload.add( this.elements.$description ),
+				$uiWithAgreement    = this.elements.$view.add( this.elements.$remove );
+
+			if ( agreementId ) {
+				$uiWithoutAgreement.hide();
+				$uiWithAgreement.show();
+			} else {
+				$uiWithoutAgreement.show();
+				$uiWithAgreement.hide();
+			}
+
+			return this;
+		},
+
+		/**
+		 * Callback for the Upload button.
+		 *
+		 * @param event
+		 * @returns {wcbSponsors.view.Agreement}
+		 */
+		upload: function( event ) {
+			event.preventDefault();
+
+			this.frame.open();
+
+			return this;
+		},
+
+		/**
+		 * Callback for the View button.
+		 *
+		 * @todo Show the agreement file in a modal window instead of opening a new tab
+		 *
+		view: function( event ) {
+			event.preventDefault();
+
+			// ...
+
+			return this;
+		},
+		*/
+
+		/**
+		 * Callback for the Remove button.
+		 *
+		 * @param event
+		 * @returns {wcbSponsors.view.Agreement}
+		 */
+		remove: function( event ) {
+			event.preventDefault();
+
+			this.$input.val( '' );
+			this.elements.$view.find( 'a' ).first().attr( 'href', '#' );
+			this.toggleElements();
+
+			return this;
+		}
+	} );
+
+	wcbSponsors.metabox.agreement = new wcbSponsors.view.Agreement( { el: '#sponsor-agreement' } );
+
+} )( jQuery );

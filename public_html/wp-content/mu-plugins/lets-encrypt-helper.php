a37714003f696c9dc70ead5a57d6545ab8ca318d
coreymckrill <coreymckrill@git.wordpress.org>
2018-01-09 18:42:45 +0000

    WordCamp SSL: Add 2017.cusco to domain list
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@6329 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/mu-plugins/lets-encrypt-helper.php
+++ public_html/wp-content/mu-plugins/lets-encrypt-helper.php
@@ -88,6 +88,7 @@ class WordCamp_Lets_Encrypt_Helper {
 		$domains[] = 'wordcampsf.com';
 		$domains[] = '2017.zilina.wordcamp.org';
 		$domains[] = '2017.niagara.wordcamp.org';
+		$domains[] = '2017.cusco.wordcamp.org';
 		$domains[] = '2018.wurzburg.wordcamp.org';
 
 		$domains = array_unique( $domains );

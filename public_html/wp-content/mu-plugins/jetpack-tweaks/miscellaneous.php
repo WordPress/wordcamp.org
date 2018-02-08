0e8d0240a2d157cbf54ca1e93768232e6f36a7ee
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2018-02-08 23:28:28 +0000

    Jetpack Tweaks: Move `wcorg_grunion_unique_subject` into Jetpack Tweaks.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@6573 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/mu-plugins/jetpack-tweaks/miscellaneous.php
+++ public_html/wp-content/mu-plugins/jetpack-tweaks/miscellaneous.php
@@ -41,3 +41,21 @@ function add_post_types_to_rest_api( $allowed_types ) {
 }
 
 add_filter( 'rest_api_allowed_post_types', __NAMESPACE__ . '\add_post_types_to_rest_api' );
+
+/**
+ * Prepend a unique string to contact form subjects.
+ *
+ * Otherwise some e-mail clients and management systems -- *cough* SupportPress *cough* -- will incorrectly group
+ * separate messages into the same thread.
+ *
+ * It'd be better to have the key appended rather than prepended, but SupportPress won't always recognize the
+ * subject as unique if we do that :|
+ *
+ * @param string $subject
+ *
+ * @return string
+ */
+function grunion_unique_subject( $subject ) {
+	return sprintf( '[%s] %s', wp_generate_password( 8, false ), $subject );
+}
+add_filter( 'contact_form_subject', __NAMESPACE__ . '\grunion_unique_subject' );

e4e70bfd503cfea89edc95023e1a0746ee29d880
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2018-02-12 17:35:19 +0000

    WordCamp Helpers: Break `wordcamp` post type helpers into separate file.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@6599 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/mu-plugins/helpers-misc.php
+++ public_html/wp-content/mu-plugins/helpers-misc.php
@@ -1,110 +1,12 @@
 <?php
 
+defined( 'WPINC' ) or die();
 use WordCamp\Logger;
 
-/**
- * Retrieves the `wordcamp` post and postmeta associated with the current site.
- *
- * `Site ID` is the most reliable way to associate a site with it's corresponding `wordcamp` post,
- * but wasn't historically assigned when new sites are created. For older sites, we fallback to
- * using the `URL` to associate them. That will only work if the site's site_url() exactly
- * matches the `wordcamp` post's `URL` meta field, though. It could also fail if we ever migrate
- * to a different URL structure.
- *
- * @return false|WP_Post
+/*
+ * Miscellaneous helper functions.
  */
-function get_wordcamp_post() {
-	$current_site_id  = get_current_blog_id();
-	$current_site_url = site_url();
 
-	switch_to_blog( BLOG_ID_CURRENT_SITE ); // central.wordcamp.org
-
-	$wordcamp = get_posts( array(
-		'post_type'   => 'wordcamp',
-		'post_status' => 'any',
-
-		'meta_query' => array(
-			'relation'  => 'OR',
-
-			array(
-				'key'   => '_site_id',
-				'value' => $current_site_id,
-			),
-
-			array(
-				'key'   => 'URL',
-				'value' => $current_site_url,
-			),
-		),
-	) );
-
-	if ( isset( $wordcamp[0]->ID ) ) {
-		$wordcamp = $wordcamp[0];
-		$wordcamp->meta = get_post_custom( $wordcamp->ID );
-	} else {
-		$wordcamp = false;
-	}
-
-	restore_current_blog();
-
-	return $wordcamp;
-}
-
-/**
- * Find the site that corresponds to the given `wordcamp` post
- *
- * @param WP_Post $wordcamp_post
- *
- * @return mixed An integer if successful, or boolean false if failed
- */
-function get_wordcamp_site_id( $wordcamp_post ) {
-	switch_to_blog( BLOG_ID_CURRENT_SITE ); // central.wordcamp.org
-
-	if ( ! $site_id = get_post_meta( $wordcamp_post->ID, '_site_id', true ) ) {
-		$url = parse_url( get_post_meta( $wordcamp_post->ID, 'URL', true ) );
-
-		if ( isset( $url['host'] ) && isset( $url['path'] ) ) {
-			if ( $site = get_site_by_path( $url['host'], $url['path'] ) ) {
-				$site_id = $site->blog_id;
-			}
-		}
-	}
-
-	restore_current_blog();
-
-	return $site_id;
-}
-
-/**
- * Get a consistent WordCamp name in the 'WordCamp [Location] [Year]' format.
- *
- * The results of bloginfo( 'name' ) don't always contain the year, but the title of the site's corresponding
- * `wordcamp` post is usually named 'WordCamp [Location]', so we can get a consistent name most of the time
- * by using that and adding the year (if available).
- *
- * @param int $site_id Optionally, get the name for a site other than the current one.
- *
- * @return string
- */
-function get_wordcamp_name( $site_id = 0 ) {
-	$name = false;
-
-	switch_to_blog( $site_id );
-
-	if ( $wordcamp = get_wordcamp_post() ) {
-		if ( ! empty( $wordcamp->meta['Start Date (YYYY-mm-dd)'][0] ) ) {
-			$name = $wordcamp->post_title .' '. date( 'Y', $wordcamp->meta['Start Date (YYYY-mm-dd)'][0] );
-		}
-	}
-
-	if ( ! $name ) {
-		$name = get_bloginfo( 'name' );
-	}
-
-	restore_current_blog();
-
-	return $name;
-}
 
 /**
  * Determine if a specific feature should be skipped on the current site
@@ -145,42 +47,6 @@ function wcorg_get_user_by_canonical_names( $name ) {
 	}
 
 	return $user;
-}
-
-/**
- * Extract pieces from a WordCamp.org URL
- *
- * @todo find other code that's doing this same task in an ad-hoc manner, and convert it to use this instead
- *
- * @param string $url
- * @param string $part 'city', 'city-domain' (without the year, e.g. seattle.wordcamp.org), 'year'
- *
- * @return false|string|int False on errors; an integer for years; a string for city and city-domain
- */
-function wcorg_get_url_part( $url, $part ) {
-	$url_parts = explode( '.', parse_url( $url, PHP_URL_HOST ) );
-	$result    = false;
-
-	// Make sure it matches the typical year.city.wordcamp.org structure
-	if ( 4 !== count( $url_parts ) ) {
-		return $result;
-	}
-
-	switch( $part ) {
-		case 'city':
-			$result = $url_parts[1];
-			break;
-
-		case 'city-domain':
-			$result = ltrim( strstr( $url, '.' ), '.' );
-			break;
-
-		case 'year':
-			$result = absint( $url_parts[0] );
-			break;
-	}
-
-	return $result;
 }
 
 /**
@@ -239,32 +105,6 @@ function wcorg_redundant_remote_get( $request_url, $request_args = array() ) {
 	}
 
 	return $response;
-}
-
-/**
- * Take the start and end dates for a WordCamp and calculate how many days it lasts.
- *
- * @param WP_Post $wordcamp
- *
- * @return int
- */
-function wcord_get_wordcamp_duration( WP_Post $wordcamp ) {
-	// @todo Make sure $wordcamp is the correct post type
-
-	$start = get_post_meta( $wordcamp->ID, 'Start Date (YYYY-mm-dd)', true );
-	$end   = get_post_meta( $wordcamp->ID, 'End Date (YYYY-mm-dd)', true );
-
-	// Assume 1 day duration if there is no end date
-	if ( ! $end ) {
-		return 1;
-	}
-
-	$duration_raw = $end - $start;
-
-	// Add one second and round up to ensure the end date counts as a day as well
-	$duration_days = ceil( ( $duration_raw + 1 ) / DAY_IN_SECONDS );
-
-	return absint( $duration_days );
 }
 
 /**

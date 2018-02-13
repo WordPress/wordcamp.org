8c962c2a5e754e8b2d26011f5e54bbb41c8e5e93
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2018-02-13 20:13:57 +0000

    WordCamp Post Types: Replace required field indicator with helper function.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@6624 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/plugins/wc-post-types/views/common/form-field-required-indicator.php
+++
@@ -1,9 +0,0 @@
-
-<span class="wcpt-form-required" aria-hidden="true">
-	<?php // translators: The symbol to indicate the form field is required ?>
-	<?php _e( '*', 'wordcamporg' ); ?>
-</span>
-
-<span class="screen-reader-text">
-	<?php _e( 'required field', 'wordcamporg' ); ?>
-</span>

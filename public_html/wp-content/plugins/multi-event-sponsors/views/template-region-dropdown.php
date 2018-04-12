392c7699b52927e76d4a7d4bc19577f1a97619b9
coreymckrill <coreymckrill@74240141-8908-4e6f-9713-ba540dce6ec7>
2018-04-12 00:19:34 +0000

    WordCamp: Require special caps in order to change a WordCamp's sponsor region
    The sponsor region is determined and assigned by a WPCS team member, but
    sometimes the camp organizer doesn't know what the field is for and changes it.
    This helps prevent unintentional changes.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@7098 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/plugins/multi-event-sponsors/views/template-region-dropdown.php
+++ public_html/wp-content/plugins/multi-event-sponsors/views/template-region-dropdown.php
@@ -3,20 +3,24 @@
 <?php /** @var $selected_region int    */ ?>
 <?php /** @var $cb_push_name    string */ ?>
 <?php /** @var $site_id         int    */ ?>
+<?php /** @var $protected       bool   */ ?>
 
 <select id="<?php echo esc_attr( $field_name ); ?>" name="<?php echo esc_attr( $field_name ); ?>">
-	<option value="">None</option>
-
-	<?php foreach ( $regions as $region ) : ?>
-
-		<option value="<?php echo esc_attr( $region->term_id ); ?>" <?php selected( $selected_region, $region->term_id ); ?>>
-			<?php echo esc_html( $region->name ); ?>
+	<?php if ( ! $protected ) : ?>
+		<option value="">None</option>
+		<?php foreach ( $regions as $region ) : ?>
+			<option value="<?php echo esc_attr( $region->term_id ); ?>" <?php selected( $selected_region, $region->term_id ); ?>>
+				<?php echo esc_html( $region->name ); ?>
+			</option>
+		<?php endforeach; ?>
+	<?php else : ?>
+		<option value="<?php echo esc_attr( $selected_region ); ?>" selected>
+			<?php echo esc_html( get_term( $selected_region, MES_Region::TAXONOMY_SLUG )->name ); ?>
 		</option>
-
-	<?php endforeach; ?>
+	<?php endif; ?>
 </select>
 
-<?php if ( $site_id ) : ?>
+<?php if ( $site_id && ! $protected ) : ?>
 	<label>
 		<input type="checkbox" name="<?php echo esc_attr( $cb_push_name ); ?>" value="1" />
 		Push sponsors to site

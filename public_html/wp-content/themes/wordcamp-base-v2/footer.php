73321f6c504f544a3ea6fc32191caf41a518a58e
iandunn <iandunn@74240141-8908-4e6f-9713-ba540dce6ec7>
2018-04-25 18:13:22 +0000

    WordCamp Themes: Apply coding standards.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@7163 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/themes/wordcamp-base-v2/footer.php
+++ public_html/wp-content/themes/wordcamp-base-v2/footer.php
@@ -7,33 +7,46 @@
  * @package WCBS
  * @since WCBS 1.0
  */
+
 ?>
 
-	</div><!-- #main -->
+	</div> <!-- #main -->
 
 	<footer id="colophon" class="site-footer" role="contentinfo">
-	
-		<?php // Footer Widget Areas ?>
+
 		<div id="footer-widgets">
 			<?php foreach ( range( 1, 5 ) as $index ) : ?>
-				<?php if ( ! is_active_sidebar( 'footer-' . $index ) ) continue; ?>
-				<div id="footer-widget-<?php echo $index; ?>" class="footer-widgets-block">
-					<?php dynamic_sidebar( 'footer-' . $index ) ; ?>
+				<?php
+
+				if ( ! is_active_sidebar( 'footer-' . $index ) ) {
+					continue;
+				}
+
+				?>
+
+				<div id="footer-widget-<?php echo esc_attr( $index ); ?>" class="footer-widgets-block">
+					<?php dynamic_sidebar( 'footer-' . $index ); ?>
 				</div>
 			<?php endforeach; ?>
-		</div><!-- #footer-widgets -->
+		</div>
 
 		<div class="site-info">
-
 			<?php do_action( 'wcbs_credits' ); ?>
 
-			<a class="site-info-generator" href="http://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'wordcamporg' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s', 'wordcamporg' ), 'WordPress' ); ?></a>
+			<a class="site-info-generator" href="https://wordpress.org/" title="<?php esc_attr_e( 'A Semantic Personal Publishing Platform', 'wordcamporg' ); ?>" rel="generator">
+				<?php
+				// translators: %s is "WordPress".
+				printf( esc_html__( 'Proudly powered by %s', 'wordcamporg' ), 'WordPress' );
+				?>
+			</a>
 
-			<a class="site-info-network" href="http://central.wordcamp.org/" title="<?php esc_attr_e( 'Return to WordCamp Central', 'wordcamporg' ); ?>"><?php _e('Go to WordCamp Central', 'wordcamporg'); ?></a>
-			
-		</div><!-- .site-info -->
-	</footer><!-- .site-footer .site-footer -->
-</div><!-- #page .hfeed .site -->
+			<a class="site-info-network" href="https://central.wordcamp.org/">
+				<?php esc_html_e( 'Go to WordCamp Central', 'wordcamporg' ); ?>
+			</a>
+
+		</div>
+	</footer>
+</div> <!-- #page -->
 
 <?php wp_footer(); ?>
 

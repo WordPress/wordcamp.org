bdaa08ab02b0c395ec29be8126a292a89ce2797c
iandunn <iandunn@git.wordpress.org>
2018-04-25 18:13:22 +0000

    WordCamp Themes: Apply coding standards.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@7163 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/themes/campsite-2017/footer.php
+++ public_html/wp-content/themes/campsite-2017/footer.php
@@ -24,7 +24,7 @@ namespace WordCamp\CampSite_2017;
 					<?php echo esc_html( sprintf( __( 'Proudly powered by %s', 'wordcamporg' ), 'WordPress' ) ); ?>
 				</a>
 
-				<a class="site-info-network" href="http://central.wordcamp.org/">
+				<a class="site-info-network" href="https://central.wordcamp.org/">
 					<?php esc_html_e( 'Go to WordCamp Central', 'wordcamporg' ); ?>
 				</a>
 			</div>
--- public_html/wp-content/themes/wordcamp-base/lib/structure/class-wcb-footer.php
+++ public_html/wp-content/themes/wordcamp-base/lib/structure/class-wcb-footer.php
@@ -1,36 +1,44 @@
 <?php
 
 class WCB_Footer extends WCB_Element {
-	function get_id() {
+	/**
+	 * Get the ID of the element.
+	 *
+	 * @return string
+	 */
+	public function get_id() {
 		return 'footer';
 	}
 
-	function content() { ?>
+	/**
+	 * Render the content of the element.
+	 */
+	public function content() {
+		?>
+
 		<div class="grid_12">
-		<div id="<?php echo $this->get_id(); ?>" role="contentinfo">
-			<div id="colophon">
-				<div id="site-info">
-					<a href="<?php echo home_url( '/' ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
-						<?php bloginfo( 'name' ); ?>
-					</a>
-				</div><!-- #site-info -->
+			<div id="<?php echo esc_attr( $this->get_id() ); ?>" role="contentinfo">
+				<div id="colophon">
+					<div id="site-info">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home">
+							<?php bloginfo( 'name' ); ?>
+						</a>
+					</div>
 
-				<div id="site-generator">
-					<?php do_action( 'twentyten_credits' ); ?>
-					<a href="<?php echo esc_url( __( 'http://wordpress.org/', 'wordcamporg' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'wordcamporg' ); ?>" rel="generator"><?php printf( __( 'Proudly powered by %s.', 'wordcamporg' ), 'WordPress' ); ?></a>
-				</div><!-- #site-generator -->
+					<div id="site-generator">
+						<?php do_action( 'twentyten_credits' ); ?>
 
-			</div><!-- #colophon -->
-		</div><!-- #footer -->
+						<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'wordcamporg' ) ); ?>" title="<?php esc_attr_e( 'Semantic Personal Publishing Platform', 'wordcamporg' ); ?>" rel="generator">
+							<?php
+							// translators: %s is "WordPress".
+							printf( esc_html__( 'Proudly powered by %s.', 'wordcamporg' ), 'WordPress' );
+							?>
+						</a>
+					</div>
+				</div>
+			</div>
 		</div>
-		<?php
-			/* Always have wp_footer() just before the closing </body>
-			 * tag of your theme, or you will break many plugins, which
-			 * generally use this hook to reference JavaScript files.
-			 */
 
-			wp_footer();
+		<?php wp_footer();
 	}
 }
-
-?>

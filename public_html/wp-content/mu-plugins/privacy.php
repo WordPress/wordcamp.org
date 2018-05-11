71d563ede9aebf8b0bb16d550b93ef26716f0b1d
coreymckrill <coreymckrill@74240141-8908-4e6f-9713-ba540dce6ec7>
2018-05-11 20:31:59 +0000

    WordCamp: Temporarily disable the privacy tools in 4.9.6
    At this point the privacy tools in Core are only designed for single site
    installations. Using them on a large multisite network like WordCamp.org could
    have negative unintended consequences, especially since the user table is
    shared across all of the sites. Once we have a system in place for handling
    privacy requests on a network-wide basis, we can re-enable the tools.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@7193 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/mu-plugins/privacy.php
+++ public_html/wp-content/mu-plugins/privacy.php
@@ -5,6 +5,7 @@ namespace WordCamp\Privacy;
 defined( 'WPINC' ) || die();
 
 add_filter( 'privacy_policy_url', __NAMESPACE__ . '\set_privacy_policy_url', 10 );
+add_filter( 'map_meta_cap', __NAMESPACE__ . '\disable_496_privacy_tools', 10, 4 );
 
 /**
  * Set a consistent Privacy Policy across all sites.
@@ -15,4 +16,27 @@ add_filter( 'privacy_policy_url', __NAMESPACE__ . '\set_privacy_policy_url', 10 
  */
 function set_privacy_policy_url( $url ) {
 	return 'https://wordpress.org/about/privacy/';
+}
+
+/**
+ * Disable the privacy tools added in WordPress 4.9.6.
+ *
+ * Note that this is temporary until we have a system in place for handling privacy-related requests
+ * on a network-wide basis.
+ *
+ * @param array  $required_capabilities The primitive capabilities that are required to perform the requested meta capability.
+ * @param string $requested_capability  The requested meta capability
+ * @param int    $user_id               The user ID.
+ * @param array  $args                  Adds the context to the cap. Typically the object ID.
+ *
+ * @return array The primitive capabilities that are required to perform the requested meta capability.
+ */
+function disable_496_privacy_tools( $required_capabilities, $requested_capability, $user_id, $args ) {
+	$privacy_capabilities = array( 'manage_privacy_options', 'erase_others_personal_data', 'export_others_personal_data' );
+
+	if ( in_array( $requested_capability, $privacy_capabilities ) ) {
+		$required_capabilities[] = 'do_not_allow';
+	}
+
+	return $required_capabilities;
 }

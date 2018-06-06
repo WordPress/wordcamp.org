534fa6cac10fd0c735c5a5b89f8aa3afa6fd7cad
coreymckrill <coreymckrill@74240141-8908-4e6f-9713-ba540dce6ec7>
2018-06-06 00:08:53 +0000

    WordCamp Utilities: Add oxr as current exchange rate source
    Fixer has a new API that does not meet our needs with the free plan.
    Open Exchange Rates has a very similar API that seems to do everything we need.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@7271 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/mu-plugins/utilities/class-currency-xrt-client.php
+++ public_html/wp-content/mu-plugins/utilities/class-currency-xrt-client.php
@@ -7,7 +7,8 @@ defined( 'WPINC' ) || die();
  * Class Currency_XRT_Client
  *
  * Get historical exchange rates for the major currencies. Designed to be able to use different API sources for
- * the exchange rate data. Initially built using the Fixer API (fixer.io). "XRT" = "exchange rate".
+ * the exchange rate data. Initially built using the Fixer API (fixer.io). Now includes Open Exchange Rates.
+ * "XRT" = "exchange rate".
  */
 class Currency_XRT_Client {
 	/**
@@ -31,6 +32,11 @@ class Currency_XRT_Client {
 	protected $api_base = '';
 
 	/**
+	 * @var string The key for accessing the source's API.
+	 */
+	protected $api_key = '';
+
+	/**
 	 * @var array Cache of exchange rates by date for reuse.
 	 */
 	protected $cache = array();
@@ -39,14 +45,15 @@ class Currency_XRT_Client {
 	 * Currency_XRT_Client constructor.
 	 *
 	 * @param string $base_currency Optional. Currency symbol for the base currency. Default 'USD'.
-	 * @param string $source        Optional. Identifier for the exchange rates source. Default 'fixer'.
+	 * @param string $source        Optional. Identifier for the exchange rates source. Default 'oxr'.
 	 */
-	public function __construct( $base_currency = 'USD', $source = 'fixer' ) {
+	public function __construct( $base_currency = 'USD', $source = 'oxr' ) {
 		$this->error = new \WP_Error();
 
 		$this->base_currency = $base_currency;
 		$this->source        = $source;
 		$this->api_base      = $this->get_api_base( $this->source );
+		$this->api_key       = $this->get_api_key( $this->source );
 	}
 
 	/**
@@ -61,12 +68,40 @@ class Currency_XRT_Client {
 
 		switch ( $source ) {
 			case 'fixer' :
-			default :
-				$base_url = 'https://api.fixer.io/';
+				$base_url = 'https://data.fixer.io/api/';
+				break;
+			case 'oxr' :
+				$base_url = 'https://openexchangerates.org/api/';
 				break;
 		}
 
 		return trailingslashit( $base_url );
+	}
+
+	/**
+	 * Get the API key based on the given source.
+	 *
+	 * @param string $source
+	 *
+	 * @return string The API key.
+	 */
+	protected function get_api_key( $source ) {
+		$key = '';
+
+		switch ( $source ) {
+			case 'fixer' :
+				if ( defined( 'WORDCAMP_FIXER_API_KEY' ) ) {
+					$key = WORDCAMP_FIXER_API_KEY;
+				}
+				break;
+			case 'oxr' :
+				if ( defined( 'WORDCAMP_OXR_API_KEY' ) ) {
+					$key = WORDCAMP_OXR_API_KEY;
+				}
+				break;
+		}
+
+		return $key;
 	}
 
 	/**
@@ -78,7 +113,7 @@ class Currency_XRT_Client {
 	 */
 	public function get_rates( $date ) {
 		$rates = array();
-		$cache_key = 'wc_currency_rates_' . strtotime( $date );
+		$cache_key = 'wc_currency_rates_' . $this->source . '_' . strtotime( $date );
 
 		try {
 			$date = new \DateTime( $date );
@@ -96,8 +131,19 @@ class Currency_XRT_Client {
 
 		switch ( $this->source ) {
 			case 'fixer' :
-			default :
 				$rates = $this->send_fixer_request( $date );
+				break;
+			case 'oxr' :
+				$rates = $this->send_oxr_request( $date );
+				break;
+			default :
+				$rates = new \WP_Error(
+					'invalid_xrt_source',
+					sprintf(
+						'%s is not a valid currency exchange rate source.',
+						esc_html( $this->source )
+					)
+				);
 				break;
 		}
 
@@ -177,9 +223,20 @@ class Currency_XRT_Client {
 	protected function send_fixer_request( \DateTime $date ) {
 		$data = array();
 
-		$request_url = add_query_arg( array(
-			'base' => $this->base_currency,
-		), $this->api_base . $date->format( 'Y-m-d' ) );
+		$api_endpoint = $date->format( 'Y-m-d' );
+		$now = new \DateTime();
+
+		if ( $date->format( 'Y-m-d' ) === $now->format( 'Y-m-d' ) || $date > $now ) {
+			$api_endpoint = 'latest';
+		}
+
+		$request_url = add_query_arg(
+			array(
+				'access_key' => $this->api_key,
+				'base'       => $this->base_currency,
+			),
+			esc_url( $this->api_base . $api_endpoint )
+		);
 
 		$response      = wcorg_redundant_remote_get( $request_url );
 		$response_code = wp_remote_retrieve_response_code( $response );
@@ -191,7 +248,11 @@ class Currency_XRT_Client {
 			} elseif ( isset( $response_body['error'] ) ) {
 				$this->error->add(
 					'request_error',
-					$response_body['error']
+					sprintf(
+						'%s: %s',
+						$response_body['error']['code'],
+						$response_body['error']['info']
+					)
 				);
 			} else {
 				$this->error->add(
@@ -200,10 +261,80 @@ class Currency_XRT_Client {
 				);
 			}
 		} else {
-			$this->error->add(
-				'http_response_code',
-				$response_code . ': ' . print_r( $response_body, true )
-			);
+			if ( isset( $response_body['error'] ) ) {
+				$this->error->add(
+					'request_error',
+					sprintf(
+						'%s: %s',
+						$response_body['error']['code'],
+						$response_body['error']['info']
+					)
+				);
+			} else {
+				$this->error->add(
+					'http_response_code',
+					$response_code . ': ' . print_r( $response_body, true )
+				);
+			}
+		}
+
+		if ( ! empty( $this->error->get_error_messages() ) ) {
+			return $this->error;
+		}
+
+		return $data;
+	}
+
+	/**
+	 * Send a request to the Open Exchange Rates API and return the results.
+	 *
+	 * @param \DateTime $date The date to retrieve rates for.
+	 *
+	 * @return array|\WP_Error An array of rates, or an error.
+	 */
+	protected function send_oxr_request( \DateTime $date ) {
+		$data = array();
+
+		$api_endpoint = 'historical/' . $date->format( 'Y-m-d' );
+		$now = new \DateTime();
+
+		if ( $date->format( 'Y-m-d' ) === $now->format( 'Y-m-d' ) || $date > $now ) {
+			$api_endpoint = 'latest';
+		}
+
+		$request_url = add_query_arg(
+			array(
+				'app_id' => $this->api_key,
+				'base'   => $this->base_currency,
+			),
+			esc_url( sprintf( '%s%s.json', $this->api_base, $api_endpoint ) )
+		);
+
+		$response      = wcorg_redundant_remote_get( $request_url );
+		$response_code = wp_remote_retrieve_response_code( $response );
+		$response_body = json_decode( wp_remote_retrieve_body( $response ), true );
+
+		if ( 200 === $response_code ) {
+			if ( isset( $response_body['rates'] ) ) {
+				$data = $response_body['rates'];
+			} else {
+				$this->error->add(
+					'unexpected_response_data',
+					'The API response did not provide the expected data.'
+				);
+			}
+		} else {
+			if ( isset( $response_body['error'], $response_body['message'], $response_body['description'] ) ) {
+				$this->error->add(
+					esc_html( $response_body['message'] ),
+					esc_html( $response_body['description'] )
+				);
+			} else {
+				$this->error->add(
+					'http_response_code',
+					$response_code . ': ' . print_r( $response_body, true )
+				);
+			}
 		}
 
 		if ( ! empty( $this->error->get_error_messages() ) ) {

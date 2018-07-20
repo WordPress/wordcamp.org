2f09ef59876165819cffa291006b65f267e44724
coreymckrill <coreymckrill@74240141-8908-4e6f-9713-ba540dce6ec7>
2018-07-20 00:26:12 +0000

    WordCamp Gutenberg Tweaks: Avoid WP doing_it_wrong notice
    In r7476 a hotfix was added to address a script localization issue that
    Gutenberg was causing. However, the way the hotfix was implemented was
    Doing It Wrong because `wp_script_is` was being called prior to any of the
    script enqueue actions. Needless to say, with this commit, we're doing it
    less wrong enough to not see that notice anymore.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@7484 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/mu-plugins/gutenberg-tweaks.php
+++ public_html/wp-content/mu-plugins/gutenberg-tweaks.php
@@ -32,17 +32,25 @@ function load() {
 		add_action( 'admin_print_scripts-edit.php', __NAMESPACE__ . '\add_classic_param_to_add_new_button', 11    );
 		add_action( 'admin_print_scripts',          __NAMESPACE__ . '\add_classic_param_to_admin_menus',     1    );
 
-		// @todo Hotfix for Gutenberg 3.1-3.2 breaking wpApiSettings. This will hopefully be unnecessary with 3.3.
-		// See https://github.com/WordPress/gutenberg/issues/7488
-		// See https://github.com/WordPress/gutenberg/pull/7566
-		if ( ! wp_script_is( 'wp-api-fetch', 'registered' ) ) {
-			add_action( 'wp_enqueue_scripts', 'gutenberg_extend_wp_api_backbone_client' );
-			add_action( 'admin_enqueue_scripts', 'gutenberg_extend_wp_api_backbone_client' );
-		}
+		// @todo Remove after Gutenberg 3.3 is released.
+		add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\wpapi_hotfix', 99 );
+		add_action( 'admin_enqueue_scripts', __NAMESPACE__ . '\wpapi_hotfix', 99 );
 	}
 }
 
 add_action( 'plugins_loaded', __NAMESPACE__ . '\load' );
+
+/**
+ * @todo Hotfix for Gutenberg 3.1-3.2 breaking wpApiSettings. This will hopefully be unnecessary with 3.3.
+ *
+ * See https://github.com/WordPress/gutenberg/issues/7488
+ * See https://github.com/WordPress/gutenberg/pull/7566
+ */
+function wpapi_hotfix() {
+	if ( ! wp_script_is( 'wp-api-fetch', 'registered' ) ) {
+		gutenberg_extend_wp_api_backbone_client();
+	}
+}
 
 /**
  * Limit which post types are editable by Gutenberg

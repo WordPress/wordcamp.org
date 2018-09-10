27a8b813bcabae203cd9f71857fec7634f6d506b
vedjain <vedjain@git.wordpress.org>
2018-09-10 07:36:18 +0000

    WordCamp: Use same capability for WordCamp and Meetup event management.
    This will allow our deputies to edit Meetup Applications as well.
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@7649 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/mu-plugins/wcorg-subroles.php
+++ public_html/wp-content/mu-plugins/wcorg-subroles.php
@@ -6,6 +6,8 @@
  */
 
 namespace WordCamp\SubRoles;
+use WordPress_Community\Applications\Meetup_Application;
+
 defined( 'WPINC' ) || die();
 
 /**
@@ -149,6 +151,14 @@ function map_subrole_caps( $primitive_caps, $meta_cap, $user_id, $args ) {
 			}
 
 			if ( defined( 'WCPT_POST_TYPE_ID' ) && WCPT_POST_TYPE_ID === $post_type ) {
+				if ( $current_user && $current_user->has_cap( 'wordcamp_wrangle_wordcamps' ) ) {
+					$required_caps[] = 'wordcamp_wrangle_wordcamps';
+				}
+			}
+
+			if ( Meetup_Application::POST_TYPE === $post_type ) {
+				// Use same permission for meetups as well as wordcamps.
+				// TODO: In future consider changing this to wrangle_events
 				if ( $current_user && $current_user->has_cap( 'wordcamp_wrangle_wordcamps' ) ) {
 					$required_caps[] = 'wordcamp_wrangle_wordcamps';
 				}

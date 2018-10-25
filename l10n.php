3e37f143963b9b1b91089df97e1e1bccd0bcaf42
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2018-10-25 11:03:36 -0700

    Updates to package and built files
    Move things out of `dependencies`. It seems like they are not needed
    there, and could be causing script conflicts if they are being
    included in the bundled files? I'm not sure...

--- l10n.php
+++ l10n.php
@@ -1,44 +1,44 @@
 <?php
 /* THIS IS A GENERATED FILE. DO NOT EDIT DIRECTLY. */
 $generated_i18n_strings = array(
-	// Reference: assets/src/speakers/edit.jsx:106
-	// Reference: assets/src/speakers/index.js:55
+	// Reference: assets/src/speakers/edit.jsx:100
+	// Reference: assets/src/speakers/index.js:18
 	__( 'Speakers', 'wordcamporg' ),
 
-	// Reference: assets/src/speakers/edit.jsx:110
+	// Reference: assets/src/speakers/edit.jsx:104
 	__( 'No posts found.', 'wordcamporg' ),
 
-	// Reference: assets/src/speakers/edit.jsx:134
+	// Reference: assets/src/speakers/edit.jsx:128
 	__( '(Untitled)', 'wordcamporg' ),
 
-	// Reference: assets/src/speakers/edit.jsx:35
+	// Reference: assets/src/speakers/edit.jsx:29
 	__( 'Which Speakers?', 'wordcamporg' ),
 
-	// Reference: assets/src/speakers/edit.jsx:38
+	// Reference: assets/src/speakers/edit.jsx:32
 	__( 'Show all', 'wordcamporg' ),
 
-	// Reference: assets/src/speakers/edit.jsx:46
+	// Reference: assets/src/speakers/edit.jsx:40
 	__( 'Number to show', 'wordcamporg' ),
 
-	// Reference: assets/src/speakers/edit.jsx:58
+	// Reference: assets/src/speakers/edit.jsx:52
 	__( 'Sort by', 'wordcamporg' ),
 
-	// Reference: assets/src/speakers/edit.jsx:66
+	// Reference: assets/src/speakers/edit.jsx:60
 	__( 'Speaker Display', 'wordcamporg' ),
 
-	// Reference: assets/src/speakers/edit.jsx:69
+	// Reference: assets/src/speakers/edit.jsx:63
 	__( 'Link titles to single posts', 'wordcamporg' ),
 
-	// Reference: assets/src/speakers/edit.jsx:76
+	// Reference: assets/src/speakers/edit.jsx:70
 	__( 'Show avatars', 'wordcamporg' ),
 
-	// Reference: assets/src/speakers/edit.jsx:84
+	// Reference: assets/src/speakers/edit.jsx:78
 	__( 'Avatar size (px)', 'wordcamporg' ),
 
-	// Reference: assets/src/speakers/edit.jsx:85
+	// Reference: assets/src/speakers/edit.jsx:79
 	__( 'Height and width in pixels.', 'wordcamporg' ),
 
-	// Reference: assets/src/speakers/index.js:56
+	// Reference: assets/src/speakers/index.js:19
 	__( 'Add a list of speakers.', 'wordcamporg' )
 );
 /* THIS IS THE END OF THE GENERATED FILE */

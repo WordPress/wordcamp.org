40b009df23894d99a2560ed88c02cf6cea7eee83
Ian Dunn <ian@iandunn.name>
2018-10-26 14:19:27 -0700

    Remove references to missing `track` and `groups` attributes.
    Their client-side counterparts were removed in 05c5afdfbb76a34a58ec8a097e71c48f8022e03a, so these references just cause PHP notices now.

--- includes/speakers.php
+++ includes/speakers.php
@@ -63,14 +63,6 @@ function render( $attributes ) {
 		$attributes['speaker_link'] = 'permalink';
 	}
 
-	if ( is_array( $attributes['track'] ) ) {
-		$attributes['track'] = implode( ',', $attributes['track'] );
-	}
-
-	if ( is_array( $attributes['groups'] ) ) {
-		$attributes['groups'] = implode( ',', $attributes['groups'] );
-	}
-
 	return $wcpt_plugin->shortcode_speakers( $attributes, '' );
 }
 

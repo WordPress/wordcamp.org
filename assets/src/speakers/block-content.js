6cb93594e5b43456e3548c33a560797f4e07caf7
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2018-10-31 17:37:03 -0700

    ESLint code cleanup

--- assets/src/speakers/block-content.js
+++ assets/src/speakers/block-content.js
@@ -10,55 +10,50 @@ const { __ } = wp.i18n;
 const { addQueryArgs } = wp.url;
 
 class SpeakerBlockContent extends Component {
-
-	render(){
+	render() {
 		const { attributes, speakerPosts } = this.props;
 		const { speaker_link, show_avatars, avatar_size } = attributes;
 
 		return (
-			<div className={'wcorg-speakers'}>
-				{speakerPosts.map((post, i) =>
+			<div className={ 'wcorg-speakers' }>
+				{ speakerPosts.map( ( post, i ) =>
 					<div
-						key={i}
-						id={'wcorg-speaker-' + post.slug}
-						className={classnames(
-							'wcorg-speaker', 'wcorg-speaker-' + post.slug)}
+						key={ i }
+						id={ 'wcorg-speaker-' + post.slug }
+						className={ classnames( 'wcorg-speaker', 'wcorg-speaker-' + post.slug ) }
 					>
 						<h2>
-							{speaker_link ?
+							{ speaker_link ?
 								<Disabled>
-									<a href={post.link}>
-										{decodeEntities(
-											post.title.rendered.trim()) ||
-										__('(Untitled)', 'wordcamporg')}
+									<a href={ post.link }>
+										{
+											decodeEntities( post.title.rendered.trim() ) ||
+											__( '(Untitled)', 'wordcamporg' )
+										}
 									</a>
 								</Disabled> :
-								decodeEntities(post.title.rendered.trim()) ||
-								__('(Untitled)', 'wordcamporg')
+								decodeEntities( post.title.rendered.trim() ) || __( '(Untitled)', 'wordcamporg' )
 							}
 						</h2>
-						<div className={'wcorg-speaker-description'}>
-							{show_avatars &&
+						<div className={ 'wcorg-speaker-description' }>
+							{ show_avatars &&
 							/* todo Turn this into a component */
 							<img
-								className={classnames('avatar', 'avatar-' +
-									avatar_size, 'photo')}
-								src={addQueryArgs(post['avatar_urls']['24'],
-									{s: avatar_size})}
-								srcSet={addQueryArgs(post['avatar_urls']['24'],
-									{s: avatar_size * 2}) + ' 2x'}
-								width={avatar_size}
-								height={avatar_size}
+								className={ classnames( 'avatar', 'avatar-' + avatar_size, 'photo' ) }
+								src={ addQueryArgs( post[ 'avatar_urls' ][ '24' ], { s: avatar_size } ) }
+								srcSet={ addQueryArgs( post[ 'avatar_urls' ][ '24' ], { s: avatar_size * 2 } ) + ' 2x' }
+								width={ avatar_size }
+								height={ avatar_size }
 							/>
 							}
 							<Disabled>
 								<RawHTML>
-									{post.content.rendered.trim()}
+									{ post.content.rendered.trim() }
 								</RawHTML>
 							</Disabled>
 						</div>
 					</div>,
-				)}
+				) }
 			</div>
 		);
 	}

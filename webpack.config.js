bb02a7f5d9be2804f7978935827a97ae1b032e1d
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2018-11-13 15:43:44 -0800

    Update list of externals in webpack config
    Following 9683770e9084d0ebfba484825422a4a6c928372e, I decided to
    get all of our other externals like lodash and the Gutenberg packages
    added to our externals list as well. I copied the method in Gutenberg's
    webpack config for programmatically adding all of its packages to the
    externals object. This seems to fix #9 and may also have positive
    implications for #14

--- webpack.config.js
+++ webpack.config.js
@@ -5,6 +5,78 @@ const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
 
 const NODE_ENV = process.env.NODE_ENV || 'development';
 
+/**
+ * Given a string, returns a new string with dash separators converted to
+ * camelCase equivalent. This is not as aggressive as `_.camelCase` in
+ * converting to uppercase, where Lodash will also capitalize letters
+ * following numbers.
+ *
+ * @see https://github.com/WordPress/gutenberg/blob/master/webpack.config.js
+ *
+ * @param {string} string Input dash-delimited string.
+ *
+ * @return {string} Camel-cased string.
+ */
+function camelCaseDash( string ) {
+	return string.replace(
+		/-([a-z])/g,
+		( match, letter ) => letter.toUpperCase()
+	);
+}
+
+const gutenbergPackages = [
+	'a11y',
+	'annotations',
+	'api-fetch',
+	'autop',
+	'blob',
+	'blocks',
+	'block-library',
+	'block-serialization-default-parser',
+	'block-serialization-spec-parser',
+	'components',
+	'compose',
+	'core-data',
+	'data',
+	'date',
+	'deprecated',
+	'dom',
+	'dom-ready',
+	'edit-post',
+	'editor',
+	'element',
+	'escape-html',
+	'format-library',
+	'hooks',
+	'html-entities',
+	'i18n',
+	'is-shallow-equal',
+	'keycodes',
+	'list-reusable-blocks',
+	'notices',
+	'nux',
+	'plugins',
+	'redux-routine',
+	'rich-text',
+	'shortcode',
+	'token-list',
+	'url',
+	'viewport',
+	'wordcount',
+];
+
+const externals = {
+	react: 'React',
+	'react-dom': 'ReactDOM',
+	lodash: 'lodash',
+};
+
+gutenbergPackages.forEach( ( name ) => {
+	externals[ `@wordpress/${ name }` ] = {
+		this: [ 'wp', camelCaseDash( name ) ],
+	};
+} );
+
 const webpackConfig = {
 	mode: NODE_ENV === 'production' ? 'production' : 'development',
 
@@ -62,10 +134,7 @@ const webpackConfig = {
 			},
 		},
 	],
-	externals: {
-		'react': 'React',
-		'react-dom': 'ReactDOM',
-	},
+	externals,
 };
 
 module.exports = webpackConfig;

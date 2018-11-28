39edd326e258775026d4e935085da9ad30616539
iandunn <iandunn@git.wordpress.org>
2018-11-28 02:17:30 +0000

    Jetpack Tweaks: Apply coding standards.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@7911 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/mu-plugins/jetpack-tweaks/sharing.php
+++ public_html/wp-content/mu-plugins/jetpack-tweaks/sharing.php
@@ -1,14 +1,15 @@
 <?php
 
 namespace WordCamp\Jetpack_Tweaks;
-defined( 'WPINC' ) or die();
+
+defined( 'WPINC' ) || die();
 
 add_filter( 'jetpack_open_graph_image_default', __NAMESPACE__ . '\default_og_image'               );
 add_filter( 'jetpack_images_get_images',        __NAMESPACE__ . '\default_single_og_image', 10, 3 );
 add_filter( 'jetpack_open_graph_tags',          __NAMESPACE__ . '\add_og_twitter_summary'         );
 add_filter( 'jetpack_twitter_cards_site_tag',   __NAMESPACE__ . '\twitter_sitetag'                );
 
-/*
+/**
  * Open Graph Default Image.
  *
  * Provides a default image for sharing WordCamp home/pages to Facebook/Twitter/Google other than the Jetpack "blank" image.
@@ -37,21 +38,23 @@ function default_single_og_image( $media, $post_id, $args ) {
 
 	if ( has_site_icon() ) {
 		$image_url = get_site_icon_url();
-	} else if ( has_header_image() ) {
+	} elseif ( has_header_image() ) {
 		$image_url = get_header_image();
 	} else {
 		$image_url = default_og_image();
 	}
 
-	return array( array(
-		'type' => 'image',
-		'from' => 'custom_fallback',
-		'src'  => esc_url( $image_url ),
-		'href' => get_permalink( $post_id ),
-	) );
+	return array(
+		array(
+			'type' => 'image',
+			'from' => 'custom_fallback',
+			'src'  => esc_url( $image_url ),
+			'href' => get_permalink( $post_id ),
+		),
+	);
 }
 
-/*
+/**
  * Add Twitter Card type.
  *
  * Added the twitter:card = summary OG tag for the home page and other ! is_singular() pages, which is not added by default by Jetpack.
@@ -64,14 +67,14 @@ function add_og_twitter_summary( $og_tags ) {
 	return $og_tags;
 }
 
-/*
+/**
  * User @WordCamp as the default Twitter account.
  *
  * Add default Twitter account as @WordCamp for when individual WCs do not set their Settings->Sharing option for Twitter cards only.
  * Sets the "via" tag to blank to avoid slamming @WordCamp moderators with a ton of shared posts.
  */
 function twitter_sitetag( $site_tag ) {
-	if ( 'jetpack' == $site_tag ) {
+	if ( 'jetpack' === $site_tag ) {
 		$site_tag = 'WordCamp';
 		add_filter( 'jetpack_sharing_twitter_via', '__return_empty_string' );
 	}

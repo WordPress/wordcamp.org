60b97cc0724af65aee825d082dbd199c578d04c9
vedjain <vedjain@git.wordpress.org>
2018-12-17 07:35:01 +0000

    WCPT Application Tracker: Coding standard fixes + Removed unused props
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@7982 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/plugins/wcpt/javascript/tracker/source/tracker.jsx
+++ public_html/wp-content/plugins/wcpt/javascript/tracker/source/tracker.jsx
@@ -18,28 +18,28 @@ require( './style.scss' );
  */
 const timeSince = ( seconds ) => {
 
-	let interval = Math.floor(seconds / 31536000);
+	let interval = Math.floor ( seconds / 31536000 );
 
-	if (interval >= 1) {
+	if ( interval >= 1)  {
 		return interval + " years";
 	}
-	interval = Math.floor(seconds / 2592000);
-	if (interval >= 1) {
+	interval = Math.floor ( seconds / 2592000);
+	if ( interval >= 1 ) {
 		return interval + " months";
 	}
-	interval = Math.floor(seconds / 86400);
-	if (interval >= 1) {
+	interval = Math.floor ( seconds / 86400);
+	if ( interval >= 1 ) {
 		return interval + " days";
 	}
-	interval = Math.floor(seconds / 3600);
-	if (interval >= 1) {
+	interval = Math.floor ( seconds / 3600);
+	if ( interval >= 1 ) {
 		return interval + " hours";
 	}
-	interval = Math.floor(seconds / 60);
-	if (interval >= 1) {
+	interval = Math.floor ( seconds / 60);
+	if ( interval >= 1 ) {
 		return interval + " minutes";
 	}
-	return Math.floor(seconds) + " seconds";
+	return Math.floor ( seconds ) + " seconds";
 };
 
 /**

9cccd2c7f32bd7de19b3d70df20c92b49ea9db0e
coreymckrill <coreymckrill@git.wordpress.org>
2018-12-22 00:14:37 +0000

    WordCamp Blocks: Add missing build files
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@8011 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/mu-plugins/blocks/webpack.config.js
@@ -0,0 +1,57 @@
+const path = require( 'path' );
+const webpack = require( 'webpack' );
+const { exec } = require( 'child_process' );
+const MiniCssExtractPlugin = require( 'mini-css-extract-plugin' );
+
+const NODE_ENV = process.env.NODE_ENV || 'development';
+
+const externals = {
+	react: 'React',
+	'react-dom': 'ReactDOM',
+	lodash: 'lodash',
+};
+
+const webpackConfig = {
+	mode: NODE_ENV === 'production' ? 'production' : 'development',
+
+	entry: {
+		blocks: path.resolve( __dirname, 'assets/src/blocks.js' ),
+	},
+	output: {
+		filename: '[name].min.js',
+		path: path.resolve( __dirname, 'assets' ),
+	},
+	module: {
+		rules: [
+			{
+				test: /\.jsx?$/,
+				exclude: [
+					/node_modules/,
+				],
+				use: 'babel-loader',
+			},
+			{
+				test: /\.(sc|sa|c)ss$/,
+				exclude: [
+					/node_modules/,
+				],
+				use: [
+					MiniCssExtractPlugin.loader,
+					'css-loader',
+					'sass-loader',
+				],
+			},
+		],
+	},
+	plugins: [
+		new MiniCssExtractPlugin( {
+			filename: '[name].min.css',
+		} ),
+		new webpack.DefinePlugin( {
+			'process.env.NODE_ENV': JSON.stringify( NODE_ENV )
+		} ),
+	],
+	externals,
+};
+
+module.exports = webpackConfig;

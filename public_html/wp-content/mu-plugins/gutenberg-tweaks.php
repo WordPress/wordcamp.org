2785c9fdcb83c9e3180c021b418799c48fff0908
iandunn <iandunn@git.wordpress.org>
2018-12-26 21:06:07 +0000

    Gutenberg Tweaks: Configure the Classic Editor default settings.
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@8016 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/mu-plugins/gutenberg-tweaks.php
+++ public_html/wp-content/mu-plugins/gutenberg-tweaks.php
@@ -4,6 +4,19 @@ namespace WordCamp\Gutenberg_Tweaks;
 
 defined( 'WPINC' ) || die();
 
+add_filter( 'classic_editor_network_default_settings', __NAMESPACE__ . '\classic_editor_default_settings' );
+
+
+/**
+ * Configure the default settings for the Classic Editor
+ */
+function classic_editor_default_settings( $defaults ) {
+	$defaults['editor']      = 'block';
+	$defaults['allow-users'] = true;
+
+	return $defaults;
+}
+
 /*
  * Customize Gutenberg for WordCamp.org
  *

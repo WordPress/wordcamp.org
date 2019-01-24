07b21b6607acf965666ee002144cb7b0ea7201ff
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2019-01-24 15:27:03 +0100

    Move major publishing actions to separate template file

--- public_html/wp-content/plugins/wordcamp-payments/views/reimbursement-request/metabox-status.php
+++ public_html/wp-content/plugins/wordcamp-payments/views/reimbursement-request/metabox-status.php
@@ -95,35 +95,6 @@ defined( 'WPINC' ) or die();
 		<div class="clear"></div>
 	</div> <!-- #minor-publishing -->
 
-
-	<div id="major-publishing-actions">
-		<?php if ( $current_user_can_edit_request ) : ?>
-			<?php if ( !empty( $submit_note ) ) : ?>
-				<div class="notice notice-<?php echo esc_attr( $submit_note_class ); ?> inline"><?php echo wpautop( $submit_note ); ?></div>
-			<?php endif; ?>
-
-			<?php if ( current_user_can( 'delete_post', $post->ID ) ) : ?>
-				<div id="delete-action">
-					<a class="submitdelete deletion" href="<?php echo get_delete_post_link( $post->ID ); ?>">
-						<?php _e( 'Delete', 'wordcamporg' ); ?>
-					</a>
-				</div>
-			<?php endif; ?>
-
-			<?php if ( can_submit_request( $post ) ) : ?>
-				<div id="publishing-action">
-					<input name="original_publish" type="hidden" id="original_publish" value="<?php esc_attr( $submit_text ) ?>" />
-					<?php submit_button( $submit_text, 'primary button-large', 'wcb-update', false, array( 'accesskey' => 'p' ) ); ?>
-				</div>
-			<?php endif; ?>
-
-			<div class="clear"></div>
-
-		<?php else : ?>
-
-			<?php _e( 'This request can not be edited.', 'wordcamporg' ); ?>
-
-		<?php endif; ?>
-	</div> <!-- #major-publishing-actions -->
+	<?php require dirname( __DIR__ ) . '/wordcamp-budgets/major-publishing-actions.php'; ?>
 
 </div> <!-- .submitbox -->

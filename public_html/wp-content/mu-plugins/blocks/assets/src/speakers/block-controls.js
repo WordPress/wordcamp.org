db7730dd356ccb532085d7930e4b0b7528797e26
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2019-02-14 13:35:46 -0800

    Block Controls: JS tweaks
    * Reduce dependency on lodash.
    * Assign values within switch statements and return afterwards as
      opposed to having each case return. See
      https://make.wordpress.org/core/handbook/best-practices/coding-standards/javascript/#switch-statements

--- public_html/wp-content/mu-plugins/blocks/assets/src/speakers/block-controls.js
+++ public_html/wp-content/mu-plugins/blocks/assets/src/speakers/block-controls.js
@@ -1,9 +1,4 @@
 /**
- * External dependencies
- */
-import { find } from 'lodash';
-
-/**
  * WordPress dependencies
  */
 const { Button, Placeholder, Spinner } = wp.components;
@@ -40,18 +35,21 @@ class SpeakersBlockControls extends Component {
 			);
 		}
 
+		let output;
+
 		switch ( mode ) {
 			case 'all' :
-				return (
+				output = (
 					<SpeakersBlockContent { ...this.props } />
 				);
+				break;
 
 			case 'specific_posts' :
-				const postsLabel = find( modeOptions, ( modeOption ) => {
+				const postsLabel = modeOptions.find( ( modeOption ) => {
 					return 'specific_posts' === modeOption.value;
 				} ).label;
 
-				return (
+				output = (
 					<Fragment>
 						<SpeakersBlockContent { ...this.props } />
 						<Placeholder
@@ -62,13 +60,14 @@ class SpeakersBlockControls extends Component {
 						</Placeholder>
 					</Fragment>
 				);
+				break;
 
 			case 'specific_terms' :
-				const termsLabel = find( modeOptions, ( modeOption ) => {
+				const termsLabel = modeOptions.find( ( modeOption ) => {
 					return 'specific_terms' === modeOption.value;
 				} ).label;
 
-				return (
+				output = (
 					<Fragment>
 						<SpeakersBlockContent { ...this.props } />
 						<Placeholder
@@ -79,35 +78,40 @@ class SpeakersBlockControls extends Component {
 						</Placeholder>
 					</Fragment>
 				);
+				break;
+
+			default :
+				output = (
+					<Placeholder
+						icon="megaphone"
+						label={ __( 'Speakers', 'wordcamporg' ) }
+					>
+						<div className="wordcamp-block-speakers-mode-option">
+							<Button
+								isDefault
+								isLarge
+								onClick={ () => {
+									setAttributes( { mode: 'all' } );
+								} }
+							>
+								{ modeOptions.find( ( modeOption ) => {
+									return 'all' === modeOption.value;
+								} ).label }
+							</Button>
+						</div>
+
+						<div className="wordcamp-block-speakers-mode-option">
+							<SpeakersSelect
+								label={ __( 'Choose specific speakers or groups', 'wordcamporg' ) }
+								{ ...this.props }
+							/>
+						</div>
+					</Placeholder>
+				);
+				break;
 		}
 
-		return (
-			<Placeholder
-				icon="megaphone"
-				label={ __( 'Speakers', 'wordcamporg' ) }
-			>
-				<div className="wordcamp-block-speakers-mode-option">
-					<Button
-						isDefault
-						isLarge
-						onClick={ () => {
-							setAttributes( { mode: 'all' } );
-						} }
-					>
-						{ find( modeOptions, ( modeOption ) => {
-							return 'all' === modeOption.value;
-						} ).label }
-					</Button>
-				</div>
-
-				<div className="wordcamp-block-speakers-mode-option">
-					<SpeakersSelect
-						label={ __( 'Choose specific speakers or groups', 'wordcamporg' ) }
-						{ ...this.props }
-					/>
-				</div>
-			</Placeholder>
-		);
+		return output;
 	}
 }
 

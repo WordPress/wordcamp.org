84b25f9d2ffb15568baa8124d15c296ba76ba0f6
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2019-02-18 16:54:20 -0800

    Error handling improvements
    * Rename 0-slack.php to 0-error-handling.php, since that file's only
      purpose is to load the Slack library for error handling purposes
      anyway.
    * Move all of the error handling stuff from wcorg-misc.php to
      0-error-handling.php. There's enough of it to warrant a separate file,
      and this way it all gets loaded before the rest of the mu-plugins
      so it can potentially catch errors there as well.
    * Run the custom error handler and shutdown function in other
      environments besides production, as long as the SANDBOX_SLACK_USERNAME
      constant is defined. This way the errors will be sent to the sandbox
      owner's Slack account, rather than the log channel.
    * In the shutdown function, only send the error to Slack if it is one
      of the types of errors that can't be handled in the custom error
      handler.

---
+++ public_html/wp-content/mu-plugins/0-error-handling.php
@@ -0,0 +1,169 @@
+<?php
+defined( 'WPINC' ) or die();
+
+define( 'ERROR_RATE_LIMITING_DIR', '/tmp/error_limiting' );
+
+if ( is_readable( __DIR__ . '/includes/slack/send.php' ) ) {
+	require_once( __DIR__ . '/includes/slack/send.php' );
+}
+
+set_error_handler( 'send_error_to_slack' );
+register_shutdown_function( 'send_fatal_to_slack' );
+
+/**
+ * Error handler to send errors to Slack.
+ *
+ * Always return false so that default error handling still occurs as well.
+ *
+ * @return bool
+ */
+function send_error_to_slack( $err_no, $err_msg, $file, $line ) {
+	if ( ! defined( 'WORDCAMP_ENVIRONMENT' )
+	     || ( 'production' !==  WORDCAMP_ENVIRONMENT && ! defined( 'SANDBOX_SLACK_USERNAME' ) )
+	) {
+		return false;
+	}
+
+	if ( ! init_error_handling() ) {
+		return false;
+	}
+
+	// Checks to see if the error-throwing expression is prepended with the @ control operator.
+	// See https://secure.php.net/manual/en/function.set-error-handler.php
+	if ( 0 === error_reporting() ) {
+		return false;
+	}
+
+	$error_safelist = [
+		E_ERROR,
+		E_USER_ERROR,
+		E_CORE_ERROR,
+		E_COMPILE_ERROR,
+		E_PARSE,
+		E_NOTICE,
+		E_DEPRECATED,
+		E_WARNING,
+	];
+
+	if ( ! in_array( $err_no, $error_safelist ) ) {
+		return false;
+	}
+
+	// Always use constants in the keys here to avoid path disclosure.
+	$error_ignorelist = [
+		// See https://core.trac.wordpress.org/ticket/29204
+		ABSPATH . 'wp-includes/SimplePie/Registry.php:215' => 'Non-static method WP_Feed_Cache::create() should not be called statically',
+	];
+
+	if ( isset( $error_ignorelist[ "$file:$line" ] ) && false !== strpos( $err_msg, $error_ignorelist[ "$file:$line" ] ) ) {
+		return false;
+	}
+
+	// Max file length for ubuntu system is 255.
+	$err_key = substr( base64_encode("$file-$line-$err_no" ), -254 );
+
+	$error_file = ERROR_RATE_LIMITING_DIR . "/$err_key";
+
+	$text = '';
+
+	$data = array(
+		'last_reported_at' => time(),
+		'error_count'      => 0, // since last reported.
+	);
+
+	if ( ! file_exists( $error_file ) ) {
+		$text = 'Error occured. ';
+		file_put_contents( $error_file, wp_json_encode( $data ) );
+	} else {
+		$data                 = json_decode( file_get_contents( $error_file ), true );
+		$data['error_count'] += 1;
+		$time_elasped         = time() - $data['last_reported_at'];
+
+		if ( $time_elasped > 600 ) {
+			$text                     = "Still happening. Happened ${data['error_count']} time(s) since last reported. ";
+			$data['last_reported_at'] = time();
+			$data['error_count']      = 0;
+
+			file_put_contents( $error_file, wp_json_encode( $data ) );
+		} else {
+			file_put_contents( $error_file, wp_json_encode( $data ) );
+			return false;
+		}
+	}
+
+	$domain    = get_site_url();
+	$page_slug = esc_html( trim( $_SERVER['REQUEST_URI'], '/' ) );
+	$text      = $text . "Message : \"$err_msg\" occurred on \"$file:$line\" \n Domain: $domain \n Page: $page_slug \n Error type: $err_no";
+
+	$message = array(
+		'fallback'    => $text,
+		'color'       => '#ff0000',
+		'pretext'     => "Error on \"$file:$line\" ",
+		'author_name' => $domain,
+		'text'        => $text,
+	);
+
+	$send = new \Dotorg\Slack\Send( SLACK_ERROR_REPORT_URL );
+	$send->add_attachment( $message );
+
+	if ( 'production' === WORDCAMP_ENVIRONMENT ) {
+		$send->send( WORDCAMP_LOGS_SLACK_CHANNEL );
+	} else {
+		$send->send( SANDBOX_SLACK_USERNAME );
+	}
+
+	return false;
+}
+
+/**
+ * Shutdown handler for catching fatal errors and sending them to Slack.
+ *
+ * Some error types cannot be handled directly by a custom error handler. However, we can catch them during shutdown
+ * and redirect them to the custom handler callback.
+ */
+function send_fatal_to_slack() {
+	$error = error_get_last();
+
+	// See https://secure.php.net/manual/en/function.set-error-handler.php
+	$unhandled_error_types = [ E_ERROR, E_PARSE, E_CORE_ERROR, E_CORE_WARNING, E_COMPILE_ERROR, E_COMPILE_WARNING ];
+
+	if ( ! empty( $error ) && in_array( $error['type'], $unhandled_error_types, true ) ) {
+		send_error_to_slack( $error['type'], $error['message'], $error['file'], $error['line'] );
+	}
+}
+
+/**
+ * Check and create filesystem dirs to manage rate limiting in error handling.
+ * For legacy bugs we are doing rate limiting via filesystem. We would be investigating to see if we can instead use memcache to rate limit sometime in the future.
+ *
+ * @return bool Return true if file permissions etc are present
+ */
+function init_error_handling() {
+	if ( ! file_exists( ERROR_RATE_LIMITING_DIR ) ) {
+		mkdir( ERROR_RATE_LIMITING_DIR );
+	}
+
+	return is_dir( ERROR_RATE_LIMITING_DIR ) && is_writeable( ERROR_RATE_LIMITING_DIR );
+}
+
+/**
+ * Function `send_error_to_slack` above also creates a bunch of files in /tmp/error_limiting folder in order to rate limit the notification.
+ * This function will be used as a cron to clear these error_limiting files periodically.
+ */
+function handle_clear_error_rate_limiting_files() {
+	if ( ! init_error_handling() ) {
+		return;
+	}
+
+	foreach ( new DirectoryIterator( ERROR_RATE_LIMITING_DIR ) as $file_info ) {
+		if ( ! $file_info->isDot() ) {
+			unlink( $file_info->getPathname() );
+		}
+	}
+}
+
+if ( ! wp_next_scheduled( 'clear_error_rate_limiting_files' ) ) {
+	wp_schedule_event( time(), 'daily', 'clear_error_rate_limiting_files' );
+}
+
+add_action( 'clear_error_rate_limiting_files', 'handle_clear_error_rate_limiting_files' );

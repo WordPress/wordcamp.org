3a3e460b9a8a642dfaf21f0470664b79f184c9c6
iandunn <iandunn@git.wordpress.org>
2019-02-20 20:47:34 +0000

    WordCamp Organizer Reminders: Move Available Placeholders to side metabox.
    This improves UX by de-cluttering the Reminder Details metabox, and making the available placeholders information more easily accessible, since much of it is available without scrolling.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@8293 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/plugins/wordcamp-organizer-reminders/views/metabox-placeholders.php
@@ -0,0 +1,76 @@
+<?php defined( 'WPINC' ) || die(); ?>
+
+<h4>Available Placeholders:</h4>
+
+<h5>The WordCamp:</h5>
+
+<ul class="ul-disc">
+	<li>[wordcamp_name]</li>
+	<li>[wordcamp_start_date]</li>
+	<li>[wordcamp_location]</li>
+	<li>[wordcamp_url]</li>
+	<li>[edit_wordcamp_url]</li>
+	<li>[wordcamp_email]</li>
+	<li>[wordcamp_twitter]</li>
+	<li>[wordcamp_hashtag]</li>
+	<li>[wordcamp_anticipated_attendees]</li>
+	<li>[multi_event_sponsor_region]</li>
+</ul>
+
+<h5>The organizing team:</h5>
+
+<ul class="ul-disc">
+	<li>[organizer_name]</li>
+	<li>[lead_organizer_username]</li>
+	<li>[lead_organizer_email]</li>
+	<li>[lead_organizer_telephone]</li>
+	<li>[organizer_address]</li>
+	<li>[sponsor_wrangler_name]</li>
+	<li>[sponsor_wrangler_email]</li>
+	<li>[budget_wrangler_name]</li>
+	<li>[budget_wrangler_email]</li>
+	<li>[venue_wrangler_name]</li>
+	<li>[venue_wrangler_email]</li>
+	<li>[speaker_wrangler_name]</li>
+	<li>[speaker_wrangler_email]</li>
+	<li>[food_wrangler_name]</li>
+	<li>[food_wrangler_email]</li>
+	<li>[swag_wrangler_name]</li>
+	<li>[swag_wrangler_email]</li>
+	<li>[volunteer_wrangler_name]</li>
+	<li>[volunteer_wrangler_email]</li>
+	<li>[printing_wrangler_name]</li>
+	<li>[printing_wrangler_email]</li>
+	<li>[design_wrangler_name]</li>
+	<li>[design_wrangler_email]</li>
+	<li>[website_wrangler_name]</li>
+	<li>[website_wrangler_email]</li>
+	<li>[social_wrangler_name]</li>
+	<li>[social_wrangler_email]</li>
+	<li>[a_v_wrangler_name]</li>
+	<li>[a_v_wrangler_email]</li>
+	<li>[party_wrangler_name]</li>
+	<li>[party_wrangler_email]</li>
+	<li>[travel_wrangler_name]</li>
+	<li>[travel_wrangler_email]</li>
+	<li>[safety_wrangler_name]</li>
+	<li>[safety_wrangler_email]</li>
+</ul>
+
+<h5>Venue</h5>
+
+<ul class="ul-disc">
+	<li>[venue_name]</li>
+	<li>[venue_address]</li>
+	<li>[venue_max_capacity]</li>
+	<li>[venue_available_rooms]</li>
+	<li>[venue_url]</li>
+	<li>[venue_contact_info]</li>
+	<li>[venue_exhibition_space_message]</li>
+</ul>
+
+<h5>Miscellaneous</h5>
+
+<ul class="ul-disc">
+	<li>[multi_event_sponsor_info]</li>
+</ul>
--- public_html/wp-content/plugins/wordcamp-organizer-reminders/wcor-reminder.php
+++ public_html/wp-content/plugins/wordcamp-organizer-reminders/wcor-reminder.php
@@ -79,6 +79,14 @@ class WCOR_Reminder {
 			self::AUTOMATED_POST_TYPE_SLUG,
 			'side'
 		);
+
+		add_meta_box(
+			'wcor_available_placeholders',
+			'Available Placeholders',
+			array( $this, 'render_available_placeholders' ),
+			self::AUTOMATED_POST_TYPE_SLUG,
+			'side'
+		);
 	}
 
 	/**
@@ -247,79 +255,14 @@ class WCOR_Reminder {
 			</tbody>
 		</table>
 
-		<h4>Available Placeholders:</h4>
-
-		<h5>The WordCamp:</h5>
-
-		<ul class="ul-disc">
-			<li>[wordcamp_name]</li>
-			<li>[wordcamp_start_date]</li>
-			<li>[wordcamp_location]</li>
-			<li>[wordcamp_url]</li>
-			<li>[edit_wordcamp_url]</li>
-			<li>[wordcamp_email]</li>
-			<li>[wordcamp_twitter]</li>
-			<li>[wordcamp_hashtag]</li>
-			<li>[wordcamp_anticipated_attendees]</li>
-			<li>[multi_event_sponsor_region]</li>
-		</ul>
-
-		<h5>The organizing team:</h5>
-		<ul class="ul-disc">
-			<li>[organizer_name]</li>
-			<li>[lead_organizer_username]</li>
-			<li>[lead_organizer_email]</li>
-			<li>[lead_organizer_telephone]</li>
-			<li>[organizer_address]</li>
-			<li>[sponsor_wrangler_name]</li>
-			<li>[sponsor_wrangler_email]</li>
-			<li>[budget_wrangler_name]</li>
-			<li>[budget_wrangler_email]</li>
-			<li>[venue_wrangler_name]</li>
-			<li>[venue_wrangler_email]</li>
-			<li>[speaker_wrangler_name]</li>
-			<li>[speaker_wrangler_email]</li>
-			<li>[food_wrangler_name]</li>
-			<li>[food_wrangler_email]</li>
-			<li>[swag_wrangler_name]</li>
-			<li>[swag_wrangler_email]</li>
-			<li>[volunteer_wrangler_name]</li>
-			<li>[volunteer_wrangler_email]</li>
-			<li>[printing_wrangler_name]</li>
-			<li>[printing_wrangler_email]</li>
-			<li>[design_wrangler_name]</li>
-			<li>[design_wrangler_email]</li>
-			<li>[website_wrangler_name]</li>
-			<li>[website_wrangler_email]</li>
-			<li>[social_wrangler_name]</li>
-			<li>[social_wrangler_email]</li>
-			<li>[a_v_wrangler_name]</li>
-			<li>[a_v_wrangler_email]</li>
-			<li>[party_wrangler_name]</li>
-			<li>[party_wrangler_email]</li>
-			<li>[travel_wrangler_name]</li>
-			<li>[travel_wrangler_email]</li>
-			<li>[safety_wrangler_name]</li>
-			<li>[safety_wrangler_email]</li>
-		</ul>
-
-		<h5>Venue</h5>
-		<ul class="ul-disc">
-			<li>[venue_name]</li>
-			<li>[venue_address]</li>
-			<li>[venue_max_capacity]</li>
-			<li>[venue_available_rooms]</li>
-			<li>[venue_url]</li>
-			<li>[venue_contact_info]</li>
-			<li>[venue_exhibition_space_message]</li>
-		</ul>
-
-		<h5>Miscellaneous</h5>
-		<ul class="ul-disc">
-			<li>[multi_event_sponsor_info]</li>
-		</ul>
-
 		<?php
+	}
+
+	/**
+	 * Builds the markup for the Available Placeholders metabox.
+	 */
+	public function render_available_placeholders() {
+		require_once( __DIR__ . '/views/metabox-placeholders.php' );
 	}
 
 	/**

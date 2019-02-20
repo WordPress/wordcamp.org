7a0da90754e8b8d83b0cbf494555a02572dbbf02
iandunn <iandunn@git.wordpress.org>
2019-02-20 20:47:54 +0000

    WordCamp Tests: Abstract generic config/bootstrapper from Remote CSS.
    This will allow us to add additional suites for other plugins without duplicating configuration or code.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@8297 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ phpunit-bootstrap.php
@@ -0,0 +1,28 @@
+<?php
+
+$core_tests_directory = getenv( 'WP_TESTS_DIR' );
+
+if ( ! $core_tests_directory ) {
+	echo "\nPlease set the WP_TESTS_DIR environment variable to the folder where WordPress' PHPUnit tests live --";
+	echo "\ne.g., export WP_TESTS_DIR=/srv/www/wordpress-develop/tests/phpunit\n";
+
+	return;
+}
+
+require_once( $core_tests_directory . '/includes/functions.php' );
+require_once( dirname( dirname( $core_tests_directory ) ) . '/build/wp-admin/includes/plugin.php' );
+
+/*
+ * Load individual plugin bootstrappers
+ *
+ * There may eventually be cases where these conflict with one another (e.g., some need to run in context of
+ * wp-admin while others need to run in front-end context), but it works for now. If they ever do conflict, then
+ * that's probably a smell that we shouldn't be using PHPUnit for integration tests, though.
+ *
+ * If we don't want to migrate to Selenium etc, then another option might be using a PHPUnit listener to load the
+ * bootstrap for a particular suite before the suite loads (see https://stackoverflow.com/a/30170762/450127). It's
+ * not clear if that would properly isolate them from each other, and allow multiple independent contexts, though.
+ */
+require_once( __DIR__ . '/public_html/wp-content/plugins/wordcamp-remote-css/tests/bootstrap.php' );
+
+require_once( $core_tests_directory . '/includes/bootstrap.php' );
--- public_html/wp-content/plugins/wordcamp-remote-css/tests/bootstrap.php
+++ public_html/wp-content/plugins/wordcamp-remote-css/tests/bootstrap.php
@@ -6,20 +6,8 @@ if ( 'cli' !== php_sapi_name() ) {
 	return;
 }
 
-$core_tests_directory = getenv( 'WP_TESTS_DIR' );
-
-if ( ! $core_tests_directory ) {
-	echo "\nPlease set the WP_TESTS_DIR environment variable to the folder where WordPress' PHPUnit tests live --";
-	echo "\ne.g., export WP_TESTS_DIR=/srv/www/wordpress-develop/tests/phpunit\n";
-
-	return;
-}
-
-require_once( $core_tests_directory . '/includes/functions.php' );
-require_once( dirname( dirname( $core_tests_directory ) ) . '/build/wp-admin/includes/plugin.php' );
-
 /**
- * Load the plugins that we'll need to be active for the tests
+ * Load the plugins that we'll need to be active for the tests.
  */
 function manually_load_plugin() {
 	$_SERVER['PHP_SELF'] = admin_url( 'themes.php?page=remote-css' );
@@ -46,5 +34,3 @@ function manually_load_plugin() {
 	require_once( dirname( dirname( dirname( __DIR__ ) ) ) . '/mu-plugins/jetpack-tweaks/css-sanitization.php' );
 }
 tests_add_filter( 'muplugins_loaded', __NAMESPACE__ . '\manually_load_plugin' );
-
-require( $core_tests_directory . '/includes/bootstrap.php' );

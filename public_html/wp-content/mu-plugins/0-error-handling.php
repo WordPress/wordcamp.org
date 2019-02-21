9f69dd9852c5e5c73b6804ccd82ef5fb613673e2
coreymckrill <coreymckrill@git.wordpress.org>
2019-02-21 21:21:54 +0000

    WordCamp: Additional error logging tweaks
    * Add a namespace.
    * Split up the error handler. The actual handler callback now only records
      the occurrence of errors, and then if conditions are met it also calls a
      new separate function that sends the info to Slack.
    * Abstract the functionality that records the error occurrences.
    * Formatting tweaks to the Slack messages.
    * Ensure that the cron job that clears out old error log files is only run
      on the main site.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@8310 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/mu-plugins/0-error-handling.php
+++ public_html/wp-content/mu-plugins/0-error-handling.php
@@ -1,24 +1,19 @@
 <?php
+namespace WordCamp\Error_Handling;
 defined( 'WPINC' ) or die();
 
-define( 'ERROR_RATE_LIMITING_DIR', '/tmp/error_limiting' );
+use DirectoryIterator;
+use Dotorg\Slack\Send;
 
-if ( is_readable( __DIR__ . '/includes/slack/send.php' ) ) {
-	require_once( __DIR__ . '/includes/slack/send.php' );
-}
+const ERROR_RATE_LIMITING_DIR = '/tmp/error_limiting';
 
-set_error_handler( 'send_error_to_slack' );
-register_shutdown_function( 'send_fatal_to_slack' );
+set_error_handler( __NAMESPACE__ . '\handle_error' );
+register_shutdown_function( __NAMESPACE__ . '\catch_fatal' );
 
 /**
- * Error handler to send errors to Slack.
+ * Error handler to track error frequency and conditionally send error messages to Slack.
  *
  * Note: This should always return false so that default error handling still occurs as well.
- *
- * @todo We should consider splitting the functionality here into two or more separate functions.
- *       This way logging errors to files could be separate from sending messages to Slack. This would
- *       also potentially allow us to load the error logging part earlier, before WP functions
- *       are available.
  *
  * @param int    $err_no
  * @param string $err_msg
@@ -27,14 +22,8 @@ register_shutdown_function( 'send_fatal_to_slack' );
  *
  * @return bool
  */
-function send_error_to_slack( $err_no, $err_msg, $file, $line ) {
-	if ( ! defined( 'WORDCAMP_ENVIRONMENT' )
-	     || ( 'production' !==  WORDCAMP_ENVIRONMENT && ! defined( 'SANDBOX_SLACK_USERNAME' ) )
-	) {
-		return false;
-	}
-
-	if ( ! init_error_handling() ) {
+function handle_error( $err_no, $err_msg, $file, $line ) {
+	if ( ! check_error_handling_dependencies() ) {
 		return false;
 	}
 
@@ -44,7 +33,7 @@ function send_error_to_slack( $err_no, $err_msg, $file, $line ) {
 		return false;
 	}
 
-	$error_safelist = [
+	$accepted_error_types = [
 		E_ERROR,
 		E_CORE_ERROR,
 		E_COMPILE_ERROR,
@@ -61,7 +50,7 @@ function send_error_to_slack( $err_no, $err_msg, $file, $line ) {
 		E_USER_DEPRECATED,
 	];
 
-	if ( ! in_array( $err_no, $error_safelist ) ) {
+	if ( ! in_array( $err_no, $accepted_error_types ) ) {
 		return false;
 	}
 
@@ -75,42 +64,136 @@ function send_error_to_slack( $err_no, $err_msg, $file, $line ) {
 		return false;
 	}
 
-	$err_key    = substr( base64_encode("$file-$line-$err_no" ), -254 ); // Max file length for ubuntu is 255.
-	$error_file = ERROR_RATE_LIMITING_DIR . "/$err_key";
-	$data       = array(
+	$err_key      = substr( base64_encode("$file-$line-$err_no" ), -254 ); // Max file length for ubuntu is 255.
+	$data         = array(
 		'last_reported_at' => time(),
-		'error_count'      => 0, // since last reported.
+		'error_count'      => 0, // Since last reported.
 	);
-	$messages    = explode( 'Stack trace:', $err_msg, 2 );
-	$pretext     = $messages[0] ?: '';
-	$stack_trace = ( ! empty( $messages[1] ) ) ? trim( sanitize_text_field( $messages[1] ) ) : '';
-	$footer      = '';
+	$send_message = false;
+	$occurrences  = 0;
 
-	if ( ! file_exists( $error_file ) ) {
-		file_put_contents( $error_file, wp_json_encode( $data ) );
-	} else {
-		$data                 = json_decode( file_get_contents( $error_file ), true );
+	if ( error_record_exists( $err_key ) ) {
+		$data                 = get_error_record( $err_key );
 		$data['error_count'] += 1;
+		$occurrences          = $data['error_count'];
 		$time_elapsed         = time() - $data['last_reported_at'];
 
 		if ( $time_elapsed > 600 ) {
-			$footer .= "Occurred *${data['error_count']} time(s)* since last reported";
-
 			$data['last_reported_at'] = time();
 			$data['error_count']      = 0;
-			file_put_contents( $error_file, wp_json_encode( $data ) );
-		} else {
-			file_put_contents( $error_file, wp_json_encode( $data ) );
-			
-			return false;
+			$send_message             = true;
 		}
+	} else {
+		$send_message = true;
 	}
 
-	$domain    = esc_url( get_site_url() );
-	$page_slug = sanitize_text_field( untrailingslashit( $_SERVER['REQUEST_URI'] ) ) ?: '/';
+	update_error_record( $err_key, $data );
+
+	if ( $send_message ) {
+		send_error_to_slack( $err_no, $err_msg, $file, $line, $occurrences );
+	}
+
+	return false;
+}
+
+/**
+ * Shutdown handler for catching fatal errors and sending them to Slack.
+ *
+ * Some error types cannot be handled directly by a custom error handler. However, we can catch them during shutdown
+ * and redirect them to the custom handler callback.
+ *
+ * @return void
+ */
+function catch_fatal() {
+	$error = error_get_last();
+
+	// See https://secure.php.net/manual/en/function.set-error-handler.php
+	$unhandled_error_types = [ E_ERROR, E_PARSE, E_CORE_ERROR, E_CORE_WARNING, E_COMPILE_ERROR, E_COMPILE_WARNING ];
+
+	if ( ! empty( $error ) && in_array( $error['type'], $unhandled_error_types, true ) ) {
+		handle_error( $error['type'], $error['message'], $error['file'], $error['line'] );
+	}
+}
+
+/**
+ * Check if an error has previously been recorded.
+ *
+ * @param string $err_key
+ *
+ * @return bool
+ */
+function error_record_exists( $err_key ) {
+	$error_file = ERROR_RATE_LIMITING_DIR . "/$err_key";
+
+	return is_readable( $error_file );
+}
+
+/**
+ * Get the data recorded for an error.
+ *
+ * Includes the timestamp of the error's last occurrence and the number of times it has occurred since it was
+ * last reported/sent to Slack.
+ *
+ * @param string $err_key
+ *
+ * @return array|mixed|object
+ */
+function get_error_record( $err_key ) {
+	$error_file = ERROR_RATE_LIMITING_DIR . "/$err_key";
+
+	return json_decode( file_get_contents( $error_file ), true );
+}
+
+/**
+ * Update the recorded data for an error.
+ *
+ * @param string $err_key
+ * @param array  $data
+ *
+ * @return bool|int
+ */
+function update_error_record( $err_key, $data ) {
+	$error_file = ERROR_RATE_LIMITING_DIR . "/$err_key";
+
+	return file_put_contents( $error_file, wp_json_encode( $data ) );
+}
+
+/**
+ * Build and dispatch an error message to a channel or user on Slack.
+ *
+ * @param int    $err_no
+ * @param string $err_msg
+ * @param string $file
+ * @param int    $line
+ * @param int    $occurrences
+ *
+ * @return void
+ */
+function send_error_to_slack( $err_no, $err_msg, $file, $line, $occurrences = 0 ) {
+	if ( ! defined( 'WORDCAMP_ENVIRONMENT' )
+	     || ( 'production' !== WORDCAMP_ENVIRONMENT && ! defined( 'SANDBOX_SLACK_USERNAME' ) )
+		 || ! is_readable( __DIR__ . '/includes/slack/send.php' )
+	) {
+		return;
+	}
+
+	require_once( __DIR__ . '/includes/slack/send.php' );
+
+	$error_name  = array_search( $err_no, get_defined_constants( true )['Core'] ) ?: '';
+	$messages    = explode( 'Stack trace:', $err_msg, 2 );
+	$text        = ( ! empty( $messages[0] ) ) ? trim( sanitize_text_field( $messages[0] ) ) : '';
+	$stack_trace = ( ! empty( $messages[1] ) ) ? trim( sanitize_text_field( $messages[1] ) ) : '';
+	$domain      = esc_url( get_site_url() );
+	$page_slug   = sanitize_text_field( untrailingslashit( $_SERVER['REQUEST_URI'] ) ) ?: '/';
+	$footer      = '';
+
+	if ( $occurrences > 0 ) {
+		$footer .= "Occurred *$occurrences time(s)* since last reported";
+	}
 
 	switch( $err_no ) {
 		case E_ERROR:
+		case E_PARSE:
 		case E_CORE_ERROR:
 		case E_COMPILE_ERROR :
 		case E_USER_ERROR:
@@ -118,7 +201,6 @@ function send_error_to_slack( $err_no, $err_msg, $file, $line ) {
 			$color = '#ff0000'; // Red.
 			break;
 		case E_WARNING:
-		case E_PARSE:
 		case E_CORE_WARNING:
 		case E_COMPILE_WARNING:
 		case E_USER_WARNING:
@@ -160,51 +242,33 @@ function send_error_to_slack( $err_no, $err_msg, $file, $line ) {
 	}
 
 	$attachment = array(
-		'fallback'    => $pretext,
-		'pretext'     => $pretext,
+		'fallback'    => $text,
+		'text'        => $text,
+		'author_name' => $error_name,
 		'color'       => $color,
-		'author_name' => 'WordCamp Logger',
 		'fields'      => $fields,
 		'footer'      => $footer,
 	);
 
-	$send = new \Dotorg\Slack\Send( SLACK_ERROR_REPORT_URL );
-	$send->add_attachment( $attachment );
+	$slack = new Send( SLACK_ERROR_REPORT_URL );
+	$slack->add_attachment( $attachment );
 
 	if ( 'production' === WORDCAMP_ENVIRONMENT ) {
-		$send->send( WORDCAMP_LOGS_SLACK_CHANNEL );
+		$slack->send( WORDCAMP_LOGS_SLACK_CHANNEL );
 	} else {
-		$send->send( SANDBOX_SLACK_USERNAME );
-	}
-
-	return false;
-}
-
-/**
- * Shutdown handler for catching fatal errors and sending them to Slack.
- *
- * Some error types cannot be handled directly by a custom error handler. However, we can catch them during shutdown
- * and redirect them to the custom handler callback.
- */
-function send_fatal_to_slack() {
-	$error = error_get_last();
-
-	// See https://secure.php.net/manual/en/function.set-error-handler.php
-	$unhandled_error_types = [ E_ERROR, E_PARSE, E_CORE_ERROR, E_CORE_WARNING, E_COMPILE_ERROR, E_COMPILE_WARNING ];
-
-	if ( ! empty( $error ) && in_array( $error['type'], $unhandled_error_types, true ) ) {
-		send_error_to_slack( $error['type'], $error['message'], $error['file'], $error['line'] );
+		$slack->send( SANDBOX_SLACK_USERNAME );
 	}
 }
 
 /**
- * Check and create filesystem dirs to manage rate limiting in error handling.
+ * Check and create the filesystem directory used to manage error rate limiting.
  *
- * For legacy bugs we are doing rate limiting via filesystem. We would be investigating to see if we can instead use memcache to rate limit sometime in the future.
+ * For legacy bugs we are doing rate limiting via filesystem. We would be investigating to see if we can instead use
+ * memcache to rate limit sometime in the future.
  *
- * @return bool Return true if file permissions etc are present
+ * @return bool Return true if file permissions etc are present.
  */
-function init_error_handling() {
+function check_error_handling_dependencies() {
 	if ( ! file_exists( ERROR_RATE_LIMITING_DIR ) ) {
 		mkdir( ERROR_RATE_LIMITING_DIR );
 	}
@@ -215,11 +279,18 @@ function init_error_handling() {
 /**
  * Remove temporary error rate limiting files.
  *
- * Function `send_error_to_slack` above also creates a bunch of files in /tmp/error_limiting folder in order to rate limit the notification.
- * This function will be used as a cron to clear these error_limiting files periodically.
+ * Function `record_error` above also creates a bunch of files in /tmp/error_limiting folder in order to rate limit
+ * the notification. This function will be used as a cron to clear these error_limiting files periodically.
+ *
+ * @return void
  */
 function handle_clear_error_rate_limiting_files() {
-	if ( ! init_error_handling() ) {
+	// This only needs to run on one site.
+	if ( BLOG_ID_CURRENT_SITE !== get_current_blog_id() ) {
+		return;
+	}
+
+	if ( ! check_error_handling_dependencies() ) {
 		return;
 	}
 
@@ -234,4 +305,4 @@ if ( ! wp_next_scheduled( 'clear_error_rate_limiting_files' ) ) {
 	wp_schedule_event( time(), 'daily', 'clear_error_rate_limiting_files' );
 }
 
-add_action( 'clear_error_rate_limiting_files', 'handle_clear_error_rate_limiting_files' );
+add_action( 'clear_error_rate_limiting_files', __NAMESPACE__ . '\handle_clear_error_rate_limiting_files' );

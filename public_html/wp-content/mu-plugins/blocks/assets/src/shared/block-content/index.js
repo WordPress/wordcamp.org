5bbff1712ced984ee7cbb0fc239b5980a82e6122
vedanshujain <vedanshu.jain.2012@gmail.com>
2019-02-26 21:48:37 +0530

    WIP: Sponsors rendering plus added a Image block in shared

--- public_html/wp-content/mu-plugins/blocks/assets/src/shared/block-content/index.js
+++ public_html/wp-content/mu-plugins/blocks/assets/src/shared/block-content/index.js
@@ -1,7 +1,16 @@
 /**
+ * External dependencies
+ */
+import { get } from 'lodash';
+import classnames from 'classnames';
+
+
+/**
  * WordPress dependencies
  */
 const { __, _x, sprintf } = wp.i18n;
+const { decodeEntities } = wp.htmlEntities;
+
 
 export function arrayToHumanReadableList( array ) {
 	if ( ! Array.isArray( array ) ) {
@@ -43,4 +52,36 @@ export function arrayToHumanReadableList( array ) {
 	}
 
 	return list;
+}
+
+/**
+ * Renderes a features image for a post.
+ *
+ * @param post
+ * @param imageClass
+ * @param defaultImageClass
+ * @returns {*}
+ * @constructor
+ */
+export function WCPTFeaturedImage( { post, imageClass, defaultImageClass } ) {
+	let image;
+
+	const url = get( post, '_embedded[\'wp:featuredmedia\'][0].media_details.sizes.thumbnail.source_url', '' );
+
+	if ( url ) {
+		image = (
+			<img
+				src={ url }
+				alt={ decodeEntities( post.title.rendered.trim() ) }
+				className={ classnames( imageClass ) }
+			/>
+		);
+	} else {
+		image = (
+			<div className={defaultImageClass}/>
+		);
+	}
+
+	return image;
+
 }

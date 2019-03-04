a37e18d0d1ee6ace1f318b51e3652220737a4148
vedanshujain <vedanshu.jain.2012@gmail.com>
2019-03-04 13:03:26 +0530

    Blocks: Bug fixes and implementation in FeaturedImage component

--- public_html/wp-content/mu-plugins/blocks/assets/src/shared/featured-image/index.js
+++ public_html/wp-content/mu-plugins/blocks/assets/src/shared/featured-image/index.js
@@ -17,7 +17,8 @@ export default class FeaturedImage extends Component {
 	 * Constructor for component. Size can be provided, if height and width both are provided, then they take precedence over size.
 	 *
 	 * @param props Props for function.
-	 * @param props.availableSizes Available sizes of image along with source URL. Should be in this format:
+	 * @oaram props.wpMediaDetails Available sizes of images in the format as returned by WP API. This is the `sizes` object inside `media_details` inside `wp:featuredMedia` object. Not required if `props.availableSizes is present.
+	 * @param props.availableSizes Available sizes of image along with source URL. Not required if `props.wpMediaDetails` is present. Should be in this format:
 	 * [
 	 * 		{
 	 * 		 	height: 999,
@@ -35,32 +36,44 @@ export default class FeaturedImage extends Component {
 	 * @param props.height Height in pixels for image.
 	 * @param props.width Width in pixels for image. If both height and width are provided then they take precedence over `size`.
 	 * @param props.className Classname for image element
-	 * @param props.url URL of image
 	 * @param props.alt Alt text for image
 	 */
 	constructor( props ) {
 		super( props );
-		this.state = {};
-		this.parseAvailableSizes();
 	}
 
-	getSizeChart() {
-		return {
-				'icon'      : { height: 20, width: 20 },
-				'thumbnail' : { height: 150, width: 150 },
-				's'         : { height: 44, width: 150 },
-				'm'         : { height: 87, width: 300 },
-				'l'         : { height: 296, width: 1024
-			},
+	componentWillMount() {
+		let availableSizes;
+		const { wpMediaDetails } = this.props;
+		if ( wpMediaDetails ) {
+			availableSizes = FeaturedImage.parseWPMediaDetails( wpMediaDetails );
+		} else {
+			availableSizes = this.props.availableSizes;
 		}
+		this.parseAvailableSizes( availableSizes );
 	}
 
 	/**
-	 * Pre-calculate and store aspect ration of all images. This will help us in picking the best image available for the size that we have.
+	 * Converts from WP API featured_media object to array of different sizes.
+	 *
+	 * @param wpMediaDetails
 	 */
-	parseAvailableSizes() {
-		const { availableSizes } = this.props;
+	static parseWPMediaDetails( wpMediaDetails ) {
+		const availableSizes = [];
+		for ( const size of Object.keys( wpMediaDetails ) ) {
+			if ( ! wpMediaDetails.hasOwnProperty( size  ) ) {
+				continue;
+			}
 
+			availableSizes.push( wpMediaDetails[ size ] );
+		}
+		return availableSizes;
+	}
+
+	/**
+	 * Pre-calculate and store aspect ratio of all images. This will help us in picking the best image available for the size that we have.
+	 */
+	parseAvailableSizes( availableSizes ) {
 		let largestSizeValue = 0;
 		let largestSize = {};
 		// Lets pre-calculate aspect ratio of all images. Also cache the largest area image.
@@ -73,10 +86,10 @@ export default class FeaturedImage extends Component {
 		} );
 
 		// Lets sort the available images based on area and then aspect ratio.
-		sizes.sort( (size1, size2) => {
+		sizes.sort( ( size1, size2 ) => {
 				if ( size1.aspectRatio < size2.aspectRatio ) {
 					return -1;
-				} else if ( sizearea1.aspectRatio === size2.aspectRatio ) {
+				} else if ( size1.aspectRatio === size2.aspectRatio ) {
 					return size1.area < size2.area ? -1 : 1;
 				} else {
 					return 1;
@@ -86,74 +99,102 @@ export default class FeaturedImage extends Component {
 
 		this.setState(
 			{
-				sizes: sizes,
+				sizes,
 				...this.state
 			}
 		);
-
-		console.log("Sizes : ", sizes );
 	}
 
+	static getSizeChart() {
+		return {
+			'icon'      : { height: 20, width: 20 },
+			'thumbnail' : { height: 150, width: 150 },
+			's'         : { height: 44, width: 150 },
+			'm'         : { height: 87, width: 300 },
+			'l'         : { height: 296, width: 1024
+			},
+		}
+	}
+
+
 	/**
-	 * Returns URL of appropriate image. We calculate appropriate by looking for an image with similar aspect ratio, and similar or greater area.
+	 * Returns URL of appropriate image. We find appropriate by looking for an image with similar aspect ratio, and similar or greater area.
 	 * If there is no image with similar aspect ratio or area, we will return strictly bigger image then currently requested.
 	 * If there is no bigger image then currently request one, we will return the max size image URL.
-	 * Note: Core Image block allows user to select image size by name, but we cannot really do that because we will be controlling multiple images at once, and one or more images may not have all the available sizes.
+	 * Note: Core Image block allows user to select image size by name, but we cannot really do that because we will be controlling multiple images at once, and one or more images may not have all the available sizes. So an automated solution is preferred here.
 	 *
 	 * @param height
 	 * @param width
 	 */
 	getSizedUrl( height, width ) {
 		const { sizes } = this.state;
+		if ( ! sizes ) {
+			return "";
+		}
 
-		// Lets loop through all possible images, see if we have an image that matches out aspect ration. if we have, we will return it. Also keep track of image which has the most closest aspect ration.
+		// Lets loop through all possible images, see if we have an image that matches requested aspect ration. if we have, we will return it. Also keep track of image which has the most closest aspect ration.
 		let smallestImage;
 		let selectedImage;
 		const requiredAspectRatio = width / height;
 
-		sizes.each( ( size ) =>
-			{
-				if ( selectedImage ) {
-					// We have already found our required image.
-					return;
-				}
+		for ( const size of sizes ) {
 
-				// Reject all the images with less height or width then 20px.
-				if ( size.height < height - 20 || size.width < width - 20) {
-					return;
-				}
-
-				// Keep track of smallest image that is similar or bigger than our requirments. In case we won't find any image with similar aspect ratio, we will use this.
-				if ( ! smallestImage ) {
-					smallestImage = size;
-				}
-
-				if (size.aspectRatio > requiredAspectRatio - 0.25 &&
-					size.aspectRatio < requiredAspectRatio + 0.25) {
-					// This image has similar aspect ratio to what we need. Also, since possibleImage array is sorted from lowest size, this is also the lowest size image that we can use. Lets go ahead and use this image.
-					selectedImage = size;
-				}
+			// Reject all the images with less height or width then 20px.
+			if (size.height < height - 20 || size.width < width - 20) {
+				continue;
 			}
-		);
+
+			// Keep track of smallest image that is similar or bigger than our requirements. In case we won't find any image with similar aspect ratio, we will use this.
+			// Since images are sorted by size, this will be the first image that is not rejected by the size filter above.
+			if (!smallestImage) {
+				smallestImage = size;
+			}
+
+			if (size.aspectRatio > requiredAspectRatio - 0.25 &&
+				size.aspectRatio < requiredAspectRatio + 0.25) {
+				// This image has similar aspect ratio to what we need. Also, since possibleImage array is sorted from lowest size, this is also the lowest size image that we can use. Lets go ahead and use this image.
+				selectedImage = size;
+				break;
+			}
+		}
 
 		if ( selectedImage ) {
-			return selectedImage;
+			return selectedImage.source_url;
 		} else if ( smallestImage ) {
-			return smallestImage;
+			return smallestImage.source_url;
 		} else {
 			// All images are small, lets just return the largest image url.
-			return sizes[ sizes.length - 1 ];
+			return sizes[ sizes.length - 1 ].source_url;
 		}
 
 	}
 
+	/**
+	 * Renders FeaturedImage component.
+	 *
+	 * @returns {*}
+	 */
 	render() {
-		const { className, alt } = this.props;
+		const { className, alt, size } = this.props;
+
+		let imageSize;
+		if ( size ) {
+			imageSize = FeaturedImage.getSizeChart()[ size ];
+		} else {
+			imageSize = this.props || { height: 150, width: 150 };
+		}
+
+		const { height, width } = imageSize;
+
+		const imageURL = this.getSizedUrl( height, width );
+
 		return(
 			<img
 				className={ classnames( 'featured-image', className ) }
-				src = { get( this.getSizedUrl(), 'source_url', '' ) }
+				src = { imageURL }
 				alt = { alt }
+				height={ height }
+				width={ width }
 			/>
 		)
 	}
--- public_html/wp-content/mu-plugins/blocks/assets/src/sponsors/block-content.js
+++ public_html/wp-content/mu-plugins/blocks/assets/src/sponsors/block-content.js
@@ -1,12 +1,19 @@
+import { get } from 'lodash';
 import RawHTML from '@wordpress/element/build-module/raw-html';
-import {get} from 'lodash';
-import {FeaturedImage} from '../shared/block-content';
+import FeaturedImage from '../shared/featured-image';
 
 const { Component } = wp.element;
 
 function SponsorDetail( { sponsorPost } ) {
+	const featuredImageSizes = get( sponsorPost, "_embedded.wp:featuredmedia[0].media_details.sizes", {} );
+
 	return (
 		<div className={"wordcamp-sponsor-details"}>
+			<FeaturedImage
+				wpMediaDetails = { featuredImageSizes }
+				size = { 'm' }
+				alt = { sponsorPost.title.rendered }
+			/>
 			<div className={"wordcamp-sponsor-name"}>
 				<a href={sponsorPost.link} > <h3> { sponsorPost.title.rendered} </h3> </a>
 			</div>

1d18b8bdd7969126feab62661794025ff7de2ec5
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2019-03-08 12:37:27 -0800

    Speakers: Pull speakers session markup out into separate component

--- public_html/wp-content/mu-plugins/blocks/assets/src/speakers/block-content.js
+++ public_html/wp-content/mu-plugins/blocks/assets/src/speakers/block-content.js
@@ -17,7 +17,67 @@ const { __, _n, sprintf } = wp.i18n;
  */
 import AvatarImage from '../shared/avatar';
 import { ItemTitle, ItemHTMLContent } from "../shared/block-content";
+import { tokenSplit, arrayTokenReplace } from "../shared/i18n";
 import './block-content.scss';
+
+function SpeakerSessions( { speaker, tracks } ) {
+	const sessions = get( speaker, '_embedded.sessions', [] );
+
+	let output =( <Fragment>{ null }</Fragment> );
+
+	if ( sessions.length ) {
+		output = (
+			<Fragment>
+				<h4 className="wordcamp-speaker-session-heading">
+					{ _n( 'Session', 'Sessions', sessions.length, 'wordcamporg' ) }
+				</h4>
+
+				<ul className="wordcamp-speaker-session-list">
+					{ sessions.map( ( session ) =>
+						<li
+							key={ session.slug }
+							className="wordcamp-speaker-session-content"
+						>
+							<Disabled>
+								<a
+									className="wordcamp-speaker-session-link"
+									href={ session.link }
+								>
+									{ decodeEntities( session.title.rendered.trim() ) || __( '(Untitled)', 'wordcamporg' ) }
+								</a>
+								<br />
+								<span className="wordcamp-speaker-session-info">
+									{ session.session_track.length &&
+										arrayTokenReplace(
+											/* translators: 1: A date; 2: A time; 3: A location; */
+											tokenSplit( __( '%1$s at %2$s in %3$s', 'wordcamporg' ) ),
+											decodeEntities( session.session_date_time.date ),
+											decodeEntities( session.session_date_time.time ),
+											get( tracks.find( ( value ) => {
+												const [ firstTrackId ] = session.session_track;
+												return parseInt( value.id ) === firstTrackId;
+											} ), 'name' )
+										)
+									}
+									{ ! session.session_track.length &&
+										arrayTokenReplace(
+											/* translators: 1: A date; 2: A time; */
+											tokenSplit( __( '%1$s at %2$s', 'wordcamporg' ), ),
+											decodeEntities( session.session_date_time.date ),
+											decodeEntities( session.session_date_time.time )
+										)
+									}
+								</span>
+							</Disabled>
+						</li>
+					) }
+				</ul>
+			</Fragment>
+		);
+	}
+
+	return output;
+}
 
 class SpeakersBlockContent extends Component {
 	render() {
@@ -80,53 +140,11 @@ class SpeakersBlockContent extends Component {
 							/>
 						}
 
-						{ true === show_session && post._embedded.sessions &&
-							<Fragment>
-								<h4 className="wordcamp-speaker-session-heading">
-									{ _n( 'Session', 'Sessions', post._embedded.sessions.length, 'wordcamporg' ) }
-								</h4>
-
-								<ul className="wordcamp-speaker-session-list">
-									{ post._embedded.sessions.map( ( session ) =>
-										<li
-											key={ session.slug }
-											className="wordcamp-speaker-session-content"
-										>
-											<Disabled>
-												<a
-													className="wordcamp-speaker-session-link"
-													href={ session.link }
-												>
-													{ decodeEntities( session.title.rendered.trim() ) || __( '(Untitled)', 'wordcamporg' ) }
-												</a>
-												<br />
-												<span className="wordcamp-speaker-session-info">
-													{ session.session_track.length &&
-														sprintf(
-															/* translators: 1: A date; 2: A time; 3: A location; */
-															__( '%1$s at %2$s in %3$s', 'wordcamporg' ),
-															session.session_date_time.date,
-															session.session_date_time.time,
-															get( tracks.find( ( value ) => {
-																const [ firstTrackId ] = session.session_track;
-																return parseInt( value.id ) === firstTrackId;
-															} ), 'name' )
-														)
-													}
-													{ ! session.session_track.length &&
-														sprintf(
-															/* translators: 1: A date; 2: A time; */
-															__( '%1$s at %2$s', 'wordcamporg' ),
-															session.session_date_time.date,
-															session.session_date_time.time
-														)
-													}
-												</span>
-											</Disabled>
-										</li>
-									) }
-								</ul>
-							</Fragment>
+						{ true === show_session &&
+							<SpeakerSessions
+								speaker={ post }
+								tracks={ tracks }
+							/>
 						}
 					</li>,
 				) }

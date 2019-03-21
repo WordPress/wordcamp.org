533fc2b873b11d6ac8b31cc9c3a3a1acb1db3b38
vedanshujain <vedanshu.jain.2012@gmail.com>
2019-03-21 23:36:49 +0530

    Blocks: Add GridLayout component.
    This component provides functionality for having a grid/list layout for CPT that we render in our blocks. This was already implemented in Speakers block, and this patch refactors it out as a shared component.
    
    props vedjain

---
+++ public_html/wp-content/mu-plugins/blocks/assets/src/shared/grid-layout/block-content.js
@@ -0,0 +1,56 @@
+/**
+ * External dependencies.
+ */
+import classnames from 'classnames';
+
+/**
+ * WordPress dependencies.
+ */
+const { Component } = wp.element;
+
+/**
+ * Internal dependencies.
+ */
+import './style.scss';
+
+/**
+ * Implements grid / list layout for WordCamp blocks. Should be used with rest of the components in this folder. Uses attribute `layout` and `columnns`.
+ */
+class GridContentLayout extends Component {
+
+	render() {
+		const { attributes, className, children } = this.props;
+		const { grid_columns, layout } = attributes;
+
+		const containerClasses = [
+			'layout-' + layout,
+			className,
+			'wordcamp-block-post-list',
+		];
+
+		if ( 'grid' === layout ) {
+			containerClasses.push( 'grid-columns-' + Number( grid_columns ) );
+		}
+
+		return (
+			<ul className={ classnames( containerClasses ) }>
+				{
+					( children || [] ).map(
+						( childComponent ) => {
+							return (
+								<li
+									className={ classnames( 'wordcamp-grid-layout-item', 'wordcamp-block-post-list-item', 'wordcamp-clearfix' ) }
+								>
+									{ childComponent }
+								</li>
+							)
+						}
+					)
+				}
+			</ul>
+		);
+	}
+
+}
+
+export default GridContentLayout;

776e8adf3f1f37a17c123d7ae5de56c3d738a96a
vedjain <vedjain@git.wordpress.org>
2019-03-25 09:44:44 +0000

    Blocks: Add FeaturedImage component.
    This adds a featured image component, which allows to display and control sizes of multiple featured images rendered inside blocks. Note that there is still some work left to be done in this component.
    
    props vedjain
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@8506 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/mu-plugins/blocks/assets/src/shared/featured-image/inspector-control.js
@@ -0,0 +1,93 @@
+/**
+ * WordPress dependencies.
+ */
+const { Component } = wp.element;
+const { PanelBody, PanelRow, TextControl, BaseControl, SelectControl, ToggleControl } = wp.components;
+const { __ } = wp.i18n;
+import './style.scss';
+
+/**
+ * Not sure if these sizes are actually useful.
+ */
+const sizePresets = [
+	{
+		label : __( 'Thumbnail', 'wordcamporg' ),
+		value : '150',
+	},
+	{
+		label : __( 'Thumbnail - Large', 'wordcamporg' ),
+		value : '926',
+	},
+	{
+		label : __( 'Medium', 'wordcamporg' ),
+		value : '300',
+	},
+	{
+		label : __( 'Medium - Large', 'wordcamporg' ),
+		value : '768',
+	},
+	{
+		label : __( 'Large', 'wordcamporg' ),
+		value : '1024',
+	},
+	{
+		label : __( '(Custom)', 'wordcamporg' ),
+		value : '',
+	}
+];
+
+/**
+ * Implements inspector control for FeaturedImage component defined in ./index.js. Uses and sets attribute `featured_image_height` and `featured_image_width`.
+ */
+class FeaturedImageInspectorControls extends Component {
+
+	componentWillMount() {
+		this.availableSizes = sizePresets.map( (size) => size.value );
+	}
+
+	onPresetSizeSelect( size ) {
+		if ( size === '' ) {
+			return;
+		}
+		const { setAttributes } = this.props;
+		setAttributes( { featured_image_width: Number( size ) } );
+	}
+
+	render() {
+
+		const { attributes, setAttributes, title, help, selectLabel, cropLabel } = this.props;
+		const { featured_image_width } = attributes;
+		const selectedValue = this.availableSizes.indexOf( featured_image_width.toString() ) === -1 ? '' : featured_image_width.toString();
+		return (
+			<PanelBody
+				title = { title }
+				initialopen = { false }
+			>
+				<PanelRow>
+					<BaseControl
+						help = { help }
+					>
+						<PanelRow>
+							<SelectControl
+								label={ selectLabel }
+								value={ selectedValue }
+								options={ sizePresets }
+								onChange={ ( size ) => this.onPresetSizeSelect( size ) }
+							/>
+						</PanelRow>
+						<PanelRow>
+							<TextControl
+								label = { __('Width (in px)', 'wordcamporg' ) }
+								type = 'number'
+								value = { featured_image_width }
+								onChange = { ( width ) => setAttributes( { featured_image_width: Number( width ) } ) }
+							/>
+						</PanelRow>
+					</BaseControl>
+				</PanelRow>
+			</PanelBody>
+		);
+	}
+}
+
+export default FeaturedImageInspectorControls;

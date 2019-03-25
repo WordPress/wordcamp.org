39adde0c7a4c61d8623b68ed2f7cfaa54bdf3222
vedjain <vedjain@git.wordpress.org>
2019-03-25 10:08:31 +0000

    Blocks: Add GridLayout component.
    This component provides functionality for having a grid/list layout for CPT that we render in our blocks. This was already implemented in Speakers block, and this patch refactors it out as a shared component.
    
    props vedjain
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@8517 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/mu-plugins/blocks/assets/src/shared/grid-layout/inspector-control.js
@@ -0,0 +1,52 @@
+/**
+ * WordPress dependencies.
+ */
+const { Component } = wp.element;
+const { PanelBody, PanelRow, RangeControl } = wp.components;
+const { __ } = wp.i18n;
+
+const DEFAULT_SCHEMA = {
+	grid_columns: {
+		default: 2,
+		minimum: 2,
+		maximum: 4,
+	},
+};
+/**
+ * Add a slider for increasing and decreasing columns. Should be used with rest of the components in this folder. Will use and set attributes `layout` and `gird_columns`.
+ */
+class GridInspectorControl extends Component {
+
+	render() {
+		const { attributes, setAttributes } = this.props;
+		const { layout, grid_columns } = attributes;
+
+		if ( 'grid' !== layout ) {
+			return null;
+		}
+		const schema = DEFAULT_SCHEMA;
+
+		return(
+			<PanelBody>
+				<PanelBody
+					title={__('Layout', 'wordcamporg')}
+					initialOpen={true}
+				>
+					<PanelRow>
+						<RangeControl
+							label={__('Grid Columns', 'wordcamporg')}
+							value={ Number( grid_columns ) }
+							min={ schema.grid_columns.minimum }
+							max={ schema.grid_columns.maximum }
+							initialPosition={ schema.grid_columns.default }
+							onChange={(option) => setAttributes(
+								{grid_columns: option})}
+						/>
+					</PanelRow>
+				</PanelBody>
+			</PanelBody>
+		);
+	}
+}
+
+export default GridInspectorControl;

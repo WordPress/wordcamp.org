79e8c8c960e5d74de782f4a0b4a593912f9c0c93
vedjain <vedjain@git.wordpress.org>
2019-03-25 09:42:41 +0000

    Blocks: Add ItemTitle and ItemHTMLContent to render posts for blocks.
    This way all our blocks will have consistent HTML markup.
    
    props coreymckrill
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@8504 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/mu-plugins/blocks/assets/src/shared/block-content/index.js
@@ -0,0 +1,73 @@
+/**
+ * External dependencies
+ */
+import classnames from 'classnames';
+
+/**
+ * WordPress dependencies
+ */
+const { Disabled } = wp.components;
+const { Fragment, RawHTML } = wp.element;
+const { decodeEntities } = wp.htmlEntities;
+const { __ } = wp.i18n;
+
+/**
+ * Internal dependencies
+ */
+import './style.scss';
+
+export function ItemTitle( { headingLevel, className, title, link } ) {
+	const validLevels = [ 1, 2, 3, 4, 5, 6 ];
+	let Tag = 'h3';
+
+	if ( validLevels.includes( headingLevel ) ) {
+		Tag = 'h' + headingLevel;
+	}
+
+	const classes = [
+		'wordcamp-item-title',
+		className,
+	];
+
+	const content = title || __( '(Untitled)', 'wordcamporg' );
+
+	return (
+		<Tag className={ classnames( classes ) }>
+			{ link &&
+				<Disabled>
+					<a href={ link }>
+						{ decodeEntities( content ) }
+					</a>
+				</Disabled>
+			}
+
+			{ ! link &&
+				<Fragment>
+					{ decodeEntities( content ) }
+				</Fragment>
+			}
+		</Tag>
+	);
+}
+
+export function ItemHTMLContent( { className, content, link, linkText } ) {
+	const classes = [
+		'wordcamp-item-content',
+		className,
+	];
+
+	return (
+		<div className={ classnames( classes ) }>
+			<Disabled>
+				<RawHTML children={ content } />
+				{ link &&
+					<p className="wordcamp-item-permalink">
+						<a href={ link }>
+							{ linkText || __( 'Read more', 'wordcamporg' ) }
+						</a>
+					</p>
+				}
+			</Disabled>
+		</div>
+	);
+}

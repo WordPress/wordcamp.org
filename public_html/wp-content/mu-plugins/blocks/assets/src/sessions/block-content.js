a8b89b1223153a51deb2f2e627c1bb8ce2015856
vedjain <vedjain@git.wordpress.org>
2019-03-25 09:46:00 +0000

    Blocks: Add Session block.
    See the discussion here https://make.wordpress.org/community/2018/10/30/wordcamp-block-sessions/
    
    props coreymckrill, iandunn, vedjain
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@8514 74240141-8908-4e6f-9713-ba540dce6ec7

---
+++ public_html/wp-content/mu-plugins/blocks/assets/src/sessions/block-content.js
@@ -0,0 +1,218 @@
+/**
+ * External dependencies
+ */
+import { get } from 'lodash';
+import classnames from 'classnames';
+
+/**
+ * WordPress dependencies
+ */
+const { Disabled } = wp.components;
+const { Component } = wp.element;
+const { decodeEntities } = wp.htmlEntities;
+const { __ } = wp.i18n;
+
+/**
+ * Internal dependencies
+ */
+import { ItemTitle, ItemHTMLContent } from '../shared/block-content';
+import { tokenSplit, arrayTokenReplace, intersperse, listify } from '../shared/i18n';
+import GridContentLayout from '../shared/grid-layout/block-content';
+import FeaturedImage from '../shared/featured-image';
+
+function SessionSpeakers( { session } ) {
+	let speakerData = get( session, '_embedded.speakers', [] );
+
+	speakerData = speakerData.map( ( speaker ) => {
+		let { link = '', title = {} } = speaker;
+		title = title.rendered || __( 'Unnamed', 'wordcamporg' );
+
+		if ( ! link ) {
+			return decodeEntities( title.trim() );
+		}
+
+		return (
+			<a
+				key={ link }
+				href={ link }
+			>
+				{ decodeEntities( title.trim() ) }
+			</a>
+		);
+	} );
+
+	const speakers = arrayTokenReplace(
+		/* translators: %s is a list of names. */
+		tokenSplit( __( 'Presented by %s', 'wordcamporg' ) ),
+		[ listify( speakerData ) ]
+	);
+
+	return (
+		<div className="wordcamp-item-meta wordcamp-session-speakers">
+			{ speakers }
+		</div>
+	);
+}
+
+function SessionImage( { session } ) {
+	let image;
+
+	const url = get( session, '_embedded[\'wp:featuredmedia\'].media_details.sizes.thumbnail.source_url', '' );
+
+	if ( url ) {
+		image = (
+			<img
+				src={ url }
+				alt={ decodeEntities( session.title.rendered.trim() ) }
+				className={ classnames( 'wordcamp-session-image' ) }
+			/>
+		);
+	} else {
+		image = (
+			<div className="wordcamp-session-default-image" />
+		);
+	}
+
+	return image;
+}
+
+function SessionDetails( { session, show_meta, show_category } ) {
+	let meta, metaContent, category;
+	const terms = get( session, '_embedded[\'wp:term\']', [] ).flat();
+
+	if ( show_meta ) {
+		if ( session.session_track.length ) {
+			const [ firstTrack ] = terms.filter( ( term ) => {
+				return 'wcb_track' === term.taxonomy;
+			} );
+
+			metaContent = arrayTokenReplace(
+				/* translators: 1: A date; 2: A time; 3: A location; */
+				tokenSplit( __( '%1$s at %2$s in %3$s', 'wordcamporg' ) ),
+				[
+					decodeEntities( session.session_date_time.date ),
+					decodeEntities( session.session_date_time.time ),
+					(
+						<span className={ classnames( 'wordcamp-session-track', 'wordcamp-session-track-' + decodeEntities( firstTrack.slug.trim() ) ) }>
+							{ decodeEntities( firstTrack.name.trim() ) }
+						</span>
+					),
+				]
+			);
+		} else {
+			metaContent = arrayTokenReplace(
+				/* translators: 1: A date; 2: A time; */
+				tokenSplit( __( '%1$s at %2$s', 'wordcamporg' ) ),
+				[
+					decodeEntities( session.session_date_time.date ),
+					decodeEntities( session.session_date_time.time ),
+				]
+			);
+		}
+
+		meta = (
+			<div className="wordcamp-session-time-location">
+				{ metaContent }
+			</div>
+		);
+	}
+
+	if ( show_category && session.session_category.length ) {
+		/* translators: used between list items, there is a space after the comma */
+		const separator = __( ', ', 'wordcamporg' );
+		const categories = terms
+			.filter( ( term ) => {
+				return 'wcb_session_category' === term.taxonomy;
+			} )
+			.map( ( term ) => {
+				return (
+					<span
+						key={ term.slug }
+						className={ classnames( 'wordcamp-session-category', 'wordcamp-session-category-' + decodeEntities( term.slug ) ) }
+					>
+						{ decodeEntities( term.name.trim() ) }
+					</span>
+				);
+			} );
+
+		category = (
+			<div className="wordcamp-session-categories">
+				{ intersperse( categories, separator ) }
+			</div>
+		);
+	}
+
+	return (
+		<div className="wordcamp-item-meta wordcamp-session-details">
+			{ meta }
+			{ category }
+		</div>
+	);
+}
+
+class SessionsBlockContent extends Component {
+	hasSpeaker( session ) {
+		return get( session, '_embedded.speakers', [] ).length > 0;
+	}
+
+	render() {
+		const { attributes, sessionPosts } = this.props;
+		const { className, show_speaker, show_images, image_align, featured_image_height, featured_image_width, content, excerpt_more, show_meta, show_category } = attributes;
+		const featuredImageSize = { height: featured_image_height, width: featured_image_width };
+
+		return (
+			<GridContentLayout { ...this.props } >
+				{ sessionPosts.map( ( post ) =>
+					<div
+						key={ post.slug }
+						className={ classnames(
+							'wordcamp-block-post-list-item',
+							'wordcamp-session',
+							'wordcamp-session-' + decodeEntities( post.slug ),
+							'wordcamp-clearfix'
+						) }
+					>
+						<ItemTitle
+							className="wordcamp-session-title"
+							headingLevel={ 3 }
+							title={ post.title.rendered.trim() }
+							link={ post.link }
+						/>
+
+						{ show_speaker && this.hasSpeaker( post ) &&
+							<SessionSpeakers session={ post } />
+						}
+
+						{ show_images &&
+							<FeaturedImage
+								className={'wordcamp-session-image-container align-' + decodeEntities( image_align )  }
+								size={featuredImageSize}
+								wpMediaDetails={ get( post, "_embedded.wp:featuredmedia[0].media_details.sizes", {} ) }
+								alt={post.title.rendered}
+							/>
+						}
+
+						{ 'none' !== content &&
+							<ItemHTMLContent
+								className={ classnames( 'wordcamp-session-content-' + decodeEntities( content ) ) }
+								content={ 'full' === content ? post.content.rendered.trim() : post.excerpt.rendered.trim() }
+								link={ ( 'full' === content || excerpt_more ) ? post.link : null }
+								linkText={ 'full' === content ? __( 'Visit session page', 'wordcamporg' ) : __( 'Read more', 'wordcamporg' ) }
+							/>
+						}
+
+						{ ( show_meta || show_category ) &&
+							<SessionDetails
+								session={ post }
+								show_meta={ show_meta }
+								show_category={ show_category }
+							/>
+						}
+					</div>
+				) }
+			</GridContentLayout>
+		);
+	}
+}
+
+export default SessionsBlockContent;
---
+++ public_html/wp-content/mu-plugins/blocks/assets/src/sessions/edit.js
@@ -0,0 +1,142 @@
+/**
+ * External dependencies
+ */
+import { isUndefined, pickBy, split } from 'lodash';
+
+/**
+ * WordPress dependencies
+ */
+const apiFetch = wp.apiFetch;
+const { withSelect } = wp.data;
+const { Component, Fragment } = wp.element;
+const { addQueryArgs } = wp.url;
+
+/**
+ * Internal dependencies
+ */
+import SessionsBlockControls from './block-controls';
+import SessionsInspectorControls from './inspector-controls';
+import GridToolbar from '../shared/grid-layout/toolbar';
+
+const blockData = window.WordCampBlocks.sessions || {};
+
+const SESSIONS_ICON = 'list-view';
+const MAX_POSTS = 100;
+
+const ALL_POSTS_QUERY = {
+	orderby  : 'title',
+	order    : 'asc',
+	per_page : MAX_POSTS,
+	_embed   : true,
+};
+
+const ALL_TERMS_QUERY = {
+	orderby  : 'name',
+	order    : 'asc',
+	per_page : MAX_POSTS,
+};
+
+class SessionsEdit extends Component {
+	constructor( props ) {
+		super();
+
+		this.state = {
+			allSessionPosts      : null,
+			allSessionTracks     : null,
+			allSessionCategories : null,
+		};
+	}
+
+	componentWillMount() {
+		this.isStillMounted = true;
+
+		const allSessionPosts = apiFetch( {
+			path: addQueryArgs( `/wp/v2/sessions`, ALL_POSTS_QUERY ),
+		} );
+		const allSessionTracks = apiFetch( {
+			path: addQueryArgs( `/wp/v2/session_track`, ALL_TERMS_QUERY ),
+		} );
+		const allSessionCategories = apiFetch( {
+			path: addQueryArgs( `/wp/v2/session_category`, ALL_TERMS_QUERY ),
+		} );
+
+		if ( this.isStillMounted ) {
+			this.setState( {
+				allSessionPosts      : allSessionPosts, // Promise
+				allSessionTracks     : allSessionTracks, // Promise
+				allSessionCategories : allSessionCategories, // Promise
+			} );
+		}
+	}
+
+	componentWillUnmount() {
+		this.isStillMounted = false;
+	}
+
+	render() {
+		const { mode } = this.props.attributes;
+
+		return (
+			<Fragment>
+				<SessionsBlockControls
+					icon={ SESSIONS_ICON }
+					{ ...this.props }
+					{ ...this.state }
+				/>
+				{ mode &&
+				<Fragment>
+					<SessionsInspectorControls { ...this.props } />
+					<GridToolbar { ...this.props } />
+				</Fragment>
+				}
+			</Fragment>
+		);
+	}
+}
+
+const sessionsSelect = ( select, props ) => {
+	const { mode, item_ids, sort } = props.attributes;
+	const { getEntityRecords } = select( 'core' );
+
+	const args = {
+		per_page           : MAX_POSTS, // -1 is not allowed for per_page.
+		_embed             : true,
+		context            : 'view',
+		_wcpt_session_type : 'session',
+	};
+
+	if ( 'session_time' !== sort ) {
+		const [ orderby, order ] = split( sort, '_', 2 );
+		args.orderby = orderby;
+		args.order = order;
+	}
+
+	if ( Array.isArray( item_ids ) ) {
+		switch ( mode ) {
+			case 'wcb_session':
+				args.include = item_ids;
+				break;
+			case 'wcb_track':
+				args.session_track = item_ids;
+				break;
+			case 'wcb_session_category':
+				args.session_category = item_ids;
+				break;
+		}
+	}
+
+	const sessionsQuery = pickBy( args, ( value ) => ! isUndefined( value ) );
+
+	const sessionPosts = getEntityRecords( 'postType', 'wcb_session', sessionsQuery );
+
+	// todo Is there a way to do this sorting via REST API parameters?
+	if ( Array.isArray( sessionPosts ) && 'session_time' === sort ) {
+		sessionPosts.sort( ( a, b ) => {
+			return Number( a.meta._wcpt_session_time ) - Number( b.meta._wcpt_session_time );
+		} );
+	}
+
+	return { blockData, sessionPosts };
+};
+
+export const edit = withSelect( sessionsSelect )( SessionsEdit );

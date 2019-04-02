9b9f64533839a7788579f229d07f32ba65382444
vedjain <vedjain@git.wordpress.org>
2019-04-02 20:24:30 +0000

    WordCamp: Disable block editor for wordcamp post type due to incompatibility
    In block editor flow, when an organizer who do not have permission to change status, but are author of the post, tries to edit a WordCamp post, "Publish" button gets replaced with "Submit for Review" button. Since we do not support review flow for WordCamp Post Type, this has unintended consequences which includes post status being set to "Needs Vetting", there by accidentally changing WordCamp Status.
    
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@8567 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/mu-plugins/gutenberg-tweaks.php
+++ public_html/wp-content/mu-plugins/gutenberg-tweaks.php
@@ -16,3 +16,19 @@ function classic_editor_default_settings( $defaults ) {
 
 	return $defaults;
 }
+
+/**
+ * Disable block editor for WordCamp post types, since its not fully compatible yet.
+ * In block editor flow, when an organizer who do not have permission to change status, but are author of the post, tries to edit a WordCamp post, "Publish" button gets replaced with "Submit for Review" button.
+ * Since we do not support review flow for WordCamp Post Type, this has unintended consequences which includes post status being set to "Needs Vetting", there by accidentally changing WordCamp Status.
+ *
+ * @param string $status
+ * @param string $post_type
+ *
+ * @return bool
+ */
+function disable_block_editor_for_wordcamp( $use_block_editor, $post_type ) {
+	return $use_block_editor && WCPT_POST_TYPE_ID !== $post_type;
+}
+
+add_filter( 'use_block_editor_for_post_type', __NAMESPACE__ . '\disable_block_editor_for_wordcamp', 10, 2 );

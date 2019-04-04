f000d2f10fc6289da3b560fe189ba566fa82b226
vedanshujain <vedanshu.jain.2012@gmail.com>
2019-04-05 00:28:15 +0530

    WC Blocks: Change output markup to suppot links for consistency
    We already support linking featured image in editor. This patch adds support for linked image in rendered post as well. Also markup is changed a bit (especially classnames) to make both editor and rendered post consistent.
    
    Towards https://github.com/WordPress/wordcamp.org/issues/36

--- public_html/wp-content/mu-plugins/blocks/includes/shared/featured-image.php
+++ public_html/wp-content/mu-plugins/blocks/includes/shared/featured-image.php
@@ -7,15 +7,14 @@ namespace WordCamp\Blocks\Shared\Components;
  *
  * @param array    $class_names        Additional classes to add inside <img> tag.
  * @param \WP_Post $post               Current post object. This will be used to calculate srcset attribute.
- * @param int      $height             Height of the image.
  * @param int      $width              Width of the image.
  *
  * @return string Output markup for featured image.
  */
-function render_featured_image( $class_names, $post, $height, $width ) {
+function render_featured_image( $class_names, $post, $width, $image_link = '' ) {
 	$class_names[] = 'wordcamp-featured-image';
-	$class_names[] = 'wordcamp-featured-image-' . $post->post_name;
 	$class_names = implode( ' ', $class_names );
+	$container_classes = "wordcamp-image-container wordcamp-featured-image-container $class_names";
 	$attachment_id = get_post_thumbnail_id( $post->ID );
 	$image_data = wp_get_attachment_metadata( $attachment_id );
 	$size = 'post-thumbnail';
@@ -25,6 +24,35 @@ function render_featured_image( $class_names, $post, $height, $width ) {
 		$size = array( $width, $height );
 	}
 
+	$image = render_featured_image_element( $post, $size, $class_names );
+	ob_start();
+	?>
+		<div class="<?php echo esc_attr( $container_classes ); ?>">
+			<?php if ( '' !== $image_link ) { ?>
+				<div class="components-disabled">
+					<a href="<?php echo esc_html( $image_link ); ?>" class="wordcamp-image-link wordcamp-featured-image-link">
+						<?php echo wp_kses_post( $image ); ?>
+					</a>
+				</div>
+			<?php } else { ?>
+					<?php echo wp_kses_post( $image ); ?>
+			<?php } ?>
+		</div>
+	<?php
+
+	return ob_get_clean();
+}
+
+/**
+ * Helper method to render thumbnail image.
+ *
+ * @param \WP_Post     $post
+ * @param string|array $size
+ * @param string       $class_names
+ *
+ * @return string
+ */
+function render_featured_image_element( $post, $size, $class_names ) {
 	return get_the_post_thumbnail(
 		$post,
 		$size,

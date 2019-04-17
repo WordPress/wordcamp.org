185578d91e6f884e0f0087aa28ac75abf95f6cc6
vedanshujain <vedanshu.jain.2012@gmail.com>
2019-04-17 05:45:12 +0530

    WC Block: Support `siteSettings` in store to prevent repeat API calls
    Sponsor blocks makes settings API call to figure out order sponsor. This commit add support for site settings in blocks store so that it can be memoized and repeat API calls can be prevented.
    Ideally this can be removed if in future core store starts supporting `siteSettings`.

--- public_html/wp-content/mu-plugins/blocks/assets/src/blocks-store.js
+++ public_html/wp-content/mu-plugins/blocks/assets/src/blocks-store.js
@@ -6,7 +6,8 @@ import { intersection, orderBy, split } from 'lodash';
 /**
  * WordPress dependencies
  */
-const { registerStore, select } = wp.data;
+const { registerStore, select, dispatch } = wp.data;
+const apiFetch                            = wp.apiFetch;
 
 /**
  * Define store name.
@@ -15,9 +16,12 @@ export const WC_BLOCKS_STORE = 'wc-blocks-store';
 
 const MAX_POSTS = -1 ; // Its supported now.
 
+const DEFAULT_STATE = {};
+
 /**
  * Helper method to apply filters. Can be moved to a seperate file.
- * Don't use the output for this in a prop because it could cause perf issues, since ref of returned object will always be different.
+ * Don't use the output for this in a prop because it could cause perf issues,
+ * since ref of returned object will always be different.
  *
  * @param { Array }  entities List of entities to filter against
  * @param { Object } args     Arguments for filter function
@@ -58,6 +62,38 @@ export const filterEntities = ( entities, args ) => {
 };
 
 /**
+ * Defines actions provided by this store.
+ */
+const actions = {
+
+	/**
+	 * Queues api fetch for settings.
+	 *
+	 * @return {{type: string}}
+	 */
+	fetchSiteSettings() {
+		return {
+			type : 'FETCH_SETTINGS',
+		};
+	},
+
+	/**
+	 * Set the site settings object in this store.
+	 *
+	 * @param { Object } settings
+
+	 * @return { Object }
+	 */
+	setSiteSettings( settings ) {
+		return {
+			type     : 'SET_SETTINGS',
+			settings : settings,
+		};
+	},
+
+};
+
+/**
  * Defines selectors provided by this store.
  *
  * @type {{getPosts(*, *)}}
@@ -65,7 +101,8 @@ export const filterEntities = ( entities, args ) => {
 const selectors = {
 
 	/**
-	 * Returns post type from current state. Caches [state, entityType] for quick resolution.
+	 * Returns post type from current state. Caches [state, entityType] for
+	 * quick resolution.
 	 *
 	 * @param {Object} state      Current store state
 	 * @param {string} entityType Type of the entity to fetch
@@ -75,13 +112,44 @@ const selectors = {
 		return select( 'core' ).getEntityRecords( entityType, entityName, { _embed: true, per_page: MAX_POSTS } );
 	},
 
+	getSiteSettings( state ) {
+		if ( ! state.hasOwnProperty( 'siteSettings' ) ) {
+			dispatch( WC_BLOCKS_STORE ).fetchSiteSettings();
+			return null;
+		}
+		return state.siteSettings;
+	}
+
 };
 
 registerStore(
 	WC_BLOCKS_STORE,
 	{
 		selectors,
-		reducer( state, action ) {
+		actions,
+		reducer( state = DEFAULT_STATE, action ) {
+
+			switch ( action.type ) {
+				case 'FETCH_SETTINGS':
+					if ( state.loadingSettings ) {
+						break;
+					}
+
+					apiFetch( { path: '/wp/v2/settings' } ).then(
+						( fetchedSettings ) => {
+							dispatch( WC_BLOCKS_STORE ).setSiteSettings( fetchedSettings );
+						}
+					);
+
+					state.loadingSettings = true;
+					break;
+
+				case 'SET_SETTINGS':
+					state.siteSettings = action.settings;
+					state.loadingSettings = false;
+					break;
+			}
+
 			return state;
 		},
 	}

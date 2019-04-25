9ef1904738a3ed5c20171971ce599e9c03c18515
Ian Dunn <ian@iandunn.name>
2019-04-25 12:03:33 -0700

    try normal wide/full support

--- public_html/wp-content/mu-plugins/blocks/assets/src/schedule/index.js
+++ public_html/wp-content/mu-plugins/blocks/assets/src/schedule/index.js
@@ -22,6 +22,10 @@ export const settings = {
 
 	// todo keywordsto other blocks as well, in master and commit now
 
+	supports : {
+		align: [ 'wide', 'full' ],
+	},
+
 	/*
 	 * Make the block full-width _only_ in the editor.
 	 *
@@ -32,7 +36,7 @@ export const settings = {
 	 * themes that do and don't support the extra alignments that Gutenberg adds.
 	 *
 	 * Using the standard mechanism for full-width blocks would also add a toolbar icon which could be used to turn
-	 * off the wider view in the editor, which could cause confusion and clutter, without add benefit.
+	 * off the wider view in the editor, which could cause confusion and clutter, without added benefit.
 	 *
 	 * Another potential solution would have been to manually add CSS rules to make the block full-width in the editor,
 	 * but that would have meant duplicating the existing rules from Core, and tightly coupling the editing experience,
@@ -41,10 +45,12 @@ export const settings = {
 	 * Adding the alignment through `getEditWrapperProps()` allows us to achieve the desired result without any side-
 	 * effects.
 	 */
-	getEditWrapperProps( props ) {
-		return {
-			'data-align': 'full',
-			// maybe apply this conditionally based on screen width? show responsive layout on smaller screen?
-		};
-    },
+	//getEditWrapperProps( props ) {
+	//	return {
+	//		'data-align': 'full',
+	//		// maybe apply this conditionally based on screen width? show responsive layout on smaller screen?
+	//	};
+    //},
+	// probably remove ^ in favor of `supports align wide full` above, but test with 1 and 2 column schedules once get live data integrated
+	// if do that, then maybe turn `full` on by default if the camp has 3+ tracks, or maybe just always, and let them turn it off if they want
 };

b556a62a3de2f46d3ff570f48d9649585d9c78fc
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2019-04-26 13:20:52 -0700

    Remove unused import

--- public_html/wp-content/mu-plugins/blocks/assets/src/blocks.js
+++ public_html/wp-content/mu-plugins/blocks/assets/src/blocks.js
@@ -10,7 +10,6 @@ import * as organizers from './organizers/';
 import * as sessions from './sessions/';
 import * as speakers from './speakers/';
 import * as sponsors from './sponsors/';
-import './blocks-store';
 
 [
 	organizers,

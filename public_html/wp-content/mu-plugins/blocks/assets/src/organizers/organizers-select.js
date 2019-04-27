3df88a895deaefbb11a0958a5d340efd67f1a2e8
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2019-04-26 19:45:50 -0700

    Organizers: Refactor OrganizersSelect
    This does a few different things:
    
    * Extracts the `buildSelectOptions` into an external, memoized
      function. External so that it can be wrapped in `createSelector`,
      and so that it persists beyond one instance of the block.
      Memoized so that it only processes the data when one of the relevant
      props changes. In this case, the allOrganizerPosts and
      allOrganizerTerms props, which contain the data fetched from the
      store.
    * Eliminates the use of `getDerivedStateFromProps`. With
      `buildSelectOptions` extracted and cached, there's no need to
      use this lifecycle method, which may be inadvisable anyway (see
      link below).
    * Improves the way that the currently selected values in the Select
      are determined.
    
    About `getDerivedStateFromProps`:
    https://reactjs.org/blog/2018/06/07/you-probably-dont-need-derived-state.html

--- public_html/wp-content/mu-plugins/blocks/assets/src/organizers/organizers-select.js
+++ public_html/wp-content/mu-plugins/blocks/assets/src/organizers/organizers-select.js
@@ -1,7 +1,8 @@
 /**
  * External dependencies
  */
-import { get, includes } from 'lodash';
+import { flatMap, includes } from 'lodash';
+import createSelector    from 'rememo';
 
 /**
  * WordPress dependencies
@@ -14,132 +15,80 @@ const { __ }        = wp.i18n;
  * Internal dependencies
  */
 import { AvatarImage } from '../shared/avatar';
-import ItemSelect  from '../shared/item-select';
-import { ICON }    from './index';
+import ItemSelect      from '../shared/item-select';
+import { ICON }        from './index';
 
-class OrganizersSelect extends Component {
-	constructor( props ) {
-		super( props );
+const parseOrganizerPosts = ( posts ) => {
+	let parsed = [];
 
-		this.state = {
-			wcb_organizer      : [],
-			wcb_organizer_team : [],
-			loading            : true,
-		};
-
-		this.buildSelectOptions = this.buildSelectOptions.bind( this );
+	if ( Array.isArray( posts ) ) {
+		parsed = posts.map( ( post ) => {
+			return {
+				label  : post.title.rendered.trim() || __( '(Untitled)', 'wordcamporg' ),
+				value  : post.id,
+				type   : 'wcb_organizer',
+				avatar : post.avatar_urls[ '24' ],
+			};
+		} );
 	}
 
-	static getDerivedStateFromProps( props, state ) {
+	return parsed;
+};
+
+const parseOrganizerTerms = ( terms ) => {
+	let parsed = [];
+
+	if ( Array.isArray( terms ) ) {
+		parsed = terms.map( ( term ) => {
+			return {
+				label : term.name.trim() || __( '(Untitled)', 'wordcamporg' ),
+				value : term.id,
+				type  : 'wcb_organizer_team',
+				count : term.count,
+			};
+		} );
+	}
+
+	return parsed;
+};
+
+/**
+ * The select options only need to be generated once after all the data is loaded,
+ * so memoize them.
+ */
+const buildSelectOptions = createSelector(
+	( props ) => { console.log('built');
 		const { allOrganizerPosts, allOrganizerTerms } = props;
 
-		if ( false === state.loading ) {
-			return;
-		}
+		const optionTypes = [ 'wcb_organizer', 'wcb_organizer_team' ];
+		const options     = [];
 
-		let organizerLoaded = false;
-		let organizerTermsLoaded = false;
+		optionTypes.forEach( ( type ) => {
+			let label, items;
 
-		if ( allOrganizerPosts && Array.isArray( allOrganizerPosts ) ) {
-			state.wcb_organizer = allOrganizerPosts.map( ( post ) => {
-				return {
-					label  : post.title.rendered.trim() || __( '(Untitled)', 'wordcamporg' ),
-					value  : post.id,
-					type   : 'wcb_organizer',
-					avatar : post.avatar_urls[ '24' ],
-				};
-			} );
-			organizerLoaded = true;
-		}
-
-		if ( organizerLoaded && allOrganizerTerms && Array.isArray( allOrganizerTerms ) ) {
-			state.wcb_organizer_team = allOrganizerTerms.map( ( term ) => {
-				return {
-					label : term.name.trim() || __( '(Untitled)', 'wordcamporg' ),
-					value : term.id,
-					type  : 'wcb_organizer_team',
-					count : term.count,
-				};
-			} );
-			organizerTermsLoaded = true;
-		}
-
-		if ( organizerLoaded && organizerTermsLoaded ) {
-			state.loading = false;
-		}
-
-		return state;
-	}
-
-	buildSelectOptions( mode ) {
-		const { getOwnPropertyDescriptors } = Object;
-		const options = [];
-
-		const labels = {
-			wcb_organizer      : __( 'Organizers', 'wordcamporg' ),
-			wcb_organizer_team : __( 'Teams',      'wordcamporg' ),
-		};
-
-		for ( const type in getOwnPropertyDescriptors( this.state ) ) {
-			if ( ! this.state[ type ].length ) {
-				continue;
+			switch ( type ) {
+				case 'wcb_organizer':
+					label = __( 'Organizers', 'wordcamporg' );
+					items = parseOrganizerPosts( allOrganizerPosts );
+					break;
+				case 'wcb_organizer_team':
+					label = __( 'Teams', 'wordcamporg' );
+					items = parseOrganizerTerms( allOrganizerTerms );
+					break;
 			}
 
-			if ( mode && type !== mode ) {
-				continue;
+			if ( items.length ) {
+				options.push( {
+					label,
+					options: items,
+				} );
 			}
-
-			options.push( {
-				label   : labels[ type ],
-				options : this.state[ type ],
-			} );
-		}
+		} );
 
 		return options;
-	}
-
-	render() {
-		const { label, attributes, setAttributes } = this.props;
-		const { mode, item_ids }                   = attributes;
-		const options                              = this.buildSelectOptions( mode );
-
-		let value = [];
-
-		if ( mode && item_ids.length ) {
-			const modeOptions = get( options, '[0].options', [] );
-
-			value = modeOptions.filter( ( option ) => {
-				return includes( item_ids, option.value );
-			} );
-		}
-
-		return (
-			<ItemSelect
-				className="wordcamp-organizer-select"
-				label={ label }
-				value={ value }
-				buildSelectOptions={ this.buildSelectOptions }
-				onChange={ ( changed ) => setAttributes( changed ) }
-				mode={ mode }
-				selectProps={ {
-					isLoading        : this.state.loading,
-					formatGroupLabel : ( groupData ) => {
-						return (
-							<span className="wordcamp-item-select-option-group-label">
-								{ groupData.label }
-							</span>
-						);
-					},
-					formatOptionLabel: ( optionData ) => {
-						return (
-							<OrganizersOption { ...optionData } />
-						);
-					},
-				} }
-			/>
-		);
-	}
-}
+	},
+	( props ) => [ props.allOrganizerPosts, props.allOrganizerTerms ]
+);
 
 function OrganizersOption( { type, label = '', avatar = '', count = 0 } ) {
 	let image, content;
@@ -188,6 +137,69 @@ function OrganizersOption( { type, label = '', avatar = '', count = 0 } ) {
 			{ content }
 		</div>
 	);
+}
+
+class OrganizersSelect extends Component {
+	constructor( props ) {
+		super( props );
+
+		this.getCurrentSelectValue = this.getCurrentSelectValue.bind( this );
+		this.isLoading             = this.isLoading.bind( this );
+	}
+
+	getCurrentSelectValue() {
+		const { attributes } = this.props;
+		const { mode, item_ids } = attributes;
+
+		const options = flatMap( buildSelectOptions( this.props ), ( group ) => {
+			return group.options;
+		} );
+
+		let value = [];
+
+		if ( mode && item_ids.length ) {
+			value = options.filter( ( option ) => {
+				return mode === option.type && includes( item_ids, option.value );
+			} );
+		}
+
+		return value;
+	}
+
+	isLoading() {
+		const { allOrganizerPosts, allOrganizerTerms } = this.props;
+
+		return ! ( Array.isArray( allOrganizerPosts ) && Array.isArray( allOrganizerTerms ) );
+	}
+
+	render() {
+		const { label, setAttributes } = this.props;
+
+		return (
+			<ItemSelect
+				className="wordcamp-organizers-select"
+				label={ label }
+				value={ this.getCurrentSelectValue() }
+				onChange={ ( changed ) => setAttributes( changed ) }
+				selectProps={ {
+					options           : buildSelectOptions( this.props ),
+					isLoading         : this.isLoading(),
+					formatGroupLabel  : ( groupData ) => {
+						return (
+							<span className="wordcamp-item-select-option-group-label">
+								{ groupData.label }
+							</span>
+						);
+					},
+					formatOptionLabel : ( optionData ) => {
+						return (
+							<OrganizersOption { ...optionData } />
+						);
+					},
+				} }
+			/>
+		);
+	}
 }
 
 export default OrganizersSelect;

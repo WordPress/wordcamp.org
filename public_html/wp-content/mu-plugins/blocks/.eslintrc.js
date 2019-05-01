080b89568d73c39e8ab6fb5ef1a0a91a9a452232
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2019-05-01 11:38:17 -0700

    JS Linting: Add exception to `id-length` rule for `id`
    `id` is not always obvious and self-documenting, but often it is.
    I think it would be better to clarify with a code comment when
    necessary.

--- public_html/wp-content/mu-plugins/blocks/.eslintrc.js
+++ public_html/wp-content/mu-plugins/blocks/.eslintrc.js
@@ -131,7 +131,7 @@ module.exports = {
 		 */
 		'id-length' : [ 'error', {
 			'min'        : 3,
-			'exceptions' : [ '__', '_n', 'a', 'b' ]
+			'exceptions' : [ '__', '_n', 'id', 'a', 'b' ]
 		} ]
 	},
 };

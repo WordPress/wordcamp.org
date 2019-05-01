045adb0ad1f82836cc9946be2daf27db41555b14
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2019-04-30 21:35:07 -0700

    Sponsors: Sync JS and PHP content templates with code from other blocks
    This brings the code that generates the block content on both the front
    and back end into sync with the other blocks. It also eliminates the
    functionality for sorting sponsors by the sponsor level taxonomy. I
    believe we had a discussion where we decided not to include this sorting
    option, though I can't find a link to it now. If we need to restore
    that sorting option, we can pull the relevant code snippets back out
    of the repository.

--- public_html/wp-content/mu-plugins/blocks/assets/src/sponsors/block-content.js
+++ public_html/wp-content/mu-plugins/blocks/assets/src/sponsors/block-content.js
@@ -1,72 +1,22 @@
 /**
  * External dependencies
  */
-import {get, difference, intersection} from 'lodash';
-import classnames          from 'classnames';
+import { get }    from 'lodash';
+import classnames from 'classnames';
 
 /**
  * WordPress dependencies
  */
 const { Component }       = wp.element;
-const { escapeAttribute } = wp.escapeHtml;
 const { __ }              = wp.i18n;
 
 /**
  * Internal dependencies
  */
-import FeaturedImage                  from '../shared/featured-image';
-import GridContentLayout              from '../shared/grid-layout/block-content';
-import {ItemTitle, ItemHTMLContent, ItemPermalink} from '../shared/block-content';
-
-/**
- * Renders individual sponsor post inside editor.
- *
- * @param {Object} sponsorPost
- * @param {Object} attributes
- *
- * @return {Element}
- */
-function SponsorDetail( { sponsorPost, attributes } ) {
-	const { show_name, show_logo, content, featured_image_width } = attributes;
-	const displayContent = 'full' === content ? sponsorPost.content.rendered.trim() : sponsorPost.excerpt.rendered.trim();
-
-	return (
-		<div className={ 'wordcamp-sponsor-details wordcamp-sponsor-details-' + escapeAttribute( sponsorPost.slug ) }>
-
-			{ ( show_name || show_name === undefined ) &&
-				<ItemTitle
-					className="wordcamp-sponsor-title"
-					headingLevel={ 3 }
-					title={ sponsorPost.title.rendered.trim() }
-					link={ sponsorPost.link }
-				/>
-			}
-
-			{ ( show_logo || show_logo === undefined ) &&
-				<FeaturedImage
-					imageData={ get( sponsorPost, '_embedded.wp:featuredmedia[0]', {} ) }
-					width={ featured_image_width }
-					className={ classnames( 'wordcamp-sponsor-featured-image', 'wordcamp-sponsor-logo' ) }
-					imageLink={ sponsorPost.link }
-				/>
-			}
-
-			{ ( 'none' !== content ) &&
-				<ItemHTMLContent
-					className={ classnames( 'wordcamp-sponsor-content' ) }
-					content={ displayContent }
-				/>
-			}
-
-			{ ( 'full' === content ) &&
-				<ItemPermalink
-					link={ sponsorPost.link }
-					linkText={ __( 'Visit sponsor page', 'wordcamporg' ) }
-				/>
-			}
-		</div>
-	);
-}
+import FeaturedImage                                                 from '../shared/featured-image';
+import GridContentLayout                                             from '../shared/grid-layout/block-content';
+import { ItemTitle, ItemHTMLContent, ItemPermalink, BlockNoContent } from '../shared/block-content';
+import { filterEntities }                                            from "../blocks-store";
 
 /**
  * Component for rendering Sponsors post inside editor.
@@ -74,65 +24,29 @@ function SponsorDetail( { sponsorPost, attributes } ) {
 class SponsorBlockContent extends Component {
 	constructor( props ) {
 		super( props );
-		this.state = {
-			selectedPosts : [],
-		};
+
+		this.getFilteredPosts = this.getFilteredPosts.bind( this );
 	}
 
-	static getDerivedStateFromProps( nextProps, state ) {
-		// Sort the sponsor posts. Since this could potentially be expensive, lets do it in getDerivedStateFromProps hook and set state with result if anything is changed.
-		const {
-			selectedPosts    : newSelectedPosts,
-			attributes       : newAttributes,
-			sponsorTermOrder : newSponsorTermOrder
-		} = nextProps;
+	getFilteredPosts() {
+		const { attributes, entities } = this.props;
+		const { wcb_sponsor: posts } = entities;
+		const { mode, item_ids, sort } = attributes;
 
-		const { sort_by: newSortBy } = newAttributes;
-		const newSelectedPostIds = newSelectedPosts.map( ( post ) => post.id ).sort();
+		const args = {};
 
-		const { selectedPosts, sortBy } = state;
-		const selectedPostsIds = selectedPosts.map( ( post ) => post.id ).sort();
-
-		if ( sortBy === newSortBy && newSelectedPosts.length === selectedPosts.length && difference( selectedPostsIds, newSelectedPostIds ).length === 0 ) {
-			// Everything is same. No need to calculate sorting. Lets bail.
-			return;
-		}
-
-		let sortedPosts;
-
-		switch ( newSortBy ) {
-			case 'sponsor_level' :
-				if ( ! Array.isArray( newSponsorTermOrder ) ||
-					newSponsorTermOrder.length === 0 ) {
-					break;
+		if ( Array.isArray( item_ids ) && item_ids.length > 0 ) {
+			args.filter  = [
+				{
+					fieldName: mode === 'wcb_sponsor' ? 'id' : 'wcb_sponsor_level',
+					fieldValue: item_ids,
 				}
-				sortedPosts = newSelectedPosts.sort( ( sponsor1, sponsor2 ) => {
-					return newSponsorTermOrder.indexOf( ( sponsor1.sponsor_level || [] )[ 0 ] ) - newSponsorTermOrder.indexOf( ( sponsor2.sponsor_level || [] )[ 0 ] );
-				} );
-				break;
-
-			case 'name_desc' :
-				sortedPosts = newSelectedPosts.sort( ( sponsor1, sponsor2 ) => {
-					const title1 = sponsor1.title.rendered.trim();
-					const title2 = sponsor2.title.rendered.trim();
-					return title1 > title2 ? -1 : 1;
-				} );
-				break;
-
-			case 'name_asc' :
-			default:
-				sortedPosts = newSelectedPosts.sort( ( sponsor1, sponsor2 ) => {
-					const title1 = sponsor1.title.rendered.trim();
-					const title2 = sponsor2.title.rendered.trim();
-					return title1 < title2 ? -1 : 1;
-				} );
-				break;
+			];
 		}
 
-		return( {
-			selectedPosts : sortedPosts,
-			sortBy        : newSortBy,
-		} );
+		args.sort = sort;
+
+		return filterEntities( posts, args );
 	}
 
 	/**
@@ -141,24 +55,65 @@ class SponsorBlockContent extends Component {
 	 * @return {Element}
 	 */
 	render() {
-		const { selectedPosts } = this.state;
-		const { attributes }    = this.props;
+		const { attributes } = this.props;
+		const { show_name, show_logo, content, featured_image_width } = attributes;
+
+		const posts     = this.getFilteredPosts();
+		const isLoading = ! Array.isArray( posts );
+		const hasPosts  = ! isLoading && posts.length > 0;
+
+		if ( isLoading || ! hasPosts ) {
+			return (
+				<BlockNoContent loading={ isLoading } />
+			);
+		}
+
 		return (
 			<GridContentLayout
 				className="wordcamp-sponsors-block"
 				{ ...this.props }
 			>
-				{
-					selectedPosts.map( ( post ) => {
-						return (
-							<SponsorDetail
-								key={ post.id }
-								sponsorPost={ post }
-								attributes={ attributes }
+				{ posts.map( ( post ) =>
+					<div
+						key={ post.slug }
+						className={ classnames(
+							'wordcamp-sponsor',
+							'wordcamp-sponsor-' + post.slug,
+						) }
+					>
+						{ show_name &&
+							<ItemTitle
+								className="wordcamp-sponsor-title"
+								headingLevel={ 3 }
+								title={ post.title.rendered.trim() }
+								link={ post.link }
 							/>
-						);
-					} )
-				}
+						}
+
+						{ show_logo &&
+							<FeaturedImage
+								imageData={ get( post, '_embedded.wp:featuredmedia[0]', {} ) }
+								width={ featured_image_width }
+								className={ classnames( 'wordcamp-sponsor-featured-image', 'wordcamp-sponsor-logo' ) }
+								imageLink={ post.link }
+							/>
+						}
+
+						{ ( 'none' !== content ) &&
+							<ItemHTMLContent
+								className={ classnames( 'wordcamp-sponsor-content-' + content ) }
+								content={ 'full' === content ? post.content.rendered.trim() : post.excerpt.rendered.trim() }
+							/>
+						}
+
+						{ ( 'full' === content ) &&
+							<ItemPermalink
+								link={ post.link }
+								linkText={ __( 'Visit sponsor page', 'wordcamporg' ) }
+							/>
+						}
+					</div>
+				) }
 			</GridContentLayout>
 		);
 	}

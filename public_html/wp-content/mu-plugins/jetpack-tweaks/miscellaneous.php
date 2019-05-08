9d3327bf22fd0238258ca5f7ae701fe1b98f26fa
iandunn <iandunn@git.wordpress.org>
2019-05-08 17:02:43 +0000

    Jetpack Tweaks: Remove duplicate JITM-disabling code.
    Similar code is being run in the `jetpack-disable-notices` plugin that's shared with w.org, so this isn't needed here.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@8750 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/mu-plugins/jetpack-tweaks/miscellaneous.php
+++ public_html/wp-content/mu-plugins/jetpack-tweaks/miscellaneous.php
@@ -7,22 +7,6 @@ defined( 'WPINC' ) || die();
 add_filter( 'jetpack_photon_reject_https',    '__return_false' );
 
 /**
- * Modify hooks after Jetpack::init()
- *
- * @todo This may be unnecessary and removable since there is now a similar wporg mu-plugin. See [dotorg13221].
- */
-function modify_hooks_after_jetpack_init() {
-	/*
-	 * Many of these messages are just marketing upsells for services we don't need, don't want, or have
-	 * alternatives for already. Those messages aren't relevant to organizers, so they just intrude on their work
-	 * unnecessarily.
-	 */
-	add_filter( 'jetpack_just_in_time_msgs', '__return_false', 11 );
-}
-
-add_action( 'plugins_loaded', __NAMESPACE__ . '\modify_hooks_after_jetpack_init' );
-
-/**
  * Filter the post types Jetpack has access to, and can synchronize with WordPress.com.
  *
  * @see Jetpack's WPCOM_JSON_API_ENDPOINT::_get_whitelisted_post_types();

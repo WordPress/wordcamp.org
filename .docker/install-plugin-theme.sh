8922b9d743cdb24b72272fa878d3e12877f2b723
vedjain <vedjain@git.wordpress.org>
2019-05-20 11:52:58 +0000

    WC Docker: Add `twenty*` themes to match production closely
    We already have default theme installed in production, so it makes sense to install them in Docker as well.
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@8824 74240141-8908-4e6f-9713-ba540dce6ec7

--- .docker/install-plugin-theme.sh
+++ .docker/install-plugin-theme.sh
@@ -2,8 +2,8 @@
 
 # Install plugins
 echo "Installing plugins"
-wp --allow-root --url=wordcamp.test plugin install akismet buddypress bbpress jetpack wp-multibyte-patch wordpress-importer polldaddy liveblog wp-super-cache custom-content-width camptix-network-tools email-post-changes tagregator supportflow camptix-pagseguro camptix-payfast-gateway camptix-trustpay camptix-trustcard camptix-mercadopago camptix-kdcpay-gateway campt-indian-payment-gateway camptix
+wp --allow-root plugin install akismet buddypress bbpress jetpack wp-multibyte-patch wordpress-importer polldaddy liveblog wp-super-cache custom-content-width camptix-network-tools email-post-changes tagregator supportflow camptix-pagseguro camptix-payfast-gateway camptix-trustpay camptix-trustcard camptix-mercadopago camptix-kdcpay-gateway campt-indian-payment-gateway camptix
 
 # Install themes
 echo "Installing themes"
-wp --allow-root --url=wordcamp.test theme install twentyten twentyeleven twentytwelve twentythirteen twentyfourteen
+wp --allow-root theme install twentyten twentyeleven twentytwelve twentythirteen twentyfourteen twentyfifteen twentysixteen twentyseventeen twentyeighteen p2

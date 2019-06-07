44bfc4613d3cc82fe92f28b62b3d13865879c1b8
vedjain <vedjain@git.wordpress.org>
2019-06-07 17:33:11 +0000

    WordCamp: Enabled blocks for WC Boston, Taipei and Sevilla.
    See https://make.wordpress.org/community/2019/05/29/who-wants-to-test-the-new-wordcamp-blocks/
    
    
    git-svn-id: https://meta.svn.wordpress.org/sites/trunk/wordcamp.org@8936 74240141-8908-4e6f-9713-ba540dce6ec7

--- public_html/wp-content/mu-plugins/load-other-mu-plugins.php
+++ public_html/wp-content/mu-plugins/load-other-mu-plugins.php
@@ -18,7 +18,7 @@ function wcorg_include_individual_mu_plugins() {
 
 	if (
 		( defined( 'WORDCAMP_ENVIRONMENT' ) && 'production' !== WORDCAMP_ENVIRONMENT )
-		|| in_array( get_current_blog_id(), [ 928, 1028, 1126 ], true ) // Beta optins.
+		|| in_array( get_current_blog_id(), [ 928, 1028, 1126, 1099, 1160, 1093 ], true ) // Beta opt-ins.
 	) {
 		require_once( __DIR__ . '/blocks/blocks.php' );
 	}

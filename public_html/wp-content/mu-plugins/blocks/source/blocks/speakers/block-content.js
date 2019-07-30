8c72b527c74c1e7c36f0a4c5422210c9ac685a7e
Kelly Dwan <ryelle@users.noreply.github.com>
2019-07-30 10:37:33 -0400

    Blocks: Match speakers output to mockups (#176)
    * Simplify speakers block content component
    * Move .scss file to consistent location for blocks
    * Update session list to use mockup styles
    * Remove use of `Disabled` over links for consistent interaction between keyboard & mouse use. See https://github.com/WordPress/wordcamp.org/pull/171#issuecomment-516200569
    
    Fixes #94.

--- public_html/wp-content/mu-plugins/blocks/source/blocks/speakers/block-content.js
+++ public_html/wp-content/mu-plugins/blocks/source/blocks/speakers/block-content.js
@@ -1,15 +1,13 @@
 /**
  * External dependencies
  */
-import { get }    from 'lodash';
-import classnames from 'classnames';
+import { get } from 'lodash';
 
 /**
  * WordPress dependencies
  */
-import { Disabled }            from '@wordpress/components';
-import { Component, Fragment } from '@wordpress/element';
-import { __, _n }              from '@wordpress/i18n';
+import { __, _n }    from '@wordpress/i18n';
+import { Component } from '@wordpress/element';
 
 /**
  * Internal dependencies
@@ -19,8 +17,6 @@ import { AvatarImage }                                               from '../..
 import { PostList }                                                  from '../../components/post-list';
 import { filterEntities }                                            from '../../data';
 import { tokenSplit, arrayTokenReplace }                             from '../../i18n';
-
-import './block-content.scss';
 
 /**
  * Component for the section of each speaker post that displays information about relevant sessions.
@@ -35,63 +31,59 @@ import './block-content.scss';
 function SpeakerSessions( { speaker, tracks } ) {
 	const sessions = get( speaker, '_embedded.sessions', [] );
 
-	let output = ( <Fragment>{ null }</Fragment> );
-
-	if ( sessions.length ) {
-		output = (
-			<div className="wordcamp-speakers__sessions">
-				<h4 className="wordcamp-speakers__sessions-heading">
-					{ _n( 'Session', 'Sessions', sessions.length, 'wordcamporg' ) }
-				</h4>
-
-				<ul className="wordcamp-speakers__sessions-list">
-					{ sessions.map( ( session ) =>
-						<li
-							key={ session.id }
-							className="wordcamp-speakers__sessions-list-item"
-						>
-							<Disabled>
-								<a
-									className="wordcamp-speakers__session-link"
-									href={ session.link }
-								>
-									{ session.title.rendered.trim() || __( '(Untitled)', 'wordcamporg' ) }
-								</a>
-								<span className="wordcamp-speakers__session-info">
-									{ session.session_track.length && Array.isArray( tracks ) &&
-										arrayTokenReplace(
-											/* translators: 1: A date; 2: A time; 3: A location; */
-											tokenSplit( __( '%1$s at %2$s in %3$s', 'wordcamporg' ) ),
-											[
-												session.session_date_time.date,
-												session.session_date_time.time,
-												get( tracks.find( ( value ) => {
-													const [ firstTrackId ] = session.session_track;
-													return parseInt( value.id ) === firstTrackId;
-												} ), 'name' ),
-											]
-										)
-									}
-									{ ( ! session.session_track.length || ! Array.isArray( tracks ) ) &&
-										arrayTokenReplace(
-											/* translators: 1: A date; 2: A time; */
-											tokenSplit( __( '%1$s at %2$s', 'wordcamporg' ), ),
-											[
-												session.session_date_time.date,
-												session.session_date_time.time,
-											]
-										)
-									}
-								</span>
-							</Disabled>
-						</li>
-					) }
-				</ul>
-			</div>
-		);
+	if ( ! sessions.length ) {
+		return null;
 	}
 
-	return output;
+	return (
+		<div className="wordcamp-speakers__sessions">
+			<h4 className="wordcamp-speakers__sessions-heading">
+				{ _n( 'Session', 'Sessions', sessions.length, 'wordcamporg' ) }
+			</h4>
+
+			<ul className="wordcamp-speakers__sessions-list">
+				{ sessions.map( ( session ) =>
+					<li
+						key={ session.id }
+						className="wordcamp-speakers__sessions-list-item"
+					>
+						<a
+							className="wordcamp-speakers__session-link"
+							href={ session.link }
+							target="_blank"
+							rel="noopener noreferrer"
+						>
+							{ session.title.rendered.trim() || __( '(Untitled)', 'wordcamporg' ) }
+						</a>
+						<span className="wordcamp-speakers__session-info">
+							{ ( session.session_track.length && Array.isArray( tracks ) ) ?
+								arrayTokenReplace(
+									/* translators: 1: A date; 2: A time; 3: A location; */
+									tokenSplit( __( '%1$s at %2$s in %3$s', 'wordcamporg' ) ),
+									[
+										session.session_date_time.date,
+										session.session_date_time.time,
+										get( tracks.find( ( value ) => {
+											const [ firstTrackId ] = session.session_track;
+											return parseInt( value.id ) === firstTrackId;
+										} ), 'name' ),
+									]
+								) :
+								arrayTokenReplace(
+									/* translators: 1: A date; 2: A time; */
+									tokenSplit( __( '%1$s at %2$s', 'wordcamporg' ), ),
+									[
+										session.session_date_time.date,
+										session.session_date_time.time,
+									]
+								)
+							}
+						</span>
+					</li>
+				) }
+			</ul>
+		</div>
+	);
 }
 
 /**
@@ -157,16 +149,13 @@ export class BlockContent extends Component {
 
 		return (
 			<PostList
-				{ ...this.props }
+				attributes={ attributes }
 				className="wordcamp-speakers"
 			>
 				{ posts.map( ( post ) =>
 					<div
 						key={ post.slug }
-						className={ classnames(
-							'wordcamp-speakers__post',
-							`slug-${ post.slug }`,
-						) }
+						className={ `wordcamp-speakers__post slug-${ post.slug }` }
 					>
 						<ItemTitle
 							className="wordcamp-speakers__title"
@@ -177,7 +166,7 @@ export class BlockContent extends Component {
 
 						{ show_avatars &&
 							<AvatarImage
-								className={ classnames( 'align-' + avatar_align ) }
+								className={ `align-${ avatar_align }` }
 								name={ post.title.rendered.trim() || '' }
 								size={ avatar_size }
 								url={ post.avatar_urls[ '24' ] }
@@ -187,7 +176,7 @@ export class BlockContent extends Component {
 
 						{ ( 'none' !== content ) &&
 							<DangerousItemHTMLContent
-								className={ classnames( 'wordcamp-speakers__content', 'is-' + content ) }
+								className={ `wordcamp-speakers__content is-${ content }` }
 								content={ 'full' === content ? post.content.rendered.trim() : post.excerpt.rendered.trim() }
 							/>
 						}

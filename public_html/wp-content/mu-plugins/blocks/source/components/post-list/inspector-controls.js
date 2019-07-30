02f8fa868b90c32789eb159753879466cd25b94b
Kelly Dwan <ryelle@users.noreply.github.com>
2019-07-30 15:13:33 -0400

    Blocks: Add propTypes to post-list InspectorControls components (#168)
    Clean up InspectorControls components and add propTypes for props validation during development.

--- public_html/wp-content/mu-plugins/blocks/source/components/post-list/inspector-controls.js
+++ public_html/wp-content/mu-plugins/blocks/source/components/post-list/inspector-controls.js
@@ -1,75 +1,61 @@
 /**
- * WordPress dependencies
+ * External dependencies
  */
-import { PanelBody, RangeControl } from '@wordpress/components';
-import { Component, Fragment }     from '@wordpress/element';
-import { __ }                      from '@wordpress/i18n';
+import { get }   from 'lodash';
+import PropTypes from 'prop-types';
 
 /**
- * Component for a range control that adjusts the number of columns in a post list grid.
- *
- * For use within a block, consider GridInspectorPanel instead unless you want to include this control
- * in a panel with other unrelated controls.
- *
- * @param {Object} props {
- *     @type {number}   grid_columns
- *     @type {Object}   schema
- *     @type {Function} setAttributes
- * }
- *
- * @return {Element}
+ * WordPress dependencies
  */
-export function GridColumnsControl( {
-	grid_columns,
-	schema,
-	setAttributes,
-} ) {
-	const { default: defaultValue = 2, maximum = 4, minimum = 4 } = schema;
-
-	return (
-		<RangeControl
-			label={ __( 'Grid Columns', 'wordcamporg' ) }
-			value={ Number( grid_columns ) }
-			min={ minimum }
-			max={ maximum }
-			initialPosition={ defaultValue }
-			onChange={ ( value ) => setAttributes( { grid_columns: value } ) }
-		/>
-	);
-}
+import { __ }                      from '@wordpress/i18n';
+import { Fragment }                from '@wordpress/element';
+import { PanelBody, RangeControl } from '@wordpress/components';
 
 /**
  * Component to add an Inspector panel
  *
- * Should be used with rest of the components in this folder. Will use and set attributes `layout` and
- * `grid_columns`.
+ * Intended to be used with PostList component, it will use and set attributes `layout` and `grid_columns`.
+ *
+ * @return {Element}
  */
-export class GridInspectorPanel extends Component {
-	/**
-	 * Render the control.
-	 *
-	 * @return {Element}
-	 */
-	render() {
-		const { attributes, setAttributes, blockData } = this.props;
-		const { layout, grid_columns } = attributes;
-		const { schema } = blockData;
+function GridInspectorPanel( { attributes, blockData, setAttributes } ) {
+	const { grid_columns, layout } = attributes;
+	const {
+		default: defaultValue = 2,
+		maximum = 4,
+		minimum = 4,
+	} = get( blockData, 'schema.grid_columns', {} );
 
-		return (
-			<Fragment>
-				{ 'grid' === layout &&
-					<PanelBody
-						title={ __( 'Grid Layout', 'wordcamporg' ) }
-						initialOpen={ true }
-					>
-						<GridColumnsControl
-							grid_columns={ grid_columns }
-							schema={ schema.grid_columns || {} }
-							setAttributes={ setAttributes }
-						/>
-					</PanelBody>
-				}
-			</Fragment>
-		);
-	}
+	return (
+		<Fragment>
+			{ 'grid' === layout &&
+				<PanelBody
+					title={ __( 'Grid Layout', 'wordcamporg' ) }
+					initialOpen={ true }
+				>
+					<RangeControl
+						label={ __( 'Grid Columns', 'wordcamporg' ) }
+						value={ Number( grid_columns ) }
+						min={ minimum }
+						max={ maximum }
+						initialPosition={ defaultValue }
+						onChange={ ( value ) => setAttributes( { grid_columns: value } ) }
+					/>
+				</PanelBody>
+			}
+		</Fragment>
+	);
 }
+
+GridInspectorPanel.propTypes = {
+	attributes: PropTypes.shape( {
+		layout       : PropTypes.string,
+		grid_columns : PropTypes.number,
+	} ).isRequired,
+	blockData: PropTypes.shape( {
+		schema: PropTypes.object,
+	} ).isRequired,
+	setAttributes: PropTypes.func.isRequired,
+};
+
+export default GridInspectorPanel;

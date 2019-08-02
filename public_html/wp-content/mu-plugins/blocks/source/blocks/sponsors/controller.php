7e0bafac0facef73b72a05d2baaaeb2fbb6c4070
Kelly Dwan <ryelle@users.noreply.github.com>
2019-08-02 15:35:10 -0400

    Add heading alignment attribute
    This uses the same configuration as the image alignment, since they have the same options & defaults

--- public_html/wp-content/mu-plugins/blocks/source/blocks/sponsors/controller.php
+++ public_html/wp-content/mu-plugins/blocks/source/blocks/sponsors/controller.php
@@ -139,6 +139,7 @@ function get_attributes_schema() {
 			'align'                => get_shared_definition( 'align_block', 'attribute' ),
 			'className'            => get_shared_definition( 'string_empty', 'attribute' ),
 			'featured_image_width' => get_shared_definition( 'image_size', 'attribute', [ 'default' => 600 ] ),
+			'headingAlign'         => get_shared_definition( 'align_image', 'attribute' ),
 			'image_align'          => get_shared_definition( 'align_image', 'attribute' ),
 			'mode'                 => [
 				'type'    => 'string',

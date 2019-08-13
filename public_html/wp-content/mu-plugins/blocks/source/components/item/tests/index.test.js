d3c135d2c4f3889c796637e266b80f657610d527
Kelly Dwan <ryelle@users.noreply.github.com>
2019-08-13 14:16:17 -0400

    Blocks: Add jest infrastructure for component testing (#211)
    Add test infrastructure and some trivial tests for demonstration. This also updates the blocks readme to include testing instructions.

---
+++ public_html/wp-content/mu-plugins/blocks/source/components/item/tests/index.test.js
@@ -0,0 +1,50 @@
+/* global describe, test, expect */
+/**
+ * External dependencies
+ */
+import renderer from 'react-test-renderer';
+
+/**
+ * Internal dependencies
+ */
+import { ItemTitle } from '../';
+
+describe( 'ItemTitle', () => {
+	test( 'should render a heading tag with the default level.', () => {
+		const component = renderer.create( <ItemTitle title="Example Title" /> );
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+
+	test( 'should render a heading tag of level 1.', () => {
+		const component = renderer.create( <ItemTitle title="Example Title" headingLevel={ 1 } /> );
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+
+	test( 'should render a heading tag with a custom class.', () => {
+		const component = renderer.create( <ItemTitle title="Example Title" className="my-test-heading" /> );
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+
+	test( 'should render a heading tag with a set alignment.', () => {
+		const component = renderer.create( <ItemTitle title="Example Title" align="right" /> );
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+
+	test( 'should render a heading tag with a link.', () => {
+		const component = renderer.create( <ItemTitle title="Example Title" link="https://wordpress.com" /> );
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+
+	test( 'should render a heading tag with a link, custom class name, heading level, and alignment.', () => {
+		const component = renderer.create(
+			<ItemTitle
+				title="Example Title"
+				headingLevel={ 1 }
+				className="my-test-heading"
+				align="right"
+				link="https://wordpress.com"
+			/>
+		);
+		expect( component.toJSON() ).toMatchSnapshot();
+	} );
+} );

3d204feb79d5a646cb8b849f6c7be7fea432b244
Kelly Dwan <ryelle@users.noreply.github.com>
2019-08-13 16:37:58 -0400

    Add custom CSS and remote CSS to the asset cache group

--- public_html/wp-content/mu-plugins/service-worker-caching.php
+++ public_html/wp-content/mu-plugins/service-worker-caching.php
@@ -1,41 +1,69 @@
 <?php
 
 namespace WordCamp\PWA\Caching;
-use WP_Service_Worker_Caching_Routes;
+use WP_Service_Worker_Caching_Routes, WP_Service_Worker_Scripts;
 
 add_action( 'wp_front_service_worker', __NAMESPACE__ . '\register_caching_routes' );
-add_action( 'wp_front_service_worker',    __NAMESPACE__ . '\set_navigation_caching_strategy' );
+add_action( 'wp_front_service_worker', __NAMESPACE__ . '\set_navigation_caching_strategy' );
 
 /**
- * Register caching routes with both service workers.
+ * Register caching routes with the frontend service worker.
+ *
+ * @param WP_Service_Worker_Scripts $scripts
  */
-function register_caching_routes() {
-	$custom_css_url_parts = wp_parse_url( wcorg_get_custom_css_url() );
-
-	$static_asset_route_params = array(
+function register_caching_routes( WP_Service_Worker_Scripts $scripts ) {
+	/*
+	 * Set up asset cache strategy to pull from the cache first, with no network request if the resource is found,
+	 * and save up to 100 cached entries for 1 day.
+	 */
+	$asset_cache_strategy_args = array(
 		'strategy'  => WP_Service_Worker_Caching_Routes::STRATEGY_CACHE_FIRST,
 		'cacheName' => 'assets',
 		'plugins'   => [
 			'expiration' => [
-				'maxEntries'    => 60,
+				'maxEntries'    => 100,
 				'maxAgeSeconds' => DAY_IN_SECONDS,
 			],
 		],
 	);
 
-	wp_register_service_worker_caching_route(
+	/*
+	 * Cache scripts, styles, images, etc from core, themes, and plugins.
+	 */
+	$scripts->caching_routes()->register(
 		'/wp-(content|includes)/.*\.(?:png|gif|jpg|jpeg|svg|webp|css|js)(\?.*)?$',
-		$static_asset_route_params
+		$asset_cache_strategy_args
 	);
 
-	if ( isset( $custom_css_url_parts['path'], $custom_css_url_parts['query'] ) ) {
-		wp_register_service_worker_caching_route(
-			$custom_css_url_parts['path'] . $custom_css_url_parts['query'] . '$',
-			$static_asset_route_params
+	/*
+	 * Cache custom CSS (from customizer).
+	 * If we don't have a URL, that's probably because the custom CSS is empty or short enough to be printed
+	 * inline instead of enqueued. In that case, the cached pages will have it printed from the `wp_head()`
+	 * call anyway.
+	 */
+	$custom_css_url = wcorg_get_custom_css_url();
+	if ( $custom_css_url ) {
+		$scripts->caching_routes()->register(
+			preg_quote( '/?'. untrailingslashit( wp_parse_url( $custom_css_url, PHP_URL_QUERY ) ), '/' ),
+			$asset_cache_strategy_args
 		);
 	}
 
-	wp_register_service_worker_caching_route(
+	/*
+	 * Cache remote CSS endpoint, if Remote CSS has been set up.
+	 */
+	if ( \WordCamp\RemoteCSS\is_configured() ) {
+		$remote_css_url = preg_quote( 'admin-ajax.php?action=' . \WordCamp\RemoteCSS\CSS_HANDLE, '/' );
+		$scripts->caching_routes()->register(
+			$remote_css_url,
+			$asset_cache_strategy_args
+		);
+	}
+
+	/*
+	 * Cache API requests for 15 minutes.
+	 */
+	$scripts->caching_routes()->register(
 		'/wp-json/.*',
 		[
 			'strategy'  => WP_Service_Worker_Caching_Routes::STRATEGY_CACHE_FIRST,

e92c69c408e8f628e8b58e05eb76c95baeeb4da8
Kelly Dwan <ryelle@users.noreply.github.com>
2019-09-25 17:17:56 -0400

    Templates: Clean up Day of Event template
    Better matches core themes, remove calls to day-of JavaScript files

--- public_html/wp-content/mu-plugins/theme-templates/templates/day-of-event/day-of-event.php
+++ public_html/wp-content/mu-plugins/theme-templates/templates/day-of-event/day-of-event.php
@@ -10,15 +10,24 @@ namespace WordCamp\Theme_Templates;
 defined( 'WPINC' ) || die();
 
 get_header();
-
 ?>
 
-<main id="day-of-event">
-	<?php esc_html_e( 'Loading...', 'wordcamporg' ); ?>
+<main id="main" class="site-main">
+<?php while ( have_posts() ) :
+	the_post();
+	?>
+	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+		<header class="entry-header">
+			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+		</header>
+
+		<div class="entry-content">
+			<?php the_content(); ?>
+		</div>
+	</article>
+<?php endwhile; ?>
 </main>
 
 <?php
-
-the_content();
 
 get_footer();

b79709d247528b0744caec1a8cee3e4c1751ea1d
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2019-09-27 16:00:40 -0700

    Reports: Check for errors after pulling report data
    Previously the WordCamp Counts report would decide whether to render
    an error message or data before actually attempting to generate the
    data.

--- public_html/wp-content/plugins/wordcamp-reports/classes/report/class-wordcamp-counts.php
+++ public_html/wp-content/plugins/wordcamp-reports/classes/report/class-wordcamp-counts.php
@@ -641,15 +641,15 @@ class WordCamp_Counts extends Base {
 	 * @return void
 	 */
 	public function render_html() {
-		if ( ! empty( $this->error->get_error_messages() ) ) {
-			$this->render_error_html();
-			return;
-		}
-
 		$data       = $this->compile_report_data( $this->get_data() );
 		$start_date = $this->range->start;
 		$end_date   = $this->range->end;
 		$statuses   = $this->statuses;
+
+		if ( ! empty( $this->error->get_error_messages() ) ) {
+			$this->render_error_html();
+			return;
+		}
 
 		include get_views_dir_path() . 'html/wordcamp-counts.php';
 	}

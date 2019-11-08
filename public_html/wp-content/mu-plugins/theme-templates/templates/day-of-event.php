617937e1a8f054dc26ca91cc5d9c9aeccf3a4996
Kelly Dwan <ryelle@users.noreply.github.com>
2019-11-07 20:50:14 -0500

    Day of Event Template: Use the theme's template if it exists

--- public_html/wp-content/mu-plugins/theme-templates/templates/day-of-event.php
+++ public_html/wp-content/mu-plugins/theme-templates/templates/day-of-event.php
@@ -15,16 +15,22 @@ get_header();
 <main id="main" class="site-main">
 <?php while ( have_posts() ) :
 	the_post();
-	?>
-	<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
-		<header class="entry-header">
-			<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
-		</header>
 
-		<div class="entry-content">
-			<?php the_content(); ?>
-		</div>
-	</article>
+	if ( locate_template( [ 'template-parts/content.php' ] ) ) :
+
+		get_template_part( 'template-parts/content' );
+
+	else : ?>
+		<article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
+			<header class="entry-header">
+				<?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
+			</header>
+
+			<div class="entry-content">
+				<?php the_content(); ?>
+			</div>
+		</article>
+	<?php endif; ?>
 <?php endwhile; ?>
 </main>
 

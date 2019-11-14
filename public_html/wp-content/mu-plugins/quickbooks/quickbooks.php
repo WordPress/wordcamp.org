15ef73717e2596801c56b0eb7543ae8c90711c96
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2019-11-14 10:26:57 -0800

    Update the file loader

--- public_html/wp-content/mu-plugins/quickbooks/quickbooks.php
+++ public_html/wp-content/mu-plugins/quickbooks/quickbooks.php
@@ -8,11 +8,15 @@ const OAUTH_CAP  = 'manage_network';
 
 add_action( 'plugins_loaded', __NAMESPACE__ . '\load_files' );
 
-
+/**
+ * Conditionally load files.
+ *
+ * @return void
+ */
 function load_files() {
-	require_once __DIR__ . '/includes/client.php';
+	require_once PLUGIN_DIR . '/includes/client.php';
 
 	if ( is_network_admin() ) {
-		require_once __DIR__ . '/includes/admin.php';
+		require_once PLUGIN_DIR . '/includes/admin.php';
 	}
 }

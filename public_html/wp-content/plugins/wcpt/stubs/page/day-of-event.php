d214da795a40f1508b5035a6ac0787ebe4eacbff
Kelly Dwan <kelly.dwan@gmail.com>
2020-01-06 16:02:56 -0500

    Fix heading level hierarchy for default content

--- public_html/wp-content/plugins/wcpt/stubs/page/day-of-event.php
+++ public_html/wp-content/plugins/wcpt/stubs/page/day-of-event.php
@@ -7,10 +7,9 @@
 <h2><?php esc_html_e( 'Schedule', 'wordcamporg' ); ?></h2>
 <!-- /wp:heading -->
 
-<!-- wp:wordcamp/live-schedule -->
-<div data-now="<?php esc_html_e( 'On Now', 'wordcamporg' ); ?>" data-next="<?php esc_html_e( 'Next Up', 'wordcamporg' ); ?>" data-level="2" class="wp-block-wordcamp-live-schedule"></div>
+<!-- wp:wordcamp/live-schedule {"level":3} -->
+<div data-now="<?php esc_html_e( 'On Now', 'wordcamporg' ); ?>" data-next="<?php esc_html_e( 'Next Up', 'wordcamporg' ); ?>" data-level="3" class="wp-block-wordcamp-live-schedule"></div>
 <!-- /wp:wordcamp/live-schedule -->
-
 
 <!-- wp:heading -->
 <h2><?php esc_html_e( 'Latest Posts', 'wordcamporg' ); ?></h2>

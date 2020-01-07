WordPress/wordcamp.org

commit 8fb0610b11e86996e3a34f2f04f7a0b786d48193

WCPT: Standardize organizer usernames for compatibility.

public_html/wp-content/plugins/wcpt/wcpt-event/class-event-admin.php
@@ -478,6 +478,17 @@ abstract class Event_Admin {
 				continue;
 			}
 
+			$username_fields = array(
+				'Primary organizer WordPress.org username',
+				'Co-Organizers usernames (seperated by comma)',
+			);
+
+			if ( in_array( $key, $username_fields, true ) ) {
+				$usernames_array        = explode( ',', $values[ $key ] );
+				$standardized_usernames = $this->standardize_usernames( $usernames_array );
+				$values[ $key ]         = implode( ', ', $standardized_usernames );
+			}
+
 			switch ( $value ) {
 				case 'text':
 				case 'deputy_list':
@@ -523,6 +534,33 @@ abstract class Event_Admin {
 
 		$this->validate_and_add_note( $post_id );
 
+	}
+
+	/**
+	 * Convert `user_nicename` values to `user_login` for compatibility.
+	 *
+	 * `WordCamp_Participation_Notifier::update_meetup_organizers` expects this to be `user_login`, and adding
+	 * a `Meetup Organizer` badge will fail if it's `user_nicename`.
+	 *
+	 * @return array
+	 */
+	protected function standardize_usernames( array $raw_usernames ) {
+		$standard_usernames = array();
+
+		foreach ( $raw_usernames as $username ) {
+			$user = wcorg_get_user_by_canonical_names( $username );
+
+			if ( ! $user instanceof WP_User ) {
+				wp_die( sprintf(
+					'%s is not a valid username.',
+					esc_html( $username )
+				) );
+			}
+
+			$standard_usernames[] = $user->user_login;
+		}
+
+		return $standard_usernames;
 	}
 
 	/**

commit 48b6da958d39a39e4c7fb6b762ba9790e499715e

WCPT: Ignore empty usernames when standardizing.
8fb0610b11e86996e3a34f2f04f7a0b786d48193 introduced a bug where an empty string would result in a `not a valid username` error. Instead, empty fields should be skipped.

public_html/wp-content/plugins/wcpt/wcpt-event/class-event-admin.php
@@ -484,7 +484,7 @@ abstract class Event_Admin {
 			);
 
 			if ( in_array( $key, $username_fields, true ) ) {
-				$usernames_array        = explode( ',', $values[ $key ] );
+				$usernames_array        = empty( $values[ $key ] ) ? array() : explode( ',', $values[ $key ] );
 				$standardized_usernames = $this->standardize_usernames( $usernames_array );
 				$values[ $key ]         = implode( ', ', $standardized_usernames );
 			}
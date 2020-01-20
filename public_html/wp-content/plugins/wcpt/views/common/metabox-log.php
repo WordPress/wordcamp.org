f5a985b422eb8ab4a5bf6e1a6c375e06dcc45ba8
Timi Wahalahti <timi@wahalahti.fi>
2020-01-21 00:13:41 +0200

    Fix linter errors

--- public_html/wp-content/plugins/wcpt/views/common/metabox-log.php
+++ public_html/wp-content/plugins/wcpt/views/common/metabox-log.php
@@ -5,10 +5,10 @@
  * "data" set is too restrictive and "post" set too broad.
  */
 $allowed_html = wp_kses_allowed_html( 'data' );
-$allowed_html['p'] = array(); // allow paragraph for easier reading
-$allowed_html['br'] = array(); // allow line changes for easier reading
-$allowed_html['a']['target'] = true; // target to ensure not losing application view
-$allowed_html['a']['rel'] = true; // we want to set nofollow so search engines don't use link for ranking, crawling or indexing
+$allowed_html['p'] = array(); // allow paragraph for easier reading.
+$allowed_html['br'] = array(); // allow line changes for easier reading.
+$allowed_html['a']['target'] = true; // target to ensure not losing application view.
+$allowed_html['a']['rel'] = true; // we want to set nofollow so search engines don't use link for ranking, crawling or indexing.
 
 /**
  * Loop all entries and search for notes. In notes, turn plaintext links
@@ -21,7 +21,7 @@ if ( $entries ) {
 			continue;
 		}
 
-		$entries[ $entry_key ]['message'] = preg_replace("/(^|[\n ])([\w]*?)(http(s)?:\/\/[\w]+[^ \,\"\n\r\t<]*)/is", "$1$2<a href=\"$3\" target=\"_blank\" rel=\"nofollow\">$3</a>", $entries[ $entry_key ]['message'] );
+		$entries[ $entry_key ]['message'] = preg_replace( '/(^|[\n ])([\w]*?)(http(s)?:\/\/[\w]+[^ \,\"\n\r\t<]*)/i', '$1$2<a href="$3" target="_blank" rel="nofollow">$3</a>', $entries[ $entry_key ]['message'] );
 	}
 } ?>
 
@@ -40,7 +40,7 @@ if ( $entries ) {
 
 			<?php foreach ( $entries as $entry ) : ?>
 				<tr class="<?php echo esc_attr( str_replace( '_', '-', $entry['type'] ) ); ?>">
-					<th><p><?php echo esc_html( date( 'Y-m-d h:ia', $entry['timestamp'] ) ); ?></p></th>
+					<th><p><?php echo esc_html( gmdate( 'Y-m-d h:ia', $entry['timestamp'] ) ); ?></p></th>
 					<th><p><?php echo esc_html( ucwords( str_replace( '_', ' ', $entry['type'] ) ) ); ?></p></th>
 					<th><p><?php echo esc_html( $entry['user_display_name'] ); ?></p></th>
 					<th><?php echo wp_kses( wpautop( $entry['message'] ), $allowed_html ); ?></th>

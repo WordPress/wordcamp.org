387159bf061fe6aa1f90148550b65ffd628f5872
Kelly Dwan <ryelle@users.noreply.github.com>
2020-01-29 11:07:51 -0500

    It's not necessary to require the top-level folder
    this is automatically included by being in mu-plugins

--- public_html/wp-content/mu-plugins/load-other-mu-plugins.php
+++ public_html/wp-content/mu-plugins/load-other-mu-plugins.php
@@ -17,7 +17,6 @@ function wcorg_include_individual_mu_plugins() {
 	require_once( __DIR__ . '/camptix-tweaks/camptix-tweaks.php' );
 	require_once( __DIR__ . '/blocks/blocks.php' );
 	require_once __DIR__ . '/quickbooks/quickbooks.php';
-	require_once __DIR__ . '/service-worker-caching.php';
 	require_once __DIR__ . '/theme-templates/bootstrap.php';
 
 	if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {

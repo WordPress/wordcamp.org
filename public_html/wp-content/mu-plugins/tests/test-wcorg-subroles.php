09f03d8002be8b444a879c414ada45044e0fdb5b
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2020-02-05 16:54:36 -0800

    Subroles: Omit user caps that have been added via usermeta values (#355)
    On WordCamp.org we grant additional capabilities to users via the map_meta_cap filter and a hard-coded list of users/caps. This way the granting/revoking of capabilities has an audit log in the form of revision history.
    
    When a WP_User object is instantiated, it populates a caps property from a user meta value that is blog-specific. Normally this value only contains the user's role, but it can also store additional capabilities if they are added via add_cap. These additional capabilities are then merged with additions from map_meta_cap when evaluating something like current_user_can.
    
    This adds a filter (and related unit tests!) that omits capabilities that have been incorporated from user meta when evaluating has_cap, so that only the map_meta_cap filter can add caps.

---
+++ public_html/wp-content/mu-plugins/tests/test-wcorg-subroles.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace WordCamp\Tests;
+
+use WP_UnitTestCase;
+use WordCamp\SubRoles;
+
+defined( 'WPINC' ) || die();
+
+/**
+ * Class Test_Omit_UserMeta_Caps
+ *
+ * @group wordcamp-mu-plugins
+ *
+ * @package WordCamp\Tests
+ */
+class Test_Omit_UserMeta_Caps extends WP_UnitTestCase {
+	/**
+	 * @covers WordCamp\SubRoles\omit_usermeta_caps()
+	 */
+	public function test_user_with_additional_caps_cannot() {
+		$user = self::factory()->user->create_and_get( array(
+			'role' => 'subscriber',
+		) );
+
+		$user->add_cap( 'wordcamp_wrangle_wordcamps' );
+		$usermeta = get_user_meta( $user->ID, 'wptests_capabilities', true );
+
+		$this->assertTrue( $user->has_cap( 'read' ) );
+		$this->assertFalse( $user->has_cap( 'wordcamp_wrangle_wordcamps' ) );
+		$this->assertTrue( array_key_exists( 'wordcamp_wrangle_wordcamps', $usermeta ) );
+	}
+
+	/**
+	 * @covers WordCamp\SubRoles\omit_usermeta_caps()
+	 */
+	public function test_user_with_subrole_can() {
+		$user = self::factory()->user->create_and_get( array(
+			'role' => 'subscriber',
+		) );
+
+		global $wcorg_subroles;
+		$wcorg_subroles = array(
+			$user->ID => array( 'wordcamp_wrangler' ),
+		);
+
+		$this->assertTrue( $user->has_cap( 'read' ) );
+		$this->assertTrue( $user->has_cap( 'wordcamp_wrangle_wordcamps' ) );
+	}
+}

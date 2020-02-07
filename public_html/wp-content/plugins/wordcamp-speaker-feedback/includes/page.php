7308714658318f23aa8528ea8e10967ec408db9d
Kelly Dwan <ryelle@users.noreply.github.com>
2020-02-06 19:20:17 -0500

    Add a label to the page in the list table

--- public_html/wp-content/plugins/wordcamp-speaker-feedback/includes/page.php
+++ public_html/wp-content/plugins/wordcamp-speaker-feedback/includes/page.php
@@ -4,6 +4,7 @@ namespace WordCamp\SpeakerFeedback\Page;
 
 add_filter( 'pre_trash_post', __NAMESPACE__ . '\prevent_deletion', 10, 2 );
 add_filter( 'pre_delete_post', __NAMESPACE__ . '\prevent_deletion', 10, 3 );
+add_filter( 'display_post_states', __NAMESPACE__ . '\add_label_to_page', 10, 2 );
 
 /**
  * Prevent deletion of the Feedback page.
@@ -25,3 +26,20 @@ function prevent_deletion( $check, $post, $force_delete = false ) {
 	return $check;
 }
 
+/**
+ * Add label to the Feedback page, to indicate it's a "special" page.
+ *
+ * @param string[] $post_states An array of post display states.
+ * @param WP_Post  $post        The current post object.
+ *
+ * @return array The filtered list of post display states.
+ */
+function add_label_to_page( $post_states, $post ) {
+	$feedback_page = (int) get_option( 'feedback_page' );
+
+	if ( $feedback_page === $post->ID ) {
+		$post_states['sft-page'] = _x( 'Speaker Feedback', 'wordcamporg' );
+	}
+
+	return $post_states;
+}

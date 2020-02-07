edd14a8c4a902e507e4b736058cc9de2a3f1dc79
Kelly Dwan <ryelle@users.noreply.github.com>
2020-02-07 14:01:57 -0500

    Add `/feedback` page to all sessions, display basic form

--- public_html/wp-content/plugins/wordcamp-speaker-feedback/wordcamp-speaker-feedback.php
+++ public_html/wp-content/plugins/wordcamp-speaker-feedback/wordcamp-speaker-feedback.php
@@ -17,10 +17,12 @@ defined( 'WPINC' ) || die();
 define( __NAMESPACE__ . '\PLUGIN_DIR', \plugin_dir_path( __FILE__ ) );
 define( __NAMESPACE__ . '\PLUGIN_URL', \plugins_url( '/', __FILE__ ) );
 define( __NAMESPACE__ . '\OPTION_KEY', 'sft_feedback_page' );
+define( __NAMESPACE__ . '\QUERY_VAR', 'sft_feedback' );
 
 register_activation_hook( __FILE__, __NAMESPACE__ . '\activate' );
 register_deactivation_hook( __FILE__, __NAMESPACE__ . '\deactivate' );
 add_action( 'plugins_loaded', __NAMESPACE__ . '\load' );
+add_action( 'init', __NAMESPACE__ . '\add_page_endpoint' );
 
 // Check if the page exists, and add it if not.
 add_action( 'init', __NAMESPACE__ . '\add_feedback_page' );
@@ -40,6 +42,8 @@ function load() {
  */
 function activate() {
 	add_feedback_page();
+	add_page_endpoint();
+	flush_rewrite_rules();
 }
 
 /**
@@ -69,6 +73,14 @@ function add_feedback_page() {
 	if ( $page_id > 0 ) {
 		update_option( OPTION_KEY, $page_id );
 	}
+}
+
+/**
+ * Register a rewrite endpoint for the API.
+ */
+function add_page_endpoint() {
+	// Uses EP_SESSIONS mask to only add this endpoint to the session.
+	add_rewrite_endpoint( 'feedback', EP_SESSIONS, QUERY_VAR );
 }
 
 /**

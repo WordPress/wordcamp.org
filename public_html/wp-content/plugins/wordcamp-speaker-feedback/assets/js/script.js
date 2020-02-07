6af0abc99c7a51a1379bbb8a3743b27c3f2dae64
Kelly Dwan <ryelle@users.noreply.github.com>
2020-02-07 14:06:17 -0500

    Load feedback page when a session is selected

--- public_html/wp-content/plugins/wordcamp-speaker-feedback/assets/js/script.js
+++ public_html/wp-content/plugins/wordcamp-speaker-feedback/assets/js/script.js
@@ -6,7 +6,7 @@
 		event.preventDefault();
 		const value = event.target[0].value;
 		// Use the fact that post IDs will redirect to the right page.
-		window.location = '/?p=' + value;
+		window.location = '/?p=' + value + '&sft_feedback=1';
 	}
 
 	const form = document.getElementById( 'sft-navigation' );

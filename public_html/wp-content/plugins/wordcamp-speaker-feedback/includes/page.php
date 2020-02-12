bee6bebfed27c62b0886fb266804e2b846fb8134
Kelly Dwan <ryelle@users.noreply.github.com>
2020-02-12 12:18:22 -0500

    Delete the saved option when the page is deleted

--- public_html/wp-content/plugins/wordcamp-speaker-feedback/includes/page.php
+++ public_html/wp-content/plugins/wordcamp-speaker-feedback/includes/page.php
@@ -8,19 +8,21 @@ add_filter( 'pre_delete_post', __NAMESPACE__ . '\prevent_deletion', 10, 3 );
 add_filter( 'display_post_states', __NAMESPACE__ . '\add_label_to_page', 10, 2 );
 
 /**
- * Prevent deletion of the Feedback page.
+ * Prevent deletion of the Feedback page, unless force_delete is true.
  *
  * @param bool|null $check Whether to go forward with trashing/deletion.
  * @param WP_Post   $post  Post object.
  * @param bool      $force_delete Whether to bypass the trash, set when deactivating the plugin to clean up.
  */
 function prevent_deletion( $check, $post, $force_delete = false ) {
-	if ( $force_delete ) {
-		return $check;
-	}
-
 	$feedback_page = (int) get_option( OPTION_KEY );
 	if ( $feedback_page === $post->ID ) {
+		// Allow it, and delete the option if the page is force-deleted.
+		if ( $force_delete ) {
+			delete_option( OPTION_KEY );
+			return $check;
+		}
+
 		return false;
 	}
 

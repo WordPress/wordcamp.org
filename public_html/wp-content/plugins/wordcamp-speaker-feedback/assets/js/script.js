39755f1e577d16607aaa62e4d8692bbbe7b0afce
Kelly Dwan <ryelle@users.noreply.github.com>
2020-02-14 15:41:58 -0500

    Fix lint issue

--- public_html/wp-content/plugins/wordcamp-speaker-feedback/assets/js/script.js
+++ public_html/wp-content/plugins/wordcamp-speaker-feedback/assets/js/script.js
@@ -4,7 +4,7 @@
 ( function() {
 	function onFormNavigate( event ) {
 		event.preventDefault();
-		const value = event.target[0].value;
+		const value = event.target[ 0 ].value;
 		// Use the fact that post IDs will redirect to the right page.
 		window.location = '/?p=' + value + '&sft_feedback=1';
 	}

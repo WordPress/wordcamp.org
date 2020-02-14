0b5b589951ce698e5d9b72ce925470423abca0bc
Kelly Dwan <ryelle@users.noreply.github.com>
2020-02-14 14:20:05 -0500

    Add the star rating UI

--- public_html/wp-content/plugins/wordcamp-speaker-feedback/views/form-feedback.php
+++ public_html/wp-content/plugins/wordcamp-speaker-feedback/views/form-feedback.php
@@ -7,8 +7,51 @@ namespace WordCamp\SpeakerFeedback\View;
 <form id="sft-feedback" class="speaker-feedback">
 	<h3><?php esc_html_e( 'Rate this talk', 'wordcamporg' ); ?></h3>
 	
-	<div>
-		star rating…
+	<div class="speaker-feedback__field">
+		<fieldset class="speaker-feedback__field-rating" aria-label="<?php esc_attr_e( 'Rate this talk', 'wordcamporg' ); ?>">
+			<input
+				type="radio"
+				name="feedback-rating"
+				value="0"
+				checked
+				aria-label="<?php esc_attr_e( 'No stars', 'wordcamporg' ); ?>"
+			/>
+			<input type="radio" id="sft-rating-val-1" name="feedback-rating" value="1" />
+			<label for="sft-rating-val-1">
+				<span class="screen-reader-text"><?php esc_html_e( '1 star', 'wordcamporg' ); ?></span>
+				<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" height="20" width="20" role="img" aria-hidden="true">
+					<path d="M10 1l3 6 6 .75-4.12 4.62L16 19l-6-3-6 3 1.13-6.63L1 7.75 7 7z"/>
+				</svg>
+			</label>
+			<input type="radio" id="sft-rating-val-2" name="feedback-rating" value="2" />
+			<label for="sft-rating-val-2">
+				<span class="screen-reader-text"><?php esc_html_e( '2 stars', 'wordcamporg' ); ?></span>
+				<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" height="20" width="20" role="img" aria-hidden="true">
+					<path d="M10 1l3 6 6 .75-4.12 4.62L16 19l-6-3-6 3 1.13-6.63L1 7.75 7 7z"/>
+				</svg>
+			</label>
+			<input type="radio" id="sft-rating-val-3" name="feedback-rating" value="3" />
+			<label for="sft-rating-val-3">
+				<span class="screen-reader-text"><?php esc_html_e( '3 stars', 'wordcamporg' ); ?></span>
+				<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" height="20" width="20" role="img" aria-hidden="true">
+					<path d="M10 1l3 6 6 .75-4.12 4.62L16 19l-6-3-6 3 1.13-6.63L1 7.75 7 7z"/>
+				</svg>
+			</label>
+			<input type="radio" id="sft-rating-val-4" name="feedback-rating" value="4" />
+			<label for="sft-rating-val-4">
+				<span class="screen-reader-text"><?php esc_html_e( '4 stars', 'wordcamporg' ); ?></span>
+				<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" height="20" width="20" role="img" aria-hidden="true">
+					<path d="M10 1l3 6 6 .75-4.12 4.62L16 19l-6-3-6 3 1.13-6.63L1 7.75 7 7z"/>
+				</svg>
+			</label>
+			<input type="radio" id="sft-rating-val-5" name="feedback-rating" value="5" />
+			<label for="sft-rating-val-5">
+				<span class="screen-reader-text"><?php esc_html_e( '5 stars', 'wordcamporg' ); ?></span>
+				<svg xmlns="http://www.w3.org/2000/svg" viewbox="0 0 20 20" height="20" width="20" role="img" aria-hidden="true">
+					<path d="M10 1l3 6 6 .75-4.12 4.62L16 19l-6-3-6 3 1.13-6.63L1 7.75 7 7z"/>
+				</svg>
+			</label>
+		</fieldset>
 	</div>
 	
 	<div class="speaker-feedback__field">

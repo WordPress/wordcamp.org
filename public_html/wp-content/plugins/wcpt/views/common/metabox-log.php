5c3ea7f1c3310b1fd149e20af30b4e182ec44206
Timi Wahalahti <timi@wahalahti.fi>
2020-02-19 17:53:42 +0200

    Event Applications: Allow links & line breaks in notes (#323)
    Fixes #322

--- public_html/wp-content/plugins/wcpt/views/common/metabox-log.php
+++ public_html/wp-content/plugins/wcpt/views/common/metabox-log.php
@@ -1,4 +1,14 @@
-<?php defined( 'WPINC' ) || die(); ?>
+<?php defined( 'WPINC' ) || die();
+
+/**
+ * Slightly modify allowed html tags in entry message because default
+ * "data" set is too restrictive and "post" set too broad.
+ */
+$allowed_html = wp_kses_allowed_html( 'data' );
+$allowed_html['p'] = array(); // allow paragraph for easier reading.
+$allowed_html['br'] = array(); // allow line changes for easier reading.
+
+?>
 
 <table class="widefat striped">
 	<thead>
@@ -15,10 +25,10 @@
 
 			<?php foreach ( $entries as $entry ) : ?>
 				<tr class="<?php echo esc_attr( str_replace( '_', '-', $entry['type'] ) ); ?>">
-					<th><p><?php echo esc_html( date( 'Y-m-d h:ia', $entry['timestamp'] ) ); ?></p></th>
+					<th><p><?php echo esc_html( gmdate( 'Y-m-d h:ia', $entry['timestamp'] ) ); ?></p></th>
 					<th><p><?php echo esc_html( ucwords( str_replace( '_', ' ', $entry['type'] ) ) ); ?></p></th>
 					<th><p><?php echo esc_html( $entry['user_display_name'] ); ?></p></th>
-					<th><?php echo wp_kses( wpautop( $entry['message'] ), wp_kses_allowed_html( 'data' ) ); ?></th>
+					<th><?php echo wp_kses( wpautop( make_clickable( $entry['message'] ) ), $allowed_html ); ?></th>
 				</tr>
 			<?php endforeach; ?>
 

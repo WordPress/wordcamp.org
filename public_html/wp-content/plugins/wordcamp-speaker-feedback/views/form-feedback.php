4d37e8cd165b9b49efe8f4830f249cd9dc94b05d
Kelly Dwan <ryelle@users.noreply.github.com>
2020-02-19 10:59:33 -0500

    Speaker Feedback: Add the star rating UI (#368)

--- public_html/wp-content/plugins/wordcamp-speaker-feedback/views/form-feedback.php
+++ public_html/wp-content/plugins/wordcamp-speaker-feedback/views/form-feedback.php
@@ -1,14 +1,48 @@
 <?php
 
 namespace WordCamp\SpeakerFeedback\View;
+use function WordCamp\SpeakerFeedback\get_assets_path;
 
 ?>
 <hr />
 <form id="sft-feedback" class="speaker-feedback">
 	<h3><?php esc_html_e( 'Rate this talk', 'wordcamporg' ); ?></h3>
 	
-	<div>
-		star rating…
+	<div class="speaker-feedback__field">
+		<fieldset class="speaker-feedback__field-rating" aria-label="<?php esc_attr_e( 'Rate this talk', 'wordcamporg' ); ?>">
+			<input
+				type="radio"
+				name="feedback-rating"
+				value="0"
+				checked
+				aria-label="<?php esc_attr_e( 'No stars', 'wordcamporg' ); ?>"
+			/>
+			<input type="radio" id="sft-rating-val-1" name="feedback-rating" value="1" />
+			<label for="sft-rating-val-1">
+				<span class="screen-reader-text"><?php esc_html_e( '1 star', 'wordcamporg' ); ?></span>
+				<?php require get_assets_path() . 'svg/star.svg'; ?>
+			</label>
+			<input type="radio" id="sft-rating-val-2" name="feedback-rating" value="2" />
+			<label for="sft-rating-val-2">
+				<span class="screen-reader-text"><?php esc_html_e( '2 stars', 'wordcamporg' ); ?></span>
+				<?php require get_assets_path() . 'svg/star.svg'; ?>
+			</label>
+			<input type="radio" id="sft-rating-val-3" name="feedback-rating" value="3" />
+			<label for="sft-rating-val-3">
+				<span class="screen-reader-text"><?php esc_html_e( '3 stars', 'wordcamporg' ); ?></span>
+				<?php require get_assets_path() . 'svg/star.svg'; ?>
+			</label>
+			<input type="radio" id="sft-rating-val-4" name="feedback-rating" value="4" />
+			<label for="sft-rating-val-4">
+				<span class="screen-reader-text"><?php esc_html_e( '4 stars', 'wordcamporg' ); ?></span>
+				<?php require get_assets_path() . 'svg/star.svg'; ?>
+			</label>
+			<input type="radio" id="sft-rating-val-5" name="feedback-rating" value="5" />
+			<label for="sft-rating-val-5">
+				<span class="screen-reader-text"><?php esc_html_e( '5 stars', 'wordcamporg' ); ?></span>
+				<?php require get_assets_path() . 'svg/star.svg'; ?>
+			</label>
+		</fieldset>
 	</div>
 	
 	<div class="speaker-feedback__field">

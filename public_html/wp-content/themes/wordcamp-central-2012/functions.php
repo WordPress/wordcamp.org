ea572a7deca80002b0ea527b600d8dbcb687d1bf
Kelly Dwan <ryelle@users.noreply.github.com>
2020-02-20 11:17:15 -0500

    WordCamp Central: Bump javascript cache buster

--- public_html/wp-content/themes/wordcamp-central-2012/functions.php
+++ public_html/wp-content/themes/wordcamp-central-2012/functions.php
@@ -180,7 +180,7 @@ class WordCamp_Central_Theme {
 	 */
 	static function enqueue_scripts() {
 		wp_enqueue_style( 'central', get_stylesheet_uri(), array(), 12 );
-		wp_enqueue_script( 'wordcamp-central', get_stylesheet_directory_uri() . '/js/central.js', array( 'jquery', 'underscore' ), 3, true );
+		wp_enqueue_script( 'wordcamp-central', get_stylesheet_directory_uri() . '/js/central.js', array( 'jquery', 'underscore' ), 4, true );
 
 		wp_localize_script( 'wordcamp-central', 'wordcampCentralOptions', self::get_javascript_options() );
 

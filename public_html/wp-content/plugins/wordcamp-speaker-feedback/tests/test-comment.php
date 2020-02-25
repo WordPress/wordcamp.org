e02a28b8a9f914c9e36ee88f941a138d453ec655
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2020-02-25 12:42:42 -0800

    Add unit tests for comment.php

---
+++ public_html/wp-content/plugins/wordcamp-speaker-feedback/tests/test-comment.php
@@ -0,0 +1,220 @@
+<?php
+
+namespace WordCamp\SpeakerFeedback\Tests;
+
+use WP_Post, WP_User;
+use WP_UnitTestCase;
+use WordCamp\SpeakerFeedback\Feedback;
+use const WordCamp\SpeakerFeedback\Comment\COMMENT_TYPE;
+use function WordCamp\SpeakerFeedback\Comment\{
+	add_feedback,
+	update_feedback,
+	get_feedback,
+	delete_feedback,
+	is_feedback,
+	get_feedback_comment,
+};
+
+defined( 'WPINC' ) || die();
+
+/**
+ * Class Test_SpeakerFeedback_Comment
+ *
+ * @group wordcamp-speaker-feedback
+ */
+class Test_SpeakerFeedback_Comment extends WP_UnitTestCase {
+	/**
+	 * @var WP_Post
+	 */
+	protected $session_post;
+
+	/**
+	 * @var WP_User
+	 */
+	protected $user;
+
+	/**
+	 * Test_SpeakerFeedback_Comment constructor.
+	 *
+	 * Setup the only needs to happen once, rather than before each test.
+	 *
+	 * @param null   $name
+	 * @param array  $data
+	 * @param string $data_name
+	 */
+	public function __construct( $name = null, array $data = array(), $data_name = '' ) {
+		parent::__construct( $name, $data, $data_name );
+
+		$this->session_post = self::factory()->post->create_and_get( array(
+			'post_type' => 'wcb_session',
+		) );
+
+		$this->user = self::factory()->user->create_and_get();
+	}
+
+	/**
+	 * Reset after each test.
+	 */
+	public function tearDown() {
+		global $wpdb;
+
+		// This ensures that only comments created during a given test exist in the database during the test.
+		$wpdb->query( "TRUNCATE TABLE {$wpdb->prefix}comments" );
+		$wpdb->query( "TRUNCATE TABLE {$wpdb->prefix}commentmeta" );
+	}
+
+	/**
+	 * @covers \WordCamp\SpeakerFeedback\Comment\is_feedback()
+	 */
+	public function test_is_feedback() {
+		$comment_object = self::factory()->comment->create_and_get( array(
+			'comment_type' => COMMENT_TYPE,
+		) );
+
+		$this->assertTrue( is_feedback( $comment_object ) );
+		$this->assertTrue( is_feedback( $comment_object->comment_ID ) );
+	}
+
+	/**
+	 * @covers \WordCamp\SpeakerFeedback\Comment\is_feedback()
+	 */
+	public function test_is_not_feedback() {
+		$comment_object = self::factory()->comment->create_and_get();
+
+		$this->assertFalse( is_feedback( $comment_object ) );
+		$this->assertFalse( is_feedback( $comment_object->comment_ID ) );
+	}
+
+	/**
+	 * @covers \WordCamp\SpeakerFeedback\Comment\get_feedback_comment()
+	 */
+	public function test_get_feedback_comment() {
+		$comment = self::factory()->comment->create_and_get( array(
+			'comment_type' => COMMENT_TYPE,
+		) );
+
+		$feedback = get_feedback_comment( $comment );
+
+		$this->assertTrue( $feedback instanceof Feedback );
+	}
+
+	/**
+	 * @covers \WordCamp\SpeakerFeedback\Comment\get_feedback_comment()
+	 */
+	public function test_get_feedback_comment_invalid() {
+		$comment = self::factory()->comment->create_and_get();
+
+		$feedback = get_feedback_comment( $comment );
+
+		$this->assertNull( $feedback );
+	}
+
+	/**
+	 * @covers \WordCamp\SpeakerFeedback\Comment\add_feedback()
+	 */
+	public function test_add_feedback_user_id() {
+		$comment_id = add_feedback(
+			$this->session_post->ID,
+			$this->user->ID,
+			array()
+		);
+
+		$comment = get_comment( $comment_id );
+
+		$this->assertTrue( is_feedback( $comment ) );
+
+		// Feedback default status is `hold`/`0`.
+		$this->assertEquals( 0, $comment->comment_approved );
+	}
+
+	/**
+	 * @covers \WordCamp\SpeakerFeedback\Comment\add_feedback()
+	 */
+	public function test_add_feedback_user_array() {
+		$comment_id = add_feedback(
+			$this->session_post->ID,
+			array(
+				'name'  => 'Foo',
+				'email' => 'bar@example.org',
+			),
+			array()
+		);
+
+		$comment = get_comment( $comment_id );
+
+		$this->assertTrue( is_feedback( $comment ) );
+	}
+
+	/**
+	 * @covers \WordCamp\SpeakerFeedback\Comment\add_feedback()
+	 */
+	public function test_add_feedback_no_user() {
+		$comment_id = add_feedback(
+			$this->session_post->ID,
+			array(),
+			array()
+		);
+
+		$this->assertFalse( $comment_id );
+	}
+
+	/**
+	 * @covers \WordCamp\SpeakerFeedback\Comment\update_feedback()
+	 */
+	public function test_update_feedback() {
+		$comment_id = add_feedback(
+			$this->session_post->ID,
+			$this->user->ID,
+			array(
+				'rating' => 1,
+			)
+		);
+
+		$feedback = get_feedback_comment( $comment_id );
+
+		$this->assertEquals( 1, $feedback->rating );
+
+		$new_meta = array(
+			'rating' => 5,
+		);
+
+		update_feedback( $comment_id, $new_meta );
+
+		$this->assertEquals( 5, $feedback->rating );
+	}
+
+	/**
+	 * @covers \WordCamp\SpeakerFeedback\Comment\get_feedback()
+	 */
+	public function test_get_feedback_exclude_other_comments() {
+		self::factory()->comment->create_many( 3 );
+		self::factory()->comment->create_many(
+			3,
+			array(
+				'comment_type' => COMMENT_TYPE,
+			)
+		);
+
+		$all_comments = get_comments();
+		$feedback     = get_feedback();
+
+		$this->assertEquals( 6, count( $all_comments ) );
+		$this->assertEquals( 3, count( $feedback ) );
+	}
+
+	/**
+	 * @covers \WordCamp\SpeakerFeedback\Comment\delete_feedback()
+	 */
+	public function test_delete_feedback() {
+		$comment_id = add_feedback(
+			$this->session_post->ID,
+			$this->user->ID,
+			array()
+		);
+
+		$deleted = delete_feedback( $comment_id );
+
+		$this->assertTrue( $deleted );
+		$this->assertEquals( 'trash', get_comment( $comment_id )->comment_approved );
+	}
+}

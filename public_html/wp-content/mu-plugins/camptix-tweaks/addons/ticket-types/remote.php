d2c878b755e3c996526fa0e63b500ca16f25b900
Kelly Dwan <ryelle@users.noreply.github.com>
2020-03-11 15:43:05 -0400

    Modify the "accommodations" question for remote tickets

--- public_html/wp-content/mu-plugins/camptix-tweaks/addons/ticket-types/remote.php
+++ public_html/wp-content/mu-plugins/camptix-tweaks/addons/ticket-types/remote.php
@@ -8,6 +8,7 @@ const SLUG = 'remote';
 
 add_filter( 'camptix_ticket_types', __NAMESPACE__ . '\add_type' );
 add_filter( 'camptix_metabox_questions_default_fields_list', __NAMESPACE__ . '\modify_default_fields_list' );
+add_filter( 'camptix_accommodations_question_text', __NAMESPACE__ . '\accommodations_question_text', 10, 2 );
 
 /**
  * Add this type to the available ticket types.
@@ -31,4 +32,17 @@ function modify_default_fields_list( $default_fields ) {
 		return __( 'Top three fields: First name, last name, e-mail address.<br />Bottom three fields: Attendee list opt-out, accessibility needs, Code of Conduct agreement.', 'wordcamporg' );
 	}
 	return $default_fields;
+}
+
+/**
+ * Modify the accommodations question.
+ *
+ * @param string $question
+ * @return string
+ */
+function accommodations_question_text( $question, $ticket_id ) {
+	if ( SLUG === get_type_slug( $ticket_id ) ) {
+		return __( 'Do you have any accessibility needs, such as a sign language interpreter, to participate in WordCamp?', 'wordcamporg' );
+	}
+	return $question;
 }

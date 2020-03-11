142239330f94692245781e17060c332ea78ed699
Kelly Dwan <ryelle@users.noreply.github.com>
2020-03-11 15:43:34 -0400

    Remove the Allergy question on Remote tickets

--- public_html/wp-content/mu-plugins/camptix-tweaks/addons/ticket-types/remote.php
+++ public_html/wp-content/mu-plugins/camptix-tweaks/addons/ticket-types/remote.php
@@ -9,6 +9,8 @@ const SLUG = 'remote';
 add_filter( 'camptix_ticket_types', __NAMESPACE__ . '\add_type' );
 add_filter( 'camptix_metabox_questions_default_fields_list', __NAMESPACE__ . '\modify_default_fields_list' );
 add_filter( 'camptix_accommodations_question_text', __NAMESPACE__ . '\accommodations_question_text', 10, 2 );
+add_filter( 'camptix_attendee_form_after_questions', __NAMESPACE__ . '\set_allergy_filter', 10, 2 );
+add_filter( 'camptix_form_edit_attendee_after_questions', __NAMESPACE__ . '\set_allergy_filter', 10 );
 
 /**
  * Add this type to the available ticket types.
@@ -46,3 +48,28 @@ function accommodations_question_text( $question, $ticket_id ) {
 	}
 	return $question;
 }
+
+/**
+ * Set a boolean filter on the "Allergy" question, to skip the question if it's remote.
+ *
+ * @param array    $form_data
+ * @param int|null $i
+ */
+function set_allergy_filter( $form_data, $i = null ) {
+	if ( ! is_null( $i ) ) {
+		$form_data = ( isset( $form_data['tix_attendee_info'][ $i ] ) ) ? $form_data['tix_attendee_info'][ $i ] : array();
+	}
+
+	if ( ! isset( $form_data['ticket_id'] ) ) {
+		return;
+	}
+
+	if ( SLUG === get_type_slug( $form_data['ticket_id'] ) ) {
+		remove_filter( 'camptix_allergy_should_skip', '__return_false' );
+		add_filter( 'camptix_allergy_should_skip', '__return_true' );
+	} else {
+		remove_filter( 'camptix_allergy_should_skip', '__return_true' );
+		add_filter( 'camptix_allergy_should_skip', '__return_false' );
+	}
+}
+

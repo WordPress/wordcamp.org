bd413bf8b893ea1204290b5261e309f1834d4905
Ian Dunn <ian@iandunn.name>
2020-04-03 14:39:42 -0700

    Blocks: Add a crash boundary for front end blocks.

--- public_html/wp-content/mu-plugins/blocks/source/utils/render-frontend.js
+++ public_html/wp-content/mu-plugins/blocks/source/utils/render-frontend.js
@@ -1,11 +1,63 @@
 /**
- * External dependencies
+ * WordPress dependencies
  */
-import { render } from 'react-dom';
+import { Component, createInterpolateElement, render } from '@wordpress/element';
+import { __ } from '@wordpress/i18n';
 
 const getPropsFallback = () => ( {
 	attributes: {},
 } );
+
+/*
+ * Catch errors thrown by children and display a friendly error message.
+ *
+ * Gutenberg does this in the back end via the internal `BlockCrashBoundary` class, but we need to replicate that
+ * for the front end.
+ */
+class FrontBlockCrashBoundary extends Component {
+	/**
+	 * Constructor.
+	 *
+	 * @param {Object} props
+	 */
+	constructor( props ) {
+		super( props );
+
+		this.state = { hasError: false };
+	}
+
+	/**
+	 * Catch errors thrown by children components and update state.
+	 *
+	 * @return {Object}
+	 */
+	static getDerivedStateFromError() {
+		return { hasError: true };
+	}
+
+	/**
+	 * Render an error message in place of the child component.
+	 *
+	 * @return {Element}
+	 */
+	render() {
+		const { hasError } = this.state;
+		let content;
+
+		if ( hasError ) {
+			content = createInterpolateElement(
+				__( 'There was an error trying to render this content, please try another browser or device. If that doesn\'t work, please <a>contact us</a>.', 'wordcamporg' ),
+				{
+					a: <a href={ 'https://central.wordcamp.org/contact-us/' } >#21441-gutenberg</a>,
+				}
+			);
+		} else {
+			content = this.props.block;
+		}
+
+		return content;
+	}
+}
 
 /**
  * Renders a block component in the place of a specified set of selectors.
@@ -29,7 +81,12 @@ export default ( selector, Block, getProps = getPropsFallback ) => {
 
 			element.classList.remove( 'is-loading' );
 
-			render( <Block { ...props } attributes={ attributes } />, element );
+			render(
+				<FrontBlockCrashBoundary
+					block={ <Block { ...props } attributes={ attributes } /> }
+				/>,
+				element
+			);
 		} );
 	}
 };

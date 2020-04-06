840a9fdae7c12f634590c9c831ac393e63bd387a
Kelly Dwan <ryelle@users.noreply.github.com>
2020-04-06 12:11:05 -0400

    Remove `noscript` exception
    This is not necessary anymore, since the filter is now added after we sanitize the post

--- public_html/wp-content/mu-plugins/jetpack-tweaks/miscellaneous.php
+++ public_html/wp-content/mu-plugins/jetpack-tweaks/miscellaneous.php
@@ -61,27 +61,6 @@ function lower_brute_protect_api_timeout( $timeout ) {
 add_filter( 'jetpack_protect_connect_timeout', __NAMESPACE__ . '\lower_brute_protect_api_timeout' );
 
 /**
- * Update kses filter.
- *
- * Allow `noscript`: this is used by Jetpack's lazy-loading before we output the content, and by default is
- * stripped by the `wp_kses_post` function, causing duplicate images.
- *
- * @param array $tags
- * @return array
- */
-function allow_noscript_blocks( $tags, $context ) {
-	global $post;
-
-	// Only allow noscript through if we're showing a post with blocks.
-	if ( 'post' === $context && isset( $post, $post->post_content ) && has_blocks( $post->post_content ) ) {
-		$tags['noscript'] = array();
-	}
-
-	return $tags;
-}
-add_action( 'wp_kses_allowed_html', __NAMESPACE__ . '\allow_noscript_blocks', 10, 2 );
-
-/**
  * Register caching routes for Jetpack with the frontend service worker.
  *
  * Jetpack uses wp.com domains for loading assets, which need to be cached regexes that match from the start of

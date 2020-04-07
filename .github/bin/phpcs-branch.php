25bf64f4838e11cf2ecee3fc1f1360999b85e8fa
Kelly Dwan <ryelle@users.noreply.github.com>
2020-04-07 16:15:38 -0400

    Tools: Try using `phpcs-changed` to limit php linting to changed lines (#417)
    * Tools: Add phpcs-changed
    
    * Tools: Add phpcs-branch script
    
    Report on phpcs violations that are introduced in the current branch (not production).
    
    * Run phpcs-branch on travis CI
    
    * Base the diffs off the PR's target branch
    
    In most cases this will be production, but not always. This will ensure that the code sniffer is run against only the changes in the given PR.
    
    * Add test file
    
    * Introduce an error in a modified line
    
    * Use exit status to indicate any errors when running each linter process
    
    * Fix the linter errors
    
    * Update composer.lock

---
+++ .github/bin/phpcs-branch.php
@@ -0,0 +1,74 @@
+#!/usr/bin/php
+<?php
+/**
+ * Report on phpcs violations that are introduced in the current branch (not production).
+ * Runs `phpcs` as normal on new files, and `phpcs-changed` on modified files. `phpcs-changed` will only report
+ * on changed lines in each modified file.
+ *
+ * How to use: php .github/bin/phpcs-branch.php
+ */
+
+// phpcs:ignoreFile
+namespace WordCamp\Bin\PHPCS_Changed;
+
+function run_phpcs( $file, $bin_dir ) {
+	exec( "$bin_dir/phpcs $file -q", $output, $exec_exit_status );
+	echo implode( "\n", $output );
+	return $exec_exit_status;
+}
+
+function run_phpcs_changed( $file, $git, $base_branch, $bin_dir ) {
+	$name = basename( $file );
+	exec( "$git diff $base_branch HEAD $file > $name.diff" );
+	exec( "$git show HEAD:$file | $bin_dir/phpcs --standard=./phpcs.xml.dist --report=json -nq > $name.orig.phpcs" );
+	exec( "cat $file | $bin_dir/phpcs --standard=./phpcs.xml.dist --report=json -nq > $name.phpcs" );
+	
+	$cmd = "$bin_dir/phpcs-changed --diff $name.diff --phpcs-orig $name.orig.phpcs --phpcs-new $name.phpcs";
+	exec( $cmd, $output, $exec_exit_status );
+	echo implode( "\n", $output );
+	echo "\n";
+
+	exec( "rm $name.diff $name.orig.phpcs $name.phpcs" );
+	return $exec_exit_status;
+}
+
+function main() {
+	$base_branch = getenv( 'TRAVIS_BRANCH' );
+	$git_dir     = dirname( dirname( __DIR__ ) );
+	$bin_dir     = dirname( dirname( __DIR__ ) ) . '/public_html/wp-content/mu-plugins/vendor/bin';
+	$git         = "git -C $git_dir";
+
+	try {
+		echo "\nScanning changed files...\n";
+		$status = 0;
+
+		$affected_files = shell_exec( "$git diff $base_branch...HEAD --name-status --diff-filter=AM 2>&1 | grep .php$" );
+		$affected_files = explode( "\n", trim( $affected_files ) );
+
+		foreach ( $affected_files as $record ) {
+			list( $change, $file ) = explode( "\t", trim( $record ) );
+
+			switch ( $change ) {
+				case 'M':
+					$cmd_status = run_phpcs_changed( $file, $git, $base_branch, $bin_dir );
+					break;
+
+				case 'A':
+					$cmd_status = run_phpcs( $file, $bin_dir );
+					break;
+			}
+
+			// If any cmd exits with 1, we want to exit with 1.
+			$status |= $cmd_status;
+		}
+
+	} catch ( Exception $exception ) {
+		echo "\nAborting because of error: {$exception->getMessage()} \n";
+		$status = 1;
+
+	}
+
+	exit( $status );
+}
+
+main();

c632f90c8d036eda8afbd85a77b63eaebdff3089
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2020-04-13 17:56:12 -0700

    Include the 'inappropriate' status in query results when 'all' statuses are called for

--- public_html/wp-content/plugins/wordcamp-speaker-feedback/includes/query.php
+++ public_html/wp-content/plugins/wordcamp-speaker-feedback/includes/query.php
@@ -8,6 +8,7 @@ use const WordCamp\SpeakerFeedback\Comment\COMMENT_TYPE;
 defined( 'WPINC' ) || die();
 
 add_action( 'pre_get_comments', __NAMESPACE__ . '\pre_get_comments', 99 );
+add_action( 'comments_clauses', __NAMESPACE__ . '\comments_clauses', 10, 2 );
 
 /**
  * Only return feedback comments from the query when that type is specifically called for.
@@ -37,4 +38,25 @@ function pre_get_comments( &$query_ref ) {
 	}
 
 	$query_ref->query_vars['type__not_in'] = $type_vars['type__not_in'];
+}
+
+/**
+ * Include custom statuses when 'all' is the status query argument.
+ *
+ * @param array            $clauses
+ * @param WP_Comment_Query $query
+ *
+ * @return string|string[]
+ */
+function comments_clauses( $clauses, $query ) {
+	$status = $query->query_vars['status'];
+	$status = (array) $status; // $query is passed by reference, don't want to alter it.
+
+	if ( in_array( 'all', $status, true ) || in_array( '', $status, true ) ) {
+		$search  = "( comment_approved = '0' OR comment_approved = '1' )";
+		$replace = "( comment_approved = '0' OR comment_approved = '1' OR comment_approved = 'inappropriate' )";
+		$clauses = str_replace( $search, $replace, $clauses );
+	}
+
+	return $clauses;
 }

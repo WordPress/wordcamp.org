4348f427932385872e710df907a9e50a3bd928e2
Corey McKrill <916023+coreymckrill@users.noreply.github.com>
2020-04-16 14:44:25 -0700

    Add custom cap to speakers when they are trying to view session feedback (#439)
    Instead of requiring the read capability for speakers when they are attempting to view the feedback on their sessions, require a custom capability instead. Then conditionally add that capability to the speaker user via a filter when the right conditions are met. This way the speaker user doesn't have to be a member of the site, they just need to be logged into the network.
    
    Fixes #425

--- public_html/wp-content/plugins/wordcamp-speaker-feedback/includes/capabilities.php
+++ public_html/wp-content/plugins/wordcamp-speaker-feedback/includes/capabilities.php
@@ -2,6 +2,7 @@
 
 namespace WordCamp\SpeakerFeedback\Capabilities;
 
+use WP_User;
 use function WordCamp\SpeakerFeedback\Comment\get_feedback_comment;
 use function WordCamp\SpeakerFeedback\Post\get_session_speaker_user_ids;
 use const WordCamp\SpeakerFeedback\Comment\COMMENT_TYPE;
@@ -9,6 +10,7 @@ use const WordCamp\SpeakerFeedback\Comment\COMMENT_TYPE;
 defined( 'WPINC' ) || die();
 
 add_filter( 'map_meta_cap', __NAMESPACE__ . '\map_meta_caps', 10, 4 );
+add_filter( 'user_has_cap', __NAMESPACE__ . '\add_caps', 10, 4 );
 
 /**
  * Determine capabilities needed for various feedback operations.
@@ -36,7 +38,7 @@ function map_meta_caps( $user_caps, $current_cap, $user_id, $args = array() ) {
 			// Current user is a speaker for the session receiving feedback comments.
 			$session_speakers = get_session_speaker_user_ids( $feedback->comment_post_ID );
 			if ( in_array( $user_id, $session_speakers, true ) ) {
-				$required_caps[] = 'read';
+				$required_caps[] = $current_cap;
 
 				// The speaker can only read approved comments, unless they already can edit them.
 				$can_edit = user_can( $user_id, 'edit_comment', $feedback->comment_ID );
@@ -67,7 +69,7 @@ function map_meta_caps( $user_caps, $current_cap, $user_id, $args = array() ) {
 			// Current user is a speaker for the session receiving feedback comments.
 			$session_speakers = get_session_speaker_user_ids( $post->ID );
 			if ( in_array( $user_id, $session_speakers, true ) ) {
-				$required_caps[] = 'read';
+				$required_caps[] = $current_cap;
 				break;
 			}
 
@@ -85,4 +87,51 @@ function map_meta_caps( $user_caps, $current_cap, $user_id, $args = array() ) {
 	}
 
 	return $user_caps;
+}
+
+/**
+ * Add new capabilities to a user for some feedback operations.
+ *
+ * @param array   $allcaps The original list of caps for the given user.
+ * @param array   $caps    Unused.
+ * @param array   $args    Arguments that accompany the requested capability check.
+ * @param WP_User $user    The user object.
+ *
+ * @return array The modified list of caps for the given user.
+ */
+function add_caps( $allcaps, $caps, $args, $user ) {
+	$requested_cap = $args[0] ?? null;
+	$context       = $args[2] ?? null;
+
+	switch ( $requested_cap ) {
+		case 'read_' . COMMENT_TYPE:
+			// Context is a comment ID or object.
+			$feedback = get_feedback_comment( $context );
+			if ( is_null( $feedback ) ) {
+				break;
+			}
+
+			// Current user is a speaker for the session receiving feedback comments.
+			$session_speakers = get_session_speaker_user_ids( $feedback->comment_post_ID );
+			if ( in_array( $user->ID, $session_speakers, true ) ) {
+				$allcaps[ $requested_cap ] = true;
+			}
+			break;
+
+		case 'read_post_' . COMMENT_TYPE:
+			// Context is a post ID or object.
+			$post = get_post( $context );
+			if ( is_null( $post ) ) {
+				break;
+			}
+
+			// Current user is a speaker for the session receiving feedback comments.
+			$session_speakers = get_session_speaker_user_ids( $post->ID );
+			if ( in_array( $user->ID, $session_speakers, true ) ) {
+				$allcaps[ $requested_cap ] = true;
+			}
+			break;
+	}
+
+	return $allcaps;
 }

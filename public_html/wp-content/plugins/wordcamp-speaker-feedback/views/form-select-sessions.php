8752e73fefe9ca3d83dc4ae9f704668d0077ec67
Kelly Dwan <ryelle@users.noreply.github.com>
2020-05-25 10:33:03 -0400

    Speaker Feedback: Add context to the sessions list (#495)
    This adds session time & speaker names to the session list on `/feedback`. It also updates the sorting from alphabetical by title to by time, so sessions are in the order they happened.
    
    Fixes #489

--- public_html/wp-content/plugins/wordcamp-speaker-feedback/views/form-select-sessions.php
+++ public_html/wp-content/plugins/wordcamp-speaker-feedback/views/form-select-sessions.php
@@ -1,15 +1,21 @@
 <?php
 
 namespace WordCamp\SpeakerFeedback\View;
+use function WordCamp\SpeakerFeedback\Post\get_session_speaker_names;
 
 $session_args = array(
 	'post_type'      => 'wcb_session',
 	'posts_per_page' => -1,
-	'orderby'        => 'title',
+	'orderby'        => 'meta_value_num',
 	'order'          => 'asc',
+	'meta_key'       => '_wcpt_session_time',
 	// get only sessions, no breaks.
-	'meta_key'       => '_wcpt_session_type',
-	'meta_value'     => 'session',
+	'meta_query'     => array(
+		array(
+			'key'   => '_wcpt_session_type',
+			'value' => 'session',
+		),
+	),
 );
 
 $sessions = new \WP_Query( $session_args );
@@ -22,10 +28,33 @@ if ( $sessions->have_posts() ) : ?>
 			<select name="sft_session" id="sft-session">
 				<?php while ( $sessions->have_posts() ) :
 					$sessions->the_post();
+					$session_title = get_the_title();
+					$session_time  = absint( get_post_meta( get_the_ID(), '_wcpt_session_time', true ) );
+					/* translators: Abbreviated date & time used in session dropdown. */
+					$session_date  = wp_date( __( 'M d, g:ia', 'wordcamporg' ), $session_time );
+
+					$speakers = get_session_speaker_names( get_the_ID() );
+					if ( ! empty( $speakers ) ) {
+						$option_text = sprintf(
+							/* translators: %1$s: Date & time of the session. %2$s: Session title. %3$s: Speaker names. */
+							__( '%1$s: %2$s - %3$s', 'wordcamporg' ),
+							$session_date,
+							$session_title,
+							/* translators: used between list items, there is a space after the comma */
+							implode( __( ', ', 'wordcamporg' ), $speakers )
+						);
+					} else {
+						$option_text = sprintf(
+							/* translators: %1$s: Date & time of the session. %2$s: Session title. */
+							__( '%1$s: %2$s', 'wordcamporg' ),
+							$session_date,
+							$session_title
+						);
+					}
 					printf(
 						'<option value="%s">%s</option>',
 						esc_attr( get_the_ID() ),
-						wp_kses_post( get_the_title() )
+						wp_kses_post( $option_text )
 					);
 				endwhile; ?>
 			</select>

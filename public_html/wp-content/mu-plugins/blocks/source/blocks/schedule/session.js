b02372e705fb325deb3b8267243f90b5ef298d13
Ian Dunn <ian@iandunn.name>
2020-06-02 09:10:00 -0700

    discard - session lint error

--- public_html/wp-content/mu-plugins/blocks/source/blocks/schedule/session.js
+++ public_html/wp-content/mu-plugins/blocks/source/blocks/schedule/session.js
@@ -268,7 +268,7 @@ function renderWarnings( spansNonContiguousTracks ) {
 	 * even if it isn't obvious on their current device.
      */
 	const pleaseRenameSlugs = createInterpolateElement(
-		__( "Warning: Sessions can't span non-contiguous tracks in the grid layout. Please <a>rename the track slugs</a> so that the tracks you want to appear next to each other are sorted alphabetically.", 'wordcamporg' ),
+		__( "pass. agd . dWarning: Sessions can't span non-contiguous tracks in the grid layout. Please <a>rename the track slugs</a> so that the tracks you want to appear next to each other are sorted alphabetically.", 'wordcamporg' ),
 		{
 			a: <a href={ '/wp-admin/edit-tags.php?taxonomy=wcb_track&post_type=wcb_session' } >#21441-gutenberg</a>,
 		}

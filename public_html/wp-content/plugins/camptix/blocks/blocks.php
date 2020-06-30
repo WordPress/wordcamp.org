ebb16bd851371b068fa7f52aaff94a9728c1ed45
Kelly Dwan <ryelle@users.noreply.github.com>
2020-06-30 15:30:15 -0400

    Switch to register_block_type_from_metadata

--- public_html/wp-content/plugins/camptix/blocks/blocks.php
+++ public_html/wp-content/plugins/camptix/blocks/blocks.php
@@ -11,34 +11,19 @@ defined( 'WPINC' ) || die();
  * @return void
  */
 function register_assets() {
-	$path        = __DIR__ . '/build/attendee-content.js';
-	$deps_path   = __DIR__ . '/build/attendee-content.asset.php';
-	$script_info = file_exists( $deps_path )
-		? require $deps_path
-		: array(
-			'dependencies' => array(),
-			'version'      => filemtime( $path ),
-		);
-
-	wp_register_script(
-		'camptix-blocks',
-		plugins_url( 'build/attendee-content.js', __FILE__ ),
-		$script_info['dependencies'],
-		$script_info['version'],
-		false
-	);
-
-	wp_set_script_translations( 'camptix-blocks', 'wordcamporg' );
-
 	// Set up the Attendee Content block.
-	register_block_type(
-		'camptix/attendee-content',
+	register_block_type_from_metadata(
+		__DIR__ . '/src/attendee-content',
 		array(
-			'editor_script'   => 'camptix-blocks',
 			'render_callback' => function( $attributes, $content ) {
-				return sprintf( '[camptix_private]%s[/camptix_private]', $content );
+				return sprintf(
+					'[camptix_private mark_attended="%s"]%s[/camptix_private]',
+					$attributes['markAttended'],
+					$content
+				);
 			},
 		)
 	);
+	wp_set_script_translations( 'camptix-attendee-content-editor-script', 'wordcamporg' );
 }
 add_action( 'init', __NAMESPACE__ . '\register_assets' );

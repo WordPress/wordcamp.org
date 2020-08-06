20f42137de103cec0fa38bc23b24b414e822237c
Timi Wahalahti <timi@wahalahti.fi>
2020-08-06 23:25:41 +0300

    Add semicolon

--- public_html/wp-content/plugins/wcpt/views/common/metabox-original-application.php
+++ public_html/wp-content/plugins/wcpt/views/common/metabox-original-application.php
@@ -9,7 +9,7 @@ defined( 'WPINC' ) || die();
 		<?php foreach ( $application_data as $question => $answer ) :
 			$question_hr = ucfirst( str_replace( '_', ' ', preg_replace('/q_\d+_/', '$1', $question ) ) ); ?>
 			<tr>
-				<th style="width:20%;"><p><b><?php echo esc_html( $question_hr ) ?></b></p></th>
+				<th style="width:20%;"><p><b><?php echo esc_html( $question_hr ); ?></b></p></th>
 				<th>
 					<?php if ( is_array( $answer ) ) {
 						echo esc_html( implode( ', ', $answer ) );

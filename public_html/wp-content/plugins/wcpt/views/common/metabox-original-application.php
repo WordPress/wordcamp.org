a32b7bcc388611a9bad4267ab240b41c169cdfc4
Timi Wahalahti <timi@wahalahti.fi>
2020-08-06 23:10:12 +0300

    Display original application answers in a table

--- public_html/wp-content/plugins/wcpt/views/common/metabox-original-application.php
+++ public_html/wp-content/plugins/wcpt/views/common/metabox-original-application.php
@@ -4,6 +4,20 @@ defined( 'WPINC' ) or die();
 /** @var array $application_data */
 ?>
 
-<pre style="white-space: pre-wrap;">
-	<?php echo esc_html( print_r( $application_data, true ) ); ?>
-</pre>
+<table class="widefat striped">
+  <tbody>
+    <?php foreach ( $application_data as $question => $answer ) :
+      $question_hr = ucfirst( str_replace( '_', ' ', preg_replace('/q_\d+_/', '$1', $question ) ) ); ?>
+      <tr>
+        <th style="width:20%;"><p><b><?php echo esc_html( $question_hr ) ?></b></p></th>
+        <th>
+          <?php if ( is_array( $answer ) ) {
+            echo esc_html( implode( ', ', $answer ) );
+          } else {
+            echo esc_html( $answer );
+          } ?>
+        </th>
+      </tr>
+    <?php endforeach; ?>
+  </tbody>
+</table>

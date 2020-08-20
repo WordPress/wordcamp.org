5e9120a70b90cdb7517362a84bdee4926cc3bfd5
Timi Wahalahti <timi@wahalahti.fi>
2020-08-20 22:47:36 +0300

    WCPT: Improve WordCamp and Meetup application tracker UX (#536)

---
+++ public_html/wp-content/plugins/wcpt/views/common/metabox-original-application.php
@@ -0,0 +1,23 @@
+<?php
+defined( 'WPINC' ) || die();
+
+/** @var array $application_data */
+?>
+
+<table class="widefat striped">
+	<tbody>
+		<?php foreach ( $application_data as $question => $answer ) :
+			$question_hr = ucfirst( str_replace( '_', ' ', preg_replace('/q_\d+_/', '$1', $question ) ) ); ?>
+			<tr>
+				<th style="width:20%;"><p><b><?php echo esc_html( $question_hr ); ?></b></p></th>
+				<th>
+					<?php if ( is_array( $answer ) ) {
+						echo esc_html( implode( ', ', $answer ) );
+					} else {
+						echo esc_html( $answer );
+					} ?>
+				</th>
+			</tr>
+		<?php endforeach; ?>
+	</tbody>
+</table>
--- public_html/wp-content/plugins/wcpt/wcpt-event/class-event-admin.php
+++ public_html/wp-content/plugins/wcpt/wcpt-event/class-event-admin.php
@@ -214,8 +214,8 @@ abstract class Event_Admin {
 			__( 'Add Private Note', 'wordcamporg' ),
 			'wcpt_add_note_metabox',
 			$this->get_event_type(),
-			'side',
-			'low'
+			'advanced',
+			'high'
 		);
 	}
 
@@ -244,7 +244,7 @@ abstract class Event_Admin {
 	 */
 	public function original_application_metabox( $post ) {
 		$application_data = get_post_meta( $post->ID, '_application_data', true );
-		require_once WCPT_DIR . 'views/wordcamp/metabox-original-application.php';
+		require_once WCPT_DIR . 'views/common/metabox-original-application.php';
 	}
 
 	/**

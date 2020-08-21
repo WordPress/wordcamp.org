13228426c6dae78ac160d090761a28f29ab3967c
Timi Wahalahti <timi@wahalahti.fi>
2020-08-21 13:57:09 +0300

    Apply coding standards

--- public_html/wp-content/mu-plugins/jetpack-tweaks/miscellaneous.php
+++ public_html/wp-content/mu-plugins/jetpack-tweaks/miscellaneous.php
@@ -8,7 +8,7 @@ defined( 'WPINC' ) || die();
 // Allow Photon to fetch images that are served via HTTPS.
 add_filter( 'jetpack_photon_reject_https',    '__return_false' );
 
-// Disable Simple Payments feature
+// Disable Simple Payments feature.
 add_filter( 'jetpack_disable_simple_payments', '__return_true' );
 
 /**

a6d2250302a4c3a26aec918fd584173dc4dad136
Ian Dunn <ian@iandunn.name>
2020-10-22 12:13:28 -0700

    Miscellaneous: Log Events API responses to test #core51130.
    See https://wordpress.slack.com/archives/C037W5S7X/p1603381736252300?thread_ts=1603380125.251100&cid=C037W5S7X

--- public_html/wp-content/mu-plugins/wcorg-misc.php
+++ public_html/wp-content/mu-plugins/wcorg-misc.php
@@ -1,6 +1,8 @@
 <?php
 
+use Dotorg\Slack\Send;
 use function WordCamp\Sunrise\get_top_level_domain;
+
 
 /*
  * Miscellaneous snippets that don't warrant their own file
@@ -651,3 +653,70 @@ function add_wcpt_cross_link( WP_Admin_Bar $wp_admin_bar ) {
 }
 // The Priority positions the link after the Dashboard link on the front end.
 add_action( 'admin_bar_menu', 'add_wcpt_cross_link', 35 );
+
+/**
+ * Log requests to the WordPress.org Events API and their responses, to aid debugging.
+ *
+ * @param array|WP_Error $response     HTTP response or WP_Error object.
+ * @param string         $context      Context under which the hook is fired.
+ * @param string         $transport    HTTP transport used.
+ * @param array          $request_args HTTP request arguments.
+ * @param string         $request_url  The request URL.
+ */
+function debug_community_events_response( $response, $context, $transport, $request_args, $request_url ) {
+	if ( false === strpos( $request_url, 'api.wordpress.org/events' ) ) {
+		return;
+	}
+
+	require_once __DIR__ . '/includes/slack/send.php';
+
+	$response_code = wp_remote_retrieve_response_code( $response );
+	$response_body = json_decode( wp_remote_retrieve_body( $response ), true );
+
+	// Avoid bloating the log with all the event data, but the titles are enough to know what was returned.
+	$response_body['events'] = array_column( $response_body['events'], 'title' );
+
+	$message = sprintf(
+		'%s %s',
+		$response_code,
+		is_wp_error( $response ) ? $response->get_error_message() : 'Valid response received'
+	);
+
+	$attachment = array(
+		'author_name' => __FUNCTION__,
+		'color'       => '#00A0D2',
+
+		'fields' => array(
+			array(
+				'title' => 'User',
+				'value' => _wp_get_current_user()->get( 'user_login' ),
+				'short' => false,
+			),
+
+			array(
+				'title' => 'Result',
+				'value' => $message,
+				'short' => false,
+			),
+
+			array(
+				'title' => 'Request URL',
+				'value' => add_query_arg( $request_args['body'], $request_url ),
+				'short' => false,
+			),
+
+			array(
+				'title' => 'Response Body',
+				'value' => print_r( $response_body, true ),
+				'short' => false,
+			),
+		),
+	);
+
+	$slack = new Send( SLACK_ERROR_REPORT_URL );
+	$slack->add_attachment( $attachment );
+	$slack->send( WORDCAMP_LOGS_SLACK_CHANNEL );
+}
+// Comment this out when not needed, but leave the code for future use.
+// todo - leave this in place until WP 5.5.2 is released, to test #51130
+add_action( 'http_api_debug', 'debug_community_events_response', 10, 5 );

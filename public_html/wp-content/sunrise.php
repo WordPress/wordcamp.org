1050788d0c91b6608bc0e7edaa6e2dc4cd3b661b
Dion Hulse <dion@wordpress.org>
2021-06-08 16:34:06 +1000

    Don't redirect to europe.wordcamp.org/2022 until July 1st of 2021.

--- public_html/wp-content/sunrise.php
+++ public_html/wp-content/sunrise.php
@@ -557,7 +557,7 @@ function get_canonical_year_url( $domain, $path ) {
 	// Special cases where the redirect shouldn't go to next year's camp until this year's camp is over.
 	switch ( $domain ) {
 		case "europe.wordcamp.$tld":
-			if ( time() <= strtotime( '2020-07-01' ) ) {
+			if ( time() <= strtotime( '2021-07-01' ) ) {
 				return "https://europe.wordcamp.$tld/2021/";
 			}
 			break;

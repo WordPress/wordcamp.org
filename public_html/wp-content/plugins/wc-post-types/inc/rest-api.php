6b5cc810fb95fc846e9cd7994ded89f0907b8eea
Kelly Dwan <ryelle@users.noreply.github.com>
2021-08-25 17:56:48 -0400

    Update duration value to 50 minutes, not 50 seconds.

--- public_html/wp-content/plugins/wc-post-types/inc/rest-api.php
+++ public_html/wp-content/plugins/wc-post-types/inc/rest-api.php
@@ -129,7 +129,7 @@ function register_session_post_meta() {
 			'show_in_rest'  => true,
 			'single'        => true,
 			'auth_callback' => __NAMESPACE__ . '\meta_auth_callback',
-			'default'       => 50,
+			'default'       => 50 * MINUTE_IN_SECONDS,
 		)
 	);
 	register_post_meta(

8ac220582322d72f176332a433fcffd81f941683
Kelly Dwan <ryelle@users.noreply.github.com>
2021-09-08 13:02:05 -0400

    Use new avatar_or_image function in speaker & organizer blocks

--- public_html/wp-content/plugins/wc-post-types/inc/utilities.php
+++ public_html/wp-content/plugins/wc-post-types/inc/utilities.php
@@ -15,14 +15,18 @@ defined( 'WPINC' ) || die();
  *
  * @param int|WP_Post $post Post ID or post object.
  * @param int         $size Size for avatar.
+ * @param string      $alt  Alt text to use, defaults to post title. Can be empty string to indicate empty alt.
  *
  * @return string
  */
-function get_avatar_or_image( $post, $size ) {
+function get_avatar_or_image( $post, $size, $alt = false ) {
 	global $wcpt_plugin;
 	$post = get_post( $post );
 	if ( ! $post ) {
 		return '';
+	}
+	if ( false === $alt ) {
+		$alt = get_the_title( $post->ID );
 	}
 
 	$email       = get_post_meta( $post->ID, '_wcb_speaker_email', true );
@@ -42,21 +46,18 @@ function get_avatar_or_image( $post, $size ) {
 		);
 	}
 
-	$avatar = sprintf(
-		'<img src="%1$s" alt="%2$s" />',
-		esc_url( $src ),
-		get_the_title( $post->ID )
-	);
+	$avatar = sprintf( '<img src="%1$s" alt="%2$s" />', esc_url( $src ), $alt );
 
+	// Remove the filter so that we can get the real featured image.
 	remove_filter( 'get_post_metadata', array( $wcpt_plugin, 'hide_featured_image_on_people' ), 10, 3 );
 
-	// Remove Jetpack filter so that we can always get the featured image.
+	// If this Jetpack function exists, we need to remove it too.
 	if ( function_exists( 'jetpack_featured_images_remove_post_thumbnail' ) ) {
 		remove_filter( 'get_post_metadata', 'jetpack_featured_images_remove_post_thumbnail', true, 4 );
-		$featured_image = get_the_post_thumbnail( $post->ID, array( $size, $size ) );
+		$featured_image = get_the_post_thumbnail( $post->ID, array( $size, $size ), array( 'alt' => $alt ) );
 		add_filter( 'get_post_metadata', 'jetpack_featured_images_remove_post_thumbnail', true, 4 );
 	} else {
-		$featured_image = get_the_post_thumbnail( $post->ID, array( $size, $size ) );
+		$featured_image = get_the_post_thumbnail( $post->ID, array( $size, $size ), array( 'alt' => $alt ) );
 	}
 	add_filter( 'get_post_metadata', array( $wcpt_plugin, 'hide_featured_image_on_people' ), 10, 3 );
 

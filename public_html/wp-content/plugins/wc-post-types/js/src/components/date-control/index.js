79753bc30fd8f9f33037fd4b5625935e606b04c0
Kelly Dwan <ryelle@users.noreply.github.com>
2021-09-17 13:52:09 -0400

    Sessions: Use the TimePicker component for session times.
    This displays a set of text inputs for the day and time of the session, including an indicator of timezone if your current TZ is different from the site's TZ. Previously, it was unclear once you closed the dropdown what timezone the date was, this removes the ambiguity.

--- public_html/wp-content/plugins/wc-post-types/js/src/components/date-control/index.js
+++ public_html/wp-content/plugins/wc-post-types/js/src/components/date-control/index.js
@@ -2,8 +2,8 @@
  * WordPress dependencies
  */
 // eslint-disable-next-line @wordpress/no-unsafe-wp-apis -- Date settings OK.
-import { __experimentalGetSettings, dateI18n } from '@wordpress/date';
-import { BaseControl, Button, DateTimePicker, Dropdown } from '@wordpress/components';
+import { __experimentalGetSettings } from '@wordpress/date';
+import { BaseControl, TimePicker } from '@wordpress/components';
 
 export default function( { date, label, onChange } ) {
 	const settings = __experimentalGetSettings();
@@ -15,22 +15,11 @@ export default function( { date, label, onChange } ) {
 			.reverse()
 			.join( '' ) // Reverse the string and test for "a" not followed by a slash
 	);
-	const formattedDate = date && dateI18n( `${ settings.formats.date } ${ settings.formats.time }`, date );
 
 	return (
 		<BaseControl>
 			<BaseControl.VisualLabel>{ label }</BaseControl.VisualLabel>
-			<Dropdown
-				position="bottom left"
-				renderToggle={ ( { onToggle, isOpen } ) => (
-					<Button onClick={ onToggle } aria-expanded={ isOpen } isLink>
-						{ formattedDate }
-					</Button>
-				) }
-				renderContent={ () => (
-					<DateTimePicker currentDate={ date } onChange={ onChange } is12Hour={ is12HourTime } />
-				) }
-			/>
+			<TimePicker currentTime={ date } onChange={ onChange } is12Hour={ is12HourTime } />
 		</BaseControl>
 	);
 }

4c7ba350c59c2df9d83abdecdd98d7e8f6df3c74
Dion Hulse <dion@wordpress.org>
2021-10-20 00:57:47 +1000

    Remote CSS: Update example URLs for clarity (#703)
    Update examples to use a generic non-specific-city repository, so it's clear it's not available to be forked.
    
    Use the `WordPress/` organization so that we can create the repo in the future, if desired.
    
    
    Co-authored-by: Ian Dunn <ian@iandunn.name>

--- public_html/wp-content/plugins/wordcamp-remote-css/tests/test-user-interface.php
+++ public_html/wp-content/plugins/wordcamp-remote-css/tests/test-user-interface.php
@@ -17,7 +17,7 @@ class Test_User_Interface extends WP_UnitTestCase {
 	 * @covers ::validate_remote_css_url()
 	 */
 	public function test_valid_url_allowed() {
-		$original_url  = 'https://api.github.com/repos/WordPressSeattle/seattle.wordcamp.org-2015/contents/style.css';
+		$original_url  = 'https://api.github.com/repos/WordPress/example.wordcamp.org-2015/contents/style.css';
 		$validated_url = validate_remote_css_url( $original_url );
 
 		$this->assertSame( $original_url, $validated_url );
@@ -60,7 +60,7 @@ class Test_User_Interface extends WP_UnitTestCase {
 	 */
 	public function test_non_http_s_protocols_invalid() {
 		$this->setExpectedException( '\Exception', 'URL was invalid' );
-		validate_remote_css_url( 'ssh://api.github.com/repos/WordPressSeattle/seattle.wordcamp.org-2015/contents/style.css' );
+		validate_remote_css_url( 'ssh://api.github.com/repos/WordPress/example.wordcamp.org-2015/contents/style.css' );
 	}
 
 	/**
@@ -80,6 +80,6 @@ class Test_User_Interface extends WP_UnitTestCase {
 	 */
 	public function test_non_css_extensions_blocked() {
 		$this->setExpectedException( '\Exception', 'URL must be a vanilla CSS file' );
-		validate_remote_css_url( 'https://api.github.com/repos/WordPressSeattle/seattle.wordcamp.org-2015/contents/style.scss' );
+		validate_remote_css_url( 'https://api.github.com/repos/WordPress/example.wordcamp.org-2015/contents/style.scss' );
 	}
 }

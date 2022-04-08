442d18357f94a42c4f6397fc670dad8c47093241
Kelly Dwan <ryelle@users.noreply.github.com>
2022-04-08 17:14:45 -0400

    Blocks: Avatar: Remove the excess margin around the figure
    This also fixes the center alignment.

--- public_html/wp-content/mu-plugins/blocks/source/blocks/avatar/index.js
+++ public_html/wp-content/mu-plugins/blocks/source/blocks/avatar/index.js
@@ -8,6 +8,7 @@ import { people as icon } from '@wordpress/icons';
  */
 import edit from './edit';
 import metadata from './block.json';
+import './edit.scss';
 
 export const NAME = metadata.name;
 

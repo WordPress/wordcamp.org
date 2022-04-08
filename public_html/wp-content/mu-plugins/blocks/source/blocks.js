62b8cc143d00ad8e38e5f588cb40d56cfa2c2e6a
Kelly Dwan <ryelle@users.noreply.github.com>
2022-04-08 17:14:17 -0400

    Blocks: Add the post-terms variations to the WordCamp category

--- public_html/wp-content/mu-plugins/blocks/source/blocks.js
+++ public_html/wp-content/mu-plugins/blocks/source/blocks.js
@@ -2,7 +2,7 @@
  * WordPress dependencies
  */
 import { __ } from '@wordpress/i18n';
-import { registerBlockType } from '@wordpress/blocks';
+import { registerBlockType, registerBlockVariation } from '@wordpress/blocks';
 
 /**
  * Internal dependencies
@@ -26,29 +26,32 @@ enabledBlocks.forEach( ( { NAME, SETTINGS } ) => {
 } );
 
 /*
- * Register the Tracks and Categories as variations on Post Terms.
+ * Register the custom taxonomies as variations on Post Terms.
  * See https://github.com/WordPress/gutenberg/blob/41325b983feabcc46615cd6b1a8a5efe64c5a9f0/packages/block-library/src/post-terms/variations.js.
  */
-wp.blocks.registerBlockVariation( 'core/post-terms', {
+registerBlockVariation( 'core/post-terms', {
 	name: 'wcb_track',
 	title: __( 'Session Tracks', 'wordcamporg' ),
 	description: __( "Display a session's tracks.", 'wordcamporg' ),
+	category: 'wordcamp',
 	attributes: { term: 'wcb_track' },
 	isActive: ( blockAttributes ) => blockAttributes.term === 'wcb_track',
 } );
 
-wp.blocks.registerBlockVariation( 'core/post-terms', {
+registerBlockVariation( 'core/post-terms', {
 	name: 'wcb_session_category',
 	title: __( 'Session Categories', 'wordcamporg' ),
 	description: __( "Display a session's categories.", 'wordcamporg' ),
+	category: 'wordcamp',
 	attributes: { term: 'wcb_session_category' },
 	isActive: ( blockAttributes ) => blockAttributes.term === 'wcb_session_category',
 } );
 
-wp.blocks.registerBlockVariation( 'core/post-terms', {
+registerBlockVariation( 'core/post-terms', {
 	name: 'wcb_organizer_team',
 	title: __( 'Organizer Teams', 'wordcamporg' ),
 	description: __( "Display an organizer's teams.", 'wordcamporg' ),
+	category: 'wordcamp',
 	attributes: { term: 'wcb_organizer_team' },
 	isActive: ( blockAttributes ) => blockAttributes.term === 'wcb_organizer_team',
 } );

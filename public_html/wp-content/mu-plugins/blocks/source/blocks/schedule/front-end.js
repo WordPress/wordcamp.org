20b73dfbc95d802a5590d1ec92bccf6c87941440
Kelly Dwan <ryelle@users.noreply.github.com>
2022-05-13 15:53:48 -0400

    Blocks: Remove the extra attributes parsing in `renderFrontend`
    All instances of `renderFrontend` do their own attribute parsing, so we can remove this extra set which also removes the need for a workaround in the schedule block.

--- public_html/wp-content/mu-plugins/blocks/source/blocks/schedule/front-end.js
+++ public_html/wp-content/mu-plugins/blocks/source/blocks/schedule/front-end.js
@@ -31,16 +31,6 @@ const rawScheduleData = window.WordCampBlocks.schedule || {};
 function ScheduleGridWithContext( props ) {
 	const { chosenSessions, allTracks, attributes, settings } = props;
 
-	/*
-	 * `attributes.attributes` is an unparsed JSON string. It's an artifact from `renderFrontend()` expecting
-	 * individual `data-{foo}` HTML attributes, instead of a single `data-attributes` one. For this block, though,
-	 * that would take extra work to maintain without providing any benefit. Removing it prevents it from causing
-	 * any confusion.
-	 *
-	 * @todo-front Maybe look at refactoring that function to avoid workarounds like this.
-	 */
-	delete attributes.attributes;
-
 	const contextValues = {
 		allTracks: allTracks,
 		attributes: attributes,

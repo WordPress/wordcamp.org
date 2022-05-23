c0f48c66078b92c9e8921c892263346611354535
Kelly Dwan <ryelle@users.noreply.github.com>
2022-05-23 17:43:47 -0400

    Remove unused function

--- public_html/wp-content/mu-plugins/blocks/source/blocks/schedule/utils/date.js
+++ public_html/wp-content/mu-plugins/blocks/source/blocks/schedule/utils/date.js
@@ -8,13 +8,3 @@
 export function getTimezone( { useClientTimezone = false } = {} ) {
 	return useClientTimezone ? Intl.DateTimeFormat().resolvedOptions().timeZone : WordCampBlocks.schedule.timezone;
 }
-
-/**
- * Get the timezone set in `derived` on a list of sessions.
- *
- * @param {Array} sessions
- * @return {string}
- */
-export function getTimezoneFromSessions( sessions ) {
-	return sessions[ 0 ]?.derived?.timezone || WordCampBlocks.schedule.timezone;
-}

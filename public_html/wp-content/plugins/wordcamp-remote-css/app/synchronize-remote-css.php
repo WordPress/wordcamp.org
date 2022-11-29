2f57780b28319796711e20aaaffe83783f93eff8
Dion Hulse <dion@wordpress.org>
2022-11-29 17:57:50 +1000

    Linting fixes.

--- public_html/wp-content/plugins/wordcamp-remote-css/app/synchronize-remote-css.php
+++ public_html/wp-content/plugins/wordcamp-remote-css/app/synchronize-remote-css.php
@@ -76,13 +76,13 @@ function fetch_unsafe_remote_css( $remote_css_url ) {
  * @throws Exception
  */
 function sanitize_unsafe_css( $unsafe_css ) {
-	// Jetpack < 11.6
+	// Jetpack < 11.6 support.
 	$jetpack_custom_css_file = JETPACK__PLUGIN_DIR . '/modules/custom-css/custom-css-4.7.php';
 	if ( ! file_exists( $file ) ) {
-		// Jetpack 11.6+
+		// Jetpack 11.6+ support.
 		$jetpack_custom_css_file = JETPACK__PLUGIN_DIR . '/modules/custom-css/custom-css.php';
 	}
-	
+
 	require_once $jetpack_custom_css_file;
 
 	$parser_rules_setup          = has_filter( 'csstidy_optimize_postparse', 'WordCamp\Jetpack_Tweaks\sanitize_csstidy_parsed_rules' );

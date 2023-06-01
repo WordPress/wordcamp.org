b36bcac26672bb5ecc58471fdef8291b994c0a62
Kelly Dwan <ryelle@users.noreply.github.com>
2023-06-01 12:43:16 -0400

    Block Patterns: Add general block patterns (#880)

---
+++ public_html/wp-content/mu-plugins/blocks/patterns/header-centered-with-menu.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ * Title: Centered header with menu
+ * Slug: wordcamp/header-centered-with-menu
+ * Categories: header
+ * Block Types: core/template-part/header
+ */
+
+?>
+<!-- wp:group {"align":"full"} -->
+<div class="wp-block-group alignfull"><!-- wp:site-title {"textAlign":"center","style":{"typography":{"fontSize":"3rem"}}} /-->
+
+<!-- wp:paragraph {"align":"center"} -->
+<p class="has-text-align-center">November 16–17. 2023 • City Conference Center</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:spacer {"height":"20px"} -->
+<div style="height:20px" aria-hidden="true" class="wp-block-spacer"></div>
+<!-- /wp:spacer -->
+
+<!-- wp:separator {"opacity":"css","className":"alignfull is-style-wide"} -->
+<hr class="wp-block-separator has-css-opacity alignfull is-style-wide"/>
+<!-- /wp:separator -->
+
+<!-- wp:navigation {"layout":{"type":"flex","justifyContent":"center"}} /-->
+
+<!-- wp:separator {"opacity":"css","className":"alignfull is-style-wide"} -->
+<hr class="wp-block-separator has-css-opacity alignfull is-style-wide"/>
+<!-- /wp:separator --></div>
+<!-- /wp:group -->

f48ec2223c924d896ef84c608ed948ae12ef53b3
Ian Dunn <ian@iandunn.name>
2023-06-15 11:51:39 -0700

    Volunteers: Add `First Time Volunteering` question to CPT

--- public_html/wp-content/plugins/wc-post-types/js/src/volunteer/panel-info.js
+++ public_html/wp-content/plugins/wc-post-types/js/src/volunteer/panel-info.js
@@ -3,7 +3,7 @@
  */
 import { __ } from '@wordpress/i18n';
 import { PluginDocumentSettingPanel } from '@wordpress/edit-post';
-import { TextControl } from '@wordpress/components';
+import { RadioControl, TextControl } from '@wordpress/components';
 
 /**
  * Internal dependencies
@@ -14,6 +14,7 @@ import UsernameControl from '../components/username-control';
 export default function VolunteerInfoPanel() {
 	const [ email, setEmail ] = usePostMeta( '_wcb_volunteer_email', '' );
 	const [ username, setUsername ] = usePostMeta( '_wcpt_user_name', '' );
+	const [ firstTime, setFirstTime ] = usePostMeta( '_wcb_volunteer_first_time', false );
 
 	return (
 		<PluginDocumentSettingPanel
@@ -27,10 +28,22 @@ export default function VolunteerInfoPanel() {
 				value={ email }
 				onChange={ setEmail }
 			/>
+
 			<UsernameControl
 				label={ __( 'WordPress.org Username', 'wordcamporg' ) }
 				value={ username }
 				onChange={ setUsername }
+			/>
+
+			<RadioControl
+				label={ __( 'Is this the first time they have volunteered at a WordPress event?', 'wordcamporg' ) }
+				selected={ firstTime }
+				onChange={ ( value ) => setFirstTime( value ) }
+				options={ [
+					{ label: 'Yes', value: 'yes' },
+					{ label: 'No', value: 'no' },
+					{ label: 'Unsure', value: 'unsure' },
+				] }
 			/>
 		</PluginDocumentSettingPanel>
 	);

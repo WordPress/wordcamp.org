d67e16e7756b00d164fd1f94c6b4d947e29a3b4b
ren <18050944+renintw@users.noreply.github.com>
2023-06-16 11:30:42 +0800

    Add redirect for events.wordcamp.test

---
+++ public_html/wp-content/mu-plugins/redirect.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace WordCamp\Redirect;
+
+defined( 'WPINC' ) || die();
+
+add_action( 'init',  __NAMESPACE__ . '\redirect_subdomain' );
+
+/**
+ * Redirects the root URL of the "events.wordpress.org" to "https://make.wordpress.org/community/events".
+ */
+function redirect_subdomain() {
+	if ( 'events.wordcamp.test' === $_SERVER['HTTP_HOST'] && wp_using_themes() && ! defined( 'REST_REQUEST' ) ) {
+		$request_uri = $_SERVER['REQUEST_URI'];
+
+		if ( '/' === $request_uri ) {
+			$new_url = 'https://make.wordpress.org/community/events';
+			wp_redirect( $new_url );
+			exit;
+		}
+	}
+}

b04355515cc64945c345e17bf75b56044aeb13ac
Steven Dufresne <steve.dufresne@automattic.com>
2023-07-12 11:57:39 +0900

    Turn on blocks and theme templates for all networks (#934)

--- public_html/wp-content/mu-plugins/load-other-mu-plugins.php
+++ public_html/wp-content/mu-plugins/load-other-mu-plugins.php
@@ -17,6 +17,9 @@ function wcorg_include_common_plugins() {
 	if ( 'local' !== WORDCAMP_ENVIRONMENT ) {
 		require_once dirname( __DIR__ ) . '/mu-plugins-private/wporg-sso.php';
 	}
+
+	require_once __DIR__ . '/blocks/blocks.php';
+	require_once __DIR__ . '/theme-templates/bootstrap.php';
 }
 
 /**
@@ -58,9 +61,7 @@ function wcorg_include_individual_mu_plugins() {
 
 	require_once __DIR__ . '/wp-cli-commands/bootstrap.php';
 	require_once __DIR__ . '/camptix-tweaks/camptix-tweaks.php';
-	require_once __DIR__ . '/blocks/blocks.php';
 	require_once __DIR__ . '/quickbooks/quickbooks.php';
-	require_once __DIR__ . '/theme-templates/bootstrap.php';
 	require_once __DIR__ . '/virtual-embeds/virtual-embeds.php';
 
 	if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {

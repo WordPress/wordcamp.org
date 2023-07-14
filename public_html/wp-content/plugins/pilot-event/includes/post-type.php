65604926c576f80b724a323dcce8b616bb8827b0
StevenDufresne <dufresnesteven@gmail.com>
2023-07-14 10:06:46 +0900

    Add Pilot event cpt.

---
+++ public_html/wp-content/plugins/pilot-event/includes/post-type.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace WordCampPilotEvents\PostType;
+
+defined( 'WPINC' ) || die();
+
+/**
+ * Actions and filters.
+ */
+add_action( 'init', __NAMESPACE__ . '\create_pilot_event_post_type' );
+
+/**
+ * Register the custom post type for Pilot WordCamp events.
+ *
+ * @return void
+ */
+function create_pilot_event_post_type() {
+    $labels = array(
+        'name'               => 'Pilot Events',
+        'singular_name'      => 'Pilot Event',
+        'menu_name'          => 'Pilot Events',
+        'name_admin_bar'     => 'Pilot Event',
+        'add_new'            => 'Add New',
+        'add_new_item'       => 'Add New Pilot Event',
+        'new_item'           => 'New Pilot Event',
+        'edit_item'          => 'Edit Pilot Event',
+        'view_item'          => 'View Pilot Event',
+        'all_items'          => 'All Pilot Events',
+        'search_items'       => 'Search Pilot Events',
+        'parent_item_colon'  => 'Parent Pilot Events:',
+        'not_found'          => 'No pilot events found.',
+        'not_found_in_trash' => 'No pilot events found in Trash.'
+    );
+
+    $args = array(
+        'labels'             => $labels,
+        'public'             => true,
+        'publicly_queryable' => true,
+        'show_ui'            => true,
+        'show_in_menu'       => true,
+        'show_in_rest'       => true,
+        'query_var'          => true,
+        'rewrite'            => array( 'slug' => 'pilot-event' ),
+        'capability_type'    => 'post',
+        'has_archive'        => true,
+        'hierarchical'       => false,
+        'menu_position'      => null,
+        'supports'           => array( 'title', 'editor', 'thumbnail', 'excerpt' ),
+    );
+
+    register_post_type( 'pilot_event', $args );
+}
---
+++ public_html/wp-content/plugins/pilot-event/pilot-event.php
@@ -0,0 +1,43 @@
+<?php
+ /**
+ * Plugin name:       WordCamp Pilot Events
+ * Plugin URI:        https://github.com/WordPress/wordcamp.org
+ * Description:       [Experimental] Creates the custom post type for Pilot WordCamp events.
+ * Version:           0.1.0-beta
+ * Author:            WordPress.org
+ * Author URI:        http://wordpress.org/
+ * License:           GPLv2 or later
+ * Text Domain:       wordcamp
+ */
+
+namespace WordCampPilotEvents;
+
+defined( 'WPINC' ) || die();
+
+/**
+ * Constants.
+ */
+define( __NAMESPACE__ . '\PLUGIN_DIR', plugin_dir_path( __FILE__ ) );
+
+/**
+ * Actions and filters.
+ */
+add_action( 'plugins_loaded', __NAMESPACE__ . '\load_files' );
+
+/**
+ * Load the other PHP files for the plugin.
+ *
+ * @return void
+ */
+function load_files() {
+	require_once get_includes_path() . 'post-type.php';
+}
+
+/**
+ * Shortcut to the includes directory.
+ *
+ * @return string
+ */
+function get_includes_path() {
+	return PLUGIN_DIR . 'includes/';
+}

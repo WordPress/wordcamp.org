a391ca549d5a66714360d6ec8a49322b90fa88d1
Ian Dunn <ian@iandunn.name>
2023-07-17 14:50:01 -0700

    Sunrise: Move URL patterns to root for reuse in plugins

--- public_html/wp-content/sunrise.php
+++ public_html/wp-content/sunrise.php
@@ -3,8 +3,70 @@
 namespace WordCamp\Sunrise;
 defined( 'WPINC' ) || die();
 
-load_network_sunrise();
 
+/*
+ * Matches `2020-foo.narnia.wordcamp.org/`, with or without additional `REQUEST_URI` params.
+ */
+const PATTERN_YEAR_DOT_CITY_DOMAIN_PATH = '
+	@ ^
+	( \d{4} [\w-]* )           # Capture the year, plus any optional extra identifier.
+	\.
+	( [\w-]+ )                 # Capture the city.
+	\.
+	( wordcamp | buddycamp )   # Capture the second-level domain.
+	\.
+	( org | test )             # Capture the top level domain.
+	/
+	@ix
+';
+
+/*
+ * Matches `narnia.wordcamp.org/2020-foo/`, with or without additional `REQUEST_URI` params.
+ */
+const PATTERN_CITY_SLASH_YEAR_DOMAIN_PATH = '
+	@ ^
+	( [\w-]+ )                 # Capture the city.
+	\.
+	( wordcamp | buddycamp )   # Capture the second-level domain.
+	\.
+	( org | test )             # Capture the top-level domain.
+	( / \d{4} [\w-]* / )       # Capture the site path (the year, plus any optional extra identifier).
+	@ix
+';
+
+/*
+ * Matches a request URI like `/2020/2019/save-the-date-for-wordcamp-vancouver-2020/`.
+ */
+const PATTERN_CITY_SLASH_YEAR_REQUEST_URI_WITH_DUPLICATE_DATE = '
+	@ ^
+	( / \d{4} [\w-]* / )   # Capture the site path (the year, plus any optional extra identifier).
+
+	(                      # Capture the `/%year%/%monthnum%/%day%/` permastruct tags.
+		[0-9]{4} /         # The year is required.
+
+		(?:                # The month and day are optional.
+			[0-9]{2} /
+		){0,2}
+	)
+
+	(.+)                   # Capture the slug.
+	$ @ix
+';
+
+/*
+ * Matches a URL path like '/vancouver/2023/diversity-day/`.
+ */
+const PATTERN_CITY_YEAR_TYPE_PATH = '
+	@ ^
+	/
+	( [\w-]+ )    # Capture the city.
+	/
+	( \d{4} )     # Capture the year.
+	/
+	( [\w-]+ )    # Capture the event type.
+	/?
+	@ix
+';
 
 /**
  * Load the sunrise file for the current network.
@@ -30,3 +92,6 @@ function load_network_sunrise() {
 function get_top_level_domain() {
 	return 'local' === WORDCAMP_ENVIRONMENT ? 'test' : 'org';
 }
+
+
+load_network_sunrise();

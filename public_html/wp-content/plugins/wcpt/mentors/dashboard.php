WordPress/wordcamp.org

commit 0899c70997cc04108911afc210e64611343b52c2

Mentors: Sort alphabetically for convenience

public_html/wp-content/plugins/wcpt/mentors/dashboard.php
@@ -239,6 +239,7 @@ function set_usernames( $raw_usernames ) {
 		}
 	}
 
+	natcasesort( $official_usernames );
 	update_site_option( USERNAMES_KEY, $official_usernames );
 }
 
@@ -393,7 +394,12 @@ function get_all_mentor_data() {
 		$data = array_merge( $data, get_mentor_data( $username ) );
 	}
 
-	ksort( $data );
+	usort(
+		$data,
+		function( $a, $b ) {
+			return strnatcasecmp( $a['name'], $b['name'] );
+		}
+	);
 
 	set_site_transient( MENTORS_CACHE_KEY, $data, DAY_IN_SECONDS );
 

commit b2b993ccf9390325ebf04d942f9c88191313e63b

Mentors: Maintain key association to show username
0899c709 introduced a bug where the array was no long keyed by the username, so the `Username` column in the table showed a numeric array index instead of the username.

public_html/wp-content/plugins/wcpt/mentors/dashboard.php
@@ -394,7 +394,7 @@ function get_all_mentor_data() {
 		$data = array_merge( $data, get_mentor_data( $username ) );
 	}
 
-	usort(
+	uasort(
 		$data,
 		function( $a, $b ) {
 			return strnatcasecmp( $a['name'], $b['name'] );
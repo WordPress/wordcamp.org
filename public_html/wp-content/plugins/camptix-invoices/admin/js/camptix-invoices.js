a94dfed6731de96d21d3bb4cf777a2dc2db22596
Timi Wahalahti <timi@wahalahti.fi>
2023-07-21 12:34:54 +0300

    remove textarea (address) browser validation

--- public_html/wp-content/plugins/camptix-invoices/admin/js/camptix-invoices.js
+++ public_html/wp-content/plugins/camptix-invoices/admin/js/camptix-invoices.js
@@ -3,7 +3,7 @@
 jQuery( document ).ready( function( $ ) {
 	function toggleInvoiceDetailsForm( showForm ) {
 		const $camptixInvoiceDetailsForm = $( '.camptix-invoice-details' );
-		const $camptixInvoiceDetailsFormFields = $camptixInvoiceDetailsForm.find( 'input,textarea,select' );
+		const $camptixInvoiceDetailsFormFields = $camptixInvoiceDetailsForm.find( 'input,textarea' );
 
 		if ( showForm ) {
 			$camptixInvoiceDetailsForm.show();

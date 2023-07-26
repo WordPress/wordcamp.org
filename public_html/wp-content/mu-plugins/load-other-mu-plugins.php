d3f8d8e69663765a6398712fda40866856b95109
Ian Dunn <ian@iandunn.name>
2023-07-26 13:27:52 -0700

    mu-plugins: Load individual plugins on Events network

--- public_html/wp-content/mu-plugins/load-other-mu-plugins.php
+++ public_html/wp-content/mu-plugins/load-other-mu-plugins.php
@@ -14,11 +14,7 @@ function wcorg_include_common_plugins() {
 		require_once dirname( __DIR__ ) . '/mu-plugins-private/wporg-mu-plugins.php';
 	}
 
-	require_once __DIR__ . '/blocks/blocks.php';
-	require_once __DIR__ . '/theme-templates/bootstrap.php';
-	require_once __DIR__ . '/quickbooks/quickbooks.php';
-	require_once __DIR__ . '/camptix-tweaks/camptix-tweaks.php';
-
+	wcorg_include_individual_mu_plugins();
 	wcorg_include_mu_plugin_folders();
 }
 
@@ -30,11 +26,6 @@ function wcorg_include_network_only_plugins() {
 		$network_folder = 'events';
 	} else {
 		$network_folder = 'wordcamp';
-
-		// @todo temporary. some of this should be moved into into wcorg_include_common_plugins(), but it all needs to
-		// be audited and updated for the Events network first.
-		// see https://github.com/WordPress/wordcamp.org/issues/906.
-		wcorg_include_individual_mu_plugins();
 	}
 
 	$muplugins = glob( __DIR__ . "/$network_folder/*.php" );
@@ -52,15 +43,14 @@ function wcorg_include_network_only_plugins() {
  * This is because the folder contains some .php files that we don't want to automatically include with glob().
  */
 function wcorg_include_individual_mu_plugins() {
-	// todo move some of this into wcorg_include_common_plugins() and wcorg_include_wordcamp_only_plugins() as
-	// they've been audited/updated for events sites.
-	// see https://github.com/WordPress/wordcamp.org/issues/906.
-
 	$shortcodes = dirname( __DIR__ ) . '/mu-plugins-private/wordcamp-shortcodes/wc-shortcodes.php';
 
-	require_once __DIR__ . '/wp-cli-commands/bootstrap.php';
+	require_once __DIR__ . '/blocks/blocks.php';
+	require_once __DIR__ . '/camptix-tweaks/camptix-tweaks.php';
 	require_once __DIR__ . '/quickbooks/quickbooks.php';
+	require_once __DIR__ . '/theme-templates/bootstrap.php';
 	require_once __DIR__ . '/virtual-embeds/virtual-embeds.php';
+	require_once __DIR__ . '/wp-cli-commands/bootstrap.php';
 
 	if ( is_readable( __DIR__ . '/vendor/autoload.php' ) ) {
 		require_once __DIR__ . '/vendor/autoload.php';
@@ -77,10 +67,6 @@ function wcorg_include_individual_mu_plugins() {
  * Load every mu-plugin in these folders
  */
 function wcorg_include_mu_plugin_folders() {
-	// todo move some of this into wcorg_include_common_plugins() and wcorg_include_wordcamp_only_plugins() as
-	// they've been audited/updated for events sites.
-	// see https://github.com/WordPress/wordcamp.org/issues/906.
-
 	$include_folders = array(
 		dirname( __DIR__ ) . '/mu-plugins-private',
 		__DIR__ . '/jetpack-tweaks',

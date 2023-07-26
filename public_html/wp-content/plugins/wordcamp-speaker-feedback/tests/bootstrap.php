5092e8e0260066a158057961bb3f837603f2a583
Ian Dunn <ian@iandunn.name>
2023-07-25 17:30:29 -0700

    Speaker Feedback: Switch to non-main site to ensure `wc-post-types` loads
    af566590ff905d151385621d207f847e7c04b8d1 made it not load on the main site.

--- public_html/wp-content/plugins/wordcamp-speaker-feedback/tests/bootstrap.php
+++ public_html/wp-content/plugins/wordcamp-speaker-feedback/tests/bootstrap.php
@@ -14,8 +14,12 @@ if ( 'cli' !== php_sapi_name() ) {
 function manually_load_plugin() {
 	require_once SUT_WPMU_PLUGIN_DIR . '/3-helpers-misc.php';
 
+	// Switch to a non-main site so that `wc-post-types` won't return early.
+	// Site 1 isn't the main site for WordCamp.org, `WORDCAMP_ROOT_BLOG_ID` is.
+	switch_to_blog( 1 );
 	// Needed for registering post types.
 	require_once WP_PLUGIN_DIR . '/wc-post-types/wc-post-types.php';
+	restore_current_blog();
 
 	// Initialize the plugin.
 	require_once dirname( __DIR__ )  . '/wordcamp-speaker-feedback.php';

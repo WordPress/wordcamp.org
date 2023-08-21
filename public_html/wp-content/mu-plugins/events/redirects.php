451f128e3163781913f3091341c752de788fd390
Ian Dunn <ian@iandunn.name>
2023-08-21 15:46:34 -0700

    Events Redirect: Show homepage to admins for testing
    This will let admins test the landing pages that are in development.

--- public_html/wp-content/mu-plugins/events/redirects.php
+++ public_html/wp-content/mu-plugins/events/redirects.php
@@ -10,13 +10,15 @@ add_action( 'init', __NAMESPACE__ . '\redirect_to_make_community' );
  * Redirects the root URL of the "events.wordpress.org" to "https://make.wordpress.org/community/events".
  */
 function redirect_to_make_community() {
-	if ( wp_using_themes() && ! defined( 'REST_REQUEST' ) ) {
-		$request_uri = $_SERVER['REQUEST_URI'];
+	if ( ! wp_using_themes() || defined( 'REST_REQUEST' ) || current_user_can( 'switch_themes' ) ) {
+		return;
+	}
 
-		if ( '/' === $request_uri ) {
-			$new_url = 'https://make.wordpress.org/community/events/';
-			wp_redirect( $new_url );
-			exit;
-		}
+	$request_uri = $_SERVER['REQUEST_URI'];
+
+	if ( '/' === $request_uri ) {
+		$new_url = 'https://make.wordpress.org/community/events/';
+		wp_redirect( $new_url );
+		exit;
 	}
 }

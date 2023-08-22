9d6e85aa64e161956dc01ff79baecb654068911f
Ian Dunn <ian@iandunn.name>
2023-08-21 17:07:20 -0700

    Events: Add child theme for root site.

---
+++ public_html/wp-content/themes/wporg-events-2023/functions.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace WordPressdotorg\Events_2023;
+
+defined( 'WPINC' ) || die();
+
+
+add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
+
+
+/**
+ * Enqueue scripts and styles.
+ */
+function enqueue_assets() {
+	wp_enqueue_style(
+		'wporg-events-2023-style',
+		get_stylesheet_uri(),
+		array( 'wporg-parent-2021-style', 'wporg-global-fonts' ),
+		filemtime( __DIR__ . '/style.css' )
+	);
+}

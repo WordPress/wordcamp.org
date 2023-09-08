0788569530048ac9d96c85b200e340a4d2976247
Ian Dunn <ian@iandunn.name>
2023-09-08 15:25:54 -0700

    Events: Add city landing pages

---
+++ public_html/wp-content/themes/wporg-events-2023/patterns/city-landing-page-map.php
@@ -0,0 +1,22 @@
+<?php
+
+/**
+ * Title: City Landing Page Map
+ * Slug: wporg-events-2023/city-landing-page-map
+ * Inserter: no
+ */
+
+namespace WordPressdotorg\Events_2023;
+
+defined( 'WPINC' ) || die();
+
+$map_options = array(
+	'id' => 'city-landing-page',
+
+	// Can't use $wp->request because Gutenberg calls this on `init`, before `parse_request`.
+	'markers' => get_city_landing_page_events( $_SERVER['REQUEST_URI'] ),
+);
+
+?>
+
+<!-- wp:wporg/google-map <?php echo wp_json_encode( $map_options ); ?> /-->

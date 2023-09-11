b017d772f4891f7592dc0d9a840f454f23cef21c
Ian Dunn <ian@iandunn.name>
2023-09-11 16:11:58 -0700

    Events: Return if front page getter doesn't exist
    This was missed in 3f63a932

--- public_html/wp-content/themes/wporg-events-2023/patterns/front-page-map.php
+++ public_html/wp-content/themes/wporg-events-2023/patterns/front-page-map.php
@@ -10,6 +10,10 @@ namespace WordPressdotorg\Events_2023;
 
 defined( 'WPINC' ) || die();
 
+if ( ! function_exists( 'get_all_upcoming_events' ) ) {
+	return;
+}
+
 $map_options = array(
 	'id'      => 'all-upcoming-events',
 	'markers' => get_all_upcoming_events(),

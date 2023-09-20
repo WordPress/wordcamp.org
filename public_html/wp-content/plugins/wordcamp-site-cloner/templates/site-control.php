71e30ef3316ea04ed946706bcbbeb67c494831e4
Ian Dunn <ian@iandunn.name>
2023-09-20 08:51:31 -0700

    Site Cloner: Warn before cloning into closed camp.
    This helps to avoid unintentionally overwriting an old site with a new one when the organizer actually intends to clone _from_ the old site _to_ a new one.

--- public_html/wp-content/plugins/wordcamp-site-cloner/templates/site-control.php
+++ public_html/wp-content/plugins/wordcamp-site-cloner/templates/site-control.php
@@ -7,6 +7,8 @@
 namespace WordCamp\Site_Cloner;
 defined( 'WPINC' ) or die();
 
+$wordcamp = get_wordcamp_post();
+
 ?>
 
 <div id="wcsc-cloner">
@@ -14,6 +16,18 @@ defined( 'WPINC' ) or die();
 		<?php esc_html_e( 'WordCamp Sites', 'wordcamporg' ); ?>
 		<span id="wcsc-sites-count" class="title-count wcsc-sites-count"></span>
 	</h3>
+
+	<?php if ( 'wcpt-closed' === $wordcamp->post_status ) : ?>
+		<div class="notice notice-warning">
+			<p>
+				<?php echo esc_html( sprintf(
+					// translators: %s is the name of the WordCamp.
+					__( '%s has already completed, are you sure you want to overwrite it with styles and settings from another site?', 'wordcamporg' ),
+					get_wordcamp_name()
+				) ); ?>
+			</p>
+		</div>
+	<?php endif; ?>
 
 	<div class="filters"></div>
 

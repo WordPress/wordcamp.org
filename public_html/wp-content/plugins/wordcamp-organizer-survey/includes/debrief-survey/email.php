5ef71c2ecf44518fc4bb7c9a1817c843af44ad32
ren <18050944+renintw@users.noreply.github.com>
2023-09-25 02:16:17 +0800

    Update email content

--- public_html/wp-content/plugins/wordcamp-organizer-survey/includes/debrief-survey/email.php
+++ public_html/wp-content/plugins/wordcamp-organizer-survey/includes/debrief-survey/email.php
@@ -27,7 +27,7 @@ function get_email_id() {
  *  @return string
  */
 function get_lead_organizer_email() {
-	$meta = get_wordcamp_post( get_current_blog_id() )->meta;
+	$meta = get_wordcamp_post()->meta;
 	return $meta['Email Address'][0];
 }
 
@@ -37,9 +37,20 @@ function get_lead_organizer_email() {
  * @return string
  */
 function get_lead_organizer_full_name() {
-	$meta = get_wordcamp_post( get_current_blog_id() )->meta;
+	$meta = get_wordcamp_post()->meta;
 	return $meta['Organizer Name'][0];
 }
+
+/**
+ * Return wordcamp location.
+ *
+ * @return string
+ */
+function get_wordcamp_location() {
+	$meta = get_wordcamp_post()->meta;
+	return $meta['Location'][0];
+}
+
 
 /**
  * Return a token to put into the survey URL.
@@ -58,22 +69,26 @@ function get_encryption_token() {
  */
 function get_email_content() {
 	$wordcamp_name   = get_wordcamp_name();
-	$closing_date    = date_i18n( 'Y-m-d', strtotime('+2 weeks') );
 	$survey_page_url = 'https://central.wordcamp.test/organizer-survey-event-debrief/?t=' . get_encryption_token()
-					 . '?e=' . base64_encode( get_lead_organizer_email() );
+					 . '&e=' . base64_encode( get_lead_organizer_email() );
 
-	$email  = "Hi [first_name],\r\n\r\n";
-	$email .= sprintf( "%s is over, thank you for lead organizing it! \r\n\r\n", esc_html( $wordcamp_name ) );
-	$email .= "As a community-led event, feedback is really important to us. It helps us improve our events and to keep providing high quality content.\r\n\r\n";
-	$email .= sprintf( "Please take a moment to answer our <a href='%s'>post-event survey</a> and help us to do amazing WordPress events!\r\n", esc_url( $survey_page_url ) );
-	$email .= "(If you can't open the link, copy and paste the following URL)\r\n";
-	$email .= $survey_page_url . "\r\n\r\n";
-	$email .= sprintf( "Please complete the survey by %s.\r\n\r\n", $closing_date );
-	$email .= "Please also note that all responses will be kept confidential, and we will not share your personal information with any third parties.\r\n\r\n";
-	$email .= "Thank you in advance, we really appreciate your time!\r\n\r\n";
-	$email .= "Best regards,\r\n";
-	$email .= "Organising Team,\r\n";
-	$email .= esc_html( $wordcamp_name );
+	$email  = "Howdy [email],\r\n\r\n";
+	$email .= sprintf( "Congratulations on completing %s! We hope you had a great time, and that you'll soon get some well-deserved rest\r\n\r\n", esc_html( $wordcamp_name ) );
+	$email .= "We'd love to hear how you feel the event went. What were your proudest moments and your greatest disappointments?\r\n";
+	$email .= sprintf( "We've created an Organizer Survey, so we can get all the details of how things went with your event. <strong>Please fill it out this form within 10 days:</strong> %s\r\n\r\n", esc_url( $survey_page_url ) );
+	$email .= "<strong>Event Budget</strong>\r\n";
+	$email .= 'Please update your working budget on your event dashboard. If you ran your money outside of WordPress Community Support, PBC, please also balance your budget spreadsheet and prepare your Transparency Report.';
+	$email .= "If there are any issues, please reach out to us at support@wordcamp.org to schedule a budget close-out meeting. Please complete these steps within the next two weeks.\r\n\r\n";
+	$email .= "<strong>Event Recording (if any)</strong>\r\n";
+	$email .= sprintf( "If you haven't yet done so, please review the submission guidelines before beginning to edit your videos (or before your videographers starts editing): %s\r\n\r\n", esc_url( 'http://blog.wordpress.tv/submission-guidelines/' ) );
+	$email .= sprintf( "To submit your video for publication to WordPress.tv, just upload them at this page: %s\r\n\r\n", esc_url( 'http://wordpress.tv/submit-video/' ) );
+	$email .= 'Our intrepid team of video moderators will review the videos and schedule them for publication. Our intrepid team of video moderators will review the videos and schedule them for publication. ';
+	$email .= sprintf( "If your content is in a language other than English, please see if you can recruit someone from your community to join the WordPress TV moderators' team and review your videos: %s\r\n\r\n", esc_url( 'http://wordpress.tv/apply-to-be-a-wordpress-tv-moderator/' ) );
+	$email .= "<strong>Event Recap</strong>\r\n";
+	$email .= "Finally, if you've published a recap on your site, please let us know, so we can reblog it on the WordCamp Central blog.\r\n\r\n";
+	$email .= sprintf( "Thanks again for all you've done to grow the WordPress community in %s!\r\n\r\n", get_wordcamp_location() );
+	$email .= "Best wishes,\r\n";
+	$email .= 'Your friendly WordCamp Central crew';
 
 	return $email;
 }
@@ -83,7 +98,7 @@ function get_email_content() {
  */
 function add_email() {
 	$email_id = get_option( EMAIL_KEY_ID );
-	if ( $email_id ) {
+	if ( $email_id || ! get_wordcamp_post() ) {
 		return;
 	}
 

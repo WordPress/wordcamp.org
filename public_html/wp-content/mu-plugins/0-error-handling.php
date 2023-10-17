WordPress/wordcamp.org

commit 2f28f250d25ebfcf419a0b27c5cede276eeb8ed2

Error Handler: Add temporary logging to troubleshoot fatal (#1089)
`Uncaught Error: Call to undefined function WordPressdotorg\MU_Plugins\Global_Fonts\get_font_stylesheet_url() in wp-content/themes/wporg-parent-2021/functions.php:46`

Related: 5ce4542e39f835c98884d08a1d5f7b7624be2b9c

public_html/wp-content/mu-plugins/0-error-handling.php
@@ -355,6 +355,26 @@ function send_error_to_slack( $err_no, $err_msg, $file, $line, $occurrences = 0 
 		),
 	);
 
+	// temporary to troubleshoot URL routing error.
+	// maybe the `$url` above has some clues about the request that are stripped out by redact/esc functions.
+	// see https://wordpress.slack.com/archives/GDDSW0WNS/p1697474288356909.
+	$fields = array(
+		array(
+			'title' => 'tmp raw HTTP_HOST / SERVER_NAME',
+			'value' => sprintf(
+				'host: %s, name: %s, redir: %s, qs: %s, uri: %s, sapi: %s, ip: %s',
+				$_SERVER['HTTP_HOST'],
+				$_SERVER['SERVER_NAME'],
+				$_SERVER['REDIRECT_URL'],
+				$_SERVER['QUERY_STRING'],
+				$_SERVER['REQUEST_URI'],
+				php_sapi_name(),
+				$_SERVER['REMOTE_ADDR']
+			),
+			'short' => false,
+		),
+	);
+
 	if ( ! str_contains( $err_msg, $file ) ) {
 		$fields[] = array(
 			'title' => 'File',

commit e2ed25eab1dd6fb56f715af4ec3fbc911262a82d

Error Handler: Append to fields rather than overwriting
2f28f250 accidentally overwrote it instead.

public_html/wp-content/mu-plugins/0-error-handling.php
@@ -353,12 +353,10 @@ function send_error_to_slack( $err_no, $err_msg, $file, $line, $occurrences = 0 
 			'value' => esc_url_raw( $url ),
 			'short' => false,
 		),
-	);
 
-	// temporary to troubleshoot URL routing error.
-	// maybe the `$url` above has some clues about the request that are stripped out by redact/esc functions.
-	// see https://wordpress.slack.com/archives/GDDSW0WNS/p1697474288356909.
-	$fields = array(
+		// temporary to troubleshoot URL routing error.
+		// maybe the `$url` above has some clues about the request that are stripped out by redact/esc functions.
+		// see https://wordpress.slack.com/archives/GDDSW0WNS/p1697474288356909.
 		array(
 			'title' => 'tmp raw HTTP_HOST / SERVER_NAME',
 			'value' => sprintf(
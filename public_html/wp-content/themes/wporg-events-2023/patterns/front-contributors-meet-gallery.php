30c50e034eceda1d03cad2458a010651664fa0bf
Ian Dunn <ian@iandunn.name>
2023-11-27 09:33:13 -0800

    Events: Fix URL to bundled images

--- public_html/wp-content/themes/wporg-events-2023/patterns/front-contributors-meet-gallery.php
+++ public_html/wp-content/themes/wporg-events-2023/patterns/front-contributors-meet-gallery.php
@@ -13,17 +13,17 @@
 		<div class="tiled-gallery__row">
 			<div class="tiled-gallery__col" style="flex-basis:65.48121%">
 				<figure class="tiled-gallery__item">
-					<img alt="" data-height="272" data-url="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/IMAGES/contributors-rectangle1.png" data-width="615" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/IMAGES/contributors-rectangle1.png" data-amp-layout="responsive" />
+					<img alt="" data-height="272" data-url="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/contributors-rectangle1.png" data-width="615" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/contributors-rectangle1.png" data-amp-layout="responsive" />
 				</figure>
 			</div>
 		</div>
 		<div class="tiled-gallery__row">
 			<div class="tiled-gallery__col">
-				<figure class="tiled-gallery__item"><img alt="" data-height="272" data-url="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/IMAGES/contributors-square1.png" data-width="298" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/IMAGES/contributors-square1.png" data-amp-layout="responsive" /></figure>
+				<figure class="tiled-gallery__item"><img alt="" data-height="272" data-url="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/contributors-square1.png" data-width="298" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/contributors-square1.png" data-amp-layout="responsive" /></figure>
 			</div>
 			<div class="tiled-gallery__col">
 				<figure class="tiled-gallery__item">
-					<img alt="" data-height="272" data-url="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/IMAGES/contributors-square2.png" data-width="297" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/IMAGES/contributors-square2.png" data-amp-layout="responsive" />
+					<img alt="" data-height="272" data-url="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/contributors-square2.png" data-width="297" src="<?php echo esc_url( get_stylesheet_directory_uri() ); ?>/images/contributors-square2.png" data-amp-layout="responsive" />
 				</figure>
 			</div>
 		</div>

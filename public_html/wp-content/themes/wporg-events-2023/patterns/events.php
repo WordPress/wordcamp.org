cb7a83daa1579d9a7c955cc021aa0010ce69d4c5
Steven Dufresne <steve.dufresne@automattic.com>
2023-12-02 03:19:44 +0900

    Updates to the wporg-events-2023 theme. (#1118)
    * Updates to the theme.
    
    * Remove the parent block style
    
    * More style tweaks.
    
    * Fix button styles.
    
    * Add footer template back in.
    
    * Update the stats in the cover.
    
    * Move content to .php patterns for translations to be added later
    
    * Add newlines to php files.
    
    * Clean up spacing.
    
    * Fix footer template on page-city.
    
    * Run the formatter.
    
    * Improve the list view.

---
+++ public_html/wp-content/themes/wporg-events-2023/patterns/events.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ * Title: Events
+ * Slug: wporg-events-2023/events
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"left":"var:preset|spacing|edge-space","right":"var:preset|spacing|edge-space"},"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|50"}}},"layout":{"type":"constrained"}} -->
+<div class="wp-block-group alignwide" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--50);padding-right:var(--wp--preset--spacing--edge-space);padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:group {"align":"wide","layout":{"type":"default"}} -->
+<div class="wp-block-group alignwide"><!-- wp:heading {"style":{"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"medium","fontFamily":"inter"} -->
+<h2 class="wp-block-heading has-inter-font-family has-medium-font-size" style="font-style:normal;font-weight:700">Upcoming events</h2>
+<!-- /wp:heading -->
+
+<!-- wp:wporg/google-map {"id":"all-upcoming-list","apiKey":"WORDCAMP_DEV_GOOGLE_MAPS_API_KEY","filterSlug":"all-upcoming","showMap":false,"showSearch":false,"listDisplayLimit":10} /-->
+
+<!-- wp:buttons {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40"}}}} -->
+<div class="wp-block-buttons" style="margin-top:var(--wp--preset--spacing--40)"><!-- wp:button {"className":"is-style-outline"} -->
+<div class="wp-block-button is-style-outline"><a class="wp-block-button__link wp-element-button" href="/upcoming-events/">See all
+				events</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->

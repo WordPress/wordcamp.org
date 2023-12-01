e78b9b1c92b6c2c9a49fc255a35dac909de6cf6b
Ian Dunn <ian@iandunn.name>
2023-12-01 10:59:26 -0800

    Events: Fix dynamic menu/gallery content
    The `ref` breaks the dynamic menu in other environments. The dynamic image links were accidentally overwritten.

--- public_html/wp-content/themes/wporg-events-2023/patterns/front-contributors.php
+++ public_html/wp-content/themes/wporg-events-2023/patterns/front-contributors.php
@@ -42,10 +42,9 @@
 	<!-- /wp:list --></div>
 	<!-- /wp:column -->
 
-	<!-- wp:column {"verticalAlignment":"top","width":"50%"} -->
-	<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:50%"><!-- wp:jetpack/tiled-gallery {"columnWidths":[["65.48121","34.51879"]],"ids":[79,85,82],"style":{"layout":{"selfStretch":"fixed","flexSize":"50%"}}} -->
-	<div class="wp-block-jetpack-tiled-gallery aligncenter is-style-rectangular"><div class="tiled-gallery__gallery"><div class="tiled-gallery__row"><div class="tiled-gallery__col" style="flex-basis:65.48121%"><figure class="tiled-gallery__item"><img alt="" data-height="272" data-url="https://events.wordpress.test/wp-content/themes/wporg-events-2023/images/contributors-rectangle1.png" data-width="615" src="https://events.wordpress.test/wp-content/themes/wporg-events-2023/images/contributors-rectangle1.png" data-amp-layout="responsive"/></figure></div></div><div class="tiled-gallery__row"><div class="tiled-gallery__col"><figure class="tiled-gallery__item"><img alt="" data-height="272" data-url="https://events.wordpress.test/wp-content/themes/wporg-events-2023/images/contributors-square1.png" data-width="298" src="https://events.wordpress.test/wp-content/themes/wporg-events-2023/images/contributors-square1.png" data-amp-layout="responsive"/></figure></div><div class="tiled-gallery__col"><figure class="tiled-gallery__item"><img alt="" data-height="272" data-url="https://events.wordpress.test/wp-content/themes/wporg-events-2023/images/contributors-square2.png" data-width="297" src="https://events.wordpress.test/wp-content/themes/wporg-events-2023/images/contributors-square2.png" data-amp-layout="responsive"/></figure></div></div></div></div>
-	<!-- /wp:jetpack/tiled-gallery --></div>
-	<!-- /wp:column --></div>
-	<!-- /wp:columns --></div>
-	<!-- /wp:group -->
+		<!-- wp:column {"verticalAlignment":"top","width":"50%"} -->
+		<div class="wp-block-column is-vertically-aligned-top" style="flex-basis:50%">
+			<!-- wp:pattern {"slug":"wporg-events-2023/front-contributors-gallery"} /-->
+		</div>	<!-- /wp:column -->
+	</div>	<!-- /wp:columns -->
+</div>	<!-- /wp:group -->

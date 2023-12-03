813f7b0404be344c5926c1538f0eb885de947d6c
StevenDufresne <dufresnesteven@gmail.com>
2023-12-03 23:50:40 +0900

    Make the callout match the latest iteration.

--- public_html/wp-content/themes/wporg-events-2023/patterns/footer-community-callout.php
+++ public_html/wp-content/themes/wporg-events-2023/patterns/footer-community-callout.php
@@ -7,15 +7,15 @@
 
 ?>
 
-<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"5rem","right":"var:preset|spacing|edge-space","bottom":"5rem","left":"var:preset|spacing|edge-space"}}},"backgroundColor":"blueberry-1","textColor":"white","className":"wporg-events__community-callout","layout":{"inherit":true,"type":"constrained"}} -->
-<div class="wp-block-group alignfull wporg-events__community-callout has-white-color has-blueberry-1-background-color has-text-color has-background has-link-color" id="get-started" style="padding-top:5rem;padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:5rem;padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"5rem","right":"var:preset|spacing|edge-space","bottom":"5rem","left":"var:preset|spacing|edge-space"}}},"backgroundColor":"charcoal-0","textColor":"white","className":"wporg-events__community-callout","layout":{"inherit":true,"type":"constrained"}} -->
+<div class="wp-block-group alignfull wporg-events__community-callout has-white-color has-charcoal-0-background-color has-text-color has-background has-link-color" id="get-started" style="padding-top:5rem;padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:5rem;padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:group {"align":"wide","layout":{"type":"flex","flexWrap":"nowrap"}} -->
 <div class="wp-block-group alignwide"><!-- wp:paragraph {"style":{"layout":{"selfStretch":"fixed","flexSize":"70%"},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontSize":"heading-3","fontFamily":"eb-garamond"} -->
 <p class="has-white-color has-text-color has-link-color has-eb-garamond-font-family has-heading-3-font-size">Can't find a WordPress event near you? Become an organizer and start hosting local events where you live today.</p>
 <!-- /wp:paragraph -->
 
 <!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right","orientation":"horizontal"},"style":{"layout":{"selfStretch":"fill","flexSize":null}}} -->
-<div class="wp-block-buttons"><!-- wp:button {"className":"has-custom-width is-style-fill-on-dark"} -->
-<div class="wp-block-button has-custom-width is-style-fill-on-dark"><a class="wp-block-button__link wp-element-button" href="/organize-an-event/">Learn more</a></div>
+<div class="wp-block-buttons"><!-- wp:button {"className":"has-custom-width is-style-outline-on-dark"} -->
+<div class="wp-block-button has-custom-width is-style-outline-on-dark"><a class="wp-block-button__link wp-element-button" href="/organize-an-event/">Learn more</a></div>
 <!-- /wp:button --></div>
 <!-- /wp:buttons --></div>
 <!-- /wp:group --></div>

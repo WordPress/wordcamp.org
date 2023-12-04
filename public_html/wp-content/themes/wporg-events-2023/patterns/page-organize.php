d04b93c8d66f6d14ab76087cb6609afb684919cd
StevenDufresne <dufresnesteven@gmail.com>
2023-12-04 11:50:24 +0900

    Add in an organizer page.

---
+++ public_html/wp-content/themes/wporg-events-2023/patterns/page-organize.php
@@ -0,0 +1,172 @@
+<?php
+/**
+ * Title: Organize Events
+ * Slug: wporg-events-2023/page-organize-events
+ * Inserter: no
+ */
+
+?>
+
+<!-- wp:post-featured-image /-->
+
+<!-- wp:group {"align":"wide","style":{"spacing":{"padding":{"right":"var:preset|spacing|edge-space","left":"var:preset|spacing|edge-space"}}},"layout":{"type":"constrained","contentSize":"1120px"}} -->
+<div class="wp-block-group alignwide" style="padding-right:var(--wp--preset--spacing--edge-space);padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:group {"style":{"spacing":{"margin":{"top":"var:preset|spacing|50","bottom":"var:preset|spacing|60"},"blockGap":"var:preset|spacing|60"}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="margin-top:var(--wp--preset--spacing--50);margin-bottom:var(--wp--preset--spacing--60)"><!-- wp:columns {"style":{"spacing":{"blockGap":{"left":"var:preset|spacing|30"}}}} -->
+<div class="wp-block-columns"><!-- wp:column {"width":"58%"} -->
+<div class="wp-block-column" style="flex-basis:58%"><!-- wp:heading {"level":1,"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|30"}}},"fontSize":"heading-2"} -->
+<h1 class="wp-block-heading has-heading-2-font-size" style="margin-bottom:var(--wp--preset--spacing--30)">Organize a local event</h1>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Local events are the heart of the WordPress community. They provide inclusive spaces where everyone is encouraged to share ideas and learn together in a fun and relaxed atmosphere.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Events can range from small meetups where a few people gather to collaborate to large international conferences where thousands travel to attend talks, networking events, and working sessions.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"style":{"spacing":{"margin":{"top":"var:preset|spacing|40","bottom":"var:preset|spacing|30"}}},"fontSize":"heading-3"} -->
+<h2 class="wp-block-heading has-heading-3-font-size" style="margin-top:var(--wp--preset--spacing--40);margin-bottom:var(--wp--preset--spacing--30)">Consider the type of event you want to host</h2>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|0"}}}} -->
+<p style="margin-top:var(--wp--preset--spacing--0)">There are plenty of ways to bring fellow WordPressers together. Whether people want to learn, build, or just connect with others, pick an event style that fits your community's interests. If you have a unique event idea you don't see represented here, you can propose it to the Community Team as part of the evolving <a href="https://make.wordpress.org/community/2023/10/06/nextgen-events/">Events Project</a>.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:list -->
+<ul><!-- wp:list-item -->
+<li><a href="https://make.wordpress.org/community/handbook/meetup-organizer/meetup-program-basics/">Meetup↗</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://make.wordpress.org/community/handbook/wordcamp-organizer/become-an-organizer/">WordCamp↗</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://make.wordpress.org/community/handbook/contributor-day/">Contributor Days↗</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://make.wordpress.org/community/handbook/virtual-events/">Virtual events↗</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://doaction.org/">do_action charity hackathons↗</a></li>
+<!-- /wp:list-item -->
+
+<!-- wp:list-item -->
+<li><a href="https://make.wordpress.org/community/handbook/community-deputy/other-event-formats/nextgen-wordpress-event/">Suggest a new type of event↗</a></li>
+<!-- /wp:list-item --></ul>
+<!-- /wp:list --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"verticalAlignment":"bottom"} -->
+<div class="wp-block-column is-vertically-aligned-bottom"><!-- wp:image {"id":144,"scale":"cover","sizeSlug":"full","linkDestination":"none"} -->
+<figure class="wp-block-image size-full"><img src="https://events.wordpress.test/files/2023/12/Image-4.png" alt="" class="wp-image-144" style="object-fit:cover"/></figure>
+<!-- /wp:image --></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"style":{"border":{"top":{"color":"var:preset|color|light-grey-1","width":"1px"}},"spacing":{"padding":{"top":"var:preset|spacing|60","bottom":"var:preset|spacing|60"},"margin":{"top":"0","bottom":"0"}}},"layout":{"type":"default"}} -->
+<div class="wp-block-group" style="border-top-color:var(--wp--preset--color--light-grey-1);border-top-width:1px;margin-top:0;margin-bottom:0;padding-top:var(--wp--preset--spacing--60);padding-bottom:var(--wp--preset--spacing--60)"><!-- wp:columns {"style":{"spacing":{"blockGap":{"left":"var:preset|spacing|30"}}}} -->
+<div class="wp-block-columns"><!-- wp:column {"width":"58%"} -->
+<div class="wp-block-column" style="flex-basis:58%"><!-- wp:group {"layout":{"type":"constrained"}} -->
+<div class="wp-block-group"><!-- wp:heading {"style":{"spacing":{"margin":{"bottom":"var:preset|spacing|30"}}},"fontSize":"heading-3"} -->
+<h3 class="wp-block-heading has-heading-3-font-size" style="margin-bottom:var(--wp--preset--spacing--30)">Frequently asked questions</h3>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph -->
+<p>Want to organize an event in your area, but aren't sure how? These answers can help you get started:</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|30"}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"normal","fontFamily":"inter"} -->
+<h4 class="wp-block-heading has-inter-font-family has-normal-font-size" style="margin-top:var(--wp--preset--spacing--30);font-style:normal;font-weight:700">There's no meetup in my area. How do I start one?</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|10"}}}} -->
+<p style="margin-top:var(--wp--preset--spacing--10)">Great question! Meetups are vital to the WordPress open source project, so more local events directly contribute to its health and success.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p><a href="https://www.meetup.com/pro/wordpress/">First, check if there is already an active WordPress meetup group in your city.</a></p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>To start a meetup in your city or town, fill out the <a href="https://central.wordcamp.org/meetup-organizer-application/">meetup interest form</a>. A Community Team volunteer will review the application and respond. This can take a week or two, depending on availability and time of year.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Once you are accepted and complete the next steps, the <a href="https://make.wordpress.org/community/">Community Team</a> will set up a Meetup.com group page for your city and make you the co-organizer.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|30"}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"normal","fontFamily":"inter"} -->
+<h4 class="wp-block-heading has-inter-font-family has-normal-font-size" style="margin-top:var(--wp--preset--spacing--30);font-style:normal;font-weight:700">Do I need organizer experience to organize a meetup or another type of event?</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|10"}}}} -->
+<p style="margin-top:var(--wp--preset--spacing--10)">You don't need previous experience to organize a meetup. All we ask is that you are a reliable person who loves WordPress, works well with others, and wants to build and foster a safe and inclusive space for other WordPressers in your area.&nbsp;</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|30"}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"normal","fontFamily":"inter"} -->
+<h4 class="wp-block-heading has-inter-font-family has-normal-font-size" style="margin-top:var(--wp--preset--spacing--30);font-style:normal;font-weight:700">I already host a WordPress meetup. Can I transfer it into the WordPress Chapter Meetup program?</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|10"}}}} -->
+<p style="margin-top:var(--wp--preset--spacing--10)">Existing meetup organizers who are willing to follow the <a href="https://make.wordpress.org/community/handbook/wordcamp-organizer/planning-details/code-of-conduct/">code of conduct</a> and agree to the <a href="https://make.wordpress.org/community/handbook/meetup-organizer/meetup-program-basics/#the-five-good-faith-rules">Five Good Faith rules</a> are welcome to apply to join the WordPress Chapter Meetup program.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>To transfer your existing group into the chapter program, use <a href="https://make.wordpress.org/community/handbook/meetup-organizer/meetup-program-basics/interest-form/">the same Meetup Organizer Application</a>. Answer “yes” when asked about an existing group and provide the URL.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|30"}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"normal","fontFamily":"inter"} -->
+<h4 class="wp-block-heading has-inter-font-family has-normal-font-size" style="margin-top:var(--wp--preset--spacing--30);font-style:normal;font-weight:700">I have a meetup that's meeting regularly and would like to organize a larger event. What steps should I take?</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|10"}}}} -->
+<p style="margin-top:var(--wp--preset--spacing--10)">First, decide with your organizing team which type of event is right for your community and see who would be willing to organize alongside you. Bigger events take more effort and it's important to have dedicated community members who are willing to help.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Next, submit the application for the event type you've chosen. One of the Community Team volunteers will review the application and respond. This can take a week or two, depending on volunteer availability.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Once approved, an orientation will be held with the lead organizer (the person who submitted the application). You're welcome to include your organizing team as well. Your event will be assigned a mentor that will help walk you through the rest of the process.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:heading {"level":4,"style":{"spacing":{"margin":{"top":"var:preset|spacing|30"}},"typography":{"fontStyle":"normal","fontWeight":"700"}},"fontSize":"normal","fontFamily":"inter"} -->
+<h4 class="wp-block-heading has-inter-font-family has-normal-font-size" style="margin-top:var(--wp--preset--spacing--30);font-style:normal;font-weight:700">I have additional questions. Where can I go for help?</h4>
+<!-- /wp:heading -->
+
+<!-- wp:paragraph {"style":{"spacing":{"margin":{"top":"var:preset|spacing|10"}}}} -->
+<p style="margin-top:var(--wp--preset--spacing--10)"><a href="https://make.wordpress.org/community/">Make.wordpress.com/community</a> is the hub for all Community Team activities. There you can read recent posts, find all of the team's <a href="https://make.wordpress.org/community/handbook/">handbooks</a>, and learn more about different events and roles.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:paragraph -->
+<p>Additionally, there are a number of very active Slack channels on Making WordPress Slack. <a href="https://make.wordpress.org/chat/">Follow this link to join</a> and meet our lively, global team—someone is generally around to answer questions whenever you need.</p>
+<!-- /wp:paragraph --></div>
+<!-- /wp:group --></div>
+<!-- /wp:column -->
+
+<!-- wp:column {"width":""} -->
+<div class="wp-block-column"></div>
+<!-- /wp:column --></div>
+<!-- /wp:columns --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->
+
+<!-- wp:group {"align":"full","style":{"elements":{"link":{"color":{"text":"var:preset|color|white"}}},"spacing":{"padding":{"top":"5rem","right":"var:preset|spacing|edge-space","bottom":"5rem","left":"var:preset|spacing|edge-space"}}},"backgroundColor":"charcoal-0","textColor":"white","className":"wporg-events__community-callout","layout":{"inherit":true,"type":"constrained","wideSize":"1120px"}} -->
+<div class="wp-block-group alignfull wporg-events__community-callout has-white-color has-charcoal-0-background-color has-text-color has-background has-link-color" id="get-started" style="padding-top:5rem;padding-right:var(--wp--preset--spacing--edge-space);padding-bottom:5rem;padding-left:var(--wp--preset--spacing--edge-space)"><!-- wp:group {"layout":{"type":"flex","flexWrap":"nowrap"}} -->
+<div class="wp-block-group"><!-- wp:paragraph {"style":{"layout":{"selfStretch":"fixed","flexSize":"70%"},"elements":{"link":{"color":{"text":"var:preset|color|white"}}}},"textColor":"white","fontSize":"heading-4","fontFamily":"eb-garamond"} -->
+<p class="has-white-color has-text-color has-link-color has-eb-garamond-font-family has-heading-4-font-size">Check out upcoming WordPress events and connect with your local community.</p>
+<!-- /wp:paragraph -->
+
+<!-- wp:buttons {"layout":{"type":"flex","justifyContent":"right","orientation":"horizontal"},"style":{"layout":{"selfStretch":"fill","flexSize":null}}} -->
+<div class="wp-block-buttons"><!-- wp:button {"className":"has-custom-width is-style-outline-on-dark"} -->
+<div class="wp-block-button has-custom-width is-style-outline-on-dark"><a class="wp-block-button__link wp-element-button" href="/upcoming-events/">Browse events</a></div>
+<!-- /wp:button --></div>
+<!-- /wp:buttons --></div>
+<!-- /wp:group --></div>
+<!-- /wp:group -->

eb2aa33c0600273d59700da50d35e9d6605fa335
Steven Dufresne <steve.dufresne@automattic.com>
2023-12-07 13:42:57 +0900

    Events: Swap out google maps on homepage for event list. (#1133)

--- public_html/wp-content/themes/wporg-events-2023/src/event-list/index.php
+++ public_html/wp-content/themes/wporg-events-2023/src/event-list/index.php
@@ -45,41 +45,57 @@ function render( $attributes, $content, $block ) {
 	// Get all the filters that are currently applied.
 	$filtered_events = array_slice( filter_events( $events ), 0, (int) $attributes['limit'] );
 
-	// Group events by month year.
-	$grouped_events = array();
-	foreach ( $filtered_events as $event ) {
-		$event_month_year                      = gmdate('F Y', esc_html( $event->timestamp) );
-		$grouped_events[ $event_month_year ][] = $event;
-	}
-
-	foreach ( $grouped_events as $month_year => $events ) {
-		$content .= get_section_title( $month_year );
-		$content .= '<ul class="wporg-marker-list__container">';
-
-		foreach ( $events as $event ) {
-			$content .= '<li class="wporg-marker-list-item">';
-			$content .= '<h3 class="wporg-marker-list-item__title"><a class="external-link" href="' . esc_url($event->url) . '">' . esc_html($event->title) . '</a></h3>';
-			$content .= '<div class="wporg-marker-list-item__location">' . esc_html($event->location) . '</div>';
-			$content .= sprintf(
-				'<time class="wporg-marker-list-item__date-time" date-time="%1$s" title="%1$s"><span class="wporg-google-map__date">%2$s</span><span class="wporg-google-map__time">%3$s</span></time>',
-				gmdate( 'c', esc_html( $event->timestamp ) ),
-				gmdate( 'l, M j', esc_html( $event->timestamp ) ),
-				esc_html( gmdate('H:i', $event->timestamp) . ' UTC' ),
-			);
-			$content .= '</li>';
+	if ( (bool) $attributes['groupByMonth'] ) {
+		// Group events by month year.
+		$grouped_events = array();
+		foreach ( $filtered_events as $event ) {
+			$event_month_year                      = gmdate( 'F Y', esc_html( $event->timestamp ) );
+			$grouped_events[ $event_month_year ][] = $event;
 		}
 
-		$content .= '</ul>';
+		$content = '';
+		foreach ( $grouped_events as $month_year => $events ) {
+			$content .= get_section_title( $month_year );
+			$content .= get_list_markup( $events );
+		}
+	} else {
+		$content = get_list_markup( $filtered_events );
 	}
 
-	$wrapper_attributes = get_block_wrapper_attributes( array(
-		'class' => 'wp-block-wporg-google-map',
-	) );
+	$wrapper_attributes = get_block_wrapper_attributes();
 	return sprintf(
 		'<div %1$s>%2$s</div>',
 		$wrapper_attributes,
 		do_blocks( $content )
 	);
+}
+
+/**
+ * Returns the event-list markup.
+ *
+ * @param array $events Array of events.
+ *
+ * @return string
+ */
+function get_list_markup( $events ) {
+	$block_markup = '<ul class="wporg-marker-list__container">';
+
+	foreach ( $events as $event ) {
+		$block_markup .= '<li class="wporg-marker-list-item">';
+		$block_markup .= '<h3 class="wporg-marker-list-item__title"><a class="external-link" href="' . esc_url( $event->url ) . '">' . esc_html( $event->title ) . '</a></h3>';
+		$block_markup .= '<div class="wporg-marker-list-item__location">' . esc_html( $event->location ) . '</div>';
+		$block_markup .= sprintf(
+			'<time class="wporg-marker-list-item__date-time" date-time="%1$s" title="%1$s"><span class="wporg-google-map__date">%2$s</span><span class="wporg-google-map__time">%3$s</span></time>',
+			gmdate( 'c', esc_html( $event->timestamp ) ),
+			gmdate( 'l, M j', esc_html( $event->timestamp ) ),
+			esc_html( gmdate('H:i', $event->timestamp) . ' UTC' ),
+		);
+		$block_markup .= '</li>';
+	}
+
+	$block_markup .= '</ul>';
+
+	return $block_markup;
 }
 
 /**

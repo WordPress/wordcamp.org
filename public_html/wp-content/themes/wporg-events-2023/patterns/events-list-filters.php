371f888807f7f329a376ac2fb91de4cbff7f6b51
Steven Dufresne <steve.dufresne@automattic.com>
2023-12-11 08:34:40 +0900

    Events: Make the search bar full width on mobile. (#1164)

--- public_html/wp-content/themes/wporg-events-2023/patterns/events-list-filters.php
+++ public_html/wp-content/themes/wporg-events-2023/patterns/events-list-filters.php
@@ -7,10 +7,10 @@
 
 ?>
 
-<!-- wp:group {"align":"wide","style":{"spacing":{"margin":{"top":"40px","bottom":"40px"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
-<div class="wp-block-group alignwide" style="margin-top:40px;margin-bottom:40px">
-	<!-- wp:group {"layout":{"type":"flex","flexWrap":"wrap"}} -->
-	<div class="wp-block-group">
+<!-- wp:group {"align":"wide","className":"wporg-events__filters","style":{"spacing":{"margin":{"top":"40px","bottom":"40px"}}},"layout":{"type":"flex","flexWrap":"wrap","justifyContent":"space-between"}} -->
+<div class="wp-block-group alignwide wporg-events__filters" style="margin-top:40px;margin-bottom:40px">
+	<!-- wp:group {"className":"wporg-events__filters__search","layout":{"type":"flex","flexWrap":"wrap"}} -->
+	<div class="wp-block-group wporg-events__filters__search">
 		<!-- wp:search {"showLabel":false,"placeholder":"Search events...","width":100,"widthUnit":"%","buttonText":"Search","buttonPosition":"button-inside","buttonUseIcon":true,"className":"is-style-secondary-search-control"} /-->
 	</div> <!-- /wp:group -->
 

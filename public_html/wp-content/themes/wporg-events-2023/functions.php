79b9186933b52e540bd65913a2d635566ac8d334
Steven Dufresne <steve.dufresne@automattic.com>
2023-12-11 10:36:18 +0900

    Events: Add the social tag code. (#1159)

--- public_html/wp-content/themes/wporg-events-2023/functions.php
+++ public_html/wp-content/themes/wporg-events-2023/functions.php
@@ -14,6 +14,9 @@ add_action( 'after_setup_theme', __NAMESPACE__ . '\theme_support' );
 add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\enqueue_assets' );
 add_filter( 'wporg_block_navigation_menus', __NAMESPACE__ . '\add_site_navigation_menus' );
 add_filter( 'wporg_block_site_breadcrumbs', __NAMESPACE__ . '\update_site_breadcrumbs' );
+add_filter( 'jetpack_open_graph_image_default', __NAMESPACE__ . '\filter_social_media_image' );
+add_filter( 'jetpack_open_graph_tags', __NAMESPACE__ . '\add_meta_description' );
+add_filter( 'jetpack_open_graph_output', __NAMESPACE__ . '\add_generic_meta_description' );
 
 
 /**
@@ -33,6 +36,39 @@ function enqueue_assets() {
 		array( 'wporg-parent-2021-style', 'wporg-global-fonts' ),
 		filemtime( __DIR__ . '/style.css' )
 	);
+}
+
+/**
+ * Replace the default social image.
+ */
+function filter_social_media_image() {
+	return get_stylesheet_directory_uri() . '/images/social-image.png';
+}
+
+/**
+ * Add the og:description via JetPack meta tags so it can be translated.
+ */
+function add_meta_description( $tags ) {
+	$tags['og:description'] = __( 'Find upcoming WordPress events near you and around the world. Join your local WordPress community at a meetup or WordCamp, or learn how to organize an event for your city.', 'wporg' );
+	return $tags;
+}
+
+/**
+ * Use the og:description tag and output an additional meta description for SEO.
+ *
+ * Jetpack uses property="thing" as template for its Meta tags,
+ * because it's built to output OG Tags. However, we we want to add a general tag here.
+ *
+ * @filter jetpack_open_graph_output
+ * @uses str_replace
+ */
+function add_generic_meta_description( $og_tag ) {
+	if ( false !== strpos( $og_tag, 'property="og:description"' ) ) {
+		// Replace property="og:description" by name="description".
+		$og_tag .= str_replace( 'property="og:description"', 'name="description"', $og_tag );
+	}
+
+	return $og_tag;
 }
 
 /**

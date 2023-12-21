406ffc3499644b67cdca3133f412a74f9923997e
Ian Dunn <ian@iandunn.name>
2023-12-21 11:10:29 -0800

    Events: Apply coding standards

--- public_html/wp-content/themes/wporg-events-2023/src/event-list/edit.js
+++ public_html/wp-content/themes/wporg-events-2023/src/event-list/edit.js
@@ -4,14 +4,11 @@
 
 import { Disabled } from '@wordpress/components';
 import ServerSideRender from '@wordpress/server-side-render';
-import { InspectorControls, useBlockProps } from '@wordpress/block-editor';
+import { useBlockProps } from '@wordpress/block-editor';
 
 export default function Edit( { attributes, name } ) {
 	return (
 		<div { ...useBlockProps() }>
-			<InspectorControls>
-
-			</InspectorControls>
 			<Disabled>
 				<ServerSideRender block={ name } attributes={ attributes } />
 			</Disabled>

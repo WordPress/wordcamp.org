65f2f4b17736c8b605549a33bd232ab49b6a74dc
Kelly Dwan <ryelle@users.noreply.github.com>
2024-01-11 14:25:08 -0500

    Jetpack Tweaks: Remove form accessibility fix.
    The radio and checkbox accessibility was fixed in Jetpack, see https://github.com/Automattic/jetpack/pull/34147. This fix is now causing a conflict, and should be fully removed.
    
    Fixes #1177.
    Props harshgajipara for the original report, sabernhardt for sharing the Jetpack fix.

--- public_html/wp-content/mu-plugins/jetpack-tweaks/miscellaneous.php
+++ public_html/wp-content/mu-plugins/jetpack-tweaks/miscellaneous.php
@@ -6,7 +6,7 @@ use WP_Service_Worker_Caching_Routes, WP_Service_Worker_Scripts;
 defined( 'WPINC' ) || die();
 
 // Allow Photon to fetch images that are served via HTTPS.
-add_filter( 'jetpack_photon_reject_https',    '__return_false' );
+add_filter( 'jetpack_photon_reject_https', '__return_false' );
 
 /**
  * Filter the post types Jetpack has access to, and can synchronize with WordPress.com.
@@ -188,89 +188,3 @@ function workaround_is_frontend( $is_frontend ) {
 	return $is_frontend;
 }
 add_filter( 'jetpack_is_frontend', __NAMESPACE__ . '\workaround_is_frontend' );
-
-/**
- * Filter the contact form HTML to replace the wrapper with a fieldset & legend
- * for radio & multi-checkbox fields. This is necessary for screen reader users
- * to understand the form questions.
- *
- * The single checkbox field is okay as-is.
- *
- * Upstream issue: https://github.com/Automattic/jetpack/issues/16685.
- * When that is fixed, this & `inject_css_for_fieldset` can be removed.
- *
- * @param string   $rendered_field Contact Form HTML output.
- * @param string   $field_label    Field label.
- * @param int|null $post_id        Post ID.
- */
-function wrap_checkbox_radio_fieldset( $rendered_field, $field_label, $post_id ) {
-	// If the field uses "animated" or "notched" UI, return early.
-	if ( str_contains( $rendered_field, 'animated-label__label' ) || str_contains( $rendered_field, 'notched-label__label' ) ) {
-		return $rendered_field;
-	}
-
-	if (
-		str_contains( $rendered_field, 'grunion-checkbox-multiple-options' ) ||
-		str_contains( $rendered_field, 'grunion-radio-options' )
-	) {
-		// remove any whitespace so the offsets below work.
-		$rendered_field = trim( $rendered_field );
-		// replace wrapper div.
-		$rendered_field = substr_replace( $rendered_field, '<fieldset ', 0, 5 );
-		$rendered_field = substr_replace( $rendered_field, '</fieldset> ', -6, 6 );
-		// replace only the first label, others are for the options.
-		$rendered_field = preg_replace( '/<label/', '<legend', $rendered_field, 1);
-		$rendered_field = preg_replace( '/<\/label>/', '</legend>', $rendered_field, 1);
-		// Pull out the legend text so we can create a separate visual element.
-		// See https://adrianroselli.com/2022/07/use-legend-and-fieldset.html.
-		if ( preg_match( '/<legend[^>]*>(.*)<\/legend>/i', $rendered_field, $matches ) ) {
-			$visible_label = sprintf(
-				'<div class="grunion-field-label" aria-hidden="true">%s</div>',
-				$matches[1]
-			);
-			$rendered_field = str_replace( $matches[0], $matches[0] . $visible_label, $rendered_field );
-		}
-	}
-	return $rendered_field;
-}
-add_filter( 'grunion_contact_form_field_html', __NAMESPACE__ . '\wrap_checkbox_radio_fieldset', 10, 3 );
-
-/**
- * Add styles for the injected fieldset & legend.
- *
- * This resets the spacing around the fieldset, and styles the legend to match the label.
- *
- * See https://github.com/Automattic/jetpack/blob/trunk/projects/plugins/jetpack/modules/contact-form/css/grunion.css.
- */
-function inject_css_for_fieldset() {
-	$form_css = <<<CSS
-:where(.contact-form) fieldset {
-	border: none;
-	padding: 0;
-}
-:where(.contact-form) legend {
-	position: absolute;
-	overflow: hidden;
-	clip: rect(0 0 0 0); 
-	clip-path: inset(50%);
-	width: 1px;
-	height: 1px;
-	white-space: nowrap; 
-}
-.grunion-field-label {
-	margin-bottom: 0.25em;
-	float: none;
-	font-weight: bold;
-	display: block;
-}
-.grunion-field-label span {
-	font-size: 85%;
-	margin-left: 0.25em;
-	font-weight: normal;
-	opacity: 0.45;
-}
-CSS;
-
-	wp_add_inline_style( 'grunion.css', $form_css );
-}
-add_action( 'wp_enqueue_scripts', __NAMESPACE__ . '\inject_css_for_fieldset' );

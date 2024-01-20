WordPress/wordcamp.org

commit f0e80a5805633bbf1079a9a7bd973118ebe37fb8

Helpers: Move `get_wordcamp_offset()` for reuse.

public_html/wp-content/mu-plugins/4-helpers-wcpt.php
@@ -244,6 +244,30 @@ function wcorg_get_wordcamp_duration( WP_Post $wordcamp ) {
 }
 
 /**
+ * Get a WordCamp's UTC offset in seconds.
+ *
+ * Forked from `official-wordpress-events`.
+ */
+function get_wordcamp_offset( WP_Post $wordcamp ): int {
+	switch_to_blog( WORDCAMP_ROOT_BLOG_ID );
+
+	if ( ! $wordcamp->{'Event Timezone'} || ! $wordcamp->{'Start Date (YYYY-mm-dd)'} ) {
+		return 0;
+	}
+
+	$wordcamp_timezone = new DateTimeZone( $wordcamp->{'Event Timezone'} );
+
+	$wordcamp_datetime = new DateTime(
+		'@' . $wordcamp->{'Start Date (YYYY-mm-dd)'},
+		$wordcamp_timezone
+	);
+
+	restore_current_blog();
+
+	return $wordcamp_timezone->getOffset( $wordcamp_datetime );
+}
+
+/**
  * Get a <select> dropdown of `wordcamp` posts with a select2 UI.
  *
  * The calling plugin is responsible for validating and processing the form, this just outputs a single field.

public_html/wp-content/themes/wporg-events-2023/inc/city-landing-pages.php
@@ -255,26 +255,6 @@ function get_city_landing_sites( string $request_uri, int $limit ): array {
 }
 
 /**
- * Get a WordCamp's UTC offset in seconds.
- *
- * Forked from `official-wordpress-events`.
- */
-function get_wordcamp_offset( WP_Post $wordcamp ): int {
-	if ( ! $wordcamp->{'Event Timezone'} || ! $wordcamp->{'Start Date (YYYY-mm-dd)'} ) {
-		return 0;
-	}
-
-	$wordcamp_timezone = new DateTimeZone( $wordcamp->{'Event Timezone'} );
-
-	$wordcamp_datetime = new DateTime(
-		'@' . $wordcamp->{'Start Date (YYYY-mm-dd)'},
-		$wordcamp_timezone
-	);
-
-	return $wordcamp_timezone->getOffset( $wordcamp_datetime );
-}
-
-/**
  * Disable the cron job that Google Map Event Filters registers.
  *
  * This plugin needs to run its own cron to prime the events cache, because the upstream one doesn't know how to handle city pages.

commit 87bae93ff2c3617ba60b5e146da4977b317fd18f

Helpers: Restore switched blog when returning early
Otherwise it stays switched, and that leads to subtle bugs elsewhere in the codebase.

Fixes the bug introduced in f0e80a5805633bbf1079a9a7bd973118ebe37fb8.

public_html/wp-content/mu-plugins/4-helpers-wcpt.php
@@ -252,6 +252,7 @@ function get_wordcamp_offset( WP_Post $wordcamp ): int {
 	switch_to_blog( WORDCAMP_ROOT_BLOG_ID );
 
 	if ( ! $wordcamp->{'Event Timezone'} || ! $wordcamp->{'Start Date (YYYY-mm-dd)'} ) {
+		restore_current_blog();
 		return 0;
 	}
 
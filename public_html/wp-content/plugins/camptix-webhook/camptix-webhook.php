5182a386ecca773630a594ea302869fa9fa7c989
Ivan Kristianto <ivan@ivankristianto.com>
2024-08-18 15:04:29 +0700

    Add integration with various metas

--- public_html/wp-content/plugins/camptix-webhook/camptix-webhook.php
+++ public_html/wp-content/plugins/camptix-webhook/camptix-webhook.php
@@ -2,16 +2,41 @@
 /**
  * Plugin Name: CampTix - Webhook
  * Description: An addon for CampTix that allows 3rd party integration via webhook.
- * Version: 0.1
+ * Version: 1.0
  * Author: Ivan Kristianto
  * Author URI: https://profiles.wordpress.org/ivankristianto/
  */
+
+ namespace CampTix\Webhook;
 
 if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
-function camptix_webhook_register() {
-	require_once( plugin_dir_path( __FILE__ ) . 'addons/webhook.php' );
+const BASE_DIR = __DIR__;
+const BASE_FILE = __FILE__;
+
+require_once( __DIR__ . '/addons/webhook.php' );
+require_once __DIR__ . '/inc/integration.php';
+
+/**
+ * Bootstrap the plugin.
+ * @return void
+ */
+function bootstrap() {
+	add_action( 'camptix_load_addons', __NAMESPACE__ . '\\camptix_webhook_register' );
+
+	Integration\bootstrap();
 }
-add_action( 'camptix_load_addons', 'camptix_webhook_register' );
+
+/**
+ * Register the webhook addon.
+ *
+ * @return void
+ */
+function camptix_webhook_register() {
+	Addon\CampTix_Webhook::register_addon();
+}
+
+// Bootstrap the plugin.
+bootstrap();
---
+++ public_html/wp-content/plugins/camptix-webhook/inc/integration.php
@@ -0,0 +1,170 @@
+<?php
+/**
+ * Camptix Webhook Integration
+ */
+
+namespace GG\RewriteRules\Rewrite;
+
+namespace CampTix\Webhook\Integration;
+
+/**
+ * Register hooks.
+ */
+function bootstrap() {
+	add_action( 'load-tix_ticket_page_camptix_options', __NAMESPACE__ . '\\test_webhook_action' );
+
+	add_filter( 'camptix_webhook_attendee_data', __NAMESPACE__ . '\\add_attendees_admin_flag', 10, 2 );
+	add_filter( 'camptix_webhook_attendee_data', __NAMESPACE__ . '\\add_attendees_meta', 10, 2 );
+	add_filter( 'camptix_webhook_attendee_data', __NAMESPACE__ . '\\add_attendees_questions_answered', 10, 2 );
+}
+
+/**
+ * Test webhook action.
+ *
+ * This hook only run on camptix Tickets > setup page.
+ */
+function test_webhook_action() {
+
+	if ( ! isset( $_GET['test_webhook'] ) || $_GET['test_webhook'] !== '1' ) {
+		return;
+	}
+
+	/** @var CampTix_Plugin $camptix */
+	global $camptix;
+
+	$camptix_options = $camptix->get_options();
+
+	$webhook_url = isset( $camptix_options['webhook-url'] ) ? $camptix_options['webhook-url'] : '';
+
+	// Get attendee data.
+	$attendee_data = [
+		'timestamp' => time(),
+		'status' => 'publish',
+		'is_new_entry' => true,
+		'tix_email' => 'camptix-webhook-test@wordcamp.org',
+		'tix_first_name' => 'Camptix Webhook',
+		'tix_last_name' => 'Test',
+		'tix_ticket_id' => '0000',
+		'tix_coupon' => 'Coupon_XXX',
+	];
+
+	// Post ID: -1 will invalid when getting post object. We use this to indicate that this is a test webhook.
+	$attendee_data = apply_filters( 'camptix_webhook_attendee_data', $attendee_data, -1 );
+
+	// Prepare webhook data.
+	$response = wp_remote_post(
+		$webhook_url,
+		[
+			'body' => wp_json_encode( $attendee_data ),
+			'headers' => [
+				'Content-Type' => 'application/json',
+			],
+		]
+	);
+
+	if ( is_wp_error( $response ) ) {
+		add_settings_error(
+			'camptix-webhook',
+			'error',
+			__( 'Webhook test failed: ', 'wordcamporg' ) . $response->get_error_message(),
+			'error'
+		);
+		return;
+	}
+
+	add_settings_error(
+		'camptix-webhook',
+		'success',
+		__( 'Webhook test success', 'wordcamporg' ),
+		'success'
+	);
+}
+
+/**
+ * Add attendees admin flag to attendee data.
+ *
+ * @param array $attendee_data Array of attendee data.
+ * @param int $post_id Post ID.
+ * @return array
+ */
+function add_attendees_admin_flag( $attendee_data, $post_id ): array {
+	// Post ID: -1 will invalid when getting post object. We use this to indicate that this is a test webhook.
+	if ( $post_id === -1 ) {
+		$admin_flag = [ 'volunteer', 'speaker', 'organiser' ];
+		$attendee_data['tix_admin_flag'] = $admin_flag[ array_rand( $admin_flag ) ];
+
+		return $attendee_data;
+	}
+
+	// Admin flag meta could be more than 1.
+	$attendee_data['tix_admin_flag'] = get_post_meta( $post_id, 'camptix-admin-flag', false );
+
+	return $attendee_data;
+}
+
+/**
+ * Add attendees meta to attendee data.
+ *
+ * @param array $attendee_data Array of attendee data.
+ * @param int $post_id Post ID.
+ * @return array
+ */
+function add_attendees_meta( $attendee_data, $post_id ): array {
+	$allowed_meta = [
+		'tix_accommodations',
+		'tix_allergy',
+		'tix_coupon',
+		'tix_first_time_attending_wp_event'
+	];
+
+	// Post ID: -1 will invalid when getting post object. We use this to indicate that this is a test webhook.
+	if ( $post_id === -1 ) {
+		$attendee_data['tix_accommodations'] = 'yes';
+		$attendee_data['tix_allergy'] = 'no';
+		$attendee_data['tix_coupon'] = 'test';
+		$attendee_data['tix_first_time_attending_wp_event'] = 'test';
+
+		return $attendee_data;
+	}
+
+	foreach ( $allowed_meta as $meta_key ) {
+		$attendee_data[ $meta_key ] = get_post_meta( $post_id, $meta_key, true );
+	}
+
+	return $attendee_data;
+}
+
+/**
+ * Add attendees questions answered to attendee data.
+ *
+ * @param array $attendee_data Array of attendee data.
+ * @param int $post_id Post ID.
+ * @return array
+ */
+function add_attendees_questions_answered( $attendee_data, $post_id ): array {
+	// Post ID: -1 will invalid when getting post object. We use this to indicate that this is a test webhook.
+	if ( $post_id === -1 ) {
+		$attendee_data['answered'] = 'N/A';
+
+		return $attendee_data;
+	}
+
+	global $camptix;
+	$ticket_id = intval( get_post_meta( $post_id, 'tix_ticket_id', true ) );
+	$questions = $camptix->get_sorted_questions( $ticket_id );
+	$answers = get_post_meta( $post_id, 'tix_questions', true );
+
+	$rows = [];
+	foreach ( $questions as $question ) {
+		if ( isset( $answers[ $question->ID ] ) ) {
+			$answer = $answers[ $question->ID ];
+			if ( is_array( $answer ) )
+				$answer = implode( ', ', $answer );
+			$rows[] = [ $question->post_title, $answer ];
+		}
+	}
+
+	$attendee_data['answered'] = $rows;
+
+	return $attendee_data;
+}

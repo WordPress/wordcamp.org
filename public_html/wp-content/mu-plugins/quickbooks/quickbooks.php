8a2e903eda3b303d36396ae4a5bc1d7e1a30144a
Paul Kevan <2290623+pkevan@users.noreply.github.com>
2024-08-21 17:00:18 +0100

    Update quickbooks.php
    Add cron to monitor QBO connection and send slack message if not connected.
    
    Sends to private channel that is monitored by those with the account details.

--- public_html/wp-content/mu-plugins/quickbooks/quickbooks.php
+++ public_html/wp-content/mu-plugins/quickbooks/quickbooks.php
@@ -11,6 +11,28 @@ require_once PLUGIN_DIR . '/includes/client.php';
 require_once PLUGIN_DIR . '/includes/admin.php';
 
 add_filter( 'wordcamp_qbo_client_config', __NAMESPACE__ . '\set_client_config' );
+add_action( 'init', __NAMESPACE__  . '\check_qbo_connection_cron' );
+add_action( 'check_qbo_connection_action', __NAMESPACE__ . '\check_qbo_connection' );
+
+/**
+ * Setup hourly qbo connection task.
+ */
+function check_qbo_connection_cron() {
+    if ( ! wp_next_scheduled( 'check_qbo_connection_action' ) ) {
+        wp_schedule_event( time(), 'hourly', 'check_qbo_connection_action' );
+    }
+}
+
+/**
+ * Monitors the Quickbooks connection, and sends message to Slack if not connected.
+ */
+function check_qbo_connection() {
+    $client  = get_client();
+
+    if ( ! $client->has_valid_token() && function_exists( 'slack_dm' ) ) {
+        slack_dm( "QBO token invalid. Visit <https://wordcamp.org/wp-admin/network/settings.php> to check connection.", 'dotorg' );
+    }
+}
 
 /**
  * Filter: Add client configuration details depending on the environment.

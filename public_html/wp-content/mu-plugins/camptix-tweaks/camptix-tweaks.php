693decf26bc73ba26e2960cf1c83324eafa0e476
Paul Kevan <2290623+pkevan@users.noreply.github.com>
2024-12-19 11:11:50 +0000

    Update camptix-tweaks.php
    Remove the default 5 posts being returned by `get_posts`

--- public_html/wp-content/mu-plugins/camptix-tweaks/camptix-tweaks.php
+++ public_html/wp-content/mu-plugins/camptix-tweaks/camptix-tweaks.php
@@ -1095,7 +1095,8 @@ function add_show_ticket_type_filter() {
 
 	$filter = isset( $_GET['tix_show_ticket_type'] ) ? $_GET['tix_show_ticket_type'] : '';
 
-	$all_tickets = get_posts( array( 'post_type' => 'tix_ticket' ) );
+	// Set posts_per_page to -1 so we show them all.
+	$all_tickets = get_posts( array( 'post_type' => 'tix_ticket', 'posts_per_page' => -1 ) );
 	?>
 		<select name="tix_show_ticket_type">
 			<option value=""><?php esc_html_e( 'All Tickets', 'wordcamporg' ); ?></option>

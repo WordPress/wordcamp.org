7035676ed38a5ab35a488a75a1b6e93a92a9e6e3
Dion Hulse <dion@wordpress.org>
2025-11-06 06:03:47 +0000

    Markup the application data.

--- public_html/wp-content/plugins/wcpt/views/common/metabox-original-application.php
+++ public_html/wp-content/plugins/wcpt/views/common/metabox-original-application.php
@@ -7,15 +7,13 @@ defined( 'WPINC' ) || die();
 <table class="widefat striped">
 	<tbody>
 		<?php foreach ( $application_data as $question => $answer ) :
-			$question_hr = ucfirst( str_replace( '_', ' ', preg_replace('/q_\d+_/', '$1', $question ) ) ); ?>
+			$question_hr = ucfirst( str_replace( '_', ' ', preg_replace('/q_\d+_/', '$1', $question ) ) );
+			$answer      = is_array( $answer ) ? implode( ', ', $answer ) : $answer;
+			?>
 			<tr>
 				<th style="width:20%;"><p><b><?php echo esc_html( $question_hr ); ?></b></p></th>
 				<th>
-					<?php if ( is_array( $answer ) ) {
-						echo esc_html( implode( ', ', $answer ) );
-					} else {
-						echo esc_html( $answer );
-					} ?>
+					<?php echo make_clickable( nl2br( esc_html( $answer ) ) ); ?>
 				</th>
 			</tr>
 		<?php endforeach; ?>

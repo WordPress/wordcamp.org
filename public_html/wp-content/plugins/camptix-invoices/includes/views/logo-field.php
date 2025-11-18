209cef06cb30e4dfe794142d7d30f9dcd9335864
Dion Hulse <dion@wordpress.org>
2025-11-18 06:46:01 +0000

    Display the default logo in place of the customised one.

--- public_html/wp-content/plugins/camptix-invoices/includes/views/logo-field.php
+++ public_html/wp-content/plugins/camptix-invoices/includes/views/logo-field.php
@@ -10,10 +10,13 @@ defined( 'WPINC' ) || die();
 <div class="camptix-media">
 	<div class="camptix-invoice-logo-preview-wrapper" data-imagewrapper>
 		<?php
+		$logo_image_url = CTX_INV_URL . '/admin/images/wp-community-support.png';
 		if ( ! empty( $value ) ) {
-			$attachment = wp_get_attachment_image_src( $value, 'full' );
-			printf( '<img src="%s" style="max-width:250px;max-height:200px;">', esc_url( $attachment[0] ) );
+			$attachment     = wp_get_attachment_image_src( $value, 'full' );
+			$logo_image_url = $attachment[0];
 		}
+
+		printf( '<img src="%s" style="max-width:250px;max-height:200px;">', esc_url( $logo_image_url ) );
 		?>
 	</div>
 

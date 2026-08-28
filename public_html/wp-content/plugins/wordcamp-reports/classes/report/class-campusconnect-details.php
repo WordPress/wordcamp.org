db75c423d7583bc17ca57505e059d8529784c998
Maciej Pilarski <mp@gomp.pl>
2026-08-28 23:12:54 +0900

    Reports: drop the blank line phpcs does not want

--- public_html/wp-content/plugins/wordcamp-reports/classes/report/class-campusconnect-details.php
+++ public_html/wp-content/plugins/wordcamp-reports/classes/report/class-campusconnect-details.php
@@ -299,7 +299,6 @@ class CampusConnect_Details extends WordCamp_Details {
 	 * @return \WP_REST_Response|\WP_Error
 	 */
 	public static function rest_callback( $request ) {
-
 		/*
 		 * Ask only for fields this caller can actually be given. The safelist is
 		 * capability-filtered, and `validate_fields_input()` rejects the whole
